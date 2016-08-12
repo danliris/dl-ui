d8957a2538a0108e1813be8deb9bf522342c57ea
Hotline Mei <Hotline.Tambunan@moonlay.jakarta>
2016-08-12 09:36:45 +0700

    Accessories UI

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: './welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: './users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: './child-router', nav: true, title: 'Child Router' },
-      { route: 'products',      name: 'products',     moduleId: './components/product/index',        nav: true, title: 'Products' },
-      { route: 'buyers',        name: 'buyers',       moduleId: './components/buyer/index',          nav: true, title: 'Buyers' },
-      { route: 'suppliers',     name: 'suppliers',    moduleId: './components/supplier/index',       nav: true, title: 'Suppliers' },
+      { route: ['', 'welcome'], name: 'welcome', moduleId: './welcome', nav: true, title: 'Welcome' },
+      { route: 'users', name: 'users', moduleId: './users', nav: true, title: 'Github Users' },
+      { route: 'child-router', name: 'child-router', moduleId: './child-router', nav: true, title: 'Child Router' },
+      { route: 'products', name: 'products', moduleId: './components/product/index', nav: true, title: 'Products' },
+      { route: 'buyers', name: 'buyers', moduleId: './components/buyer/index', nav: true, title: 'Buyers' },
+      { route: 'suppliers', name: 'suppliers', moduleId: './components/supplier/index', nav: true, title: 'Suppliers' },
+      { route: 'accessories', name: 'accessories', moduleId: './components/accessories/index', nav: true, title: 'Accessories' },
     ]);
 
     this.router = router;
