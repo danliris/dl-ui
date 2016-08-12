2d3f71b8da26ac38788c0568f3ce3445aa9dbbd7
indriHutabalian <indri.hutabalian@moonlay.com>
2016-08-12 18:59:48 +0700

    Add General Merchandise (#5)
    * Add Master Textile
    
    * Add General Merchandise
    
    * Add general Merchandise

--- src/app.js
+++ src/app.js
@@ -9,7 +9,9 @@ export class App {
       { route: 'fabrics', name: 'fabrics', moduleId: './components/fabric/index', nav: true, title: 'Fabrics' },
       { route: 'buyers', name: 'buyers', moduleId: './components/buyer/index', nav: true, title: 'Buyers' },
       { route: 'suppliers', name: 'suppliers', moduleId: './components/supplier/index', nav: true, title: 'Suppliers' },
+      { route: 'textiles',     name: 'textiles',    moduleId: './components/textile/index',       nav: true, title: 'Textiles' },
       { route: 'accessories', name: 'accessories', moduleId: './components/accessories/index', nav: true, title: 'Accessories' },
+      { route: 'generalmerchandises', name: 'generalmerchandises', moduleId: './components/general-merchandise/index', nav: true, title: 'General Merchandises' }
     ]);
 
     this.router = router;
---
+++ src/components/general-merchandise/service.js
@@ -0,0 +1,47 @@
+import {inject, Lazy} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import {RestService} from '../../rest-service';
+ 
+const serviceUri = 'http://localhost:8900/v1/core/generalmerchandises';
+ 
+export class Service extends RestService{
+
+  constructor(http, aggregator) {
+    super(http, aggregator);
+  }
+
+  search(keyword) {
+    return super.get(serviceUri);
+  }
+
+  getById(id)
+  {
+    var endpoint = `${serviceUri}/${id}`;
+    return super.get(endpoint);
+  }
+
+  create(data)
+  {
+    var endpoint = `${serviceUri}`;
+    return super.post(endpoint, data);
+  }
+
+  update(data)
+  {
+    var endpoint = `${serviceUri}/${data._id}`;
+    return super.put(endpoint, data);
+  }  
+
+  delete(data)
+  {
+    var endpoint = `${serviceUri}/${data._id}`;
+    return super.delete(endpoint, data);
+  }
+  
+  getByCode(code) 
+  {
+      var endpoint = `${serviceUri}?keyword=${code}`;
+      return super.get(endpoint);
+  }
+ 
+}
