872bcb31c4c150a07b905441531001455df4fc7d
Indri.Hutabalian <indri.hutabalian@moonlay.com>
2016-08-12 11:35:13 +0700

    Add Master Textile

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ export class App {
       { route: 'products',      name: 'products',     moduleId: './components/product/index',        nav: true, title: 'Products' },
       { route: 'buyers',        name: 'buyers',       moduleId: './components/buyer/index',          nav: true, title: 'Buyers' },
       { route: 'suppliers',     name: 'suppliers',    moduleId: './components/supplier/index',       nav: true, title: 'Suppliers' },
+      { route: 'textiles',     name: 'textiles',    moduleId: './components/textile/index',       nav: true, title: 'Textiles' },
     ]);
 
     this.router = router;
