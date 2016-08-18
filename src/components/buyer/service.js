f87bace123d048afd36327da04012a834c7a22ea
tr4nquility <tr4nquility_s@yahoo.com>
2016-08-18 10:26:51 +0700

    Update service.js

--- src/components/buyer/service.js
+++ src/components/buyer/service.js
@@ -2,7 +2,7 @@ import {inject, Lazy} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-fetch-client';
 import {RestService} from '../../rest-service';
  
-const serviceUri = 'http://localhost:8900/v1/core/buyers';
+const serviceUri = require('../../host').core + + '/buyers';
  
 export class Service extends RestService{
 
