e5ab29c111f9c8a1a632f1439183f6fb9c3a1324
Hotline Mei <Hotline.Tambunan@moonlay.jakarta>
2016-08-19 18:30:25 +0700

    po garment sparepart dl-ui

--- src/host.js
+++ src/host.js
@@ -1,3 +1,5 @@
 module.exports = {
-    core: "http://dl-core-api.mybluemix.net/v1/core"
+    core: "http://dl-core-api.mybluemix.net/v1/core",
+    // po: "http://dl-core-api.mybluemix.net/v1/po"
+    po: "http://localhost:8800/v1/po"
 }
