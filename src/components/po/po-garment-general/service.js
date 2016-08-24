c1eed640f1382940038f6fb1e04e3c574490a9bf
kanisiusandrew <kanisius.andrew@moonlay.com>
2016-08-24 14:29:43 +0700

    remove auto prno, change host.js

--- src/components/po/po-garment-general/service.js
+++ src/components/po/po-garment-general/service.js
@@ -22,7 +22,6 @@ export class Service extends RestService {
     }
 
     create(data) {
-        data.PRNo = "pr/test/212312312"
         var endpoint = `${serviceUri}`;
         return super.post(endpoint, data);
     }
