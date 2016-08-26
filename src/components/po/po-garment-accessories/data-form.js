70737556592b00002ed4185ffe470d57e8db1bd7
achensetyawati <achenzetya@gmail.com>
2016-08-26 09:53:44 +0700

    updating view JO and PO acc

--- src/components/po/po-garment-accessories/data-form.js
+++ src/components/po/po-garment-accessories/data-form.js
@@ -6,8 +6,9 @@ export class DataForm {
     @bindable data = {};
     @bindable error = {};
 
-    uri = require('../../../host').core + "/v1/core/accesories";
+   
     uriSupplier = require('../../../host').core + "/v1/core/suppliers";
+    uriBuyer = require('../../../host').core + "/v1/core/buyers";
 
     constructor(bindingEngine, element) {
         this.bindingEngine = bindingEngine;
@@ -25,23 +26,31 @@ export class DataForm {
             this.data.supplier = newValue;
             this.data.supplierId = newValue._id;
         });
+
+        this.bindingEngine.propertyObserver(this.data, "buyer").subscribe((newValue, oldValue) => {
+            this.data.buyer = newValue;
+            this.data.buyerId = newValue._id;
+        });
     }
 
-    addItem() {
-        this.data.items = this.data.items ? this.data.items : [];
-        this.data.items.push({ product: { name: 'item' } });
-        console.log(this.data);
-    }
-
-    removeItem(item) {
-        var itemIndex = this.data.items.indexOf(item);
-        this.data.items.splice(itemIndex, 1);
-    }
+    
 
     mapSupplier(result) {
         var list = result.data.map(item => {
             var _item = item;
             _item.labelSupplier = `${_item.code} - ${_item.name}`;
+
+            console.log(_item);
+            return _item
+        });
+
+        return list;
+    }
+
+    mapBuyer(result) {
+        var list = result.data.map(item => {
+            var _item = item;
+            _item.labelBuyer = `${_item.code} - ${_item.name}`;
 
             console.log(_item);
             return _item
--- src/components/po/po-garment-job-order-accessories/edit.js
+++ src/components/po/po-garment-job-order-accessories/edit.js
@@ -1,21 +1,43 @@
-import {inject, Lazy} from 'aurelia-framework';
+import {inject, bindable, BindingEngine, observable} from 'aurelia-framework'
 import {Router} from 'aurelia-router';
 import {Service} from './service';
 
-
-@inject(Router, Service)
+@inject(Router, Service, BindingEngine, Element)
 export class Edit {
-    constructor(router, service) {
+    uri = require('../../../host').core + "/v1/core/accesories";
+
+        name = '';
+        qty = '';
+        price = '';
+        product = {};
+
+    constructor(router, service, bindingEngine, element) {
         this.router = router;
-        this.service = service; 
+        this.service = service;
+        this.bindingEngine = bindingEngine;
+        this.element = element;
     }
 
-    activate(params) {
+    async activate(params) {
         var id = params.id;
-        this.service.getById(id)
-            .then(data => {
-                this.data = data;
-            })
+        this.data = await this.service.getById(id);
+    }
+
+    attached() {  
+        for (var item of this.data.items) {
+            this.product = item.product;
+            this.name = item.product.code + ' - ' + item.product.name + '@' + item.product.price;
+            this.qty = item.qty;
+            this.price = item.price
+        }
+          
+        this.bindingEngine.propertyObserver(this, "product").subscribe((newValue, oldValue) => {
+            this.updatePrice(newValue, this.qty);
+        });
+        this.bindingEngine.propertyObserver(this, "qty").subscribe((newValue, oldValue) => {
+            this.updatePrice(this.product, newValue);
+        });
+        
     }
 
     view() {
@@ -23,13 +45,28 @@ export class Edit {
     }
 
     save() {
-        console.log(this.data);
-        this.service.update(this.data)
-            .then(result => {
-                this.view();
-            })
-            .catch(e => {
-                this.error = e;
-            })
+        this.data.items = [];
+        this.data.items.push({ product: this.product, qty: this.qty, price: this.price });
+        
+        this.service.update(this.data).then(result => {
+            this.view();
+        }).catch(e => {
+            this.error = e;
+        })
+    }
+
+    updatePrice(product, qty) {
+        console.log('update')
+        this.price = product.price * qty;
+    }
+    
+    map(result) {
+        var list = result.data.map(item => {
+            var _item = item;
+            _item.label = `${_item.code} - ${_item.name} @ ${_item.price}`;
+            return _item
+        });
+        console.log(list);
+        return list;
     }
 }
