137d8791c9a669107799911e85eb70f2dfb70645
Indri.Hutabalian <indri.hutabalian@moonlay.com>
2016-08-26 18:01:17 +0700

    Add PO Garment JO Fabric

---
+++ src/components/po/po-garment-job-order-fabric/create.js
@@ -0,0 +1,29 @@
+import {inject, Lazy} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
+import {Service} from './service';
+
+
+@inject(Router, Service)
+export class Create {
+    constructor(router, service) {
+        this.router = router;
+        this.service = service;
+        this.data = {};
+    }
+
+    back() {
+        this.router.navigateToRoute('list');
+    }
+
+    save() {
+        console.log(JSON.stringify(this.data));
+        this.service.create(this.data)
+            .then(result => {
+                this.back();
+            })
+            .catch(e => {
+                console.log(e);
+                this.error = e;
+            })
+    }
+}
---
+++ src/components/po/po-garment-job-order-fabric/data-form.js
@@ -0,0 +1,64 @@
+import {inject, bindable, BindingEngine, observable} from 'aurelia-framework'
+var moment = require('moment');
+
+@inject(BindingEngine, Element)
+export class DataForm {
+    @bindable data = {};
+    @bindable error = {};
+
+    uri = "http://dl-core-api.mybluemix.net/v1/core/textiles";
+    uriSupplier = "http://dl-core-api.mybluemix.net/v1/core/suppliers";
+    uriBuyer = "http://dl-core-api.mybluemix.net/v1/core/buyers";
+
+    constructor(bindingEngine, element) {
+        this.bindingEngine = bindingEngine;
+        this.element = element;
+
+        this.data.deliveryDate = moment().format('YYYYMMDD');
+        this.data.PRNo = 'pr123456';
+    }
+    
+    attached() {
+        this.bindingEngine.propertyObserver(this.data, "supplier").subscribe((newValue, oldValue) => {
+            this.data.supplier = newValue;
+            this.data.supplierId = newValue._id;
+        });
+        this.bindingEngine.propertyObserver(this.data, "buyer").subscribe((newValue, oldValue) => {
+            this.data.buyer = newValue;
+            this.data.buyerId = newValue._id;
+        });
+    }
+    
+    addItem() {
+        this.data.items = this.data.items ? this.data.items : [];
+        this.data.items.push({product:{name:'santos'}});
+        console.log(this.data);
+    }
+    removeItem(item)
+    {
+        var itemIndex = this.data.items.indexOf(item);
+        this.data.items.splice(itemIndex, 1);
+    }
+    mapSupplier(result) {
+        var list = result.data.map(item => {
+            var _item = item;
+            _item.labelSupplier = `${_item.code} - ${_item.name}`;
+
+            console.log(_item);
+            return _item
+        });
+
+        return list;
+    }
+     mapBuyer(result) {
+        var list = result.data.map(item => {
+            var _item = item;
+            _item.labelBuyer = `${_item.code} - ${_item.name}`;
+
+            console.log(_item);
+            return _item
+        });
+
+        return list;
+    }
+} 
