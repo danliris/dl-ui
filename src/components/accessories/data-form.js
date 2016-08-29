5c1cff24d78aa12b9547a441d41b28765d9bad38
Indri.Hutabalian <indri.hutabalian@moonlay.com>
2016-08-29 17:27:05 +0700

    bug fixing

--- src/components/accessories/data-form.js
+++ src/components/accessories/data-form.js
@@ -4,6 +4,7 @@ export class DataForm {
     @bindable data = {};
     @bindable error = {};
     
+    uomApiUri = require('../../host').core + '/v1/core/uoms';
     constructor() {}
     activate() {}
     attached() {} 
