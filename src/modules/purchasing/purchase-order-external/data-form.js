a5db82f90cf7ddbaf5100687c98afe05db578162
Indri.Hutabalian <indri.hutabalian@moonlay.com>
2016-09-22 10:59:28 +0700

    fix payment method

--- src/modules/purchasing/purchase-order-external/data-form.js
+++ src/modules/purchasing/purchase-order-external/data-form.js
@@ -7,7 +7,7 @@ export class DataForm {
     @bindable data = {};
     @bindable error = {};
 
-    termPaymentOptions = ['FREE', 'CASH', 'CREDIT', 'LETTER OF CREDIT'];
+    termPaymentOptions = ['CASH', 'KREDIT', 'DP (DOWN PAYMENT) + BP (BALANCE PAYMENT)', 'DP (DOWN PAYMENT) + TERMIN 1 + BP (BALANCE PAYMENT)','RETENSI'];
     freightCostByOptions = ['BUYER', 'SUPPLIER', '3rd PARTY'];
     currencyOptions = ['IDR', 'USD'];
     usePphOptions = [{ value: true, label: 'YA' }, { value: false, label: 'TIDAK' }];
