7d2733c20f2f63dd74f61d6f3c9d3f7c606bb413
Tris Setiawan <tris.setiawan@yahoo.com>
2016-09-30 19:48:18 +0700

    fix numeric control, add thousand separator capability

--- src/components/react/basic/numeric-react.jsx
+++ src/components/react/basic/numeric-react.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import DynamicNumber from 'react-dynamic-number';
 
 'use strict';
 
@@ -12,9 +13,11 @@ export default class NumericReact extends React.Component {
     }
 
     init(props) {
-        var initialValue = props.value;
-        if (props.value != initialValue && props.onChange)
+        var initialValue = props.value || NumericReact.defaultProps.value;
+        if (props.value != initialValue && props.onChange) {
+            // console.log('onChange');
             props.onChange(initialValue);
+        }
 
         var options = Object.assign({}, NumericReact.defaultProps.options, props.options);
         this.setState({ value: initialValue, options: options });
@@ -40,11 +43,19 @@ export default class NumericReact extends React.Component {
         var control = null;
         var usePostFix = this.state.options.postFix.trim() != '';
         var postFix = usePostFix ? this.state.options.postFix : '';
+        console.log(this.state.value);
         if (this.state.options.readOnly) {
-            control = <p className="form-control-static">{ this.state.value } {postFix}</p>;
+            control = <p className="form-control-static">{ parseFloat(this.state.value).toLocaleString() } {postFix}</p>;
         }
         else {
-            control = <input type="number" value={this.state.value} onChange={this.handleValueChange} className="form-control" min={this.state.options.min} max={this.state.options.max instanceof Number ? this.state.options.max : "" } style={{ textAlign: "right" }}></input>;
+            // control = <input type="number" value={this.state.value} onChange={this.handleValueChange} className="form-control" min={this.state.options.min} max={this.state.options.max instanceof Number ? this.state.options.max : "" } style={{ textAlign: "right" }}></input>;
+            control = <DynamicNumber className="form-control" value={this.state.value} onChange={this.handleValueChange}
+                separator={this.state.options.separator}
+                integer={this.state.options.integer}
+                fraction={this.state.options.fraction}
+                negative={this.state.options.negative}
+                positive={this.state.options.positive}
+                thousand={this.state.options.thousand} />
 
             if (this.state.options.postFix.trim() != '') {
                 control = <div className="input-group">
@@ -64,7 +75,13 @@ NumericReact.propTypes = {
         readOnly: React.PropTypes.bool,
         min: React.PropTypes.number,
         max: React.PropTypes.number,
-        postFix: React.PropTypes.string
+        postFix: React.PropTypes.string,
+        separator: React.PropTypes.string,
+        interger: React.PropTypes.number,
+        fraction: React.PropTypes.number,
+        negative: React.PropTypes.bool,
+        positive: React.PropTypes.bool,
+        thousand: React.PropTypes.bool,
     })
 };
 
@@ -74,5 +91,11 @@ NumericReact.defaultProps = {
         readOnly: false,
         min: 0,
         postFix: "",
+        separator: ".",
+        integer: 20,
+        fraction: 4,
+        negative: true,
+        positive: true,
+        thousand: true,
     }
 };
