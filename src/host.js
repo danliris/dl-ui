5e59166f23c7f629eab39a130a1eaec437a98055
Tris Setiawan <tris.setiawan@yahoo.com>
2016-10-04 11:43:50 +0700

    update host.js for uat environment

--- src/host.js
+++ src/host.js
@@ -1,8 +1,4 @@
-module.exports = {
-    //  core: "http://127.0.0.1:8900"
-    core: "http://dl-core-api.mybluemix.net"
-    // core:"https://dl-core-webapi-pinkgorilla.c9users.io"
-    ,
-    auth: "http://dl-auth-api.mybluemix.net"
-    // auth: "https://dl-auth-webapi-pinkgorilla.c9users.io"
+module.exports = { 
+    core: "http://dl-core-api-uat.mybluemix.net",
+    auth: "http://dl-auth-api-uat.mybluemix.net"
 }
