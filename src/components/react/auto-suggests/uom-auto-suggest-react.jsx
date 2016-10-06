1326580d3e7f3aa31ac32ccca22518e0856189c5
Tris Setiawan <tris.setiawan@yahoo.com>
2016-10-06 15:55:20 +0700

    add autosuggest styling + add Auth token in header.

--- src/components/react/auto-suggests/uom-auto-suggest-react.jsx
+++ src/components/react/auto-suggests/uom-auto-suggest-react.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AutoSuggestReact from './auto-suggest-react.jsx';
+import {Session} from '../../../utils/session';
 
 const serviceUri = require('../../../host').core + '/v1/master/uoms';
 
@@ -59,8 +60,13 @@ UomAutoSuggestReact.defaultProps = {
         readOnly: false,
         suggestions:
         function (text) {
-            var uri = serviceUri+'?keyword='+text; 
-            return fetch(uri).then(results => results.json()).then(json => {
+            var uri = serviceUri + '?keyword=' + text;
+            
+            var session = new Session();
+            var requestHeader = new Headers();
+            requestHeader.append('Authorization', `JWT ${session.token}`);
+
+            return fetch(uri, { headers: requestHeader }).then(results => results.json()).then(json => {
                 return json.data.map(uom => {
                     uom.toString = function () {
                         return `${this.unit}`;
