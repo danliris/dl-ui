c18a9e9c9de8088323821abfbfadd71dad43bb80
JanuitaM <januita.marpaung@moonlay.com>
2016-10-11 14:33:49 +0700

    change default value jumlah barang dipesan dan diterima

--- src/host.js
+++ src/host.js
@@ -1,4 +1,7 @@
-module.exports = { 
-    core: "http://dl-core-api-dev.mybluemix.net",
+module.exports = {   
+    core: "http://127.0.0.1:9080"
+    //core: "http://dl-core-api-dev.mybluemix.net"
+    ,
+   // auth: "http://127.0.0.1:9070"
     auth: "http://dl-auth-api-dev.mybluemix.net"
 }
