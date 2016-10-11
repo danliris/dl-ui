e1042156e65b8f514a7cf9d682ac05971bd83fa3
JanuitaM <januita.marpaung@moonlay.com>
2016-10-11 17:56:57 +0700

    merge host in ui

--- src/host.js
+++ src/host.js
@@ -1,5 +1,7 @@
-module.exports = {    
+module.exports = {
+    //core: "http://127.0.0.1:9080"
     core: "http://dl-core-api-dev.mybluemix.net"
-    , 
+    ,
+    // auth: "http://127.0.0.1:9070"
     auth: "http://dl-auth-api-dev.mybluemix.net"
 }
