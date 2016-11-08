d695c3772fed45121a9071f17eefc4d85bfcd77b
achensetyawati <achenzetya@gmail.com>
2016-11-08 14:27:12 +0700

    fixing UI for purchase report unit

--- src/modules/purchasing/reports/purchase-order-report/unit-report/list.js
+++ src/modules/purchasing/reports/purchase-order-report/unit-report/list.js
@@ -12,7 +12,7 @@ export class List {
     }
 
 
-    async activate(params) {
+    activate(params) {
         if (params.sdate != null || params.edate != null) {
             this.dateFrom = params.sdate;
             this.dateTo = params.edate;
@@ -35,32 +35,25 @@ export class List {
                         if (item.pricetotal != 0 && this.pricetotals != 0) {
                             this.persen = ((item.pricetotal * 100) / this.pricetotals).toFixed(2);
                         }
-                        this.pricetotals = pricetotals;
-
-                        for (var item of data) {
-                            if (item.pricetotal != 0 && this.pricetotals != 0) {
-                                this.persen = ((item.pricetotal * 100) / this.pricetotals).toFixed(2);
-                            }
-                            else {
-                                this.persen = 0;
-                            }
-                            percentage.push(this.persen);
-                            var x = item.pricetotal.toFixed(2).toString().split('.');
-                            var x1 = x[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-                            var amount = x1 + '.' + x[1];
-                            amounts.push(amount);
+                        else {
+                            this.persen = 0;
                         }
-                        for (var p of percentage) {
-                            percentagetotal += parseFloat(p);
-                        }
-                        this.percentage = percentage;
-                        this.percentagetotal = Math.round(percentagetotal).toFixed(2);
-                        this.amounts = amounts;
-                        var y = this.pricetotals.toFixed(2).toString().split('.');
-                        var y1 = y[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-                        this.pricetotals = y1 + '.' + y[1];
+                        percentage.push(this.persen);
+                        var x = item.pricetotal.toFixed(2).toString().split('.');
+                        var x1 = x[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                        var amount = x1 + '.' + x[1];
+                        amounts.push(amount);
                     }
-                }
+                    for (var p of percentage) {
+                        percentagetotal += parseFloat(p);
+                    }
+                    this.percentage = percentage;
+                    this.percentagetotal = Math.round(percentagetotal).toFixed(2);
+                    this.amounts = amounts;
+                    var y = this.pricetotals.toFixed(2).toString().split('.');
+                    var y1 = y[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    this.pricetotals = y1 + '.' + y[1];
+                    }
                 )
 
         }
