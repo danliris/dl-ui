065ae3554501628a713b15372a72cde8198f6dc2
Kanisius Andrew <kanisius.andrew@moonlay.com>
2016-11-09 14:15:13 +0700

    copy logo to folder images

--- webpack.config.js
+++ webpack.config.js
@@ -104,7 +104,8 @@ switch (ENV) {
         ({ minify: true }),
 
       require('@easy-webpack/config-copy-files')
-        ({ patterns: [{ from: 'favicon.ico', to: 'favicon.ico' }] }),
+        ({ patterns: [{ from: 'favicon.ico', to: 'favicon.ico' }, 
+        { from: './images/Logo-DL-Blue.png', to: './images/Logo-DL-Blue.png' }] }),
 
       require('@easy-webpack/config-common-chunks-simple')
         ({ appChunkName: 'app', firstChunk: 'aurelia-bootstrap' }),
