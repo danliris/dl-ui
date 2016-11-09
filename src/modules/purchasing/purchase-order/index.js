a5b1ced53945810c743b89660c142d66d26de84d
indriHutabalian <indri.hutabalian@moonlay.com>
2016-11-09 18:12:35 +0700

    add thousand separator (#102)
    * add thousand separator on nilai koreksi dan nilai nota intern
    
    * set validation of PO Internal
    
    * fix btnCreate
    
    * fix index of product
    
    * fix index of po internal
    
    * set table of list pr
    
    * fix fulfillment
    
    * add thousand separator

--- src/modules/purchasing/purchase-order/index.js
+++ src/modules/purchasing/purchase-order/index.js
@@ -1,14 +1,10 @@
 export class Index {
     configureRouter(config, router) {
         config.map([
-            { route: ['', 'list'], moduleId: './list', name: 'list', nav: true, title: 'List Internal PO Tekstil' },
-            { route: ['list-podl'], moduleId: './list-podl', name: 'list-podl', nav: false, title: 'List Eksternal PO Tekstil' },
-            { route: 'view/:id', moduleId: './view', name: 'view', nav: false, title: 'View:Internal PO Tekstil' },
-            { route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit:Internal PO Tekstil' },
-            { route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create:Internal PO Tekstil' },
-            { route: 'create-podl', moduleId: './create-podl', name: 'create-podl', nav: false, title: 'Create:Eksternal PO Tekstil' },
-            { route: 'view-podl/:id', moduleId: './view-podl', name: 'view-podl', nav: false, title: 'View:Eksternal PO Tekstil' },
-            { route: 'edit-podl/:id', moduleId: './edit-podl', name: 'edit-podl', nav: false, title: 'Edit:Eksternal PO Tekstil' },
+            { route: ['', 'list'], moduleId: './list', name: 'list', nav: true, title: 'List: Purchase Order Internal' },
+            { route: 'view/:id', moduleId: './view', name: 'view', nav: false, title: 'View: Purchase Order Internal' },
+            { route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit: Purchase Order Internal' },
+            { route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create: Purchase Order Internal' }
         ]);
 
         this.router = router;
