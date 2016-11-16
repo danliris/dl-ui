b2a95ed47dba18fda25f166d9c06f05a994fd494
Indri.Hutabalian <indri.hutabalian@moonlay.com>
2016-11-16 15:49:09 +0700

    add unit payment quantity correction

---
+++ src/modules/purchasing/unit-payment-quantity-correction-note/service.js
@@ -0,0 +1,48 @@
+import {inject, Lazy} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import {RestService} from '../../../rest-service';
+import {SecureService} from '../../../utils/secure-service';
+
+const serviceUri = require('../../../host').core + '/v1/purchasing/unit-payment-note/quantity-correction/by-user';
+const serviceUriretur = require('../../../host').core + '/v1/purchasing/unit-payment-note/quantity-correction/retur';
+
+export class Service extends SecureService {
+
+    constructor(http, aggregator) {
+        super(http, aggregator);
+    }
+
+    search(info) {
+        var endpoint = `${serviceUri}`;
+        return super.list(endpoint, info);
+    }
+
+    getById(id) {
+        var endpoint = `${serviceUri}/${id}`;
+        return super.get(endpoint);
+    }
+
+    create(data) {
+        var endpoint = `${serviceUri}`;
+        return super.post(endpoint, data);
+    }
+
+    update(data) {
+        var endpoint = `${serviceUri}/${data._id}`;
+        return super.put(endpoint, data);
+    }
+
+    delete(data) {
+        var endpoint = `${serviceUri}/${data._id}`;
+        return super.delete(endpoint, data);
+    }
+
+    getPdfById(id) {
+        var endpoint = `${serviceUri}/${id}`;
+        return super.getPdf(endpoint);
+    }
+    getPdfReturById(id) {
+        var endpoint = `${serviceUriretur}/${id}`;
+        return super.getPdf(endpoint);
+    }
+}
