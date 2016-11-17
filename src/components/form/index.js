99cf5e200de3cfbe9008c722b294fb0f1c0f3f0a
SirDimas <dimassixer@gmail.com>
2016-11-17 14:25:42 +0700

    Add Auto suggest
    machine auto suggest
    product thread auto suggest for winding quality sampling
    update index.js for auto suggest

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -25,6 +25,8 @@ export function configure(config) {
         './auto-suggests/unit-receipt-note-auto-suggest',
         './auto-suggests/budget-auto-suggest',
         './auto-suggests/unit-payment-order-auto-suggest',
+        './auto-suggests/machine-auto-suggest',
+        './auto-suggests/product-by-uster-auto-suggest',
 
         './do/do-item-collection',
         './po/po-item-collection',
