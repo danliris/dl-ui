25c4c56a186c48a40c89584dd7cb5971cc9ebdc9
SirDimas <dimassixer@gmail.com>
2016-11-17 14:22:47 +0700

    UI for winding quality sampling

---
+++ src/modules/production/spinning/winding/winding-quality-sampling/service.js
@@ -0,0 +1,44 @@
+import {inject, Lazy} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import {RestService} from '../../../../../rest-service';
+import {SecureService} from '../../../../../utils/secure-service';
+
+const serviceUri = require('../../../../../host').core + '/v1/production/spinning/winding/winding-quality-samplings';
+
+export class Service extends SecureService {
+
+  constructor(http, aggregator) {
+    super(http, aggregator);
+  }
+
+  search(info) {
+    var endpoint = `${serviceUri}`;
+    return super.list(endpoint, info);
+  }
+
+  getById(id) {
+    var endpoint = `${serviceUri}/${id}`;
+    return super.get(endpoint);
+  }
+
+  create(data) {
+    var endpoint = `${serviceUri}`;
+    return super.post(endpoint, data);
+  }
+
+  update(data) {
+    var endpoint = `${serviceUri}/${data._id}`;
+    return super.put(endpoint, data);
+  }
+
+  delete(data) {
+    var endpoint = `${serviceUri}/${data._id}`;
+    return super.delete(endpoint, data);
+  }
+
+  getByCode(code) {
+    var endpoint = `${serviceUri}?keyword=${code}`;
+    return super.get(endpoint);
+  }
+
+}
