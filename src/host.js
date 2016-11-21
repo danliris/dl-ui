1c53259a7a75ebadc2b87e9e45eeaa47411fd8ac
SirDimas <dimassixer@gmail.com>
2016-11-21 10:35:18 +0700

    update host.js

--- src/host.js
+++ src/host.js
@@ -1,7 +1,8 @@
 module.exports = {
-    core: "http://127.0.0.1:9080"
-    //core: "http://dl-core-api-dev.mybluemix.net"
+    
+    core: "http://dl-core-api-dev.mybluemix.net"
     ,
-    // auth: "http://127.0.0.1:9070"
-    auth: "http://dl-auth-api-dev.mybluemix.net"
+    auth: "http://dl-auth-api-dev.mybluemix.net",
+    
+    production : "https://dl-production-webapi-dev.mybluemix.net"
 }
