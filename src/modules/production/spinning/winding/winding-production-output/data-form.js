90f42eca38bf283f713fa46817699aeb397f1166
achensetyawati <achenzetya@gmail.com>
2016-11-24 19:54:35 +0700

    fixing autocomplete lot

--- src/modules/production/spinning/winding/winding-production-output/data-form.js
+++ src/modules/production/spinning/winding/winding-production-output/data-form.js
@@ -22,9 +22,10 @@ export class DataForm {
 
     machineChanged(e) {
         var selectedmachine= e.detail || {};
+        var lot='';
         if (selectedmachine){
             this.data.machineId = selectedmachine._id ? selectedmachine._id : "";
-            if(this.data.lotMachine==undefined)
+            if(this.data.lotMachine==undefined|| this.data.lotMachine==null )
             {
                 if(this.data.machineId!=""&&this.data.productId!=""&&this.data.machineId!=undefined&&this.data.productId!=undefined){
                     this.service.getLot(this.data.productId,this.data.machineId).then(result => {
@@ -34,21 +35,37 @@ export class DataForm {
                     
                 }
             }
+            else if(this.data.machineId==""|| this.data.machineId==undefined) {
+                this.data.lotMachine.lot="";
+                this.data.lotMachine=undefined;
+            }
             
         }
+        
+       
+
     }
 
     threadChanged(e) {
         var selectedThread = e.detail || {};
+        var lot='';
         if (selectedThread) {
             this.data.productId=selectedThread._id? selectedThread._id :"";
-            if(this.data.machineId!=""&&this.data.productId!=""&&this.data.machineId!=undefined&&this.data.productId!=undefined){
-                this.service.getLot(this.data.productId,this.data.machineId).then(result => {
-                this.data.lotMachine=result[0];
-                this.data.lotMachine.lot=result[0].lot;
-            })
+            if(this.data.lotMachine==undefined|| this.data.lotMachine==null )
+            {
+                if(this.data.machineId!=""&&this.data.productId!=""&&this.data.machineId!=undefined&&this.data.productId!=undefined){
+                    this.service.getLot(this.data.productId,this.data.machineId).then(result => {
+                    this.data.lotMachine=result[0];
+                    this.data.lotMachine.lot=result[0].lot;
+                })
+                    
+                }
                 
-            }
+            }else if(this.data.productId==""|| this.data.productId==undefined) {
+                    this.data.lotMachine.lot="";
+                    this.data.lotMachine=undefined;
+                }
+            
         }
      }
 }
