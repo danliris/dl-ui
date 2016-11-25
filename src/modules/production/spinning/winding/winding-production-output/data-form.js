1cb74a40cae088a4045286eecb92c6656072e5f9
achensetyawati <achenzetya@gmail.com>
2016-11-25 15:08:45 +0700

    fixing winding production output (#115)
    * removing delete button in PO
    
    * fixing PO create and split PO
    
    when PR no is empty, reset data
    
    * fixing list PR
    
    * fixing integrate PR to PO
    
    * fixing PR autosuggest
    
    * fixing auto-suggest PR
    
    * fixing autosuggest PR
    
    * fixing UI for purchase report unit
    
    * adding productId
    
    * adding budgetId
    
    * fixing view PR
    
    * fixing uri for PO report
    
    * adding subunit uri for reportPO
    
    * fixing uri report PO
    
    * fixing export to excel report PO
    
    * Fixing view expected delivery date in PR view
    
    * fixing uri report per category
    
    * fixing view for po report per unit
    
    * removing uom in PR items
    
    * removing uom in PR items
    
    * removing uom in PR items
    
    * changing uom in PR items
    
    * fixing autosuggest pr-item
    
    * adding view for winding production output
    
    * adding service for winding production output
    
    * list for winding production
    
    * index for winding production
    
    * app for winding production
    
    * create winding production
    
    * data form for winding production
    
    * edit winding production
    
    * adding view for winding output
    
    * adding service for winding output
    
    * fixing list for winding output
    
    * adding edit winding output
    
    * fixing data form in winding output
    
    * fixing create winding output
    
    * adding thread(lot) autosuggest
    
    * adding thread autosuggest
    
    * adding thread autosuggest
    
    * fixing UI for winding production
    
    * fixing thread autosuggest
    
    * fixing thread autosuggest
    
    * fixing lot in data form winding output
    
    * adding search lot
    
    * merge conflict
    
    * fixing autocomplete lot
    
    * fixing autocomplete lot

--- src/modules/production/spinning/winding/winding-production-output/data-form.js
+++ src/modules/production/spinning/winding/winding-production-output/data-form.js
@@ -1,7 +1,8 @@
 import {inject, bindable, BindingEngine, observable, computedFrom} from 'aurelia-framework'
 var moment = require('moment');
+import {Service} from './service';
 
-@inject(BindingEngine, Element)
+@inject(BindingEngine, Element,Service)
 export class DataForm {
     @bindable readOnly = false;
     @bindable data = {};
@@ -9,9 +10,10 @@ export class DataForm {
 
     spinningOptions = ['Spinning 1','Spinning 2','Spinning 3'];
     shiftOptions = ['Shift I: 06.00 – 14.00', 'Shift II: 14.00 – 22.00', 'Shift III: 22:00 – 06.00'];
-    constructor(bindingEngine, element) {
+    constructor(bindingEngine, element,service) {
         this.bindingEngine = bindingEngine;
         this.element = element;
+        this.service = service;
     }
     @computedFrom("data._id")
     get isEdit() {
@@ -20,18 +22,50 @@ export class DataForm {
 
     machineChanged(e) {
         var selectedmachine= e.detail || {};
-        if (selectedmachine)
+        var lot='';
+        if (selectedmachine){
             this.data.machineId = selectedmachine._id ? selectedmachine._id : "";
+            if(this.data.lotMachine==undefined|| this.data.lotMachine==null )
+            {
+                if(this.data.machineId!=""&&this.data.productId!=""&&this.data.machineId!=undefined&&this.data.productId!=undefined){
+                    this.service.getLot(this.data.productId,this.data.machineId).then(result => {
+                    this.data.lotMachine=result[0];
+                    this.data.lotMachine.lot=result[0].lot;
+                })
+                    
+                }
+            }
+            else if(this.data.machineId==""|| this.data.machineId==undefined) {
+                this.data.lotMachine.lot="";
+                this.data.lotMachine=undefined;
+            }
+            
+        }
+        
+       
+
     }
 
     threadChanged(e) {
         var selectedThread = e.detail || {};
+        var lot='';
         if (selectedThread) {
-            this.data.productId=selectedThread.productId ? selectedThread.productId : "";
-        }
-        else
-        {
-            this.data.lotmachine.lot='';
+            this.data.productId=selectedThread._id? selectedThread._id :"";
+            if(this.data.lotMachine==undefined|| this.data.lotMachine==null )
+            {
+                if(this.data.machineId!=""&&this.data.productId!=""&&this.data.machineId!=undefined&&this.data.productId!=undefined){
+                    this.service.getLot(this.data.productId,this.data.machineId).then(result => {
+                    this.data.lotMachine=result[0];
+                    this.data.lotMachine.lot=result[0].lot;
+                })
+                    
+                }
+                
+            }else if(this.data.productId==""|| this.data.productId==undefined) {
+                    this.data.lotMachine.lot="";
+                    this.data.lotMachine=undefined;
+                }
+            
         }
      }
 }
