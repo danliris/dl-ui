ee3390e069f13265604ea98a5f47ff1a8b479d5c
achensetyawati <achenzetya@gmail.com>
2016-12-01 16:05:00 +0700

    adding service for PO report per unit per category

---
+++ src/modules/purchasing/reports/purchase-order-report/unit-category-report/service.js
@@ -0,0 +1,34 @@
+import {inject, Lazy} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import {RestService} from '../../../../../rest-service';
+import {SecureService} from '../../../../../utils/secure-service';
+
+const serviceUri = require('../../../../../host').purchasing+ '/v1/purchase-oders/reports/units-categories';
+
+export class Service extends SecureService {
+
+    constructor(http, aggregator) {
+        super(http, aggregator);
+    }
+
+    getByDate(sdate, edate) {
+        var endpoint = `${serviceUri}?dateFrom=${sdate}&dateTo=${edate}`;
+        return super.get(endpoint);
+    }
+
+    generateExcel(sdate, edate) {
+        var endpoint = `${serviceUri}?dateFrom=${sdate}&dateTo=${edate}`;
+        return super.getXls(endpoint);
+    }
+
+    getallData() {
+        var endpoint = `${serviceUri}`;
+        return super.get(endpoint);
+    }
+
+    generateExcelnoDate() {
+        var endpoint = `${serviceUri}`;
+        return super.getXls(endpoint);
+    }
+
+}
