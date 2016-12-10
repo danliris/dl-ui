dc3ccae4795a734b2d26ffb413e1db90e117fe9e
achensetyawati <achenzetya@gmail.com>
2016-12-10 11:26:36 +0700

    adding service report unit category

---
+++ src/modules/purchasing/reports/purchase-order-report/unit-category-report/service.js
@@ -0,0 +1,33 @@
+import {inject, Lazy} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import { RestService } from '../../../../../utils/rest-service';
+
+const serviceUri ='purchase-oders/reports/units-categories';
+
+export class Service extends RestService {
+
+    constructor(http, aggregator, config, endpoint) {
+        super(http, aggregator, config, "purchasing");
+    }
+
+    getByDate(sdate, edate) {
+        var endpoint = `${serviceUri}?dateFrom=${sdate}&dateTo=${edate}`;
+        return super.get(endpoint);
+    }
+
+    generateExcel(sdate, edate) {
+        var endpoint = `${serviceUri}?dateFrom=${sdate}&dateTo=${edate}`;
+        return super.getXls(endpoint);
+    }
+
+    getallData() {
+        var endpoint = `${serviceUri}`;
+        return super.get(endpoint);
+    }
+
+    generateExcelnoDate() {
+        var endpoint = `${serviceUri}`;
+        return super.getXls(endpoint);
+    }
+
+}
