74fa0f1ffcd139bd52cf208aee461f08001c5a39
Tris Setiawan <pinkgorilla@users.noreply.github.com>
2016-12-17 00:38:34 +0800

    Delete host.js

--- src/host.js
+++
@@ -1,6 +0,0 @@
-module.exports = { 
-    core: "http://dl-core-api-uat.mybluemix.net",
-    auth: "http://dl-auth-api-uat.mybluemix.net",
-    production : "https://dl-production-webapi-uat.mybluemix.net",
-    purchasing: "https://dl-purchasing-webapi-uat.mybluemix.net"
-}
