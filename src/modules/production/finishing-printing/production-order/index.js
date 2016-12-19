551f8d5ec04c2d483ec85da1936b8847009889d3
achensetyawati <achenzetya@gmail.com>
2016-12-19 13:08:27 +0700

    production order index

--- src/modules/production/finishing-printing/production-order/index.js
+++ src/modules/production/finishing-printing/production-order/index.js
@@ -1,10 +1,10 @@
 export class Index {
     configureRouter(config, router) {
         config.map([
-            { route: ['', 'list'], moduleId: './list', name: 'list', nav: false, title: 'List: Output Hasil Produksi Spinning' },
-            { route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create: Output Hasil Produksi Spinning' },
-            { route: 'view/:id', moduleId: './view', name: 'view', nav: false, title: 'View:  Output Hasil Produksi Spinning' },
-            { route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit: Output Hasil Produksi Spinning' },
+            { route: ['', 'list'], moduleId: './list', name: 'list', nav: false, title: 'List: Surat Order Produksi' },
+            { route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create: Surat Order Produksi' },
+            { route: 'view/:id', moduleId: './view', name: 'view', nav: false, title: 'View:  Surat Order Produksi' },
+            { route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit: Surat Order Produksi' },
         ]);
 
         this.router = router;
