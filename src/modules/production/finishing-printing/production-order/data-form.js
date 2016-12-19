8b6d245577fb3fe7e6da42819a55f80fe06c854f
achensetyawati <achenzetya@gmail.com>
2016-12-19 13:07:01 +0700

    production order data-form

---
+++ src/modules/production/finishing-printing/production-order/data-form.js
@@ -0,0 +1,57 @@
+import {inject, bindable, BindingEngine, observable, computedFrom} from 'aurelia-framework'
+var moment = require('moment');
+import {Service} from './service';
+
+@inject(BindingEngine, Element,Service)
+export class DataForm {
+    @bindable readOnly = false;
+    @bindable data = {};
+    @bindable error = {};
+    @bindable uom = {};
+
+    processOptions = ['Finishing','Printing','Yarn Dyed'];
+    RUNOptions=['Tanpa RUN','1 RUN', '2 RUN'];
+
+    constructor(bindingEngine, element,service) {
+        this.bindingEngine = bindingEngine;
+        this.element = element;
+        this.service = service;
+    }
+    exportChanged(e){
+        var selectedExport =  e.srcElement.checked || false;
+        this.data.isExport=selectedExport;
+    }
+
+    uomChanged(e) {
+        var selectedUom = e.detail;
+        if (selectedUom)
+        {
+            this.data.uomId = selectedUom._id;
+            for(var i of this.data.details){
+                i.uom=selectedUom;
+            }
+        }
+    }
+
+    buyerChanged(e){
+        var selectedBuyer = e.detail;
+        if (selectedBuyer)
+            this.data.buyerId = selectedBuyer._id;
+    }
+
+    lampStandardChanged(e){
+        var selectedLamp = e.detail;
+        if (selectedLamp)
+            this.data.lampStandardId = selectedLamp._id;
+    }
+
+    addDetail(e){
+        var qty=0;
+        for(var i=0;i<this.data.details.length;i++){
+            qty=qty+this.data.details[i].quantity;
+            this.data.details[i].uom=this.data.uom;
+            this.data.orderQuantity=qty;
+        }
+        
+    }
+}
