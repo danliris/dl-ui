d530c5a59f8cf74a327ddc0fd5ac52a8aaa7554c
Kanisius Andrew <kanisius.andrew@moonlay.com>
2016-12-21 15:15:22 +0700

    change purchasing router to dev

--- src/main.js
+++ src/main.js
@@ -24,8 +24,7 @@ export async function configure(aurelia) {
       var core = "https://dl-core-api-dev.mybluemix.net/v1/";
       var auth = "https://dl-auth-api-dev.mybluemix.net/v1/";
       var production = "https://dl-production-webapi-dev.mybluemix.net/v1/";
-      // var purchasing = "https://dl-purchasing-webapi-dev.mybluemix.net/v1/";
-      var purchasing = "http://127.0.0.1:9090/";
+      var purchasing = "https://dl-purchasing-webapi-dev.mybluemix.net/v1/";
 
       config.registerEndpoint('auth', auth);
       config.registerEndpoint('core', core);
