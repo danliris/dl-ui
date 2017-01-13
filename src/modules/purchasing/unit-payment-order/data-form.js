7ca76ef44e6ee7e3691c222753a354732c124767
indriHutabalian <indri.hutabalian@moonlay.com>
2017-01-13 15:58:20 +0700

    fix filter spb

--- src/modules/purchasing/unit-payment-order/data-form.js
+++ src/modules/purchasing/unit-payment-order/data-form.js
@@ -7,8 +7,6 @@ export class DataForm {
     @bindable data = {};
     @bindable error = {};
 
-
-
     termPaymentOptions = ['CASH', 'KREDIT', 'DP (DOWN PAYMENT) + BP (BALANCE PAYMENT)', 'DP (DOWN PAYMENT) + TERMIN 1 + BP (BALANCE PAYMENT)', 'RETENSI'];
 
     constructor(bindingEngine, element) {
@@ -27,19 +25,18 @@ export class DataForm {
                 return this.code + " - " + this.name;
             };
     }
-    
+
     @computedFrom("data.division", "data.supplier", "data.category", "data.paymentMethod", "data.currency", "data.vatRate", "data.useIncomeTax")
-    get xFilter() {
-        var filter =  {
+    get filter() {
+        var filter = {
             divisionId: this.data.divisionId,
             supplierId: this.data.supplierId,
             categoryId: this.data.categoryId,
             paymentMethod: this.data.paymentMethod,
-            currencyCode: (this.data.currency||{}).code||"",
+            currencyCode: (this.data.currency || {}).code || "",
             vatRate: this.data.vatRate,
             useIncomeTax: this.data.useIncomeTax
         }
-        console.log(filter);
         return filter;
     }
 
@@ -47,26 +44,7 @@ export class DataForm {
         var selectedSupplier = e.detail;
         if (selectedSupplier) {
             this.data.supplierId = selectedSupplier._id ? selectedSupplier._id : "";
-            if (!this.readOnly)
-                this.data.items = [];
-            if (this.data.currency) {
-                if (!this.data.currency.code) {
-                    this.data.currency.code = '';
-                }
-            } else {
-                this.data.currency = {
-                    code: ''
-                };
-            }
-            this.filter = {
-                divisionId: this.data.divisionId,
-                supplierId: this.data.supplierId,
-                categoryId: this.data.categoryId,
-                paymentMethod: this.data.paymentMethod,
-                currencyCode: this.data.currency.code,
-                vatRate: this.data.vatRate,
-                useIncomeTax: this.data.useIncomeTax
-            };
+            this.data.items = [];
         }
 
     }
@@ -74,51 +52,13 @@ export class DataForm {
         var selectedDivision = e.detail || {};
         if (selectedDivision) {
             this.data.divisionId = selectedDivision._id ? selectedDivision._id : "";
-            if (!this.readOnly)
-                this.data.items = [];
-            if (this.data.currency) {
-                if (!this.data.currency.code) {
-                    this.data.currency.code = '';
-                }
-            } else {
-                this.data.currency = {
-                    code: ''
-                };
-            }
-            this.filter = {
-                divisionId: this.data.divisionId,
-                supplierId: this.data.supplierId,
-                categoryId: this.data.categoryId,
-                paymentMethod: this.data.paymentMethod,
-                currencyCode: this.data.currency.code,
-                vatRate: this.data.vatRate,
-                useIncomeTax: this.data.useIncomeTax
-            };
+            this.data.items = [];
         }
     }
     currencyChanged(e) {
         var selectedCurrency = e.detail || {};
         if (selectedCurrency) {
-            if (!this.readOnly)
-                this.data.items = [];
-            if (this.data.currency) {
-                if (!this.data.currency.code) {
-                    this.data.currency.code = '';
-                }
-            } else {
-                this.data.currency = {
-                    code: ''
-                };
-            }
-            this.filter = {
-                divisionId: this.data.divisionId,
-                supplierId: this.data.supplierId,
-                categoryId: this.data.categoryId,
-                paymentMethod: this.data.paymentMethod,
-                currencyCode: this.data.currency.code,
-                vatRate: this.data.vatRate,
-                useIncomeTax: this.data.useIncomeTax
-            };
+            this.data.items = [];
         }
     }
 
@@ -126,133 +66,40 @@ export class DataForm {
         var selectedPayment = e.srcElement.value;
         if (selectedPayment) {
             this.data.paymentMethod = selectedPayment ? selectedPayment : "";
-            if (!this.readOnly)
-                this.data.items = [];
-            if (this.data.currency) {
-                if (!this.data.currency.code) {
-                    this.data.currency.code = '';
-                }
-            } else {
-                this.data.currency = {
-                    code: ''
-                };
-            }
-            this.filter = {
-                divisionId: this.data.divisionId,
-                supplierId: this.data.supplierId,
-                categoryId: this.data.categoryId,
-                paymentMethod: this.data.paymentMethod,
-                currencyCode: this.data.currency.code,
-                vatRate: this.data.vatRate,
-                useIncomeTax: this.data.useIncomeTax
-            };
+            this.data.items = [];
         }
     }
 
     categoryChanged(e) {
-        var category = e.detail || {};
-        this.data.categoryId = category._id ? category._id : "";
-        if (!this.readOnly)
+        var selectedCategory = e.detail || {};
+        if (selectedCategory) {
+            this.data.categoryId = selectedCategory._id ? selectedCategory._id : "";
             this.data.items = [];
-        if (this.data.currency) {
-            if (!this.data.currency.code) {
-                this.data.currency.code = '';
-            }
-        } else {
-            this.data.currency = {
-                code: ''
-            };
         }
-        this.filter = {
-            divisionId: this.data.divisionId,
-            supplierId: this.data.supplierId,
-            categoryId: this.data.categoryId,
-            paymentMethod: this.data.paymentMethod,
-            currencyCode: this.data.currency.code,
-            vatRate: this.data.vatRate,
-            useIncomeTax: this.data.useIncomeTax
-        };
     }
 
     vatChanged(e) {
-        var selectedVat = e.detail;
+        var selectedVat = e.detail || {};
         this.data.vatRate = this.data.vatRate ? this.data.vatRate : 0;
         this.data.useIncomeTax = this.data.useIncomeTax ? this.data.useIncomeTax : false;
-        if (selectedVat)
+        if (selectedVat) {
             this.data.vatRate = selectedVat.rate ? selectedVat.rate : 0;
-        else
-            this.data.vatRate = 0;
-        if (this.data.currency) {
-            if (!this.data.currency.code) {
-                this.data.currency.code = '';
-            }
-        } else {
-            this.data.currency = {
-                code: ''
-            };
+            this.data.items = [];
         }
-        this.filter = {
-            divisionId: this.data.divisionId,
-            supplierId: this.data.supplierId,
-            categoryId: this.data.categoryId,
-            paymentMethod: this.data.paymentMethod,
-            currencyCode: this.data.currency.code,
-            vatRate: this.data.vatRate,
-            useIncomeTax: this.data.useIncomeTax
-        };
     }
 
     useVatChanged(e) {
-        if (!this.readOnly)
-            this.data.items = [];
-
+        this.data.items = [];
         this.data.vat = {};
         this.data.vatRate = 0;
         this.data.vatNo = "";
         this.data.vatDate = null;
-        if (this.data.currency) {
-            if (!this.data.currency.code) {
-                this.data.currency.code = '';
-            }
-        } else {
-            this.data.currency = {
-                code: ''
-            };
-        }
-        this.filter = {
-            divisionId: this.data.divisionId,
-            supplierId: this.data.supplierId,
-            categoryId: this.data.categoryId,
-            paymentMethod: this.data.paymentMethod,
-            currencyCode: this.data.currency.code,
-            vatRate: this.data.vatRate,
-            useIncomeTax: this.data.useIncomeTax
-        };
     }
 
     useIncomeTaxChanged(e) {
-        if (!this.readOnly)
-            this.data.items = [];
+        this.data.items = [];
         this.data.incomeTaxNo = "";
         this.data.incomeTaxDate = null;
-        if (this.data.currency) {
-            if (!this.data.currency.code) {
-                this.data.currency.code = '';
-            }
-        } else {
-            this.data.currency = {
-                code: ''
-            };
-        }
-        this.filter = {
-            divisionId: this.data.divisionId,
-            supplierId: this.data.supplierId,
-            categoryId: this.data.categoryId,
-            paymentMethod: this.data.paymentMethod,
-            currencyCode: this.data.currency.code,
-            vatRate: this.data.vatRate,
-            useIncomeTax: this.data.useIncomeTax
-        };
     }
 
 } 
