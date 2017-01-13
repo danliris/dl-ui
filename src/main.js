c751d37a633939f92692bcbb39a84140312bb5f0
dhifa.irawan <dhifa.irawan@moonlay.com>
2017-01-13 17:57:30 +0700

    add monitoring-event

--- src/main.js
+++ src/main.js
@@ -21,9 +21,13 @@ export async function configure(aurelia) {
 
     .plugin("aurelia-api", config => {
 
-      var core = "https://dl-core-api-dev.mybluemix.net/v1/";
-      var auth = "https://dl-auth-api-dev.mybluemix.net/v1/";
-      var production = "https://dl-production-webapi-dev.mybluemix.net/v1/";
+      // var core = "https://dl-core-api-dev.mybluemix.net/v1/";
+      // var auth = "https://dl-auth-api-dev.mybluemix.net/v1/";
+      // var production = "https://dl-production-webapi-dev.mybluemix.net/v1/";
+      // var purchasing = "https://dl-purchasing-webapi-dev.mybluemix.net/v1/";
+      var core = "http://dl-core-api-dev.mybluemix.net/v1/";
+      var auth = "http://dl-auth-api-dev.mybluemix.net/v1/";
+      var production = "http://localhost:8550/v1/";
       var purchasing = "https://dl-purchasing-webapi-dev.mybluemix.net/v1/";
 
       config.registerEndpoint('auth', auth);
