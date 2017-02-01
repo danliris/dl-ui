50dd8c43110c26e770b6a9fbec136a1f71200205
danliris <danliris.cloud@gmail.com>
2017-02-01 21:38:50 +0700

    Update purchasing.js

--- src/routes/purchasing.js
+++ src/routes/purchasing.js
@@ -57,11 +57,11 @@ module.exports = [
         name: 'purchase-order-monitoring',
         moduleId: './modules/purchasing/monitoring-purchase-order-all-user/index',
         nav: true,
-        title: 'Monitoring Purchase',
+        title: 'Monitoring Purchase All',
         auth: true,
         settings: {
             group: "purchasing",
-            permission: {"C9": 1 },
+            permission: {"C9": 1, "P1": 7, "P3": 7, "P4": 7, "P6": 7, "P7": 7},
             iconClass: 'fa fa-dashboard'
         }
     },
@@ -243,7 +243,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "purchasing",
-            permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PG": 1, "C9" : 1 },
+            permission: {"C9" : 1 },
             iconClass: 'fa fa-dashboard'
         }
     }]
