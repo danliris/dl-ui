bea242f6e451776471dee059741059e03a26fba4
achensetyawati <achenzetya@gmail.com>
2017-02-06 14:16:25 +0700

    color auto suggest

--- src/components/customs/auto-suggests/react/color-type-auto-suggest-react.jsx
+++ src/components/customs/auto-suggests/react/color-type-auto-suggest-react.jsx
@@ -3,30 +3,30 @@ import AutoSuggestReact from '../../../form/basic/react/auto-suggest-react.jsx';
 import { Container } from 'aurelia-dependency-injection';
 import { Config } from "aurelia-api";
 
-const resource = 'sales/color-type-by-material-order-type-constructions';
+const resource = 'master/color-types';
 
 const empty = {
-    colorType :{name: ''}
+    name: ''
 }
 
 'use strict';
 
-export default class SalesColorTypeAutoSuggestReactByOrderTypeByMaterialByConstruction extends AutoSuggestReact {
+export default class ColorTypeAutoSuggestReact extends AutoSuggestReact {
     constructor(props) {
         super(props);
     }
 
     init(props) {
-        var options = Object.assign({}, SalesColorTypeAutoSuggestReactByOrderTypeByMaterialByConstruction.defaultProps.options, props.options);
+        var options = Object.assign({}, ColorTypeAutoSuggestReact.defaultProps.options, props.options);
         var initialValue = Object.assign({}, empty, props.value);
         initialValue.toString = function () {
-            return `${this.colorType.name}`;
+            return `${this.name}`;
         };
         this.setState({ value: initialValue, label: initialValue.toString(), options: options, suggestions: [initialValue] });
     }
 }
 
-SalesColorTypeAutoSuggestReactByOrderTypeByMaterialByConstruction.propTypes = {
+ColorTypeAutoSuggestReact.propTypes = {
     options: React.PropTypes.shape({
         readOnly: React.PropTypes.bool,
         suggestions: React.PropTypes.oneOfType([
@@ -36,19 +36,19 @@ SalesColorTypeAutoSuggestReactByOrderTypeByMaterialByConstruction.propTypes = {
     })
 };
 
-SalesColorTypeAutoSuggestReactByOrderTypeByMaterialByConstruction.defaultProps = {
+ColorTypeAutoSuggestReact.defaultProps = {
     options: {
         readOnly: false,
         suggestions:
         function (keyword, filter) {
             var config = Container.instance.get(Config);
-            var endpoint = config.getEndpoint("production");
+            var endpoint = config.getEndpoint("core");
 
             return endpoint.find(resource, { keyword: keyword, filter: JSON.stringify(filter) })
                 .then(results => {
                     return results.data.map(color => {
                         color.toString = function () {
-                            return `${this.colorType.name}`;
+                            return `${this.name}`;
                         }
                         return color;
                     })
