4284f86fe85af67a3e9194314828aead4a2cbbab
danliris <danliris.cloud@gmail.com>
2017-02-12 01:22:40 +0700

    Update production.js

--- src/routes/production.js
+++ src/routes/production.js
@@ -70,6 +70,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "production",
+            permission: { "C9": 1, "F1": 1, "F2": 1 },
             iconClass: 'fa fa-dashboard'
         }
     },
@@ -94,6 +95,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "production",
+            permission: { "C9": 1, "F1": 1, "F2": 1 },
             iconClass: 'fa fa-dashboard'
         }
 
@@ -107,7 +109,6 @@ module.exports = [
         auth: true,
         settings: {
             group: "production",
-            permission: { "*": 0 },
             iconClass: 'fa fa-dashboard'
         }
     }]
