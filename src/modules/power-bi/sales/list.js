7e8761e94e98d6dcd305939d3c7071fa4d96050b
indriHutabalian <indri.hutabalian@moonlay.com>
2017-02-17 17:45:45 +0700

    add power bi sales report

---
+++ src/modules/power-bi/sales/list.js
@@ -0,0 +1,34 @@
+import {inject, Lazy} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
+import {Service} from './service';
+
+
+@inject(Router, Service)
+export class List {
+    data = [];
+    listReport = [
+        "f2dec423-9b47-42e3-9d82-6103c8132015",
+        "b8e30ee2-eb32-485c-933b-f9fd91093d1f",
+        "0bee3506-5950-485b-9cda-6132e44c5764"
+    ];
+    constructor(router, service) {
+        this.router = router;
+        this.service = service;
+    }
+
+    activate() {
+        this.service.search('')
+            .then(data => {
+                for (var report of this.listReport) {
+                    var _data = data.find((_data) => _data.id === report);
+                    if (_data) {
+                        this.data.push(_data);
+                    }
+                }
+            })
+    }
+
+    view(data) {
+        this.router.navigateToRoute('view', { id: data.id });
+    }
+}
