34f254ee178bbf5b7b894dcc04d558a19c39f58b
indriHutabalian <indri.hutabalian@moonlay.com>
2017-02-17 17:45:17 +0700

    add power bi production report

---
+++ src/modules/power-bi/production/list.js
@@ -0,0 +1,35 @@
+import {inject, Lazy} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
+import {Service} from './service';
+
+
+@inject(Router, Service)
+export class List {
+    data = [];
+    listReport = [
+        "5316a960-fe55-46ee-a6ca-0f4ce2bf76c3",
+        "a6181ce8-f1cd-4624-9cbf-8eebe7792f82",
+        "c59843bc-b2b5-4b8d-a0a4-e8e2c1127973",
+        "f2dec423-9b47-42e3-9d82-6103c8132015"
+    ];
+    constructor(router, service) {
+        this.router = router;
+        this.service = service;
+    }
+
+    activate() {
+        this.service.search('')
+            .then(data => {
+                for (var report of this.listReport) {
+                    var _data = data.find((_data) => _data.id === report);
+                    if (_data) {
+                        this.data.push(_data);
+                    }
+                }
+            })
+    }
+
+    view(data) {
+        this.router.navigateToRoute('view', { id: data.id });
+    }
+}
