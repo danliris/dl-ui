bcb8971205467706c99083edfa18e016d5499a9e
danliris <danliris.cloud@gmail.com>
2017-02-20 20:34:30 +0700

    Update report.js

--- src/routes/report.js
+++ src/routes/report.js
@@ -1,19 +1,5 @@
 module.exports = [
     {
-        route: 'power-bi',
-        name: 'power-bi',
-        moduleId: './modules/power-bi/index',
-        nav: true,
-        title: 'Power BI Reports',
-        auth: true,
-        settings: {
-            group: "reports",
-            permission : {"P1": 7, "P3": 7, "P4": 7, "P6": 7, "P7": 7, "C9": 1},
-            iconClass: 'fa fa-dashboard'
-        }
-    }]
-module.exports = [
-    {
         route: 'power-bi/purchasing',
         name: 'power-bi-purchasing',
         moduleId: './modules/power-bi/purchasing/index',
@@ -35,7 +21,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "reports",
-            permission : {"C9": 1, "F1": 1, "F2": 1},
+            permission : {"C9": 1, "F1": 7, "F2": 7},
             iconClass: 'fa fa-dashboard'
         }
     },{
@@ -47,7 +33,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "reports",
-            permission : {"A2":1,"C9":1},
+            permission : {"A2":7,"C9":1},
             iconClass: 'fa fa-dashboard'
         }
     }]
