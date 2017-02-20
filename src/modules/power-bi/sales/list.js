ed39e140025878aae01396af3d2505689ddb2faf
danliris <danliris.cloud@gmail.com>
2017-02-20 19:42:49 +0700

    Update list.js

--- src/modules/power-bi/sales/list.js
+++ src/modules/power-bi/sales/list.js
@@ -9,7 +9,6 @@ export class List {
     listReport = [
         "7a433e2e-25fe-4b2e-8252-c806a4c1167a",//Top Ten Buyer
         "26d7b499-2350-4833-a4ff-ddaf01d3b4cd"//Total Order Produksi
-        "2072be69-b79e-496e-b872-959fd881970c"//Top Ten Konstruksi
     ];
     constructor(router, service) {
         this.router = router;
