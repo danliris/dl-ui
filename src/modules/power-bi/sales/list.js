28c4c37c8d86ba2ab9866a8531fc1a768ccacab4
indriHutabalian <indri.hutabalian@moonlay.com>
2017-02-20 14:41:03 +0700

    add report Power BI

--- src/modules/power-bi/sales/list.js
+++ src/modules/power-bi/sales/list.js
@@ -6,6 +6,10 @@ import {Service} from './service';
 @inject(Router, Service)
 export class List {
     data = [];
+    listReport = [
+        "7a433e2e-25fe-4b2e-8252-c806a4c1167a",//Top Ten Buyer
+        "26d7b499-2350-4833-a4ff-ddaf01d3b4cd"//Total Order Produksi
+    ];
     constructor(router, service) {
         this.router = router;
         this.service = service;
@@ -14,11 +18,16 @@ export class List {
     activate() {
         this.service.search('')
             .then(data => {
-                this.data = data;
+                for (var report of this.listReport) {
+                    var _data = data.find((_data) => _data.id === report);
+                    if (_data) {
+                        this.data.push(_data);
+                    }
+                }
             })
     }
 
     view(data) {
         this.router.navigateToRoute('view', { id: data.id });
-    } 
+    }
 }
