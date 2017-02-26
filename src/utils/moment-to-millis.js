39fed5a6053b1747503758fe49f6378f7e650618
vfenk <Orochinag1>
2017-02-26 16:31:55 +0700

    fix monitoringEvent

--- src/utils/moment-to-millis.js
+++ src/utils/moment-to-millis.js
@@ -1,5 +1,5 @@
 var moment = require("moment");
 
 module.exports = function(timeInMoment) {
-    return ((moment(timeInMoment).hour() * 3600) + (moment(timeInMoment).minute() * 60)) * 1000;
+    return Number(timeInMoment.format('x'));
 }
