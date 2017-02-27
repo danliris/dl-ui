342e7f700942c704a659cfc56670d36ddd77da63
Leslie Aula <cristiano.ulo@gmail.com>
2017-02-27 11:35:13 +0700

    routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,5 +6,6 @@ var purchasingRoutes = require("./purchasing");
 var reportRoutes = require("./report");
 var authRoutes = require("./auth"); 
 var salesRoutes = require("./sales");
+var testRoutes = require("./test-sales");
 
-export default [].concat(publicRoutes, generalRoutes, masterRoutes, productionRoutes, purchasingRoutes, salesRoutes, authRoutes, reportRoutes);
+export default [].concat(publicRoutes, generalRoutes, masterRoutes, productionRoutes, purchasingRoutes, salesRoutes, authRoutes, reportRoutes, testRoutes);
