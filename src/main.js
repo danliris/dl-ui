8a33e77765707c0a4cd46528b2de1dc3935d7cb2
danliris <danliris.cloud@gmail.com>
2017-03-08 16:41:24 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -23,10 +23,10 @@ export async function configure(aurelia) {
     .plugin("aurelia-api", config => {
 
 
-      var core = "https://dl-core-api-dev.mybluemix.net/v1/";
-      var auth = "https://dl-auth-api-dev.mybluemix.net/v1/";
-      var production = "https://dl-production-webapi-dev.mybluemix.net/v1/";
-      var purchasing = "https://dl-purchasing-webapi-dev.mybluemix.net/v1/";
+      var core = "https://ag-core-api-dev.mybluemix.net/v1/";
+      var auth = "https://ag-auth-api-dev.mybluemix.net/v1/";
+      var production = "https://ag-production-webapi-dev.mybluemix.net/v1/";
+      var purchasing = "https://ag-purchasing-webapi-dev.mybluemix.net/v1/";
 
 
       config.registerEndpoint('auth', auth);
