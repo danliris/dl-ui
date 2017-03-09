62bb8b1ba832219cab4ba6d2babf40b800ca5e74
danliris <danliris.cloud@gmail.com>
2017-03-09 12:16:31 +0700

    Update report.js

--- src/routes/report.js
+++ src/routes/report.js
@@ -21,7 +21,6 @@ module.exports = [
         auth: true,
         settings: {
             group: "reports",
-            permission : {"C9": 1, "F1": 7, "F2": 7},
             iconClass: 'fa fa-dashboard'
         }
     },{
@@ -33,7 +32,6 @@ module.exports = [
         auth: true,
         settings: {
             group: "reports",
-            permission : {"A2":7,"C9":1},
             iconClass: 'fa fa-dashboard'
         }
     }]
