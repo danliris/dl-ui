a1d32ebc8727c98ace2fc97cc5c4e966090e3d40
Allan Fadlansyah <mfadlans@gmail.com>
2017-03-13 10:12:08 +0700

    clearing commented section

--- src/modules/master/machine-type-new/data-form.js
+++ src/modules/master/machine-type-new/data-form.js
@@ -54,15 +54,6 @@ export class DataForm {
 
     }
 
-    columns = ["name", "age", "email"];
-
-  people = [
-    { id: 1, name: "Alice Ecila", age: 27, email: "alice.ecila@live.com" },
-    { id: 2, name: "Beatrix Xirtaeb", age: 26, email: "beatrix.xirtaeb@facebook.com" },
-    { id: 3, name: "Clara Aralc", age: 29, email: "clara.aralc@google.com" },
-    { id: 4, name: "Donna Annod", age: 28, email: "donna.annod@twitter.com" }];
-
-
 
 
 } 
