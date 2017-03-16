c088f3cf7d7f75f725ff8a3f21648eb86c68b6bc
danliris <danliris.cloud@gmail.com>
2017-03-16 15:43:12 +0700

    Update sales.js

--- src/routes/sales.js
+++ src/routes/sales.js
@@ -8,7 +8,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "sales",
-            permission: {"*": 0},
+            permission: {"A2": 1, "C9": 1 },
             iconClass: 'fa fa-dashboard'
         }
     },
@@ -60,7 +60,6 @@ module.exports = [
         auth: true,
         settings: {
             group: "sales",
-            permission: { "A2": 1, "C9": 1 },
             iconClass: 'fa fa-dashboard'
         }
     },
@@ -73,7 +72,6 @@ module.exports = [
         auth: true,
         settings: {
             group: "sales",
-            permission: { "A2": 1, "C9": 1 },
             iconClass: 'fa fa-dashboard'
         }
     }
