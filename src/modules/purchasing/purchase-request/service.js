80a19fa9ea9a0ff9436664cb46ab5a6ce5f04708
adityahenanda <henanda.aditya@gmail.com>
2017-03-20 18:05:55 +0700

    update weaving sales contract
    update weaving sales contract

--- src/modules/purchasing/purchase-request/service.js
+++ src/modules/purchasing/purchase-request/service.js
@@ -12,6 +12,7 @@ export class Service extends RestService {
     }
 
     search(info) {
+        debugger
         var endpoint = `${serviceUri}`;
         return super.list(endpoint, info);
     }
