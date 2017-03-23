60c37b6c7e8a41d6cb449d4a65c766b4fa794ee2
indriHutabalian <indri.hutabalian@moonlay.com>
2017-03-23 11:56:24 +0700

    add report Kapasitas Mesin VS Total Input Produksi, Perbandingan Kualitas Produksi

--- src/modules/power-bi/production/list.js
+++ src/modules/power-bi/production/list.js
@@ -7,7 +7,9 @@ import {Service} from './service';
 export class List {
     data = [];
     listReport = [
-        "9401ddc4-47e3-49a3-9a8e-8ff59043cdb5"//Top Ten Event Monitoring
+        "9401ddc4-47e3-49a3-9a8e-8ff59043cdb5",//Top Ten Event Monitoring
+        "507891a8-daa8-4b2f-bc69-d37e631ab499",//Kapasitas Mesin VS Total Input Produksi
+        "0d7d1879-bb1e-4282-a690-5454ddec91c1",//Perbandingan Kualitas Produksi
     ];
     constructor(router, service) {
         this.router = router;
