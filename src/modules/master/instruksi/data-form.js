982d836e67911fc34057a80d8672f2588091cdfa
JanuitaM <januita.marpaung@moonlay.com>
2017-03-24 10:29:30 +0700

    repair ui instruction and change product become intruksi in view

--- src/modules/master/instruksi/data-form.js
+++ src/modules/master/instruksi/data-form.js
@@ -6,8 +6,8 @@ export class DataForm {
     @bindable error = {};
     @bindable isEdit;
 
-    constructor() { } 
-    
+    constructor() { }
+
     activate() { }
 
     attached() {
@@ -22,6 +22,7 @@ export class DataForm {
 
     stepInfo = {
         columns: [
+            { header: "No.", value: "index" },
             { header: "Step", value: "step" },
         ],
         onAdd: function () {
@@ -38,6 +39,29 @@ export class DataForm {
         var selectedUom = e.detail;
         if (selectedUom)
             this.data.uomId = selectedUom._id;
+    }
+    moveItemUp(event) {
+        var steps = this.data.steps;
+        if (steps && steps.length > 0 && steps[0].selectedIndex != null && steps[0].selectedIndex > 0) {
+            var selectedSteps = steps.splice(steps[0].selectedIndex, 1);
+            steps.splice(steps[0].selectedIndex - 1, 0, selectedSteps[0])
+            this.setCurrentIndex(steps[0].selectedIndex - 1);
+        }
+    }
+
+    moveItemDown(event) {
+        var steps = this.data.steps;
+        if (steps && steps.length > 0 && steps[0].selectedIndex != null && steps[0].selectedIndex < steps.length) {
+            var selectedSteps = steps.splice(steps[0].selectedIndex, 1);
+            steps.splice(steps[0].selectedIndex + 1, 0, selectedSteps[0])
+            this.setCurrentIndex(steps[0].selectedIndex + 1);
+        }
+    }
+
+    setCurrentIndex(currentIndex) {
+        for (var step of this.data.steps) {
+            step.selectedIndex = currentIndex;
+        }
     }
 
 }
