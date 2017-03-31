11efaa862d68bc83b2550a7091d81bd3a4f58d52
achensetyawati <achenzetya@gmail.com>
2017-03-31 10:52:44 +0700

    Revert "new UI SC f/p"
    This reverts commit 695d7643bdfa2a369a2aab3d2c461e2bfe5a8692.

--- src/modules/sales/finishing-printing-sales-contract/create.js
+++ src/modules/sales/finishing-printing-sales-contract/create.js
@@ -1,36 +1,26 @@
 import {inject, Lazy} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
 import {Service} from './service';
-import moment from 'moment';
+
 
 @inject(Router, Service)
 export class Create {
-    hasCancel = true;
-    hasSave = true;
-
     constructor(router, service) {
         this.router = router;
         this.service = service;
-    }
-
-    activate(params) {
-
-    }
-
-    bind() {
-
-        this.data = this.data || {};
+        this.data = {};
         this.error = {};
     }
 
-    cancel(event) {
+
+    back() {
         this.router.navigateToRoute('list');
     }
-    save(event) {
-        this.data.deliverySchedule=moment(this.data.deliverySchedule).format("YYYY-MM-DD");
+
+    save() {
         this.service.create(this.data)
             .then(result => {
-                this.cancel();
+                this.back();
             })
             .catch(e => {
                 this.error = e;
