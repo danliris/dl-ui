6e5710efcf971e0580fc2b8b353f90142223116b
Leslie Aula <cristiano.ulo@gmail.com>
2017-05-12 11:53:28 +0700

    update navbar and index router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 var generalRoutes = require("./general");
 var masterRoutes = require("./master");
 var productionRoutes = require("./production");
+var inventoryRoutes = require("./inventory");
 var publicRoutes = require("./public");
 var purchasingRoutes = require("./purchasing");
 var reportRoutes = require("./report");
-var authRoutes = require("./auth"); 
+var authRoutes = require("./auth");
 var salesRoutes = require("./sales");
 
-export default [].concat(publicRoutes, generalRoutes, masterRoutes, productionRoutes, purchasingRoutes, salesRoutes, authRoutes, reportRoutes);
+export default [].concat(publicRoutes, generalRoutes, masterRoutes, productionRoutes, purchasingRoutes, salesRoutes, inventoryRoutes, authRoutes, reportRoutes);
