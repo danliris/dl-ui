1f9ff1270faf13145041ef786a2d5c7d35b880c0
achensetyawati <achenzetya@gmail.com>
2017-05-13 10:13:40 +0700

    adding unit filter to duration report

--- src/modules/purchasing/reports/duration-reports/purchase-request-purchase-order-external-duration-report/service.js
+++ src/modules/purchasing/reports/duration-reports/purchase-request-purchase-order-external-duration-report/service.js
@@ -32,6 +32,10 @@ export class Service extends RestService {
             if (query === '') query = `dateFrom=${info.dateFrom}`;
             else query = `${query}&dateFrom=${info.dateFrom}`;
         }
+        if (info.unitId) {
+            if (query === '') query = `unitId=${info.unitId}`;
+            else query = `${query}&unitId=${info.unitId}`;
+        }
         if (info.dateTo) {
             if (query === '') query = `dateTo=${info.dateTo}`;
             else query = `${query}&dateTo=${info.dateTo}`;
