9c7ebf64c8663e0728160d6bf24d21175b834688
danliris <danliris.cloud@gmail.com>
2017-05-15 00:28:48 +0700

    Update inventory.js

--- src/routes/inventory.js
+++ src/routes/inventory.js
@@ -8,7 +8,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "Inventory",
-            permission : {"C9":1},
+            //permission : {"C9":1},
             iconClass: 'fa fa-dashboard'
         }
     }]
