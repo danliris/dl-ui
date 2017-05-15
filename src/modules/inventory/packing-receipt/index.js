a0c90650e37e9a307835cd8fcb3d05be1c29fb29
Leslie Aula <cristiano.ulo@gmail.com>
2017-05-15 15:57:52 +0700

    update indec

--- src/modules/inventory/packing-receipt/index.js
+++ src/modules/inventory/packing-receipt/index.js
@@ -1,10 +1,10 @@
 export class Index {
     configureRouter(config, router) {
         config.map([
-            { route: ['', 'list'], moduleId: './list', name: 'list', nav: true, title: 'List: Purchase Order Internal' },
-            { route: 'view/:id', moduleId: './view', name: 'view', nav: false, title: 'View: Purchase Order Internal' },
-            { route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit: Purchase Order Internal' },
-            { route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create: Purchase Order Internal' }
+            { route: ['', 'list'], moduleId: './list', name: 'list', nav: true, title: 'List: Penerimaan Packing' },
+            { route: 'view/:id', moduleId: './view', name: 'view', nav: false, title: 'View: Penerimaan Packing' },
+            { route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit: Penerimaan Packing' },
+            { route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create: Penerimaan Packing' }
         ]);
 
         this.router = router;
