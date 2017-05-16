d798e823e07a3fa12de116f8e9b5128513e90deb
Leslie Aula <cristiano.ulo@gmail.com>
2017-05-16 16:42:15 +0700

    update packing item

--- src/modules/inventory/packing-receipt/template/packing-item.js
+++ src/modules/inventory/packing-receipt/template/packing-item.js
@@ -1,8 +1,19 @@
+import { bindable } from 'aurelia-framework'
+
 export class PackingItem {
+
+  @bindable newQuantity;
+  @bindable newNotes;
 
   activate(context) {
     this.data = context.data;
+    // console.log(this.data);
+    this.newQuantity = this.data.quantity;
+    this.newNotes = this.data.notes;
+
+    // this.items = this.data;
     this.error = context.error;
+    console.log(this.error);
     this.options = context.options;
   }
 
@@ -10,6 +21,16 @@ export class PackingItem {
     control: {
       length: 12
     }
+  }
+
+  newQuantityChanged() {
+    this.data.quantity = this.newQuantity;
+    console.log(this.data);
+  }
+
+  newNotesChanged() {
+    this.data.notes = this.newNotes;
+    console.log(this.data);
   }
 
 }
