4f1f4e5d69197e9f0f2c5a05d728d100f48f65c4
indriHutabalian <indri.hutabalian@moonlay.com>
2017-05-19 17:28:30 +0700

    add detail report production order

---
+++ src/modules/sales/reports/production-order-report/view.js
@@ -0,0 +1,59 @@
+import {inject, Lazy} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
+import {Service} from './service';
+
+@inject(Router, Service)
+export class View {
+
+    constructor(router, service) {
+        this.router = router;
+        this.service = service;
+    }
+
+
+    listSPPColumns = [
+        { value: "index", header: "No." },
+        { value: "_id", header: "Nomor Order" },
+        { value: "orderQuantity", header: "Jumlah Order" },
+        { value: "uom", header: "Satuan" }
+    ];
+
+    listQCColumns = [
+        { value: "index", header: "No." },
+        { value: "productionOrderNo", header: "Nomor Order" },
+        { value: "_id", header: "Grade" },
+        { value: "qty", header: "Panjang" }
+    ];
+
+    listDOColumns = [
+        { value: "index", header: "No." },
+        { value: "orderNo", header: "Nomor Order" },
+        { value: "machine", header: "Mesin" },
+        { value: "step", header: "Step" },
+        { value: "qty", header: "Input" }
+    ];
+
+    salesContractNo = "";
+    dailyOperations = [];
+    productionOrders = [];
+    async activate(params) {
+        this.salesContractNo = params.id;
+        this.data = await this.service.getDetailReport(this.salesContractNo);
+        this.productionOrders = this.data.productionOrders;
+        this.dailyOperations = this.data.dailyOperations;
+        this.qualityControls = this.data.qualityControls;
+
+    }
+
+    list() {
+        this.router.navigateToRoute('list');
+    }
+
+    cancel() {
+        this.service.cancel(this.poExId).then(result => {
+            this.list();
+        }).catch(e => {
+            this.error = e;
+        })
+    }
+}
