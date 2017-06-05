4f247a5e37acc3bb7474a117631284338969151b
indriHutabalian <indri.hutabalian@moonlay.com>
2017-06-05 15:46:35 +0700

    update permission

--- src/routes/master.js
+++ src/routes/master.js
@@ -191,7 +191,7 @@ module.exports = [
     auth: true,
     settings: {
       group: "master",
-      permission: { "C9": 1, "B7": 1 },
+      permission: { "C9": 1, "F1": 1, "F2": 1 },
       iconClass: 'fa fa-dashboard'
     }
   },
@@ -204,7 +204,7 @@ module.exports = [
     auth: true,
     settings: {
       group: "master",
-      permission: { "C9": 1, "B7": 1 },
+      permission: { "C9": 1, "F1": 1, "F2": 1 },
       iconClass: 'fa fa-dashboard'
     }
   }
