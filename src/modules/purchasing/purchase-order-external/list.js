6908c9cf90add6e0670e2f43d0ab98c7b1130004
indriHutabalian <indri.hutabalian@moonlay.com>
2017-06-06 10:02:04 +0700

    update list po external

--- src/modules/purchasing/purchase-order-external/list.js
+++ src/modules/purchasing/purchase-order-external/list.js
@@ -1,84 +1,111 @@
 import {inject} from 'aurelia-framework';
 import {Service} from "./service";
 import {Router} from 'aurelia-router';
+import moment from 'moment';
 
 @inject(Router, Service)
 export class List {
-    dataToBePosting = [];
+    dataToBePosted = [];
     info = { page: 1, keyword: '' };
+
+    rowFormatter(data, index) {
+        if (data.isPosted)
+            return { classes: "success" }
+        else
+            return {}
+    }
+
+    context = ["detail", "print"]
+
+    columns = [
+        {
+            field: "isPosting", title: "Post", checkbox: true, sortable: false,
+            formatter: function (value, data, index) {
+                this.checkboxEnabled = !data.isPosted;
+                return ""
+            }
+        },
+        { field: "no", title: "Nomor PO Eksternal" },
+        {
+            field: "date", title: "Tanggal PO Eksternal", formatter: function (value, data, index) {
+                return moment(value).format("DD MMM YYYY");
+            }
+        },
+        { field: "supplier.name", title: "Nama Supplier" },
+        { field: "purchaseRequestNo", title: "Nomor Purchase Request" },
+        {
+            field: "isPosted", title: "Status Post",
+            formatter: function (value, row, index) {
+                return value ? "SUDAH" : "BELUM";
+            }
+        }
+    ];
+
+    loader = (info) => {
+        var order = {};
+        if (info.sort)
+            order[info.sort] = info.order;
+        var arg = {
+            page: parseInt(info.offset / info.limit, 10) + 1,
+            size: info.limit,
+            keyword: info.search,
+            select: ["date", "no", "supplier.name", "items.purchaseRequest.no", "isPosted"],
+            order: order
+        }
+
+        return this.service.search(arg)
+            .then(result => {
+                for (var _data of result.data) {
+                    var prNo = _data.items.map(function (item) {
+                        return `<li>${item.purchaseRequest.no}</li>`;
+                    });
+                    _data.purchaseRequestNo = `<ul>${prNo.join()}</ul>`;
+                }
+                return {
+                    total: result.info.total,
+                    data: result.data
+                }
+            });
+    }
 
     constructor(router, service) {
         this.service = service;
         this.router = router;
     }
 
-    async activate() {
-        this.info.keyword = '';
-        var result = await this.service.search(this.info);
-        this.data = result.data;
-        this.info = result.info;
-    }
-
-    loadPage() {
-        var keyword = this.info.keyword;
-        this.service.search(this.info)
-            .then(result => {
-                this.data = result.data;
-                this.info = result.info;
-                this.info.keyword = keyword;
-            })
-    }
-
-    changePage(e) {
-        var page = e.detail;
-        this.info.page = page;
-        this.loadPage();
-    }
-
-    pushDataToBePosting(item) {
-        if (item.isPosting) {
-            this.dataToBePosting.push(item);
-        }
-        else {
-            var index = this.dataToBePosting.indexOf(item);
-            this.dataToBePosting.splice(index, 1);
+    contextClickCallback(event) {
+        var arg = event.detail;
+        var data = arg.data;
+        switch (arg.name) {
+            case "detail":
+                this.router.navigateToRoute('view', { id: data._id });
+                break;
+            case "print":
+                this.service.getPdfById(data._id);
+                break;
         }
     }
 
-    back() {
-        this.router.navigateToRoute('list');
+    contextShowCallback(index, name, data) {
+        switch (name) {
+            case "print":
+                return data.isPosted;
+            default:
+                return true;
+        }
     }
 
     posting() {
-        if (this.dataToBePosting.length > 0) {
-            this.service.post(this.dataToBePosting).then(result => {
-                this.info.keyword = '';
-                this.loadPage();
+        if (this.dataToBePosted.length > 0) {
+            this.service.post(this.dataToBePosted).then(result => {
+                this.table.refresh();
             }).catch(e => {
                 this.error = e;
             })
         }
     }
 
-    view(data) {
-        this.router.navigateToRoute('view', { id: data._id });
-    }
-
     create() {
         this.router.navigateToRoute('create');
-    }
-
-    tooglePostingTrue() {
-        this.isPosting = true;
-        this.isPrint = false;
-    }
-
-    tooglePostingFalse() {
-        this.isPosting = false;
-        this.isPrint = false;
-    }
-
-    exportPDF(data) {
-        this.service.getPdfById(data._id);
     }
 }
