d1d8281e2ac0a1a4223fae875bf9695105997dda
indriHutabalian <indri.hutabalian@moonlay.com>
2017-07-04 12:09:40 +0700

    update ui to au components

--- src/modules/purchasing/unit-payment-order/list.js
+++ src/modules/purchasing/unit-payment-order/list.js
@@ -1,59 +1,81 @@
 import {inject} from 'aurelia-framework';
 import {Service} from "./service";
 import {Router} from 'aurelia-router';
-
+import moment from 'moment';
 
 @inject(Router, Service)
-export class List { 
-    data = [];
-    dataToBePrinting = [];
-    keyword = '';
+export class List {
     info = { page: 1, keyword: '' };
 
-    today = new Date();
+    context = ["Rincian", "Cetak PDF"]
 
-    isPrint = false;
+    columns = [
+        { field: "division.name", title: "Divisi" },
+        { field: "supplier.name", title: "Supplier" },
+        {
+            field: "date", title: "Tanggal Surat Perintah Bayar", formatter: function (value, data, index) {
+                return moment(value).format("DD MMM YYYY");
+            }
+        },
+        { field: "no", title: "Nomor Surat Perintah Bayar" },
+        { field: "unitReceiptNoteNo", title: "List Nomor Bon Unit-Nomor Surat Jalan" }
+    ];
+
+    loader = (info) => {
+        var order = {};
+        if (info.sort)
+            order[info.sort] = info.order;
+        var arg = {
+            page: parseInt(info.offset / info.limit, 10) + 1,
+            size: info.limit,
+            keyword: info.search,
+            select: ["date", "no", "supplier.name", "division.name", "items.unitReceiptNote.no", "items.unitReceiptNote.deliveryOrder.no", "isPosted"],
+            order: order
+        }
+
+        return this.service.search(arg)
+            .then(result => {
+                for (var _data of result.data) {
+                    var btuNo = _data.items.map(function (item) {
+                        return `<li>${item.unitReceiptNote.no} - ${item.unitReceiptNote.deliveryOrder.no} </li>`;
+                    });
+                    _data.unitReceiptNoteNo = `<ul>${btuNo.join()}</ul>`;
+                }
+                return {
+                    total: result.info.total,
+                    data: result.data
+                }
+            });
+    }
 
     constructor(router, service) {
         this.service = service;
         this.router = router;
     }
 
-    async activate() {
-        this.info.keyword = '';
-        var result = await this.service.search(this.info);
-        this.data = result.data;
-        this.info = result.info;
+    contextClickCallback(event) {
+        var arg = event.detail;
+        var data = arg.data;
+        switch (arg.name) {
+            case "Rincian":
+                this.router.navigateToRoute('view', { id: data._id });
+                break;
+            case "Cetak PDF":
+                this.service.getPdfById(data._id);
+                break;
+        }
     }
 
-    loadPage() {
-        var keyword = this.info.keyword;
-        this.service.search(this.info)
-            .then(result => {
-                this.data = result.data;
-                this.info = result.info;
-                this.info.keyword = keyword;
-            })
-    }
-
-    changePage(e) {
-        var page = e.detail;
-        this.info.page = page;
-        this.loadPage();
-    }
-
-    back() {
-        this.router.navigateToRoute('list');
-    }
-
-    view(data) {
-        this.router.navigateToRoute('view', { id: data._id });
+    contextShowCallback(index, name, data) {
+        switch (name) {
+            case "Cetak PDF":
+                return true;
+            default:
+                return true;
+        }
     }
 
     create() {
         this.router.navigateToRoute('create');
-    }
-    getPDF(data) {
-        this.service.getPdfById(data._id);
     }
 }
