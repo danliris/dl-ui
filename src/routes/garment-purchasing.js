b41de5df4840d8d69dad11f2a8df88af89cd4d3b
indriHutabalian <indri.hutabalian@moonlay.com>
2017-07-13 15:51:05 +0700

    add garment purchasing

---
+++ src/routes/garment-purchasing.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        route: '/garment/pr',
+        name: 'purchase-request',
+        moduleId: './modules/garment-purchasing/purchase-request/index',
+        nav: true,
+        title: 'Purchase Request',
+        auth: true,
+        settings: {
+            group: "g-purchasing",
+            permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "S4": 1, "C3": 1, "E": 1, "K": 1, "S1": 1, "S2": 1, "S3": 1, "U1": 1, "F1": 1, "F2": 1, "L3": 1, "LK": 1, "L8": 1, "L2": 1, "C2": 1, "A2": 1, "C1": 1, "B5": 1, "L1": 1, "B4": 1, "B3": 1, "C4": 1, "OJ": 1, "C9": 1, "A1": 1, "B9": 1, "A4": 1, "C5": 1, "P1A": 1, "C2A": 1, "C2B": 1, "FP": 1, "PI": 1, "P": 1, "FC": 1, "GU": 1, "GS": 1, "PG": 1, "C1A": 1, "C1B": 1, "KK": 1, "B1": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
+    },
+    {
+        route: '/garment/pr/monitoring',
+        name: 'purchase-request-monitoring',
+        moduleId: './modules/garment-purchasing/monitoring-purchase-request/index',
+        nav: true,
+        title: 'Monitoring Purchase Request',
+        auth: true,
+        settings: {
+            group: "g-purchasing",
+            permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "S4": 1, "C3": 1, "E": 1, "K": 1, "S1": 1, "S2": 1, "S3": 1, "U1": 1, "F1": 1, "F2": 1, "L3": 1, "LK": 1, "L8": 1, "L2": 1, "C2": 1, "A2": 1, "C1": 1, "B5": 1, "L1": 1, "B4": 1, "B3": 1, "C4": 1, "OJ": 1, "C9": 1, "A1": 1, "B9": 1, "A4": 1, "C5": 1, "P1A": 1, "C2A": 1, "C2B": 1, "FP": 1, "PI": 1, "P": 1, "FC": 1, "GU": 1, "GS": 1, "PG": 1, "C1A": 1, "C1B": 1, "KK": 1, "B1": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
+    },
+    {
+        route: '/garment/pr/monitoring-purchase-request/all-unit',
+        name: 'purchase-request-monitoring-all-unit',
+        moduleId: './modules/garment-purchasing/monitoring-purchase-request-all-unit/index',
+        nav: true,
+        title: 'Monitoring Purchase Request Semua Unit',
+        auth: true,
+        settings: {
+            group: "g-purchasing",
+            permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PG": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
+    },
+]
