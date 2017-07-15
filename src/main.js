69971c44f8e5bcc720c9194ddfec530aa00517ad
danliris <danliris.cloud@gmail.com>
2017-07-15 09:15:06 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -29,12 +29,12 @@ export async function configure(aurelia) {
           'x-timezone-offset': offset
         }
       }
-      var core = "https://dl-core-api-dev.mybluemix.net/v1/";
-      var auth = "https://dl-auth-api-dev.mybluemix.net/v1/";
-      var production = "https://dl-production-webapi-dev.mybluemix.net/v1/";
-      var purchasing = "https://dl-purchasing-webapi-dev.mybluemix.net/v1/";
-      var garmentPurchasing = "https://dl-purchasing-garment-webapi-dev.mybluemix.net/v1/";
-      var inventory = "https://dl-inventory-webapi-dev.mybluemix.net/v1/";
+      var core = "https://dl-core-api-uat.mybluemix.net/v1/";
+      var auth = "https://dl-auth-api-uat.mybluemix.net/v1/";
+      var production = "https://dl-production-webapi-uat.mybluemix.net/v1/";
+      var purchasing = "https://dl-purchasing-webapi-uat.mybluemix.net/v1/";
+      //var garmentPurchasing = "https://dl-purchasing-garment-webapi-uat.mybluemix.net/v1/";
+      var inventory = "https://dl-inventory-webapi-uat.mybluemix.net/v1/";
       // var purchasing = "http://127.0.0.1:9010/v1/";
 
 
