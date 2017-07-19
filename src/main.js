f0348ba46ff415c7198e231686f0d808611c771a
lightmild <rian.sulistyawan@gmail.com>
2017-07-19 17:21:46 +0700

    edit list.js list.html service.js
    edit list.js list.html service.js

--- src/main.js
+++ src/main.js
@@ -35,9 +35,8 @@ export async function configure(aurelia) {
       var purchasing = "https://dl-purchasing-webapi-dev.mybluemix.net/v1/";
       var garmentPurchasing = "https://dl-purchasing-garment-webapi-dev.mybluemix.net/v1/";
       var inventory = "https://dl-inventory-webapi-dev.mybluemix.net/v1/";
-      // var purchasing = "http://127.0.0.1:9010/v1/";
-
-
+      
+      
       config.registerEndpoint('auth', auth);
       config.registerEndpoint('core', core);
       config.registerEndpoint('production', production, defaultConfig);
