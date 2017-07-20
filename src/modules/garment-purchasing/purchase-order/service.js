cf79d0313f6a9522f0d66557150cb71322077f2c
indriHutabalian <indri.hutabalian@moonlay.com>
2017-07-20 17:21:48 +0700

    add router purchase order

---
+++ src/modules/garment-purchasing/purchase-order/service.js
@@ -0,0 +1,62 @@
+import { inject, Lazy } from 'aurelia-framework';
+import { HttpClient } from 'aurelia-fetch-client';
+import { RestService } from '../../../utils/rest-service';
+var moment = require('moment');
+
+const serviceUri = 'purchase-orders';
+const servicePRUri = 'purchase-requests/by-tags';
+
+export class Service extends RestService {
+
+    constructor(http, aggregator, config, endpoint) {
+        super(http, aggregator, config, "garment-purchasing");
+    }
+
+    search(info) {
+        var endpoint = `${serviceUri}`;
+        return super.list(endpoint, info);
+    }
+
+    getById(id) {
+        var endpoint = `${serviceUri}/${id}`;
+        return super.get(endpoint);
+    }
+
+    create(data) {
+        var endpoint = `${serviceUri}`;
+        return super.post(endpoint, data);
+    }
+
+    split(data) {
+        var endpoint = 'purchase-orders/split';
+        return super.post(endpoint, data);
+    }
+
+    update(data) {
+        var endpoint = `${serviceUri}/${data._id}`;
+        return super.put(endpoint, data);
+    }
+
+    delete(data) {
+        var endpoint = `${serviceUri}/${data._id}`;
+        return super.delete(endpoint, data);
+    }
+
+    searchByTags(keyword, shipmentDate) {
+        var endpoint = `${servicePRUri}`;
+        var filter = {};
+        if (keyword && shipmentDate) {
+            filter = { shipmentDate: moment(shipmentDate).format("YYYY-MM-DD"), tag: keyword };
+            return super.list(endpoint, { filter: JSON.stringify(filter) });
+        }
+        else if (keyword) {
+            filter = { tag: keyword };
+            return super.list(endpoint, { filter: JSON.stringify(filter) });
+        } else if (shipmentDate) {
+            filter = { shipmentDate: moment(shipmentDate).format("YYYY-MM-DD") };
+            return super.list(endpoint, { filter: JSON.stringify(filter) });
+        } else {
+            return super.list(endpoint);
+        }
+    }
+}
