f733e661ee42568eb8c2acbc081905dbfd215b0e
kristika <kristika.sachi@gmail.com>
2017-07-21 14:32:26 +0700

    update list
    update list

--- src/main.js
+++ src/main.js
@@ -29,11 +29,10 @@ export async function configure(aurelia) {
           'x-timezone-offset': offset
         }
       }
-      var purchasing = "http://127.0.0.1:9070/v1/";
       var core = "https://dl-core-api-dev.mybluemix.net/v1/";
       var auth = "https://dl-auth-api-dev.mybluemix.net/v1/";
       var production = "https://dl-production-webapi-dev.mybluemix.net/v1/";
-      //var purchasing = "https://dl-purchasing-webapi-dev.mybluemix.net/v1/";
+      var purchasing = "https://dl-purchasing-webapi-dev.mybluemix.net/v1/";
       var garmentPurchasing = "https://dl-purchasing-garment-webapi-dev.mybluemix.net/v1/";
       var inventory = "https://dl-inventory-webapi-dev.mybluemix.net/v1/";
     
