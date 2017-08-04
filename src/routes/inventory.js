2f43a2c0c581a98707a7fdbdbda9e811fd8add80
danliris <danliris.cloud@gmail.com>
2017-08-05 01:21:23 +0700

    Update inventory.js

--- src/routes/inventory.js
+++ src/routes/inventory.js
@@ -8,7 +8,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "Inventory",
-            permission: { "*": 0 },
+            permission: { "*": 1 },
             iconClass: 'fa fa-dashboard'
         }
     },
@@ -21,7 +21,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "Inventory",
-            permission: { "*": 0 },
+            permission: { "C9": 1 },
             iconClass: 'fa fa-dashboard'
         }
     },
@@ -50,7 +50,7 @@ module.exports = [
             permission: { "C9": 1 },
             iconClass: 'fa fa-dashboard'
         }
-    }, 
+    },
     {
         route: 'inventory/reports/fp-packing-receipt-report',
         name: 'inventory/reports/fp-packing-receipt-report',
