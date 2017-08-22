c5e8bd759f1a6ea534f619da1294143a85e6cdef
Leslie Aula <cristiano.ulo@gmail.com>
2017-08-22 11:03:52 +0700

    add kanban pengganti bad output

---
+++ src/modules/production/finishing-printing/bad-output-kanban/template/cart-item.js
@@ -0,0 +1,25 @@
+export class CartItem {
+  activate(context) {
+    this.context = context;
+    this.cart = context.data;
+    this.error = context.error;
+    this.options = context.options;
+    this.contextOptions = context.context.options;
+  } 
+
+  cartClicked() {
+    if(this.context.context.selectedCart) {
+      this.context.context.selectedCart.trCart.removeAttribute("class")
+    }
+
+    this.trCart.setAttribute("class", "active");
+    this.context.context.selectedCart = { trCart: this.trCart };
+    this.contextOptions.reprocessClick(this.cart.reprocess);
+  }
+
+  controlOptions = {
+    control: {
+      length: 12
+    }
+  };
+}
