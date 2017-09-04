fdbe93b60a4ac504e84f85ac003857c31cd0c1ab
Leslie Aula <cristiano.ulo@gmail.com>
2017-09-04 16:45:23 +0700

    add routes invoice note quantity correction

--- src/routes/garment-purchasing.js
+++ src/routes/garment-purchasing.js
@@ -128,4 +128,17 @@ module.exports = [
             iconClass: 'fa fa-dashboard'
         }
     },
+    {
+        route: '/garment/invoice-note-quantity-correction',
+        name: 'invoice-note-quantity-correction',
+        moduleId: './modules/garment-purchasing/invoice-note-quantity-correction/index',
+        nav: true,
+        title: 'Nota Invoice Koreksi Jumlah',
+        auth: true,
+        settings: {
+            group: "g-purchasing",
+            permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PG": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
+    },
 ]
