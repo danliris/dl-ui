0b9d7c9e8f77725cb7fcaf1a98782827f6ad100d
adityahenanda <henanda.aditya@gmail.com>
2017-09-13 15:38:56 +0700

    ui garment currency
    ui garment currency

---
+++ src/modules/garment-purchasing/garment-currency/service.js
@@ -0,0 +1,24 @@
+import {inject, Lazy} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import {RestService} from '../../../utils/rest-service';
+
+
+const serviceUri = 'garment-currencies';
+
+export class Service extends RestService {
+
+  constructor(http, aggregator, config, api) {
+    super(http, aggregator, config, "garment-purchasing");
+  }
+
+  search(info) {
+    var endpoint = `${serviceUri}`;
+    return super.list(endpoint, info);
+  }
+
+  getById(id) {
+    var endpoint = `${serviceUri}/${id}`;
+    return super.get(endpoint);
+  } 
+
+}
