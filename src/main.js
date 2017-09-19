5939747bffa7e53f367a0bedb83858965f51513f
lightmild <rian.sulistyawan@gmail.com>
2017-09-19 15:15:58 +0700

    update servisces
    update servisces

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ export async function configure(aurelia) {
       var core = "https://dl-core-api-dev.mybluemix.net/v1/";
       var auth = "https://dl-auth-api-dev.mybluemix.net/v1/";
       var production = "https://dl-production-webapi-dev.mybluemix.net/v1/";
-     var purchasing = "https://dl-purchasing-webapi-dev.mybluemix.net/v1/";
+      var purchasing = "https://dl-purchasing-webapi-dev.mybluemix.net/v1/";
       var garmentPurchasing = "https://dl-purchasing-garment-webapi-dev.mybluemix.net/v1/";
       var inventory = "https://dl-inventory-webapi-dev.mybluemix.net/v1/";
 
