c88556a0e94ae6e36d4bb40a23c568f589a89a82
adityahenanda <henanda.aditya@gmail.com>
2017-10-03 17:57:54 +0700

    update garment category
    update garment category

--- src/modules/production/finishing-printing/reports/monitoring-montly-operation-machine/list.js
+++ src/modules/production/finishing-printing/reports/monitoring-montly-operation-machine/list.js
@@ -1,6 +1,5 @@
 import {inject} from 'aurelia-framework';
 import {Service} from "./service";
-import {CoreService} from "./core-service";
 import {Router} from 'aurelia-router';
 import moment from 'moment';
 
@@ -13,12 +12,11 @@ var machineLoader = require('../../../../../loader/machine-loader');
 var Unit = require('../../../../../loader/unit-loader');
 
 
-@inject(Router, Service, CoreService)
+@inject(Router, Service)
 export class List {
 
-    constructor(router, service, coreService) {
+    constructor(router, service) {
         this.service = service;
-        this.coreService = coreService;
         this.router = router;
 
     }
@@ -26,29 +24,19 @@ export class List {
     tableOptions = {
         search: false,
         showToggle: false,
-        showColumns: false
+        showColumns: false,
+        pagination: false,
     }
 
     Values() {
-        // debugger
-        // this.arg.dateFrom = this.dateFrom ? moment(this.dateFrom).format("YYYY-MM-DD") : undefined;
-        // this.arg.dateTo = this.dateTo ? moment(this.dateTo).format("YYYY-MM-DD") : undefined;
-        // this.arg.machine = this.machine ? this.machine.code : undefined;
-        // this.arg.buyer = this.buyer ? this.buyer._id : undefined;
-        // this.arg.category = this.category ? this.category._id : undefined;
-        // this.arg.unit = this.unit ? this.unit._id : undefined;
 
-        this.arg.filter = JSON.stringify({
-            $and: [{ "machine.code": this.machine.code },
-                { "type": "input" },
-                {
-                    "dateInput": {
-                        $gte: new Date(this.dateFrom),
-                        $lte: new Date(this.dateTo)
-                    }
-                }]
-        });
-        // this.arg.filter = JSON.stringify({$and:[{"machine.name":this.machine.code},{"type":"input"},{$where:"this.dateInput.getMonth()==6 && this.dateInput.getFullYear()==2017"}]});
+        this.arg = {
+            page: 1,
+            size: Number.MAX_SAFE_INTEGER,
+            select: ["machine.name", "input", "machine.monthlyCapacity", "machine.code", "dateInput"]
+        };
+
+        this.arg.filter = JSON.stringify({ "$and": [{ "machine.code": this.machine.code }, { "type": "input" }, { "$where": "this.dateInput >=new Date('" + this.dateFrom.toString() + "') && this.dateInput <= new Date('" + this.dateTo.toString() + "')" }] });
     }
 
     filter = {};
@@ -58,14 +46,7 @@ export class List {
         { field: "name", title: "name" },
         { field: "capacity", title: "capacity" },
         { field: "monthlyCapacity", title: "input" },
-        { field: "date", title: "date" },
-        // {
-        //     field: "date", title: "period",
-        //     formatter: (value, data) => {
-        //         return moment(value).format("DD-MMM-YYYY");
-        //     }
-        // },
-
+        { field: "date", title: "period" },
     ]
 
     loader = (info) => {
@@ -73,18 +54,13 @@ export class List {
 
         // if (info.sort)
         //     order[info.sort] = info.order;
-        this.arg = {
-            // page: parseInt(info.offset / info.limit, 10) + 1,
-            // size: info.limit,
-            // keyword: info.search,
-            // order: order,
 
-        };
 
         return this.listDataFlag ? (
             this.Values(),
             this.service.search(this.arg).then((result) => {
                 var data;
+
                 if (result.data.length == 0) {
                     data = {};
                 } else {
@@ -97,43 +73,15 @@ export class List {
                         name: result.data[0].machine.name,
                         capacity: result.data[0].machine.monthlyCapacity,
                         monthlyCapacity: sum,
-                        date: this.dateFrom + " " + this.dateTo,
+                        date: moment(this.dateFrom).format("DD-MMM-YYYY") + " hingga " + moment(this.dateTo).format("DD-MMM-YYYY"),
                     }];
 
                 }
-
                 return {
-                    // total: result.info.total,
+                    total: result.info.total,
                     data: data
                 }
             })
-            // this.service.search(this.arg)
-            //     .then(result => {
-
-            //         var datas = [];
-            //         var i = ((result.info.page - 1) * result.info.size) + 1;
-            //         for (var data of result.data) {
-            //             for (var item of data.items) {
-            //                 Object.assign(item, {
-            //                     index: i++,
-            //                     no: data.refNo,
-            //                     date: data.date,
-            //                     shipmentDate: data.shipmentDate,
-            //                     roNo: data.roNo,
-            //                     buyer: data.buyer.name,
-            //                     artikel: data.artikel,
-            //                     unit: data.unit.name,
-            //                     _createdBy: data._createdBy
-            //                 })
-            //                 datas.push(item)
-            //             }
-
-            //         }
-            //         return {
-            //             total: result.info.total,
-            //             data: datas,
-            //         }
-            //     })
         ) : { total: 0, data: {} };
     }
 
@@ -159,7 +107,8 @@ export class List {
             this.error = e;
         }
 
-        if (e == {}) {
+        if (Object.getOwnPropertyNames(e) == 0) {
+
             this.listDataFlag = true;
             this.table.refresh();
         }
