c5837e274a6addf386b22c1c1d55ae4942692693
lightmild <rian.sulistyawan@gmail.com>
2017-10-11 10:19:30 +0700

    update ui
    update ui

--- src/modules/purchasing/reports/bon-unit-blm-spb/service.js
+++ src/modules/purchasing/reports/bon-unit-blm-spb/service.js
@@ -1,47 +1,27 @@
 import { inject, Lazy } from 'aurelia-framework';
 import { HttpClient } from 'aurelia-fetch-client';
-import { RestService } from '../../../../utils/rest-service'; 
+import { RestService } from '../../../../utils/rest-service';
 
 const serviceUri = 'unit-receipt-without-spb';
+
 export class Service extends RestService {
 
     constructor(http, aggregator, config, endpoint) {
         super(http, aggregator, config, "purchasing");
     }
 
-    
-
-    search(unitId, dateFrom, dateTo) {
-    var endpoint = `${serviceUri}?unitId=${unitId}&dateFrom=${dateFrom}&dateTo=${dateTo}`;
-    return super.get(endpoint);
-    
-   }
+    search(unitId, staffName, dateFrom, dateTo) {
+        var endpoint = `${serviceUri}?unitId=${unitId}&staffName=${staffName}&dateFrom=${dateFrom}&dateTo=${dateTo}`;
+        return super.get(endpoint);
+    }
 
     getById(id) {
         var endpoint = `${serviceUri}/${id}`;
         return super.get(endpoint);
     }
 
-    
-    generateExcel(unitId, sdate, edate) {
-     var endpoint = `${serviceUri}`;
-     var query = '';
-     if (unitId) {
-            if (query == '') query = `unitId=${encodeURIComponent(unitId._id)}`;
-            else query = `${query}&unitId=${encodeURIComponent(unitId._id)}`;
-        }
-
-     if (sdate) {
-            if (query == '') query = `dateFrom=${sdate}`;
-            else query = `${query}&dateFrom=${sdate}`;
-        }
-        if (edate) {
-            if (query == '') query = `dateTo=${edate}`;
-            else query = `${query}&dateTo=${edate}`;
-        }
-     if (query != '')
-            endpoint = `${serviceUri}?${query}`;
+    generateExcel(unitId, staffName, dateFrom, dateTo) {
+        var endpoint = `${serviceUri}?unitId=${unitId}&staffName=${staffName}&dateFrom=${dateFrom}&dateTo=${dateTo}`;
         return super.getXls(endpoint);
     }
-
 }
