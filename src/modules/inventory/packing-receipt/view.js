3d1f2f4185f2f3eaa1e74a4b30ab569f0439e16e
Leslie Aula <cristiano.ulo@gmail.com>
2017-11-06 21:47:10 +0700

    update packing receipt

--- src/modules/inventory/packing-receipt/view.js
+++ src/modules/inventory/packing-receipt/view.js
@@ -20,6 +20,26 @@ export class View {
         this.packing = this.data;
         this.packing.code = this.data.packingCode;
         this.data.packing = this.data;
+
+        if (this.data.items.length > 0) {
+            var delivered = this.data.items.find((item) => item.isDelivered.toString().toLowerCase() === "true");
+            if (delivered) {
+                this.isVoid = false;
+            } else {
+                this.isVoid = true;
+            }
+        }
+    }
+
+    update() {
+        this.data.isVoid = true;
+        this.service.update(this.data)
+            .then((result) => {
+                this.router.navigateToRoute('list');
+            })
+            .catch((e) => {
+                this.error = e;
+            })
     }
 
     cancel(event) {
