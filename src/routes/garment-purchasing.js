89e718ad72bc1b5d90ebd019ec1e9028262d5840
indriHutabalian <indri.hutabalian@moonlay.com>
2017-12-05 16:46:10 +0700

    add report over budget

--- src/routes/garment-purchasing.js
+++ src/routes/garment-purchasing.js
@@ -116,6 +116,19 @@ module.exports = [
         }
     },
     {
+        route: '/garment/po-external/report/over-budget',
+        name: 'monitoring-purchase-order-external-over-budget',
+        moduleId: './modules/garment-purchasing/monitoring-purchase-order-external-over-budget/index',
+        nav: true,
+        title: 'Monitorting Purchase Order External Over Budget',
+        auth: true,
+        settings: {
+            group: "g-purchasing",
+            permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PG": 1 },
+            conClass: 'fa fa-dashboard'
+        }
+    },
+    {
         route: '/garment/monitoring-purchase',
         name: 'monitoring-purchase',
         moduleId: './modules/garment-purchasing/monitoring-purchase/index',
