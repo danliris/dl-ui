154a13fbbbe8d52ae5dc8a6d66508ef3b167e926
danliris <danliris.cloud@gmail.com>
2017-12-29 09:23:08 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -30,13 +30,13 @@ export async function configure(aurelia) {
           'x-timezone-offset': offset
         }
       }
-      var core = "https://dl-core-api-uat.mybluemix.net/v1/";
-      var auth = "https://dl-auth-api-uat.mybluemix.net/v1/";
-      var production = "https://dl-production-webapi-uat.mybluemix.net/v1/";
-      var purchasing = "https://dl-purchasing-webapi-uat.mybluemix.net/v1/";
-      var garmentPurchasing = "https://dl-purchasing-garment-webapi-uat.mybluemix.net/v1/";
-      var inventory = "https://dl-inventory-webapi-uat.mybluemix.net/v1/";
-      var garmentMasterPlan = "https://dl-garment-master-plan-webapi-uat.mybluemix.net/v1/";
+      var core = "https://dl-core-api.mybluemix.net/v1/";
+      var auth = "https://dl-auth-api.mybluemix.net/v1/";
+      var production = "https://dl-production-webapi.mybluemix.net/v1/";
+      var purchasing = "https://dl-purchasing-webapi.mybluemix.net/v1/";
+      var garmentPurchasing = "https://dl-purchasing-garment-webapi.mybluemix.net/v1/";
+      var inventory = "https://dl-inventory-webapi.mybluemix.net/v1/";
+      var garmentMasterPlan = "https://dl-garment-master-plan-webapi.mybluemix.net/v1/";
 
       config.registerEndpoint('auth', auth);
       config.registerEndpoint('core', core);
