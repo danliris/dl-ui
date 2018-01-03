0430661c3df751c51adc67cbcb8c81a8a961008f
jackyruslymoonlay <jacky.rusly@moonlay.com>
2018-01-03 14:27:50 +0700

    Machine Queue Report

--- src/routes/production.js
+++ src/routes/production.js
@@ -325,6 +325,19 @@ module.exports = [
             permission: { "C9": 1, "F1": 1, "F2": 1 },
             iconClass: 'fa fa-dashboard'
         }
+    },
+    {
+        route: 'production/finishing-printing/reports/machine-queue-report',
+        name: 'machine-queue-report',
+        moduleId: './modules/production/finishing-printing/reports/machine-queue-report/index',
+        nav: true,
+        title: 'Laporan Order Belum Diproduksi Mesin',
+        auth: true,
+        settings: {
+            group: "production",
+            permission: { "C9": 1, "F1": 1, "F2": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
     }
 ]
 
