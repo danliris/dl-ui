9ed640264c4ed10c48846af1c58efebf43aded58
achensetyawati <achenzetya@gmail.com>
2018-01-09 15:58:11 +0700

    fix master plan

--- src/modules/garment-master-plan/master-plan/view.js
+++ src/modules/garment-master-plan/master-plan/view.js
@@ -21,57 +21,68 @@ export class View {
     async activate(params) {
         var id = params.id;
         this.data = await this.service.getById(id);
-        if(this.data && this.data.bookingOrderId){
-            this.booking = {};
-            var bookingData = await this.service.getBookingById(this.data.bookingOrderId);
-            if(this.data.bookingDate !== bookingData.bookingDate)
-                this.booking["bookingDate"] = bookingData.booking;
-            if(this.data.quantity !== bookingData.orderQuantity)
-                this.booking["quantity"] = bookingData.orderQuantity;
-            if(this.data.deliveryDate !== bookingData.deliveryDate)
-                this.booking["deliveryDate"] = bookingData.deliveryDate;
-            if(this.data.remark !== bookingData.remark)
-                this.booking["remark"] = bookingData.remark;
-            var details = [];
-            for(var detail of this.data.bookingItems){
-                console.log(detail);
-                var bookingDetail = bookingData.items.find(item => item.code === detail.code);
-                console.log(bookingDetail);
-                if(bookingDetail){
-                    if(bookingDetail.masterPlanComodityId !== detail.masterPlanComodityId){
-                        detail["bookingMasterPlanComodity"] = bookingDetail.masterPlanComodity;
-                        detail["bookingMasterPlanComodityId"] = bookingDetail.masterPlanComodityId;
+        if(this.data.status=== "Booking Dihapus"){
+            this.hasEdit=false;
+        }
+        else if(this.data.status=== "Booking Dibatalkan"){
+            this.hasEdit=false;
+        }
+        if(this.data.status!== "Booking Dihapus")
+        {
+            if(this.data && this.data.bookingOrderId){
+                this.booking = {};
+                var bookingData = await this.service.getBookingById(this.data.bookingOrderId);
+                if(this.data.bookingDate !== bookingData.bookingDate)
+                    this.booking["bookingDate"] = bookingData.bookingDate;
+                if(this.data.quantity !== bookingData.orderQuantity)
+                    this.booking["quantity"] = bookingData.orderQuantity;
+                if(this.data.deliveryDate !== bookingData.deliveryDate)
+                    this.booking["deliveryDate"] = bookingData.deliveryDate;
+                if(this.data.remark !== bookingData.remark)
+                    this.booking["remark"] = bookingData.remark;
+                var details = [];
+                var bookItems=[];
+                var index=0;
+                for(var detail of this.data.bookingItems){
+                    console.log(detail);
+                    var bookingDetail = bookingData.items.find(item => item.code === detail.code);
+                    console.log(bookingDetail);
+                    if(bookingDetail){
+                        if(bookingDetail.masterPlanComodityId !== detail.masterPlanComodityId){
+                            detail["bookingMasterPlanComodity"] = bookingDetail.masterPlanComodity;
+                            detail["bookingMasterPlanComodityId"] = bookingDetail.masterPlanComodityId;
+                        }
+                        if(bookingDetail.quantity !== detail.quantity){
+                            console.log(bookingDetail.quantity);
+                            detail["bookingQuantity"] = bookingDetail.quantity;
+                        }
+                        if(bookingDetail.remark !== detail.remark)
+                            detail["bookingRemark"] = bookingDetail.remark;
+                        if(bookingDetail.deliveryDate && detail.deliveryDate && bookingDetail.deliveryDate !== detail.deliveryDate)
+                            detail["bookingDeliveryDate"] = `${(new Date(bookingDetail.deliveryDate)).getDay()} - ${((new Date(bookingDetail.deliveryDate)).getMonth() + 1)} - ${(new Date(bookingDetail.deliveryDate)).getFullYear()}`;
+                    }else{
+                        detail["deletedData"] = "MD telah menghapus detail ini"
                     }
-                    if(bookingDetail.quantity !== detail.quantity){
-                        console.log(bookingDetail.quantity);
-                        detail["bookingQuantity"] = bookingDetail.quantity;
-                    }
-                    if(bookingDetail.remark !== detail.remark)
-                        detail["bookingRemark"] = bookingDetail.remark;
-                    if(bookingDetail.deliveryDate && detail.deliveryDate && bookingDetail.deliveryDate !== detail.deliveryDate)
-                        detail["bookingDeliveryDate"] = `${(new Date(bookingDetail.deliveryDate)).getDay()} - ${((new Date(bookingDetail.deliveryDate)).getMonth() + 1)} - ${(new Date(bookingDetail.deliveryDate)).getFullYear()}`;
-                }else{
-                    detail["deletedData"] = "MD telah menghapus detail ini"
+                    details.push(detail);
                 }
-                details.push(detail);
-            }
-            for(var item of bookingData.items){
-                var detail = this.data.bookingItems.find(detail => detail.code === item.code);
-                if(!detail){
-                    var newDetail= {
-                        code:item.code,
-                        masterPlanComodityId:item.masterPlanComodityId,
-                        masterPlanComodity:item.masterPlanComodity,
-                        quantity:item.quantity,
-                        remark:item.remark,
-                        deliveryDate:item.deliveryDate,
-                        newData:"MD telah menambah detail ini"
+                for(var item of bookingData.items){
+                    var detail = this.data.bookingItems.find(detail => detail.code === item.code);
+                    if(!detail){
+                        var newDetail= {
+                            code:item.code,
+                            masterPlanComodityId:item.masterPlanComodityId,
+                            masterPlanComodity:item.masterPlanComodity,
+                            quantity:item.quantity,
+                            remark:item.remark,
+                            deliveryDate:item.deliveryDate,
+                            newData:"MD telah menambah detail ini"
+                        }
+                        details.push(newDetail);
                     }
-                    details.push(newDetail);
                 }
-            }
-            this.data.bookingItems = details;
+                this.data.bookingItems = details;
 
+            }
         }
     }
 
