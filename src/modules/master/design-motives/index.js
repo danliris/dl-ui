09da2b27545fbb0cc5fa7f874f07ccfdf4cdcf77
mocia <mo_cia@yahoo.com>
2018-01-25 09:58:32 +0700

    ui design motives

--- src/modules/master/design-motives/index.js
+++ src/modules/master/design-motives/index.js
@@ -1,10 +1,10 @@
 export class Index {
     configureRouter(config, router) {
         config.map([
-            { route: ['', 'list', 'index'], moduleId: './list', name: 'list', nav: false, title: 'List: Komoditas' },
-            { route: 'view/:id', moduleId: './view', name: 'view', nav: false, title: 'View: Komoditas' },
-            { route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit: Komoditas' },
-            { route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create: Komoditas' },
+            { route: ['', 'list', 'index'], moduleId: './list', name: 'list', nav: false, title: 'List: Desain Motif' },
+            { route: 'view/:id', moduleId: './view', name: 'view', nav: false, title: 'View: Desain Motif' },
+            { route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit: Desain Motif' },
+            { route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create: Desain Motif' },
             
         ]);
 
