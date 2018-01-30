167b43d452e025c2a28dd84c4092439fa55cff99
adityahenanda <henanda.aditya@gmail.com>
2018-01-30 11:24:43 +0700

    update delivery order
    update delivery order

--- src/modules/garment-purchasing/delivery-order/template/delivery-order-item-footer.js
+++ src/modules/garment-purchasing/delivery-order/template/delivery-order-item-footer.js
@@ -67,9 +67,10 @@ export class DetailFooter {
       var total = this.context.items
         .map((item) => {
           if (item.data.fulfillments instanceof Array) {
+            debugger
             var qty = item.data.fulfillments
-              .map((fulfillment) =>((fulfillment.deliveredQuantity) * (fulfillment.pricePerDealUnit)));
-              return qty
+              .map((fulfillment) => parseFloat(((fulfillment.deliveredQuantity) * (fulfillment.pricePerDealUnit)).toFixed(2)));
+            return qty
               .reduce((prev, curr, index) => { return prev + curr }, 0);
           }
           else {
@@ -77,8 +78,9 @@ export class DetailFooter {
           }
         });
       return total
-        .reduce((prev, curr, index) => { 
-          return prev + curr }, 0);
+        .reduce((prev, curr, index) => {
+          return prev + parseFloat(curr.toFixed(2))
+        }, 0);
     }
     else {
       return 0
