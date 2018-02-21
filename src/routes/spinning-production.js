3a88848d0a20184447696eb39e7c76e647c0e7a7
Leslie Aula <cristiano.ulo@gmail.com>
2018-02-21 16:56:13 +0700

    Add Report Output Yarn

--- src/routes/spinning-production.js
+++ src/routes/spinning-production.js
@@ -24,5 +24,18 @@ module.exports = [
             permission: { "*": 1 },
             iconClass: 'fa fa-dashboard'
         }
+    },
+    {
+        route: 'production/spinning/yarn-output-production-report',
+        name: 'yarn-output-productions-report',
+        moduleId: './modules/production/spinning/reports/yarn-output-productions-report/index',
+        nav: true,
+        title: 'Laporan Output Produksi Spinning',
+        auth: true,
+        settings: {
+            group: "s-production",
+            permission: { "*": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
     }
 ]
