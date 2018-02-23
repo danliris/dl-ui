eea5bdd10e5890c4d94864e1580c93f4ac60a719
danliris <danliris.cloud@gmail.com>
2018-02-23 14:25:47 +0700

    Update garment-master-plan.js

--- src/routes/garment-master-plan.js
+++ src/routes/garment-master-plan.js
@@ -21,7 +21,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "g-master-plan",
-            //permission: { "C5": 1, "C9": 1 },
+            permission: { "C5": 1, "C9": 1 },
             iconClass: 'fa fa-dashboard'
         }
     },
