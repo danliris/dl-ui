e80bceb2520216311abb771ce154641d5a41f3ae
Leslie Aula <cristiano.ulo@gmail.com>
2018-03-06 14:43:58 +0700

    Add Materials Request Note.

---
+++ src/modules/production/finishing-printing/materials-request-note/list.js
@@ -0,0 +1,74 @@
+import { inject } from 'aurelia-framework';
+import { Service } from "./service";
+import { Router } from 'aurelia-router';
+var moment = require("moment");
+
+@inject(Router, Service)
+export class List {
+
+    constructor(router, service) {
+        this.service = service;
+        this.router = router;
+    }
+
+    bind() {
+        this.setContext();
+        this.setColumns();
+    }
+
+    setContext() {
+        this.context = ["Rincian"];
+    }
+
+    setColumns() {
+        this.columns = [
+            { field: "Code", title: "No. SPB" },
+            { field: "Unit.name", title: "Unit" },
+            { field: "RequestType", title: "Tipe" },
+            { field: "ProductionOrderList", title: "Benang" }
+        ];
+    }
+
+
+    loadData = (info) => {
+        var order = {};
+        if (info.sort)
+            order[info.sort] = info.order;
+
+        var arg = {
+            page: parseInt(info.offset / info.limit, 10) + 1,
+            size: info.limit,
+            keyword: info.search,
+            order: order
+        }
+
+        return this.service.search(arg)
+            .then((result) => {
+                for (var data of result.data) {
+                    data.ProductionOrderList = data.MaterialsRequestNote_Items.map((item) => {
+                        return "- " + item.ProductionOrder.orderNo;
+                    });
+                    data.ProductionOrderList = data.ProductionOrderList.join("\n");
+                }
+
+                return {
+                    total: result.info.total,
+                    data: result.data
+                }
+            });
+    }
+
+    contextClickCallback(event) {
+        var arg = event.detail;
+        var data = arg.data;
+        switch (arg.name) {
+            case "Rincian":
+                this.router.navigateToRoute('view', { id: data.Id });
+                break;
+        }
+    }
+
+    create() {
+        this.router.navigateToRoute('create');
+    }
+}
