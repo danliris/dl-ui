a20f1bde392cf5f28a6feef8005ab2009443a7c3
kuswandanu <kuswandanu@gmail.com>
2018-03-21 13:22:22 +0700

    Cancel Confirm using dialog.prompt instead of javascript confirm

--- src/modules/garment-master-plan/booking-order/templates/booking-order-detail.js
+++ src/modules/garment-master-plan/booking-order/templates/booking-order-detail.js
@@ -1,11 +1,15 @@
 import { inject, bindable, computedFrom } from 'aurelia-framework';
 import { Container } from 'aurelia-dependency-injection';
 import { Config } from "aurelia-api";
+import { Dialog } from '../../../../components/dialog/dialog';
 import moment from 'moment';
 var comodityLoader = require('../../../../loader/garment-master-plan-comodity-loader');
 
+@inject(Dialog)
 export class DetailItem {
-
+  constructor(dialog) {
+    this.dialog = dialog;
+  }
 
   activate(item) {
     this.data = item.data;
@@ -31,8 +35,14 @@ export class DetailItem {
   }
 
   oncancel(data) {
-    if (confirm("Cancel Confirm Comodity " + this.data.masterPlanComodity.name + " (Jumlah : " + this.data.quantity + ")\nLanjutkan?")) {
-      this.data.isCanceled = true;
-    }
+    // if (confirm("Cancel Confirm Comodity " + this.data.masterPlanComodity.name + " (Jumlah : " + this.data.quantity + ")\nLanjutkan?")) {
+    //   this.data.isCanceled = true;
+    // }
+    this.dialog.prompt("Cancel Confirm Comodity " + this.data.masterPlanComodity.name + " (Jumlah : " + this.data.quantity + ")", "Lanjutkan?")
+      .then(response => {
+        if (response == "ok") {
+          this.data.isCanceled = true;
+        }
+      });
   }
 }
