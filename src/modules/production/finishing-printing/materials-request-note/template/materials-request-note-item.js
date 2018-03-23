ac3c8b6c7d2150a8a0c29dcf1548bd406b5f1fb7
Leslie Aula <cristiano.ulo@gmail.com>
2018-03-23 18:46:45 +0700

    Enhance Material Request Note and Material Distribution Note

--- src/modules/production/finishing-printing/materials-request-note/template/materials-request-note-item.js
+++ src/modules/production/finishing-printing/materials-request-note/template/materials-request-note-item.js
@@ -9,10 +9,30 @@ export class MaterialsRequestNoteItem {
         this.options = context.options;
         this.productionOrderFilter = context.context.options.productionOrderFilter;
         this.isTest = context.context.options.isTest;
+        this.isComplete = context.context.options.isComplete;
+        this.isView = context.context.options.isView;
+        this.isAwal = context.context.options.isAwal;
+        this.isEdit = context.context.options.isEdit;
+        this.alreadyChecked = this.data.ProductionOrder && this.data.ProductionOrder.isCompleted ? true : false;
+        this.data.checked = false;
+        console.log(this.data)
+        
     }
 
     get productLoader() {
         return ProductLoader;
+    }
+
+    checkboxClick() {
+        console.log("click")
+        if (this.alreadyChecked) {
+            return false;
+        }
+        return true;
+    }
+
+    checkboxChanges(e) {
+        this.data.ProductionOrder.isCompleted = this.data.checked ? true : false;
     }
 
     productChanged(e) {
