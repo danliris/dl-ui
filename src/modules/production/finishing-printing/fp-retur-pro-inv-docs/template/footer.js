4bc1a87cf8ea57c7318e3b46deeadbfae6470d18
adityahenanda <henanda.aditya@gmail.com>
2018-03-28 19:10:36 +0700

    update fp retur pro inv docs
    update fp retur pro inv docs

--- src/modules/production/finishing-printing/fp-retur-pro-inv-docs/template/footer.js
+++ src/modules/production/finishing-printing/fp-retur-pro-inv-docs/template/footer.js
@@ -1,42 +1,42 @@
-export class footer {
+// export class footer {
 
-    activate(context) {
-        this.context = context;
-    }
+//     activate(context) {
+//         this.context = context;
+//     }
 
-    get LengthTotal() {
-        if (this.context.items) {
-            if (this.context.items.length > 0) {
-                var qty = this.context.items
-                    .map((item) => parseFloat(item.data.Length.toFixed(2)));
-                return qty
-                    .reduce((prev, curr, index) => { return prev + parseFloat(curr.toFixed(2)) }, 0);
-            } else {
-                return 0;
-            }
-        } else {
-            return 0;
-        }
-    }
+//     get LengthTotal() {
+//         if (this.context.items) {
+//             if (this.context.items.length > 0) {
+//                 var qty = this.context.items
+//                     .map((item) => parseFloat(item.data.Length.toFixed(2)));
+//                 return qty
+//                     .reduce((prev, curr, index) => { return prev + parseFloat(curr.toFixed(2)) }, 0);
+//             } else {
+//                 return 0;
+//             }
+//         } else {
+//             return 0;
+//         }
+//     }
 
-    get QuantityTotal() {
-        if (this.context.items) {
-            if (this.context.items.length > 0) {
-                var qty = this.context.items
-                    .map((item) => parseFloat(item.data.Quantity.toFixed(2)));
-                return qty
-                    .reduce((prev, curr, index) => { return prev + parseFloat(curr.toFixed(2)) }, 0);
-            } else {
-                return 0;
-            }
-        } else {
-            return 0;
-        }
-    }
+//     get QuantityTotal() {
+//         if (this.context.items) {
+//             if (this.context.items.length > 0) {
+//                 var qty = this.context.items
+//                     .map((item) => parseFloat(item.data.Quantity.toFixed(2)));
+//                 return qty
+//                     .reduce((prev, curr, index) => { return prev + parseFloat(curr.toFixed(2)) }, 0);
+//             } else {
+//                 return 0;
+//             }
+//         } else {
+//             return 0;
+//         }
+//     }
 
-    controlOptions = {
-        control: {
-            length: 12
-        }
-    };
-}
+//     controlOptions = {
+//         control: {
+//             length: 12
+//         }
+//     };
+// }
