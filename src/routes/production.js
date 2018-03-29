99e3d40a93ab1dcd39ac2202fba50ee6f00aafc0
Leslie Aula <leslieaula@users.noreply.github.com>
2018-03-29 15:15:22 +0700

    Add Materials Request Note Report

--- src/routes/production.js
+++ src/routes/production.js
@@ -377,8 +377,20 @@ module.exports = [
             permission: { "*": 1 },
             iconClass: "fa fa-dashboard"
         }
+    },
+    {
+        route: 'production/finishing-printing/reports/materials-request-note-report',
+        name: 'materials-request-note-report',
+        moduleId: './modules/production/finishing-printing/reports/materials-request-note-report/index',
+        nav: true,
+        title: 'Laporan Surat Permintaan Barang',
+        auth: true,
+        settings: {
+            group: "production",
+            permission: { "C9": 1, "F1": 1, "F2": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
     }
-
 ]
 
 
