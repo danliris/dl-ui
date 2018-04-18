341f73b14f7b130361a872525a71484457b09fe0
achensetyawati <achenzetya@gmail.com>
2018-04-18 09:09:02 +0700

    add report routes

--- src/routes/int-purchasing.js
+++ src/routes/int-purchasing.js
@@ -11,5 +11,18 @@ module.exports = [
             permission: { "C5": 1, "C9": 1 },
             iconClass: 'fa fa-dashboard'
         }
+    },
+    {
+        route: '/int-purchasing/transfer-request-report',
+        name: 'transfer-request-report',
+        moduleId: './modules/int-purchasing/transfer-request-report/index',
+        nav: true,
+        title: 'Laporan Transfer Request',
+        auth: true,
+        settings: {
+            group: "int-purchasing",
+            permission: { "C5": 1, "C9": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
     }
 ]
