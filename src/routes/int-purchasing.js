ddfd21b37e2cb11008be7afe2113f48212b817e3
mocia <mo_cia@yahoo.com>
2018-04-18 08:40:34 +0700

    modify route

--- src/routes/int-purchasing.js
+++ src/routes/int-purchasing.js
@@ -1,11 +1,24 @@
 module.exports = [
     {
-
-        route: ['int-purchasing/transfer-delivery-order'],
-        name: 'transfer-delivery-order',
-        moduleId: './modules/int-purchasing/transfer-delivery-order/index',
+        route: '/int-purchasing/transfer-request',
+        name: 'transfer-request',
+        moduleId: './modules/int-purchasing/transfer-request/index',
         nav: true,
-        title: 'Surat Jalan',
+        title: 'Transfer Request',
+        auth: true,
+        settings: {
+            group: "int-purchasing",
+            permission: { "C5": 1, "C9": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
+    },
+    
+    {
+        route: ['int-purchasing/internal-transfer-order'],
+        name: 'internal-transfer-order',
+        moduleId: './modules/int-purchasing/internal-transfer-order/index',
+        nav: true,
+        title: 'Transfer Order Internal',
         auth: true,
         settings: {
             group: "int-purchasing",
@@ -14,11 +27,12 @@ module.exports = [
         }
     },
     {
-        route: ['int-purchasing/internal-transfer-order'],
-        name: 'internal-transfer-order',
-        moduleId: './modules/int-purchasing/internal-transfer-order/index',
+
+        route: ['int-purchasing/transfer-delivery-order'],
+        name: 'transfer-delivery-order',
+        moduleId: './modules/int-purchasing/transfer-delivery-order/index',
         nav: true,
-        title: 'Transfer Order Internal',
+        title: 'Surat Jalan',
         auth: true,
         settings: {
             group: "int-purchasing",
@@ -36,19 +50,6 @@ module.exports = [
         settings: {
             group: "int-purchasing",
             permission: { "C9": 1, "F1": 1, "F2": 1 },
-            iconClass: 'fa fa-dashboard'
-        }
-    },
-    {
-        route: '/int-purchasing/transfer-request',
-        name: 'transfer-request',
-        moduleId: './modules/int-purchasing/transfer-request/index',
-        nav: true,
-        title: 'Transfer Request',
-        auth: true,
-        settings: {
-            group: "int-purchasing",
-            permission: { "C5": 1, "C9": 1 },
             iconClass: 'fa fa-dashboard'
         }
     }
