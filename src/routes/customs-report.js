7539b31eac28b770506a1f544691287d1dda9d75
achensetyawati <achenzetya@gmail.com>
2018-05-02 16:44:11 +0700

    add customs report out route

--- src/routes/customs-report.js
+++ src/routes/customs-report.js
@@ -1,10 +1,23 @@
 module.exports = [
     {
         route: '/customs-report/customs-report-in',
-        name: 'customs-report',
+        name: 'customs-report-in',
         moduleId: './modules/customs-report/customs-report-in/index',
         nav: true,
         title: 'Laporan Pemasukan Barang per Dokumen Pabean',
+        auth: true,
+        settings: {
+            group: "customs-report",
+            permission: { "C5": 1, "C9": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
+    },
+    {
+        route: '/customs-report/customs-report-out',
+        name: 'customs-report-out',
+        moduleId: './modules/customs-report/customs-report-out/index',
+        nav: true,
+        title: 'Laporan Pengeluaran Barang per Dokumen Pabean',
         auth: true,
         settings: {
             group: "customs-report",
