d62e0650cdf853a071d88a11dca8efb22fadbd01
mocia <mo_cia@yahoo.com>
2018-05-03 10:38:11 +0700

    add report Scrap (Beacukai)

--- src/routes/customs-report.js
+++ src/routes/customs-report.js
@@ -11,5 +11,31 @@ module.exports = [
             permission: { "C5": 1, "C9": 1 },
             iconClass: 'fa fa-dashboard'
         }
+    },
+    {
+        route: '/customs-report/customs-report-out',
+        name: 'customs-report',
+        moduleId: './modules/customs-report/customs-report-out/index',
+        nav: true,
+        title: 'Laporan Pengeluaran Barang per Dokumen Pabean',
+        auth: true,
+        settings: {
+            group: "customs-report",
+            permission: { "C5": 1, "C9": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
+    },
+    {
+        route: '/customs-report/Scrap',
+        name: 'customs-report',
+        moduleId: './modules/customs-report/Scrap/index',
+        nav: true,
+        title: 'Laporan Pertanggungjawaban Barang Reject dan Scrap',
+        auth: true,
+        settings: {
+            group: "customs-report",
+            permission: { "C5": 1, "C9": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
     }
 ]
