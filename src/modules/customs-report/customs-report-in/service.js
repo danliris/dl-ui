50532dbd93148026f65a8b3420757ce6224c9518
achensetyawati <achenzetya@gmail.com>
2018-05-04 09:04:08 +0700

    fix customs reports

--- src/modules/customs-report/customs-report-in/service.js
+++ src/modules/customs-report/customs-report-in/service.js
@@ -2,7 +2,7 @@ import { inject, Lazy } from 'aurelia-framework';
 import { HttpClient } from 'aurelia-fetch-client';
 import { RestService } from '../../../utils/rest-service';
 
-const serviceUri = 'fact-beacukai-reports';
+const serviceUri = 'customs-reports';
 
 export class Service extends RestService {
     constructor(http, aggregator, config, endpoint) {
@@ -15,9 +15,4 @@ export class Service extends RestService {
         return super.list(endpoint, info);
     }
 
-    getXls(info) {
-        console.log(info);
-        let endpoint = `${serviceUri}/download?trNo=${info.trNo}&unitId=${info.unitId}&status=${info.status}&dateFrom=${info.dateFrom}&dateTo=${info.dateTo}`;
-        return super.getXls(endpoint);
-    }
 }
