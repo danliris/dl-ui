cb6889b61c75afeca978223f6260944e3bb135a0
achensetyawati <achenzetya@gmail.com>
2018-05-04 15:52:14 +0700

    add item mutation central

--- src/modules/customs-report/fact-item-mutation-bp-central/list.js
+++ src/modules/customs-report/fact-item-mutation-bp-central/list.js
@@ -8,15 +8,11 @@ export class List {
   constructor(router, service) {
         this.service = service;
         this.router = router;
+        
     }
     
     info = { page: 1,size:50};
-     
-   
-    search(){
-        this.info.page = 1;
-        this.searching();
-    }
+
     controlOptions = {
         label: {
             length: 4
@@ -25,36 +21,46 @@ export class List {
             length: 4
         }
     };
-    
+     
+
+    search(){
+        this.error = {};
+
+        if (!this.dateTo || this.dateTo == "Invalid Date")
+            this.error.dateTo = "Tanggal Akhir harus diisi";
+
+        if (!this.dateFrom || this.dateFrom == "Invalid Date")
+            this.error.dateFrom = "Tanggal Awal harus diisi";
+
+
+        if (Object.getOwnPropertyNames(this.error).length === 0) {
+            this.flag = true;
+            this.info.page = 1;
+            this.info.total=0;
+            this.searching();
+        }
+    }
+
     searching() {
      
     var args = {
             page: this.info.page,
             size: this.info.size,
-            date  : this.date ? moment(this.date ).format("YYYY-MM-DD") : ""
-             
+            dateFrom : this.dateFrom ? moment(this.dateFrom).format("YYYY-MM-DD") : "",
+            dateTo : this.dateTo ? moment(this.dateTo).format("YYYY-MM-DD") : ""
         }
         this.service.search(args)
      
             .then(result => {
+               this.info.total=result.info.total;    
                this.data=result.data;
-            //    this.info.total=result.info.total;              
+               for(var a of this.data){
+                   a.LastQty=a.BeginQty+a.ReceiptQty-a.ExpenditureQty+a.AdjustmentQty+a.OpnameQty;
+                   a.Diff=0;
+               }
             });
-           
+            
     }
-    // ExportToExcel() {
-    //     var info = {
-    //         section : this.section ? this.section.code.code : "",
-    //         code : this.code ? this.code.code : "",
-    //         buyer : this.buyer ? this.buyer.name : "",
-    //         comodity : this.comodity ? this.comodity.name : "",
-    //         confirmState : this.confirmState ? this.confirmState : "",
-    //         bookingOrderState : this.bookingOrderState ? this.bookingOrderState : "",
-    //         dateFrom : this.dateFrom ? moment(this.dateFrom).format("YYYY-MM-DD") : "",
-    //         dateTo : this.dateTo ? moment(this.dateTo).format("YYYY-MM-DD") : ""
-    //     }
-    //     this.service.generateExcel(info);
-    // }
 
     changePage(e) {
         var page = e.detail;
