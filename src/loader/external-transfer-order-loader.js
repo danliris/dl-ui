51596aadc356f62c0391687f869da5eda79cee8f
kharissuryandaru <35208779+kharissuryandaru@users.noreply.github.com>
2018-05-04 14:35:14 +0700

    Fixed TransferDeliveryOrder

--- src/loader/external-transfer-order-loader.js
+++ src/loader/external-transfer-order-loader.js
@@ -9,22 +9,12 @@ module.exports = function(keyword, filter) {
     var endpoint = config.getEndpoint("int-purchasing");
     var filterTemp = Object.assign({}, filter);
     var currentUsed = filterTemp ? filterTemp.currentUsed : null;
-    console.log(currentUsed);
     
     if (filterTemp && filterTemp.currentUsed) {
         delete filterTemp.currentUsed;
     }
-    console.log(filterTemp);
     return endpoint.find(resource, { keyword: keyword, filter: JSON.stringify(filterTemp), currentUsed: currentUsed })
     .then(results => {
-        // return results.data.map(result => {
-        //     result.toString = function () {
-        //         return `${this.ETONo}`;
-        //     }
-        //     // console.log(results);
-        //     return result;
-            
-        // });
         if (currentUsed) {
             return results.data.filter((data) => data && currentUsed.indexOf(data.Id) < 0);
         }
