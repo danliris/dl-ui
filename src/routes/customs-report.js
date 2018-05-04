0c632222adfd28ed636d1c153be0f57b6235a71a
achensetyawati <achenzetya@gmail.com>
2018-05-04 08:51:08 +0700

    add route

--- src/routes/customs-report.js
+++ src/routes/customs-report.js
@@ -14,7 +14,7 @@ module.exports = [
     },
     {
         route: '/customs-report/customs-report-out',
-        name: 'customs-report',
+        name: 'customs-report-out',
         name: 'customs-report-out',
         moduleId: './modules/customs-report/customs-report-out/index',
         nav: true,
@@ -28,10 +28,24 @@ module.exports = [
     },
     {
         route: '/customs-report/Scrap',
-        name: 'customs-report',
+        name: 'customs-report-scrap',
         moduleId: './modules/customs-report/Scrap/index',
         nav: true,
         title: 'Laporan Pertanggungjawaban Barang Reject dan Scrap',
+        auth: true,
+        settings: {
+            group: "customs-report",
+            permission: { "C5": 1, "C9": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
+
+    },
+    {
+        route: '/customs-report/fact-item-mutation-bb-unit',
+        name: 'customs-report-item-mutation-bb-unit',
+        moduleId: './modules/customs-report/fact-item-mutation-bb-unit/index',
+        nav: true,
+        title: 'Laporan Pertanggungjawaban Mutasi Bahan Baku Unit',
         auth: true,
         settings: {
             group: "customs-report",
