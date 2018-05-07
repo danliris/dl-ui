e6350229821db3f4c21a3559782143fb9b18e23c
achensetyawati <achenzetya@gmail.com>
2018-05-07 14:53:26 +0700

    add router for SMV and Comodity

--- src/routes/garment-master-plan.js
+++ src/routes/garment-master-plan.js
@@ -13,6 +13,19 @@ module.exports = [
         }
     },
     {
+        route: '/garment-master-plan/standard-minute-value',
+        name: 'standard-hour',
+        moduleId: './modules/garment-master-plan/standard-hour-list/index',
+        nav: true,
+        title: 'Standard Minute Value',
+        auth: true,
+        settings: {
+            group: "g-master-plan",
+            permission: { "C5": 1, "C9": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
+    },
+    {
         route: '/garment-master-plan/garment-section',
         name: 'garment-section',
         moduleId: './modules/garment-master-plan/garment-section/index',
@@ -29,6 +42,19 @@ module.exports = [
         route: '/garment-master-plan/master-plan-comodity',
         name: 'master-plan-comodity',
         moduleId: './modules/garment-master-plan/master-plan-comodity/index',
+        nav: true,
+        title: 'Komoditi',
+        auth: true,
+        settings: {
+            group: "g-master-plan",
+            permission: { "C5": 1, "C9": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
+    },
+    {
+        route: '/garment-master-plan/master-plan-comodity',
+        name: 'master-plan-comodity',
+        moduleId: './modules/garment-master-plan/master-plan-comodity-list/index',
         nav: true,
         title: 'Komoditi',
         auth: true,
