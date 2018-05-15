8ceedf5f2ae56734c9a35d936a2f7c7d9772382d
danliris <danliris.cloud@gmail.com>
2018-05-15 14:46:35 +0700

    Update expedition.js

--- src/routes/expedition.js
+++ src/routes/expedition.js
@@ -8,7 +8,7 @@ module.exports =  [
         auth: true,
         settings: {
             group: "expedition",
-            permission: { "P1": 1,"P2": 1,"P3": 1, "C9": 1 },
+            permission: { "P1": 1,"P2": 1,"P3": 1,"P4": 1,"P5": 1,"P6": 1,"P7": 1,"PI": 1,"PG": 1,"PK": 1, "C9": 1 },
             iconClass: 'fa fa-clone'
         }
     },
