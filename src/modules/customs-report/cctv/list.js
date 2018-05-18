d8c4161849007250eaa3d1e9928268ad37f5766e
zilongakai44 <zilongakai44@gmail.com>
2018-05-18 11:51:46 +0700

    CCTV Beacukai
    Mengurangi menu link cctv beacukai , hanya 1 yang dibutuhkan

--- src/modules/customs-report/cctv/list.js
+++ src/modules/customs-report/cctv/list.js
@@ -13,15 +13,6 @@ export class List {
       tv1() {
     window.open("http://cctv.danliris.com:8001");  
   }
-      tv2() {
-    window.open("http://cctv.danliris.com:8002");
-
-    }
-      tv3() {
-    window.open("http://cctv.danliris.com:8006");  
-  }
-      tv4() {
-    window.open("http://cctv.danliris.com:8007");
-    }
+ 
    
 }
