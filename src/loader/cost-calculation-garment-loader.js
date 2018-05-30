807ebc01298b6b552a10ca2bc8de3f87536823ef
jackyruslymoonlay <jacky.rusly@moonlay.com>
2018-05-30 14:55:14 +0700

    MD

--- src/loader/cost-calculation-garment-loader.js
+++ src/loader/cost-calculation-garment-loader.js
@@ -6,7 +6,7 @@ const resource = 'cost-calculation-garments';
 module.exports = function (keyword, filter) {
 
     var config = Container.instance.get(Config);
-    var endpoint = config.getEndpoint("md");
+    var endpoint = config.getEndpoint("merchandiser");
 
     return endpoint.find(resource, { keyword: keyword, filter: JSON.stringify(filter) })
         .then(results => {
