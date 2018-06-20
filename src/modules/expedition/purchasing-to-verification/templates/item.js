5312f12d9f00039839654c6bd191888e4e629fc0
jackyruslymoonlay <jacky.rusly@moonlay.com>
2018-06-20 17:01:39 +0700

    Expedition Unit

--- src/modules/expedition/purchasing-to-verification/templates/item.js
+++ src/modules/expedition/purchasing-to-verification/templates/item.js
@@ -1,12 +1,15 @@
-import { bindable } from 'aurelia-framework';
+import { bindable, inject } from 'aurelia-framework';
 import moment from 'moment';
 import numeral from 'numeral';
+import { Service } from '../service';
 const UnitPaymentOrderLoader = require('../../../../loader/unit-payment-order-loader');
 
+@inject(Service)
 export class Item {
     @bindable unitPaymentOrder;
 
-    constructor() {
+    constructor(service) {
+        this.service = service;
         this.queryUPO = { position: 1 }; // PURCHASING_DIVISION
         this.selectUPO = [
             'invoceNo', 'division.code', 'division.name',
@@ -21,7 +24,7 @@ export class Item {
             'items.unitReceiptNote.items.deliveredUom.unit',
             'items.unitReceiptNote.items.pricePerDealUnit',
             'items.unitReceiptNote.items.purchaseOrder.purchaseOrderExternal.no',
-            'items.unitReceiptNote.unit._id', 'items.unitReceiptNote.unit.code', 'items.unitReceiptNote.unit.name',
+            'items.unitReceiptNote.no',
             'items.unitReceiptNote.items.correction',
         ];
 
@@ -40,70 +43,88 @@ export class Item {
     unitPaymentOrderChanged(newV, oldV) {
         if (newV) {
             let items = [], totalPaid = 0;
-            for (let item of newV.items) {
-                for (let detail of item.unitReceiptNote.items) {
-                    let corrections = detail.correction;
-                    let price, quantity;
 
-                    if (corrections && corrections.length !== 0) {
-                        if (corrections[corrections.length - 1].correctionRemark === 'Koreksi Jumlah') {
-                            let pricePerUnit = corrections[corrections.length - 1].correctionPricePerUnit;
-                            let correctionQuantity = detail.deliveredQuantity;
+            let unitReceiptsNo = newV.items.map(p => p.unitReceiptNote.no);
 
-                            for (let correction of corrections.filter(p => p.correctionRemark === 'Koreksi Jumlah')) {
-                                correctionQuantity -= correction.correctionQuantity;
+            let filter = {
+                page: 1,
+                size: Number.MAX_SAFE_INTEGER,
+                filter: JSON.stringify({ no: { $in: unitReceiptsNo } }),
+                select: ['no', 'unit._id', 'unit.code', 'unit.name']
+            };
+
+            this.service.getURN(filter)
+                .then((response) => {
+                    let urn = response.data;
+
+                    for (let item of newV.items) {
+                        let urnObj = urn.find(p => p.no === item.unitReceiptNote.no);
+                        
+                        for (let detail of item.unitReceiptNote.items) {
+                            let corrections = detail.correction;
+                            let price, quantity;
+
+                            if (corrections && corrections.length !== 0) {
+                                if (corrections[corrections.length - 1].correctionRemark === 'Koreksi Jumlah') {
+                                    let pricePerUnit = corrections[corrections.length - 1].correctionPricePerUnit;
+                                    let correctionQuantity = detail.deliveredQuantity;
+
+                                    for (let correction of corrections.filter(p => p.correctionRemark === 'Koreksi Jumlah')) {
+                                        correctionQuantity -= correction.correctionQuantity;
+                                    }
+
+                                    price = pricePerUnit * correctionQuantity;
+                                    quantity = correctionQuantity;
+                                }
+                                else {
+                                    price = corrections[corrections.length - 1].correctionPriceTotal;
+                                    quantity = corrections[corrections.length - 1].correctionQuantity;
+                                }
+                            }
+                            else {
+                                price = Number((detail.pricePerDealUnit * detail.deliveredQuantity).toFixed(2));
+                                quantity = detail.deliveredQuantity;
                             }
 
-                            price = pricePerUnit * correctionQuantity;
-                            quantity = correctionQuantity;
+                            items.push({
+                                productId: detail.product._id,
+                                productCode: detail.product.code,
+                                productName: detail.product.name,
+                                quantity: quantity,
+                                uom: detail.deliveredUom.unit,
+                                price: price,
+                                unitId: urnObj.unit._id,
+                                unitCode: urnObj.unit.code,
+                                unitName: urnObj.unit.name
+                            });
+
+                            totalPaid += price;
                         }
-                        else {
-                            price = corrections[corrections.length - 1].correctionPriceTotal;
-                            quantity = corrections[corrections.length - 1].correctionQuantity;
-                        }
-                    }
-                    else {
-                        price = numeral(detail.pricePerDealUnit * detail.deliveredQuantity).format('0,000.00');
-                        quantity = detail.deliveredQuantity;
                     }
 
-                    items.push({
-                        productId: detail.product._id,
-                        productCode: detail.product.code,
-                        productName: detail.product.name,
-                        quantity: quantity,
-                        uom: detail.deliveredUom.unit,
-                        price: price,
-                        unitId: item.unitReceiptNote.unit._id,
-                        unitCode: item.unitReceiptNote.unit.code,
-                        unitName: item.unitReceiptNote.unit.name
+                    let vat = newV.useIncomeTax ? Number((totalPaid * 0.1).toFixed(2)) : 0;
+                    let incomeTax = newV.useVat ? Number(((newV.vat.rate * totalPaid) / 100).toFixed(2)) : 0;
+             
+                    Object.assign(this.data, {
+                        id: newV._id,
+                        no: newV.no,
+                        date: newV.date,
+                        dueDate: newV.dueDate,
+                        invoceNo: newV.invoceNo,
+                        supplierCode: newV.supplier.code,
+                        supplierName: newV.supplier.name,
+                        divisionCode: newV.division.code,
+                        divisionName: newV.division.name,
+                        incomeTax: incomeTax,
+                        vat: vat,
+                        incomeTaxId: newV.vat._id,
+                        incomeTaxName: newV.vat.name,
+                        incomeTaxRate: newV.vat.rate,
+                        totalPaid: Number((totalPaid + vat).toFixed(2)),
+                        currency: newV.currency.code,
+                        items: items,
                     });
-
-                    totalPaid += price;
-                }
-            }
-
-            let vat = newV.useIncomeTax ? (totalPaid * 0.1) : 0;
-
-            Object.assign(this.data, {
-                id: newV._id,
-                no: newV.no,
-                date: newV.date,
-                dueDate: newV.dueDate,
-                invoceNo: newV.invoceNo,
-                supplierCode: newV.supplier.code,
-                supplierName: newV.supplier.name,
-                divisionCode: newV.division.code,
-                divisionName: newV.division.name,
-                incomeTax: newV.useVat ? ((newV.vat.rate * totalPaid) / 100) : 0,
-                vat: vat, /* Dari SPB terbalik (salah) */
-                incomeTaxId: newV.vat._id,
-                incomeTaxName: newV.vat.name,
-                incomeTaxRate: newV.vat.rate,
-                totalPaid: totalPaid + vat,
-                currency: newV.currency.code,
-                items: items,
-            });
+                });
         }
         else {
             Object.assign(this.data, {
