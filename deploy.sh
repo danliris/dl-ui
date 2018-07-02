76c7a51360b170fed7d1bfe22eb03d9caa87c238
jackyruslymoonlay <jacky.rusly@moonlay.com>
2018-07-02 14:07:50 +0700

    Test Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,16 @@
 echo "Build"
 npm install
 npm run build:prod
+
+git config --global user.email $GH_EMAIL
+git config --global user.name $GH_NAME
+
+git clone https://${GH_TOKEN}@github.com/jackyruslymoonlay/jackyruslymoonlay.github.io.git out
+
+cp -a dist/. out/.
+cd out
+
+git add .
+git commit -m "Automated Deployment"
+
+git push origin master
