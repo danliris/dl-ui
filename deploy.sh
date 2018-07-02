52f8f946a3f3069c976f9505973623b6fa218f10
jackyruslymoonlay <jacky.rusly@moonlay.com>
2018-07-02 14:47:15 +0700

    Travis

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ npm run build:prod
 git config --global user.email $GH_EMAIL
 git config --global user.name $GH_NAME
 
-git clone https://${GH_TOKEN}@https://github.com/danliris/danliris.github.io.git out
+git clone https://${GH_TOKEN}@github.com/danliris/danliris.github.io.git out
 
 cp -a dist/. out/.
 cd out
