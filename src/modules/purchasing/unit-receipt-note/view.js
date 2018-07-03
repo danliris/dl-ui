f914362478a721d161927cd7f773717bfaafcad4
achensetyawati <achenzetya@gmail.com>
2018-07-03 08:25:19 +0700

    Unit receipt note migration

--- src/modules/purchasing/unit-receipt-note/view.js
+++ src/modules/purchasing/unit-receipt-note/view.js
@@ -19,10 +19,20 @@ export class View {
                 item.showDetails = false
             })
         }
-
         this.unit = this.data.unit;
         this.supplier = this.data.supplier;
-        this.deliveryOrder = this.data.deliveryOrder;
+        this.deliveryOrder = this.data.items;
+        if(this.data.doNo){
+            this.deliveryOrder.no=this.data.doNo;
+            
+        }
+        for(var _item of this.deliveryOrder){
+            _item.deliveredUom=_item.product.uom;
+        }
+        if(this.data.unit && this.data.supplier){
+            this.data.unitId=this.data.unit._id;
+            this.data.supplierId=this.data.supplier._id;
+        }
     }
 
     list() {
