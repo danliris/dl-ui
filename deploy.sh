5458838f07e249c55de2e9516594f47384ada098
jackyruslymoonlay <jacky.rusly@moonlay.com>
2018-07-16 10:47:09 +0700

    Test Deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,13 @@ npm run build:prod
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
 
-git clone $GIT_URL out
+if [ "$BRANCH" == "dev" ]; then
+    git clone $GIT_URL_DEV out;
+else if [ "$BRANCH" == "uat" ]; then
+    git clone $GIT_URL_UAT out;
+else if [ "$BRANCH" == "master" ]; then
+    git clone $GIT_URL_MASTER out;
+fi
 
 cp -a dist/. out/.
 cd out
