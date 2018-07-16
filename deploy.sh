1347f0d2cc07f2eaf86d3d7975d5da25cd531d3d
jackyruslymoonlay <jacky.rusly@moonlay.com>
2018-07-16 11:08:59 +0700

    ELIF

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 echo "Build"
-echo $BRANCH
 npm install
 npm run build:prod
 
@@ -7,11 +6,11 @@ git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
 
 if [ "$BRANCH" == "dev" ]; then
-    git clone $GIT_URL_DEV out;
-else if [ "$BRANCH" == "uat" ]; then
-    git clone $GIT_URL_UAT out;
-else if [ "$BRANCH" == "master" ]; then
-    git clone $GIT_URL_MASTER out;
+    git clone $GIT_URL_DEV out
+elif [ "$BRANCH" == "uat" ]; then
+    git clone $GIT_URL_UAT out
+elif [ "$BRANCH" == "master" ]; then
+    git clone $GIT_URL_MASTER out
 fi
 
 cp -a dist/. out/.
