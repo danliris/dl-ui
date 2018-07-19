c56c9b7f5d478ff85837a0ddc3650469d9fbfa3d
danliris <danliris.cloud@gmail.com>
2018-07-19 18:25:15 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,15 @@ npm run build:prod
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
 
-git clone $GIT_URL out
+if [ "$BRANCH" == "dev" ]; then
+    git clone $GIT_URL_DEV out
+elif [ "$BRANCH" == "uat" ]; then
+    git clone $GIT_URL_UAT out
+elif [ "$BRANCH" == "master" ]; then
+    git clone $GIT_URL_MASTER out
+else
+    git clone $GIT_URL out
+fi
 
 cp -a dist/. out/.
 cd out
