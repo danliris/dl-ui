ac46f545705bcc5a7d028411c101823f9d81dc8e
achensetyawati <achenzetya@gmail.com>
2018-07-27 08:28:05 +0700

    Price Correction

---
+++ src/modules/purchasing/unit-payment-price-correction-note/template/item.js
@@ -0,0 +1,24 @@
+export class UnitReceiptNoteItem {
+  
+  activate(context) {
+    this.context = context;
+    this.data = context.data;
+    this.error = context.error;
+    this.readOnly = context.options.readOnly;
+    this.pricePerUnitCorrectionReadOnly = this.context.context.options;
+  }
+  
+  get product() {
+		return `${this.data.product.code} - ${this.data.product.name}`;
+	}
+
+  // get totalPrice(){
+  //   return this.data.pricePerDealUnit * this.data.deliveredQuantity
+  // }
+
+  controlOptions = {
+    control: {
+      length: 12
+    }
+  };
+}
