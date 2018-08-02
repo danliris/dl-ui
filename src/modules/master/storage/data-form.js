6c78657afdc5de7471e4f38eec67708421d290c0
achensetyawati <achenzetya@gmail.com>
2018-08-02 15:37:16 +0700

    fix storage dataform (unit)

--- src/modules/master/storage/data-form.js
+++ src/modules/master/storage/data-form.js
@@ -4,6 +4,7 @@ var UnitLoader = require('../../../loader/unit-loader');
 export class DataForm {
   @bindable title;
   @bindable readOnly;
+  @bindable selectedUnit;
   formOptions = {
     cancelText: "Kembali",
     saveText: "Simpan",
@@ -24,13 +25,25 @@ export class DataForm {
     this.deleteCallback = this.context.deleteCallback;
     this.editCallback = this.context.editCallback;
     this.saveCallback = this.context.saveCallback;
+    if(this.data.unit){
+      this.selectedUnit=this.data.unit;
+    }
   }
 
-  unitChanged(e) {
-        var selectedUnit = e.detail;
-        if (selectedUnit)
-            this.data.unitId = selectedUnit._id;
-            
+  selectedUnitChanged(newdata) {
+        var _selectedUnit = newdata;
+        if (_selectedUnit){
+          console.log(_selectedUnit)
+            this.data.unitId = _selectedUnit._id || _selectedUnit.Id || "" ;
+            this.data.unit=_selectedUnit;
+            this.data.unit._id=_selectedUnit._id || _selectedUnit.Id || "";
+            this.data.unit.name=_selectedUnit.name || _selectedUnit.Name || "";
+            this.data.unit.code=_selectedUnit.code || _selectedUnit.Code || "";
+            this.data.unit.division=_selectedUnit.division || _selectedUnit.Division || {};
+            this.data.unit.division._id=_selectedUnit.division._id || _selectedUnit.Division.Id || "";
+            this.data.unit.division.name=_selectedUnit.division.name || _selectedUnit.Division.Name || "";
+            this.data.unit.division.code=_selectedUnit.division.code || _selectedUnit.Division.Code || "";
+        }
     }
 
   get unitLoader() {
@@ -38,6 +51,6 @@ export class DataForm {
     }
   
   unitView = (unit) => {
-        return `${unit.division.name} - ${unit.name}`;
+        return unit.division ?`${unit.division.name} - ${unit.name}` : `${unit.Division.Name} - ${unit.Name}`
     }
 } 
