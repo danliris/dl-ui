d9d827fd7789829f38c7ef3f02612df7651299d3
achensetyawati <achenzetya@gmail.com>
2018-08-13 08:35:37 +0700

    price-correction monitoring

--- src/modules/purchasing/koreksi-harga/service.js
+++ src/modules/purchasing/koreksi-harga/service.js
@@ -2,78 +2,21 @@ import { inject, Lazy } from 'aurelia-framework';
 import { HttpClient } from 'aurelia-fetch-client';
 import { RestService } from '../../../utils/rest-service';
 
-const serviceUri = 'unit-payment-orders/monitoring/koreksi-harga';
+const serviceUri = 'unit-payment-correction-notes/price-correction/monitoring';
 
 export class Service extends RestService {
 
     constructor(http, aggregator, config, endpoint) {
-        super(http, aggregator, config, "purchasing");
+        super(http, aggregator, config, "purchasing-azure");
     }
 
-    getReport(sdate, edate, machine, kanban) {
-        var endpoint = `${serviceUri}`;
-        var query = '';
-        if (sdate) {
-            if (query === '') query = `dateFrom=${sdate}`;
-            else query = `${query}&dateFrom=${sdate}`;
-        }
-        if (edate) {
-            if (query === '') query = `dateTo=${edate}`;
-            else query = `${query}&dateTo=${edate}`;
-        }
-        // if (machine) {
-        //     if (query === '') query = `machine=${machine._id}`;
-        //     else query = `${query}&machine=${machine._id}`;
-        // }
-        // if (kanban) {
-        //     if (query === '') query = `kanban=${kanban._id}`;
-        //     else query = `${query}&kanban=${kanban._id}`;
-        // }
-        if (query !== '')
-            endpoint = `${serviceUri}?${query}`;
-
-        return super.get(endpoint);
+    search(info) {
+        let endpoint = `${serviceUri}`;
+        return super.list(endpoint, info);
     }
 
-    // generateExcel(sdate, edate, machine, kanban) {
-    //     var endpoint = `${serviceUri}`;
-    //     var query = '';
-    //     if (sdate) {
-    //         if (query === '') query = `dateFrom=${sdate}`;
-    //         else query = `${query}&dateFrom=${sdate}`;
-    //     }
-    //     if (edate) {
-    //         if (query === '') query = `dateTo=${edate}`;
-    //         else query = `${query}&dateTo=${edate}`;
-    //     }
-    //     if (machine) {
-    //         if (query === '') query = `machine=${machine._id}`;
-    //         else query = `${query}&machine=${machine._id}`;
-    //     }
-    //     if (kanban) {
-    //         if (query === '') query = `kanban=${kanban._id}`;
-    //         else query = `${query}&kanban=${kanban._id}`;
-    //     }
-    //     if (query !== '')
-    //         endpoint = `${serviceUri}?${query}`;
-
-    //     return super.getXls(endpoint);
-    // }
-
-    generateExcel(sdate, edate) {
-        var endpoint = `${serviceUri}`;
-        var query = '';
-        if (sdate) {
-            if (query === '') query = `dateFrom=${sdate}`;
-            else query = `${query}&dateFrom=${sdate}`;
-        }
-        if (edate) {
-            if (query === '') query = `dateTo=${edate}`;
-            else query = `${query}&dateTo=${edate}`;
-        }       
-        if (query !== '')
-            endpoint = `${serviceUri}?${query}`;
-
+    getXls(info) {
+        var endpoint = `${serviceUri}/download?dateFrom=${info.dateFrom}&dateTo=${info.dateTo}`;
         return super.getXls(endpoint);
     }
 }
