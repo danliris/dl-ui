08b436fa2a435d28b808ff95d0b6216158e29a14
achensetyawati <achenzetya@gmail.com>
2018-08-16 08:30:57 +0700

    Monitoring Purchase All

--- src/modules/purchasing/monitoring-purchase-order-all-user/service.js
+++ src/modules/purchasing/monitoring-purchase-order-all-user/service.js
@@ -8,21 +8,21 @@ const serviceUri = 'purchase-orders/monitoring';
 export class Service extends RestService {
 
     constructor(http, aggregator, config, endpoint) {
-        super(http, aggregator, config, "purchasing");
+        super(http, aggregator, config, "purchasing-azure");
     }
 
-    search(unitId, categoryId, PODLNo, PRNo, supplierId, dateFrom, dateTo, state,budgetId,staffName) {
-        var endpoint = `${serviceUri}?unitId=${unitId}&categoryId=${categoryId}&PODLNo=${PODLNo}&PRNo=${PRNo}&supplierId=${supplierId}&dateFrom=${dateFrom}&dateTo=${dateTo}&state=${state}&budgetId=${budgetId}&staffName=${staffName}`;
-        return super.get(endpoint);
+    search(info) {
+        let endpoint = `${serviceUri}`;
+        return super.list(endpoint, info);
     }
 
-    getById(id) {
-        var endpoint = `${serviceUri}/${id}`;
-        return super.get(endpoint);
-    }
+    // getById(id) {
+    //     var endpoint = `${serviceUri}/${id}`;
+    //     return super.get(endpoint);
+    // }
 
-    generateExcel(unitId, categoryId, PODLNo, PRNo, supplierId, dateFrom, dateTo, state,budgetId,staffName) {
-        var endpoint = `${serviceUri}?unitId=${unitId}&categoryId=${categoryId}&PODLNo=${PODLNo}&PRNo=${PRNo}&supplierId=${supplierId}&dateFrom=${dateFrom}&dateTo=${dateTo}&state=${state}&budgetId=${budgetId}&staffName=${staffName}`;
+    generateExcel(args) {
+        var endpoint = `${serviceUri}/download?prNo=${args.prNo}&supplierId=${args.supplierId}&unitId=${args.unitId}&categoryId=${args.categoryId}&epoNo=${args.epoNo}&dateFrom=${args.dateFrom}&dateTo=${args.dateTo}&status=${args.status}&budgetId=${args.budgetId}&staff=${args.staff}`;
         return super.getXls(endpoint);
     }
 
