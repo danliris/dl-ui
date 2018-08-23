c8b575578c74202fbc68bb48cfb067cf5e75999e
danliris <danliris.cloud@gmail.com>
2018-08-23 11:23:18 +0700

    Update master.js

--- src/routes/master.js
+++ src/routes/master.js
@@ -60,7 +60,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "master",
-            permission: { "C5": 1, "C9": 1 },
+            //permission: { "C5": 1, "C9": 1 },
             iconClass: 'fa fa-dashboard'
         }
     },
