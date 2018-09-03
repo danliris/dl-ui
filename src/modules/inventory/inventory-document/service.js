cefffd7fc7a03787d8715b9adb12eae5fe2171f0
achensetyawati <achenzetya@gmail.com>
2018-09-03 10:35:03 +0700

    fix service inv-doc

--- src/modules/inventory/inventory-document/service.js
+++ src/modules/inventory/inventory-document/service.js
@@ -13,7 +13,6 @@ export class Service extends RestService {
 
     search(info) {
         var endpoint = `${serviceUri}`;
-        console.log(endpoint)
         return super.list(endpoint, info);
     }
 
