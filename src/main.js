cd35db430c2822fe31e00311b872e0a8519e8479
Ali Reza <ali.syariati@moonlay.com>
2018-09-12 20:29:28 +0700

    Penyerahan Produksi Report Get

--- src/main.js
+++ src/main.js
@@ -33,8 +33,8 @@ export async function configure(aurelia) {
       var core = "https://com-danliris-service-core-dev.azurewebsites.net/v1/";
       var auth = "https://dl-auth-api-dev.azurewebsites.net/v1/";
       var production = "https://dl-production-webapi-dev.azurewebsites.net/v1/";
-      var productionAzure = "https://com-danliris-service-finishing-printing-dev.azurewebsites.net/v1/";
-      // var productionAzure = "http://localhost:2677/v1/";
+      // var productionAzure = "https://com-danliris-service-finishing-printing-dev.azurewebsites.net/v1/";
+      var productionAzure = "http://localhost:2677/v1/";
       var purchasing = "https://dl-purchasing-webapi-dev.azurewebsites.net/v1/";
       var purchasingAzure = "https://com-danliris-service-purchasing-dev.azurewebsites.net/v1/";
       var garmentPurchasing = "https://dl-purchasing-garment-webapi-dev.azurewebsites.net/v1/";
