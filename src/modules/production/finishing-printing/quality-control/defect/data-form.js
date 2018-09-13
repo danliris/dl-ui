994617d0dd4955e60ab6173fc34332ceeb7488c1
leslie.aula <leslie.aula@moonlay.com>
2018-09-13 16:15:06 +0700

    Update Defect Order

--- src/modules/production/finishing-printing/quality-control/defect/data-form.js
+++ src/modules/production/finishing-printing/quality-control/defect/data-form.js
@@ -477,7 +477,7 @@ class FabricGradeTest {
 
 
 class FabricTestCriterion {
-    constructor(group, code, name, score) {
+    constructor(group, code, name, score, index) {
         this.Code = code || "";
         this.Group = group || "";
         this.Name = name || "";
@@ -487,48 +487,49 @@ class FabricTestCriterion {
             C: 0,
             D: 0
         };
+        this.index = index || 0;
     }
 }
 var generalCriteria = () => [
-    new FabricTestCriterion("BENANG", "B001", "Slubs"),
-    new FabricTestCriterion("BENANG", "B002", "Neps"),
-    new FabricTestCriterion("BENANG", "B003", "Kontaminasi Fiber"),
-    new FabricTestCriterion("WEAVING", "W001", "Pakan Renggang"),
-    new FabricTestCriterion("WEAVING", "W002", "Pakan Rapat"),
-    new FabricTestCriterion("WEAVING", "W003", "Pakan Double"),
-    new FabricTestCriterion("WEAVING", "W004", "Pakan Tebal Tipis"),
-    new FabricTestCriterion("WEAVING", "W005", "Lusi Tebal Tipis"),
-    new FabricTestCriterion("WEAVING", "W006", "Lusi Putus"),
-    new FabricTestCriterion("WEAVING", "W007", "Lusi Double"),
-    new FabricTestCriterion("WEAVING", "W008", "Madal Sumbi"),
-    new FabricTestCriterion("WEAVING", "W009", "Salah Anyam / UP"),
-    new FabricTestCriterion("WEAVING", "W010", "Reed Mark"),
-    new FabricTestCriterion("WEAVING", "W011", "Temple Mark"),
-    new FabricTestCriterion("WEAVING", "W012", "Snarl"),
-    new FabricTestCriterion("PRODUKSI", "P001", "Sobek Tepi"),
-    new FabricTestCriterion("PRODUKSI", "P002", "Kusut Mati"),
-    new FabricTestCriterion("PRODUKSI", "P003", "Kusut / Krismak"),
-    new FabricTestCriterion("PRODUKSI", "P004", "Belang Kondensat"),
-    new FabricTestCriterion("PRODUKSI", "P005", "Belang Absorbsi"),
-    new FabricTestCriterion("PRODUKSI", "P006", "Flek Minyak / Dyest"),
-    new FabricTestCriterion("PRODUKSI", "P007", "Flek Oil Jarum"),
-    new FabricTestCriterion("PRODUKSI", "P008", "Bintik Htm, Mrh, Biru"),
-    new FabricTestCriterion("PRODUKSI", "P009", "Tepi Melipat"),
-    new FabricTestCriterion("PRODUKSI", "P010", "Lebar Tak Sama"),
-    new FabricTestCriterion("PRODUKSI", "P011", "Lubang / Pin Hole"),
-    new FabricTestCriterion("PRODUKSI", "P012", "Bowing"),
-    new FabricTestCriterion("PRODUKSI", "P013", "Skewing"),
+    new FabricTestCriterion("BENANG", "B001", "Slubs", null, 1),
+    new FabricTestCriterion("BENANG", "B002", "Neps", null, 2),
+    new FabricTestCriterion("BENANG", "B003", "Kontaminasi Fiber", null, 3),
+    new FabricTestCriterion("WEAVING", "W001", "Pakan Renggang", null, 4),
+    new FabricTestCriterion("WEAVING", "W002", "Pakan Rapat", null, 5),
+    new FabricTestCriterion("WEAVING", "W003", "Pakan Double", null, 6),
+    new FabricTestCriterion("WEAVING", "W004", "Pakan Tebal Tipis", null, 7),
+    new FabricTestCriterion("WEAVING", "W005", "Lusi Tebal Tipis", null, 8),
+    new FabricTestCriterion("WEAVING", "W006", "Lusi Putus", null, 9),
+    new FabricTestCriterion("WEAVING", "W007", "Lusi Double", null, 10),
+    new FabricTestCriterion("WEAVING", "W008", "Madal Sumbi", null, 11),
+    new FabricTestCriterion("WEAVING", "W009", "Salah Anyam / UP", null, 12),
+    new FabricTestCriterion("WEAVING", "W010", "Reed Mark", null, 13),
+    new FabricTestCriterion("WEAVING", "W011", "Temple Mark", null, 14),
+    new FabricTestCriterion("WEAVING", "W012", "Snarl", null, 15),
+    new FabricTestCriterion("PRODUKSI", "P001", "Sobek Tepi", null, 16),
+    new FabricTestCriterion("PRODUKSI", "P002", "Kusut Mati", null, 17),
+    new FabricTestCriterion("PRODUKSI", "P003", "Kusut / Krismak", null, 18),
+    new FabricTestCriterion("PRODUKSI", "P004", "Belang Kondensat", null, 19),
+    new FabricTestCriterion("PRODUKSI", "P005", "Belang Absorbsi", null, 20),
+    new FabricTestCriterion("PRODUKSI", "P006", "Flek Minyak / Dyest", null, 21),
+    new FabricTestCriterion("PRODUKSI", "P007", "Flek Oil Jarum", null, 22),
+    new FabricTestCriterion("PRODUKSI", "P008", "Bintik Htm, Mrh, Biru", null, 23),
+    new FabricTestCriterion("PRODUKSI", "P009", "Tepi Melipat", null, 24),
+    new FabricTestCriterion("PRODUKSI", "P010", "Lebar Tak Sama", null, 25),
+    new FabricTestCriterion("PRODUKSI", "P011", "Lubang / Pin Hole", null, 26),
+    new FabricTestCriterion("PRODUKSI", "P012", "Bowing", null, 27),
+    new FabricTestCriterion("PRODUKSI", "P013", "Skewing", null, 28),
 ];
 
 var printingCriteria = () => [
-    new FabricTestCriterion("PRODUKSI", "P201", "Meleset"),
-    new FabricTestCriterion("PRODUKSI", "P202", "Flek"),
-    new FabricTestCriterion("PRODUKSI", "P203", "Print Kosong / Bundas"),
-    new FabricTestCriterion("PRODUKSI", "P204", "Nyetrip")
+    new FabricTestCriterion("PRODUKSI", "P201", "Meleset", null, 29),
+    new FabricTestCriterion("PRODUKSI", "P202", "Flek", null, 30),
+    new FabricTestCriterion("PRODUKSI", "P203", "Print Kosong / Bundas", null, 31),
+    new FabricTestCriterion("PRODUKSI", "P204", "Nyetrip", null, 32)
 ];
 
 var finishingCriteria = () => [
-    new FabricTestCriterion("PRODUKSI", "P101", "Kotor Tanah / Debu"),
-    new FabricTestCriterion("PRODUKSI", "P102", "Kotor Hitam"),
-    new FabricTestCriterion("PRODUKSI", "P103", "Belang Kusut")
+    new FabricTestCriterion("PRODUKSI", "P101", "Kotor Tanah / Debu", null, 33),
+    new FabricTestCriterion("PRODUKSI", "P102", "Kotor Hitam", null, 34),
+    new FabricTestCriterion("PRODUKSI", "P103", "Belang Kusut", null, 35)
 ];
