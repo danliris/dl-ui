626664041b2a6a48ffac74dcc88158dcd479e253
kharissuryandaru <35208779+kharissuryandaru@users.noreply.github.com>
2018-09-14 13:10:59 +0700

    merge

--- src/routes/production.js
+++ src/routes/production.js
@@ -74,7 +74,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "production",
-            // permission: { "C9": 1, "F1": 1, "F2": 1 },
+            permission: { "C9": 1, "F1": 1, "F2": 1 },
             iconClass: 'fa fa-dashboard'
         }
     },
@@ -192,7 +192,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "production",
-            //permission: { "C9": 1, "F1": 1, "F2": 1 },
+            permission: { "C9": 1, "F1": 1, "F2": 1 },
             iconClass: 'fa fa-dashboard'
         }
     },
@@ -244,7 +244,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "production",
-            //permission: { "C9": 1, "F1": 1, "F2": 1 },
+            permission: { "C9": 1, "F1": 1, "F2": 1 },
             iconClass: 'fa fa-dashboard'
         }
     },
