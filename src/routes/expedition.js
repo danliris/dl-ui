16873bc7de3ee1c66379b69cc58860f50e2eb47c
danliris <danliris.cloud@gmail.com>
2018-09-20 10:44:23 +0700

    Update expedition.js

--- src/routes/expedition.js
+++ src/routes/expedition.js
@@ -34,7 +34,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "finance",
-            permission: {"P1": 1, "P2": 1, "P3": 1, "P4": 1, "P5": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "C5": 1, "C9": 1 },
+            permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "B9": 1, "B4": 1, "C5":1, "C9": 1 },
             iconClass: 'fa fa-clone'
         }
     },
@@ -47,7 +47,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "finance",
-            permission: { "C5": 1, "C9": 1 },
+            permission: { "C5": 1, "B9":1, "C9": 1 },
             iconClass: 'fa fa-calendar-check-o'
         }
     },
@@ -60,7 +60,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "finance",
-            permission: { "C5": 1, "C9": 1 },
+            permission: { "B4": 1,"C5": 1, "C9": 1 },
             iconClass: 'fa fa-calendar-check-o'
         }
     },
@@ -73,7 +73,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "finance",
-            permission: { "C5": 1, "C9": 1 },
+            permission: { "B4": 1,"C5": 1, "C9": 1 },
             iconClass: 'fa fa-calendar-check-o'
         }
     },
@@ -86,7 +86,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "finance",
-            permission: { "C5": 1, "C9": 1 },
+            permission: { "B4": 1,"C5": 1, "C9": 1 },
             iconClass: 'fa fa-calendar-check-o'
         }
     },
@@ -99,7 +99,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "finance",
-            permission: { "C5": 1, "C9": 1 },
+            permission: { "B4": 1,"C5": 1, "C9": 1 },
             iconClass: 'fa fa-clone'
         }
     },
@@ -112,7 +112,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "finance",
-            permission: { "C5": 1, "C9": 1 },
+            permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "B9": 1, "B4": 1, "C5":1, "C9": 1 },
             iconClass: 'fa fa-calendar-check-o'
         }
     },
@@ -125,7 +125,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "purchasing",
-            permission: { "C5": 1, "C9": 1 },
+            permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "B9": 1, "B4": 1, "C5":1, "C9": 1 },
             iconClass: 'fa fa-dashboard'
         }
     },
@@ -135,19 +135,6 @@ module.exports = [
         moduleId: './modules/expedition/reports/unit-payment-order-not-verified-report/index',
         nav: true,
         title: 'Laporan SPB Not Verified',
-        auth: true,
-        settings: {
-            group: "finance",
-            permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "B9": 1, "C9": 1},
-            iconClass: 'fa fa-dashboard'
-        }
-    },
-    {
-        route: 'daily-bank-transaction-document',
-        name: 'daily-bank-transaction-document',
-        moduleId: './modules/expedition/daily-bank-transaction-document/index',
-        nav: true,
-        title: 'Transaksi Harian Bank',
         auth: true,
         settings: {
             group: "finance",
