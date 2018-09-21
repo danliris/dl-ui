067403240f68c37c9ea80563c923b40a3e38b4be
leslie.aula <leslie.aula@moonlay.com>
2018-09-21 16:24:16 +0700

    Modify Permissions

--- src/modules/expedition/shared/permission-constants.js
+++ src/modules/expedition/shared/permission-constants.js
@@ -11,7 +11,7 @@ export const CASHIER = {
     label: 'Kasir',
     position: 7, //CASHIER_DIVISION
     positionAutocomplete: 4, //SEND_TO_CASHIER_DIVISION
-    code: 'P3',
+    code: 'B4',
 };
 
 export const ACCOUNTING = {
