b15113bfa263ccbf30a8b8fa998ae77ed0559441
Lucia Widyana <mo_cia@yahoo.com>
2018-10-01 11:49:20 +0700

    add master Efficiency

--- src/modules/master/efficiency/service.js
+++ src/modules/master/efficiency/service.js
@@ -7,7 +7,7 @@ const serviceUri = "efficiencies";
 export class Service extends RestService {
 
   constructor(http, aggregator, config, api) {
-    super(http, aggregator, config, "merchandiser");
+    super(http, aggregator, config, "sales");
   }
   
   search(info) {
