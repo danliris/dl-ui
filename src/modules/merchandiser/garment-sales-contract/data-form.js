9bb82774fadba50ee56b75fe9c84a130fab4a81f
achensetyawati <achenzetya@gmail.com>
2018-10-09 15:16:09 +0700

    fix garment SC

--- src/modules/merchandiser/garment-sales-contract/data-form.js
+++ src/modules/merchandiser/garment-sales-contract/data-form.js
@@ -59,7 +59,10 @@ export class DataForm {
         }
         this.hasItems=true;
       }
-
+      this.data.Amount=this.data.Amount.toLocaleString('en-EN', { minimumFractionDigits: 2})
+      if(this.data.Price){
+        this.data.Price=this.data.Price.toLocaleString('en-EN', { minimumFractionDigits: 4})
+      }
     if(!this.data.DocPresented || this.data.DocPresented==""){
       this.data.DocPresented="INVOICE OF COMMERCIAL VALUE \nPACKING LIST \nEXPORT LICENSE \nCERTIFICATE OF ORIGIN / G.S.P FORM A \nINSPECTION CERTIFICATE ";
     }
@@ -120,10 +123,10 @@ export class DataForm {
         this.data.Uom=newValue.UOM;
         this.data.UomId=newValue.UOM.Id;
         this.data.UomUnit=newValue.UOM.Unit;
-        this.data.Price=newValue.ConfirmPrice;
+        this.data.Price=newValue.ConfirmPrice.toLocaleString('en-EN', { minimumFractionDigits: 4});
         this.data.DeliveryDate=newValue.DeliveryDate;
         if(this.data.Items.length==0){
-          this.data.Amount=parseFloat(this.data.Quantity*this.data.Price).toFixed(2);
+          this.data.Amount=parseFloat(this.data.Quantity*parseFloat(this.data.Price)).toLocaleString('en-EN', { minimumFractionDigits: 2});
         }
       }
       
@@ -203,9 +206,9 @@ export class DataForm {
   }
 
   PriceChanged(e){
-    this.data.Price=parseFloat(e.srcElement.value);
+    this.data.Price=parseFloat(e.srcElement.value).toLocaleString('en-EN', { minimumFractionDigits: 4});
 
-    this.data.Amount=parseFloat(this.data.Quantity*this.data.Price).toFixed(2);
+    this.data.Amount=parseFloat(this.data.Quantity*parseFloat(this.data.Price)).toLocaleString('en-EN', { minimumFractionDigits: 2});
     
   }
 
@@ -219,12 +222,12 @@ export class DataForm {
           this.data.Amount+=item.Price*item.Quantity;
         }
       }
-      this.data.Amount=parseFloat(this.data.Amount).toFixed(2);
+      this.data.Amount=parseFloat(this.data.Amount).toLocaleString('en-EN', { minimumFractionDigits: 2});
       if(this.data.Items.length==0){
         this.hasItems=false;
         var price= await this.service.getCostCalById(this.data.CostCalculationId);
-        this.data.Price=price.ConfirmPrice;
-        this.data.Amount=parseFloat(this.data.Quantity*this.data.Price).toFixed(2);
+        this.data.Price=price.ConfirmPrice.toLocaleString('en-EN', { minimumFractionDigits: 4});
+        this.data.Amount=parseFloat(this.data.Quantity*parseFloat(this.data.Price)).toLocaleString('en-EN', { minimumFractionDigits: 2});
       }
     }
 
@@ -240,14 +243,14 @@ export class DataForm {
               this.data.Amount+=item.Price*item.Quantity;
           }
         }
-        this.data.Amount=parseFloat(this.data.Amount).toFixed(2);
+        this.data.Amount=parseFloat(this.data.Amount).toLocaleString('en-EN', { minimumFractionDigits: 2});
       }
       if(this.data.Items.length==0){
         this.hasItems=false;
         var price= await this.service.getCostCalById(this.data.CostCalculationId);
         this.data.Price=await price.ConfirmPrice;
-        this.data.Amount=parseFloat(this.data.Quantity*this.data.Price).toFixed(2);
-        this.data.Price=price.ConfirmPrice;
+        this.data.Amount=parseFloat(this.data.Quantity*parseFloat(this.data.Price)).toLocaleString('en-EN', { minimumFractionDigits: 2});
+        this.data.Price=price.ConfirmPrice.toLocaleString('en-EN', { minimumFractionDigits: 4});
       }
     }
   }
