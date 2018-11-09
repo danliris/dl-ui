3d04611fe463dc21d187c19b8cffaa824111a8d4
Ali Reza <ali.syariati@moonlay.com>
2018-11-09 19:13:25 +0700

    add credit balance

--- src/routes/expedition.js
+++ src/routes/expedition.js
@@ -193,5 +193,18 @@ module.exports = [
             permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "B9": 1, "C9": 1 },
             iconClass: 'fa fa-dashboard'
         }
+    },
+    {
+        route: '/expedition/reports/credit-balance',
+        name: 'credit-balance-report',
+        moduleId: './modules/expedition/reports/credit-balance-report/index',
+        nav: true,
+        title: 'Saldo Hutang',
+        auth: true,
+        settings: {
+            group: "finance",
+            permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "B9": 1, "C9": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
     }
 ];
