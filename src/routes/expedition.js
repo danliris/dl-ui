82dec124daf5604af8a4956743ea063062851863
kharissuryandaru <35208779+kharissuryandaru@users.noreply.github.com>
2018-11-23 16:50:42 +0700

    Fixed PurchaseDispositionExpedition

--- src/routes/expedition.js
+++ src/routes/expedition.js
@@ -206,5 +206,18 @@ module.exports = [
             permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "B9": 1, "C9": 1 },
             iconClass: 'fa fa-dashboard'
         }
+    },
+    {
+        route: '/expedition/purchasing-disposition-expedition',
+        name: 'purchasing-disposition-expedition',
+        moduleId: './modules/expedition/purchasing-disposition-expedition/index',
+        nav: true,
+        title: 'Penyerahan Dokumen Disposisi Pembelian ke Verifikasi',
+        auth: true,
+        settings: {
+            group: "finance",
+            permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "B9": 1, "C9": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
     }
 ];
