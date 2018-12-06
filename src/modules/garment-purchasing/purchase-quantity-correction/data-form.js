cba4f142f4537f8301f62b2c54b60518c1fa2ad6
widiartowahyu3 <43228909+widiartowahyu3@users.noreply.github.com>
2018-12-06 09:56:55 +0700

    bug fix Garment Quantity Correction

--- src/modules/garment-purchasing/purchase-quantity-correction/data-form.js
+++ src/modules/garment-purchasing/purchase-quantity-correction/data-form.js
@@ -74,7 +74,6 @@ export class DataForm {
         if (newValue && newValue.Id) {
             this.service.getdeliveryOrderById(newValue.Id)
                 .then(deliveryOrder => {
-                    console.log(deliveryOrder.doNo);
                     this.data.CorrectionType = "Jumlah",
 
                     this.data.CorrectionDate = new Date(new Date().setHours(0, 0, 0, 0));
@@ -89,6 +88,13 @@ export class DataForm {
                     this.data.UseVat = deliveryOrder.useVat;
                     this.data.UseIncomeTax = deliveryOrder.useIncomeTax;
                     this.data.IncomeTax = deliveryOrder.incomeTax;
+                    console.log(this.data.IncomeTax);
+                    this.data.IncomeTax.toString = function () {
+                        return [this.Name, this.Rate]
+                            .filter((item, index) => {
+                                return item && item.toString().trim().length > 0;
+                            }).join(" - ");
+                    }
 
                     this.data.Items = [];
                     this.itemsTemp = [];
@@ -110,7 +116,7 @@ export class DataForm {
     
                             correctionNoteItem.Product = detail.product;
     
-                            correctionNoteItem.Quantity = parseFloat((detail.quantityCorrection).toFixed(2));
+                            correctionNoteItem.Quantity = parseFloat((detail.quantityCorrection - detail.receiptQuantity).toFixed(2));
     
                             correctionNoteItem.Uom = detail.purchaseOrderUom;
     
