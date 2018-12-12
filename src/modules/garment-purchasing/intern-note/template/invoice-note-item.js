80577021c9e1a996fe2e8cb4d68cb0a52191689a
widiartowahyu3 <43228909+widiartowahyu3@users.noreply.github.com>
2018-12-12 15:29:42 +0700

    update 2 bug fix garment intern note

--- src/modules/garment-purchasing/intern-note/template/invoice-note-item.js
+++ src/modules/garment-purchasing/intern-note/template/invoice-note-item.js
@@ -1,31 +1,27 @@
+
 export class InvoiceNoteItem {
 	
 	activate(context) {
 		this.data = context.data;
-		console.log(this.data);
 		this.error = context.error;
 		this.readOnly = context.options.readOnly;
 		this.data.pricePerDealUnit=this.data.pricePerDealUnit.toLocaleString('en-EN', { maximumFractionDigits: 4,minimumFractionDigits:4});
 		this.data.quantity=this.data.quantity.toLocaleString('en-EN', { maximumFractionDigits: 2,minimumFractionDigits:2});
 		this.data.priceTotal=this.data.priceTotal.toLocaleString('en-EN', { maximumFractionDigits: 2,minimumFractionDigits:2});
-		//this.data.doDetailId = 
+		var receiptQuantityTotal = 0;
+		var deliveryOrderItems = this.data.deliveryOrder.items || [];
+		for(let coba of deliveryOrderItems){
+			for(let deliveryOrderDetail of coba.fulfillments){
+				if(deliveryOrderDetail.Id == this.data.dODetailId){
+					receiptQuantityTotal += deliveryOrderDetail.receiptQuantity;
+				}
+			}
+		}
+		this.status = receiptQuantityTotal > 0 ? "Sudah" : "Belum";
 	}
 
 	get total() {
 		return this.data.dOQuantity * this.data.pricePerDealUnit;
-	}
-
-	get status() {
-		var receiptQuantityTotal = 0;
-		var deliveryOrderItems = this.data.deliveryOrder.items || [];
-		for(var deliveryOrderItem of deliveryOrderItems){			
-			for(var deliveryOrderDetail of deliveryOrderItem.fulfillments){
-				if(deliveryOrderDetail.Id){
-					receiptQuantityTotal =+ deliveryOrderDetail.receiptQuantity;
-				}
-			}
-		}
-		return receiptQuantityTotal > 0 ? "Sudah" : "Belum";
 	}
 	
 	get product() {
