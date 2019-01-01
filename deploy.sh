52330b9c8f5fac4e53df7571acbe415e0e977522
danliris <danliris.cloud@gmail.com>
2019-01-01 23:08:32 +0700

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+echo "Build"
+npm install
+npm run build:prod
+
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_NAME
+
+if [ "$BRANCH" == "dev" ]; then
+    git clone $GIT_URL_DEV out
+elif [ "$BRANCH" == "uat" ]; then
+    git clone $GIT_URL_UAT out
+elif [ "$BRANCH" == "master" ]; then
+    git clone $GIT_URL_MASTER out
+elif [ "$BRANCH" == "prdMigration" ]; then
+    git clone $GIT_URL_MIGRATION out
+else
+    git clone $GIT_URL out
+fi
+
+cp -a dist/. out/.
+cd out
+
+git add .
+git commit -m "Automated Deployment"
+
+git push origin master
