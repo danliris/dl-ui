5c65b02552410dafe66cff663186868b102c623b
kharissuryandaru <35208779+kharissuryandaru@users.noreply.github.com>
2019-01-09 16:32:56 +0700

    Fixed Conversion in GarmentDeliveryOrder and GarmentUnitReceiptNote

--- src/modules/garment-purchasing/unit-receipt-note/template/delivery-order-item.js
+++ src/modules/garment-purchasing/unit-receipt-note/template/delivery-order-item.js
@@ -15,7 +15,28 @@ export class DeliveryOrderItem {
 
     @computedFrom("data.ReceiptQuantity", "data.Conversion")
     get SmallQuantity() {
-		this.data.SmallQuantity = parseFloat((this.data.ReceiptQuantity * this.data.Conversion).toFixed(2));
-        return this.data.SmallQuantity;
-    }
+		// this.data.SmallQuantity = parseFloat((this.data.ReceiptQuantity * this.data.Conversion).toFixed(2));
+		this.data.SmallQuantity = parseFloat(this.data.ReceiptQuantity * this.data.Conversion).toLocaleString('en-EN', { minimumFractionDigits: 2,maximumFractionDigits: 2  });;
+        return parseFloat(this.data.ReceiptQuantity * this.data.Conversion).toLocaleString('en-EN', { minimumFractionDigits: 2,maximumFractionDigits: 2  });
+	}
+	
+	conversionChanged(e) {
+		if(!this.error)
+		  this.error={};
+		  if(this.data.Conversion%1>=0){
+			  if(!((this.data.Conversion.length<=16 && this.data.Conversion.indexOf(".")>0) || (this.data.Conversion.length<=15 && this.data.Conversion.indexOf(".")<0))){
+				this.error.Conversion="Konversi tidak boleh lebih dari 15 digit";
+			  }
+			  else if(this.data.Conversion==0 || this.data.Conversion=='0'){
+				this.error.Conversion="Conversion can not 0";
+			  }
+			  else {
+				this.error.Conversion=null;
+			}
+		  }
+		  else {
+			this.error.Conversion="Konversi Harus Diisi Dengan Angka";
+		  }
+		  this.data.Conversion=e.srcElement.value;
+	    }
 }
