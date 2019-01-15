9bc6679c8870a985a56a23061ea5cf6f544736bf
Lucia Widyana <mo_cia@yahoo.com>
2019-01-15 16:28:00 +0700

    dv monitoring purchase all

---
+++ src/loader/garment-purchase-request-name-loader.js
@@ -0,0 +1,16 @@
+import { Container } from 'aurelia-dependency-injection';
+import { Config } from "aurelia-api";
+
+const resource = 'garment-purchase-requests/by-name';
+
+module.exports = function(keyword, filter) {
+
+    var config = Container.instance.get(Config);
+    var endpoint = config.getEndpoint("purchasing-azure");
+
+    return endpoint.find(resource, { keyword: keyword, filter: JSON.stringify(filter) })
+        .then(results => {
+            return results.data;
+            
+        });
+}
