3c8845af082d94b2c3de65c76e0a4defdcff7298
danliris <danliris.cloud@gmail.com>
2019-01-22 14:36:33 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -35,8 +35,8 @@ export async function configure(aurelia) {
       var production = "https://dl-production-webapi-dev.azurewebsites.net/v1/";
       var productionAzure = "https://com-danliris-service-finishing-printing-dev.azurewebsites.net/v1/";
       var purchasing = "https://dl-purchasing-webapi-dev.azurewebsites.net/v1/";
-      var purchasingAzure = "https://com-danliris-service-purchasing-dev.azurewebsites.net/v1/";
-      var garmentPurchasing = "https://dl-purchasing-garment-webapi-dev.azurewebsites.net/v1/";
+      var purchasingAzure = "https://com-danliris-service-purchasing.azurewebsites.net/v1/";
+      var garmentPurchasing = "https://dl-purchasing-garment-webapi.azurewebsites.net/v1/";
       var inventory = "https://dl-inventory-webapi-dev.azurewebsites.net/v1/";
       var inventoryAzure = "https://com-danliris-service-inventory-dev.azurewebsites.net/v1/";
       var garmentMasterPlan = "https://dl-garment-master-plan-webapi-dev.azurewebsites.net/v1/";
