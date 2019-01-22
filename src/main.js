315cf55ca39d93853518705472add1e711de20a0
danliris <danliris.cloud@gmail.com>
2019-01-22 15:12:05 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -30,16 +30,16 @@ export async function configure(aurelia) {
         }
       }
 
-      var core = "https://com-danliris-service-core-dev.azurewebsites.net/v1/";
-      var auth = "https://dl-auth-api-dev.azurewebsites.net/v1/";
-      var production = "https://dl-production-webapi-dev.azurewebsites.net/v1/";
-      var productionAzure = "https://com-danliris-service-finishing-printing-dev.azurewebsites.net/v1/";
+      var core = "https://com-danliris-service-core.azurewebsites.net/v1/";
+      var auth = "https://dl-auth-api.azurewebsites.net/v1/";
+      var production = "https://dl-production-webapi.azurewebsites.net/v1/";
+      var productionAzure = "https://com-danliris-service-finishing-printing.azurewebsites.net/v1/";
       var purchasing = "https://dl-purchasing-webapi-dev.azurewebsites.net/v1/";
       var purchasingAzure = "https://com-danliris-service-purchasing.azurewebsites.net/v1/";
       var garmentPurchasing = "https://dl-purchasing-garment-webapi.azurewebsites.net/v1/";
       var inventory = "https://dl-inventory-webapi-dev.azurewebsites.net/v1/";
       var inventoryAzure = "https://com-danliris-service-inventory-prd.azurewebsites.net/v1/";
-      var garmentMasterPlan = "https://dl-garment-master-plan-webapi-dev.azurewebsites.net/v1/";
+      var garmentMasterPlan = "https://dl-garment-master-plan-webapi.azurewebsites.net/v1/";
       var spMasterPlan = "https://dl-sp-master-plan-webapi-dev.mybluemix.net/v1/";
       var spinning = "https://com-danliris-service-spinning-dev.azurewebsites.net/v1/";
       var intPurchasing = "https://com-danliris-service-internal-transfer-dev.azurewebsites.net/v1/";
