081b2b54020047e881069fbb4a5694e38adcd988
achensetyawati <achenzetya@gmail.com>
2019-01-25 16:44:46 +0700

    fix verification disposition

--- src/modules/verification/disposition-verification/data-form.js
+++ src/modules/verification/disposition-verification/data-form.js
@@ -24,8 +24,10 @@ export class DataForm {
     }
     itemsColumns = [{ header: "Nomor External PO" },
     { header: "Kena PPN" },
+    { header: "Nominal PPN" },
     { header: "Kena PPH" },
     { header: "PPH" },
+    { header: "Nominal PPH" },
     { header: "" }];
 
     constructor(service, bindingEngine) {
@@ -98,7 +100,7 @@ export class DataForm {
         if (this.DispositionNo) {
 
             this.data = Object.assign(this.data, this.DispositionNo)
-            console.log(this.data)
+            
             this.supplierName = this.data.Supplier.code + " - " + this.data.Supplier.name;
 
         } else {
