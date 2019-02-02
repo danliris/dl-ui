d68352217ed053bf88ff466ddaef72d5446940b4
achensetyawati <achenzetya@gmail.com>
2019-02-02 12:09:53 +0700

    update disposition verification

--- src/modules/verification/disposition-verification/view.js
+++ src/modules/verification/disposition-verification/view.js
@@ -77,6 +77,9 @@ export class View {
         if(this.dataExpedition.Id!=lastExpedition.Id){
                 this.canEdit=false;
         }
+        if(this.dataExpedition.isPaid){
+                this.canEdit=false;
+        }
         var arg = {
             filter: JSON.stringify({ DispositionNo : this.dataExpedition.dispositionNo })
         }
