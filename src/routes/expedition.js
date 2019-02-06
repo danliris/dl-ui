1f2f1518c5116a8dfcef7205ae4ae7d059f938b5
danliris <danliris.cloud@gmail.com>
2019-02-06 09:04:15 +0700

    Update expedition.js

--- src/routes/expedition.js
+++ src/routes/expedition.js
@@ -151,7 +151,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "finance",
-            //permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "B9": 1, "C9": 1 },
+            permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "B9": 1, "C9": 1 },
             iconClass: 'fa fa-dashboard'
         }
     },
@@ -164,7 +164,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "finance",
-            //permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "B9": 1, "C9": 1 },
+            permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "B9": 1, "C9": 1 },
             iconClass: 'fa fa-dashboard'
         }
     },
@@ -177,7 +177,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "finance",
-            //permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "B9": 1, "C9": 1 },
+            permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "B9": 1, "C9": 1 },
             iconClass: 'fa fa-dashboard'
         }
     },
