31d2dd24654c0dca7a42b2538b0715aa357cb6e5
AchmadSetiaji <achmad.setiaji@moonlay.com>
2019-03-13 17:28:57 +0700

    update validation on material type and yarn number

--- src/modules/weaving/master-yarn-number/edit.js
+++ src/modules/weaving/master-yarn-number/edit.js
@@ -20,13 +20,36 @@ export class Edit {
   }
 
   saveCallback(event) {
-    this.service
-      .update(this.data)
-      .then(result => {
-        this.router.navigateToRoute("view", { Id: this.data.Id });
-      })
-      .catch(e => {
-        this.error = e;
-      });
+    let errorIndex = 0;
+    this.error = {};
+    var CodeRegEx = new RegExp("([1-9])");
+    if (!this.data.Code || this.data.Code === "") {
+      this.data.Code = "";
+    }
+
+    if (!this.data.Number) {
+      this.data.Number = 0;
+    } else {
+      if (!CodeRegEx.test(this.data.Number)) {
+        this.error.Number = "Only Numbers (1-9) Allowed";
+        errorIndex++;
+      }
+    }
+
+    if (!this.data.RingType || this.data.RingType === "") {
+      this.data.RingType = "";
+    }
+
+    if (errorIndex == 0) {
+      this.service
+        .update(this.data)
+        .then(result => {
+          this.router.navigateToRoute("view", { Id: this.data.Id });
+        })
+        .catch(e => {
+          this.error = e;
+        });
+    }
   }
+
 }
