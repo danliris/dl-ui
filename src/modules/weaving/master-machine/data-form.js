93e04dd9bf21cf30cd4ccb64a020af71948a188c
AchmadSetiaji <achmad.setiaji@moonlay.com>
2019-03-25 17:51:29 +0700

    update for functionalities on master machine

--- src/modules/weaving/master-machine/data-form.js
+++ src/modules/weaving/master-machine/data-form.js
@@ -1,11 +1,15 @@
 import { inject, bindable, computedFrom } from "aurelia-framework";
-
-// var CurrencyLoader = require('../../../loader/currency-loader');
+var UnitLoader = require("../../../loader/unit-loader");
+var MachineTypeLoader = require('../../../loader/weaving-machine-type-loader');
 // var UomLoader = require('../../../loader/uom-loader');
 
 export class DataForm {
   @bindable title;
   @bindable readOnly;
+  @bindable WeavingMachineType;
+  @bindable WeavingUnit;
+  @bindable readOnlyValue;
+
   formOptions = {
     cancelText: "Kembali",
     saveText: "Simpan",
@@ -13,32 +17,54 @@ export class DataForm {
     editText: "Ubah"
   };
 
-  //   customShortOptions={
-  //       control:{
-  //           length:6
-  //       }
-  //   };
-
-  weavingUnit = ["", "Weaving1", "Weaving2", "Weaving3"];
-  machineType = ["", "Jacquard", "Rappier Dobby", "Tsudakoma", "Toyota"];
-  unit = ["", "cmpx", "m"];
   location = ["", "Utara", "Timur", "Selatan", "Barat"];
 
-  constructor() {}
-
-  // @computedFrom("data.id")
-  // get isEdit() {
-  //   return (this.data.id || "").toString() != "";
-  // }
+  constructor() { }
 
   bind(context) {
     this.context = context;
     this.data = this.context.data;
     this.error = this.context.error;
 
+    if (this.data.WeavingMachineType) {
+      this.WeavingMachineType = this.data.WeavingMachineType;
+      this.data.Speed = this.WeavingMachineType.Speed;
+      this.data.MachineUnit = this.WeavingMachineType.MachineUnit;
+    }
+
+    if (this.data.WeavingUnit) {
+      this.WeavingUnit = this.data.WeavingUnit;
+    }
+
     this.cancelCallback = this.context.cancelCallback;
     this.deleteCallback = this.context.deleteCallback;
     this.editCallback = this.context.editCallback;
     this.saveCallback = this.context.saveCallback;
+  }
+
+  get units() {
+    return UnitLoader;
+  }
+
+  get machineTypes() {
+    return MachineTypeLoader;
+  }
+
+  WeavingMachineTypeChanged(newValue) {
+
+    if (newValue) {
+      this.data.WeavingMachineType = newValue
+      this.data.Speed = newValue.Speed;
+      this.data.MachineUnit = newValue.MachineUnit;
+    } else {
+      this.data.WeavingMachineType = newValue
+      this.data.Speed = 0;
+      this.data.MachineUnit = '';
+    }
+  }
+
+  WeavingUnitChanged(newValue) {
+
+    this.data.WeavingUnit = newValue;
   }
 }
