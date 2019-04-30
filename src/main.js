94d76ebef1169ec2b86d489fc1e90e0f5b33a630
ANDRI PURNOMO <andrea.kinan@gmail.com>
2019-04-30 14:55:10 +0700

    Add Module PO Internal Belu Dibuat PO External
    Add Module PO Internal Belu Dibuat PO External
    Add Route PO Internal Belu Dibuat PO External

--- src/main.js
+++ src/main.js
@@ -35,7 +35,9 @@ export async function configure(aurelia) {
       var production = "https://dl-production-webapi.azurewebsites.net/v1/";
       var productionAzure = "https://com-danliris-service-finishing-printing-dev.azurewebsites.net/v1/";
       var purchasing = "https://dl-purchasing-webapi-dev.azurewebsites.net/v1/";
-      var purchasingAzure = "https://com-danliris-service-purchasing-dev.azurewebsites.net/v1/";
+      //var purchasingAzure = "https://com-danliris-service-purchasing-dev.azurewebsites.net/v1/";
+      var purchasingAzure = "http://localhost:35494/v1/";
+      
       var garmentPurchasing = "https://dl-purchasing-garment-webapi-dev.azurewebsites.net/v1/";
       var inventory = "https://dl-inventory-webapi-dev.azurewebsites.net/v1/";
       var inventoryAzure = "https://com-danliris-service-inventory-dev.azurewebsites.net/v1/";
