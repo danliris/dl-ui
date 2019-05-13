275862128f00f960895a72b4e5c93ab099c8be6d
AchmadSetiaji <achmad.setiaji@moonlay.com>
2019-05-13 10:27:47 +0700

    update form create with add loader

--- src/modules/weaving/daily-operation/loom/template/create-form.js
+++ src/modules/weaving/daily-operation/loom/template/create-form.js
@@ -1,12 +1,16 @@
 import { inject, bindable } from "aurelia-framework";
 import { Router } from "aurelia-router";
-
 import { Service } from "../service";
+var Operator = require("../../../../../loader/weaving-operator-loader");
+var Unit = require("../../../../../loader/unit-loader");
+var WeavingOrder = require("../../../../../loader/weaving-order-loader");
+var Machine = require("../../../../../loader/weaving-machine-loader");
 
 @inject(Service, Router)
 export class CreateForm {
     @bindable title;
     @bindable readOnly;
+    @bindable OrderProduction;
 
     constructor(service, router) {
         this.service = service;
@@ -22,5 +26,36 @@ export class CreateForm {
         this.context = context;
         this.data = this.context.data;
         this.error = this.context.error;
+    }
+
+    // Bindable Method
+    OrderProductionChanged(newValue) {
+        if (newValue.Id) {
+            this.data.OrderProduction = newValue;
+        }
+    }
+
+    // Auto Initialize
+    get FabricConstruction() {
+        if (this.OrderProduction) {
+            return this.OrderProduction.ConstructionNumber;
+        }
+    }
+
+    // Loaders
+    get Operators() {
+        return Operator;
+    }
+
+    get WeavingUnits() {
+        return Unit;
+    }
+
+    get OrderProductions() {
+        return WeavingOrder;
+    }
+
+    get Machines() {
+        return Machine;
     }
 }
