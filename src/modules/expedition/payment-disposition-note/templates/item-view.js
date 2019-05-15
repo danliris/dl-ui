87ac91b14130682654bb9cb14706d2205cfdb52f
achensetyawati <achenzetya@gmail.com>
2019-05-15 15:02:27 +0700

    payment disposition note

---
+++ src/modules/expedition/payment-disposition-note/templates/item-view.js
@@ -0,0 +1,15 @@
+export class ItemView {
+
+    constructor() {
+        this.columns = ['Unit', 'Nama Barang', 'Jumlah', 'Satuan', 'Harga Total'];
+    }
+
+    activate(context) {
+        this.data = context.data
+        this.isShowing = false;
+    }
+
+    toggle() {
+        this.isShowing = !this.isShowing;
+    }
+}
