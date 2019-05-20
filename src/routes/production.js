4441d6fe25885807f7f558913fc513312a90c162
danliris <danliris.cloud@gmail.com>
2019-05-20 09:33:20 +0700

    Update production.js

--- src/routes/production.js
+++ src/routes/production.js
@@ -8,7 +8,8 @@ module.exports = [
         auth: true,
         settings: {
             group: "finishing-printing",
-            //permission: { "C9": 1, "F1": 1, "F2": 1 },
+            permission: { "C9": 1},
+                         //, "F1": 1, "F2": 1 },
             iconClass: 'fa fa-dashboard'
         }
     },
