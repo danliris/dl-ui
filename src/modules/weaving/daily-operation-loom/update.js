f1d33a34e9ddc054f433450847ef29baf9f66b35
AchmadSetiaji <achmad.setiaji@moonlay.com>
2019-06-04 04:39:18 +0700

    Bug Fix, loom
    update resume

--- src/modules/weaving/daily-operation-loom/update.js
+++ src/modules/weaving/daily-operation-loom/update.js
@@ -18,7 +18,7 @@ export class Update {
         { header: "Operator", value: "BeamOperatorName" },
         { header: "Group", value: "BeamOperatorGroup" },
         { header: "Status", value: "OperationStatus" }
-      ];
+    ];
 
     async activate(params) {
         var Id = params.Id;
@@ -39,18 +39,25 @@ export class Update {
             });
 
         if (this.data.LoomHistory.length > 0) {
-            var result = this.data.LoomHistory.map( item => {
-                var DateOperation = moment(new Date(item.DateTimeOperation)).format('DD/MM/YYYY');
-                var TimeOperation =  moment(new Date(item.DateTimeOperation)).format('LT');
 
-                item.DateOperation = DateOperation;
-                item.TimeOperation = TimeOperation;
-
-                return item;
-            });
+            var result = this.remappingModels(this.data.LoomHistory);
 
             this.data.LoomHistory = result;
         }
+    }
+
+    remappingModels(data) {
+        var result = data.map(item => {
+            var DateOperation = moment(item.DateTimeOperation).format('DD/MM/YYYY');
+            var TimeOperation = moment(item.DateTimeOperation).format('LT');
+
+            item.DateOperation = DateOperation;
+            item.TimeOperation = TimeOperation;
+
+            return item;
+        });
+
+        return result;
     }
 
     start() {
@@ -60,8 +67,13 @@ export class Update {
         this.service
             .updateForStartProcess(this.data)
             .then(result => {
-                
+
                 this.data.LoomHistory = [];
+
+                if (result.length > 0) {
+                    result = this.remappingModels(result);
+                }
+
                 this.data.LoomHistory = result;
             });
     }
@@ -73,7 +85,13 @@ export class Update {
         this.service
             .updateForStopProcess(this.data)
             .then(result => {
+
                 this.data.LoomHistory = [];
+
+                if (result.length > 0) {
+                    result = this.remappingModels(result);
+                }
+
                 this.data.LoomHistory = result;
             });
     }
@@ -85,19 +103,31 @@ export class Update {
         this.service
             .updateForResumeProcess(this.data)
             .then(result => {
+
                 this.data.LoomHistory = [];
+
+                if (result.length > 0) {
+                    result = this.remappingModels(result);
+                }
+
                 this.data.LoomHistory = result;
             });
     }
 
     finish() {
-        $("#Melanjutkan").modal('hide');
+        $("#Selesai").modal('hide');
         this.error = {};
 
         this.service
             .updateForFinishProcess(this.data)
             .then(result => {
+                
                 this.data.LoomHistory = [];
+
+                if (result.length > 0) {
+                    result = this.remappingModels(result);
+                }
+
                 this.data.LoomHistory = result;
             });
     }
