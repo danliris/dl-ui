04686c6eabb39778b7951b626373ebf349985f7c
LeslieAula <LeslieAula@DESKTOP-FKFTBC0>
2019-06-13 17:10:27 +0700

    Add Daily Bank Transactions In

--- src/routes/expedition.js
+++ src/routes/expedition.js
@@ -141,5 +141,18 @@ module.exports =  [
             permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "B9": 1, "C9": 1 },
             iconClass: 'fa fa-dashboard'
         }
+    },
+    {
+        route: '/expedition/daily-bank-transaction-document-in',
+        name: 'daily-bank-transaction-document-in',
+        moduleId: './modules/expedition/daily-bank-transaction-document-in/index',
+        nav: true,
+        title: 'Transaksi Harian Bank Masuk',
+        auth: true,
+        settings: {
+            group: "finance",
+            permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "B9": 1, "C9": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
     }
 ];
