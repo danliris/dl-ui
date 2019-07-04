970f5dcf57dec34a696c60b2dde325e32fd3c2fc
danliris <danliris.cloud@gmail.com>
2019-07-04 13:57:31 +0700

    Update accounting.js

--- src/routes/accounting.js
+++ src/routes/accounting.js
@@ -8,7 +8,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "accounting",
-            permission: { "P1": 1, "P2": 1, "P3": 1, "P4": 1, "P5": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "C9": 1 },
+            permission: { "B1": 1, "C9": 1 },
             iconClass: 'fa fa-clone'
         }
     },
@@ -21,7 +21,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "accounting",
-            permission: { "P1": 1, "P2": 1, "P3": 1, "P4": 1, "P5": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "C9": 1 },
+            permission: { "B1": 1, "C9": 1 },
             iconClass: 'fa fa-clone'
         }
     },
@@ -34,7 +34,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "accounting",
-            permission: { "P1": 1, "P2": 1, "P3": 1, "P4": 1, "P5": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "C9": 1 },
+            permission: { "B1": 1, "C9": 1 },
             iconClass: 'fa fa-clone'
         }
     },
@@ -47,7 +47,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "accounting",
-            permission: { "P1": 1, "P2": 1, "P3": 1, "P4": 1, "P5": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "C9": 1 },
+            permission: { "B1": 1, "C9": 1 },
             iconClass: 'fa fa-clone'
         }
     },
@@ -60,7 +60,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "accounting",
-            permission: { "P1": 1, "P2": 1, "P3": 1, "P4": 1, "P5": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "C9": 1 },
+            permission: { "B1": 1, "C9": 1 },
             iconClass: 'fa fa-clone'
         }
     },
@@ -73,7 +73,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "accounting",
-            permission: { "P1": 1, "P2": 1, "P3": 1, "P4": 1, "P5": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "C9": 1 },
+            permission: { "B1": 1, "C9": 1 },
             iconClass: 'fa fa-clone'
         }
     },
@@ -86,7 +86,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "accounting",
-            permission: { "P1": 1, "P2": 1, "P3": 1, "P4": 1, "P5": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "C9": 1 },
+            permission: { "B1": 1, "C9": 1 },
             iconClass: 'fa fa-clone'
         }
     }
