41e051d7de390ad98903d7f28a61e532e1a897fb
Leslie Aula <leslie.aula@moonlay.com>
2019-07-05 08:15:08 +0700

    Add Retur From Buyer And Bugs Fix

--- src/routes/inventory.js
+++ src/routes/inventory.js
@@ -126,7 +126,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "Inventory",
-            //permission: { "C9": 1, "F1": 1, "F2": 1 },
+            permission: { "C9": 1, "F1": 1, "F2": 1 },
             iconClass: "fa fa-dashboard"
         }
     },
@@ -139,7 +139,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "Inventory",
-            //permission: { "C9": 1, "F1": 1, "F2": 1 },
+            permission: { "C9": 1, "F1": 1, "F2": 1 },
             iconClass: "fa fa-dashboard"
         }
     },
