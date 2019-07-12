773472d9a85d5291cc164611392bb1be1e6e1231
ali-moonlay <43127426+ali-moonlay@users.noreply.github.com>
2019-07-12 19:57:26 +0700

    fix spinning module

---
+++ src/modules/production/finishing-printing/cost-calculation/list.js
@@ -0,0 +1,63 @@
+import { inject } from 'aurelia-framework';
+import { Service } from "./service";
+import { Router } from 'aurelia-router';
+
+@inject(Router, Service)
+export class List {
+    context = ["Detail", "Cetak Cost Calculation", "Cetak Budget"];
+    columns = [
+        { field: "RO_Number", title: "No RO" },
+        { field: "Article", title: "Artikel" },
+        { field: "UnitName", title: "Unit" },
+        { field: "Quantity", title: "Kuantitas" },
+        { field: "ConfirmPrice", title: "Harga Konfirmasi" }
+    ];
+
+    loader = (info) => {
+        var order = {};
+
+        if (info.sort)
+            order[info.sort] = info.order;
+
+        var arg = {
+            page: parseInt(info.offset / info.limit, 10) + 1,
+            size: info.limit,
+            keyword: info.search,
+            order: order,
+        }
+
+        return this.service.search(arg)
+            .then(result => {
+                console.log(result)
+                return {
+                    total: result.info.total,
+                    data: result.data
+                }
+            });
+    }
+
+    constructor(router, service) {
+        this.service = service;
+        this.router = router;
+    }
+
+    contextCallback(event) {
+        var arg = event.detail;
+        var data = arg.data;
+        switch (arg.name) {
+            case "Detail":
+                this.router.navigateToRoute('view', { id: data.Id });
+                break;
+            case "Cetak Cost Calculation":
+                this.service.getPdfById(data.Id)
+                break;
+            case "Cetak Budget":
+                this.service.getBudgetById(data.Id)
+                break;
+        }
+    }
+
+    create() {
+        this.router.navigateToRoute('create');
+    }
+}
