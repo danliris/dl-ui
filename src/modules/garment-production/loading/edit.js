1914b07127835e4c71548d3d48f42048488c20b3
achensetyawati <achenzetya@gmail.com>
2019-09-06 15:17:56 +0700

    Loading Initial

---
+++ src/modules/garment-production/loading/edit.js
@@ -0,0 +1,41 @@
+import { inject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+import { Service } from './service';
+
+@inject(Router, Service)
+export class Edit {
+    isEdit = true;
+
+    constructor(router, service) {
+        this.router = router;
+        this.service = service;
+    }
+
+    async activate(params) {
+        let id = params.id;
+        this.data = await this.service.getById(id);
+    }
+
+    bind() {
+        this.error = {};
+    }
+
+    cancelCallback(event) {
+        this.router.navigateToRoute('view', { id: this.data.Id });
+    }
+
+    saveCallback(event) {
+        this.service.update(this.data)
+            .then(result => {
+                this.cancelCallback();
+            })
+            .catch(e => {
+                this.error = e;
+                if (typeof (this.error) == "string") {
+                    alert(this.error);
+                } else {
+                    alert("Missing Some Data");
+                }
+            })
+    }
+}
