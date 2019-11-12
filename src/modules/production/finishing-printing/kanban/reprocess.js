77debfaef8daa001950a5203695d9ea5428850d6
Leslie Aula <cristiano.ulo@gmail.com>
2019-11-12 11:55:36 +0700

    Fix bugs kanban

--- src/modules/production/finishing-printing/kanban/reprocess.js
+++ src/modules/production/finishing-printing/kanban/reprocess.js
@@ -156,7 +156,7 @@ export class Reprocess {
       }
 
       if (!hasError) {
-        event.event.toElement.disabled = true;
+        // event.event.toElement.disabled = true;
 
         var createPromise = [];
         this.data.CurrentStepIndex = 0;
