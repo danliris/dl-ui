393a511d58a8fa13808687537022b074b7d87291
putrapradesa <49633721+putrapradesa@users.noreply.github.com>
2020-01-21 09:18:37 +0700

    Add Monitoring Expenditure Good

--- src/routes/customs.js
+++ src/routes/customs.js
@@ -11,5 +11,18 @@ module.exports = [
             permission: { "C5": 1, "C9": 1 },
             iconClass: 'fa fa-dashboard'
         },
+    },
+    {
+        route: '/customs/monitoring-expenditure-good',
+        name: 'customs-report-in',
+        moduleId: './modules/customs/monitoring-barang-jadi/index',
+        nav: true,
+        title: 'Laporan Pengeluaran Barang Jadi',
+        auth: true,
+        settings: {
+            group: "customs",
+            permission: { "C5": 1, "C9": 1 },
+            iconClass: 'fa fa-dashboard'
+        },
     }
 ]
