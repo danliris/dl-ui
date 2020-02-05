affc3c1337a74b348e807c639639e2dc26274173
ANDRI PURNOMO <andrea.kinan@gmail.com>
2020-02-05 13:16:26 +0700

    Update Module & Route | Garment Report
    Update Module & Route | Garment Report

--- src/modules/merchandiser/report/pr-master-garment-validation-report/service.js
+++ src/modules/merchandiser/report/pr-master-garment-validation-report/service.js
@@ -13,13 +13,13 @@ export class Service extends RestService {
   }
 
 search(info) { 
-        var endpoint = `${serviceUri}?unit=${info.unit}&dateFrom=${info.dateFrom}&dateTo=${info.dateTo}`;
+        var endpoint = `${serviceUri}?unit=${info.unit}&sectionName=${info.sectionName}&dateFrom=${info.dateFrom}&dateTo=${info.dateTo}`;
         return super.get(endpoint);
         
     }
     
 generateExcel(info) {
-        var endpoint = `${serviceUri}/download?unit=${info.unit}&dateFrom=${info.dateFrom}&dateTo=${info.dateTo}`;
+        var endpoint = `${serviceUri}/download?unit=${info.unit}&sectionName=${info.sectionName}&dateFrom=${info.dateFrom}&dateTo=${info.dateTo}`;
         return super.getXls(endpoint);
     }
 }
