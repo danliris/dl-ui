29408de14ad4bb969f85393c7480e0f3f7dec97a
achensetyawati <achenzetya@gmail.com>
2020-02-11 08:49:39 +0700

    update finishing in subcon

--- src/modules/garment-production/subcon-finishing-in/data-form.js
+++ src/modules/garment-production/subcon-finishing-in/data-form.js
@@ -80,7 +80,6 @@ export class DataForm {
             };
             return this.purchasingService.getGarmentURN(info)
                 .then((result) => {
-                    console.log(result)
                     var doList=[];
                         for(var a of result.data){
                             if(doList.length==0){
@@ -88,7 +87,6 @@ export class DataForm {
                             }
                             else{
                                 var dup= doList.find(d=>d.DONo==a.DONo);
-                                console.log(dup)
                                 if(!dup){
                                     doList.push(a);
                                 }
@@ -163,62 +161,19 @@ export class DataForm {
             Promise.resolve(this.purchasingService.getGarmentDOById(newValue.DOId))
             .then(result => {
                 console.log(result)
-            })
-            // this.context.error.Items = [];
-            // this.data.RONo = newValue.RONo;
-            // this.data.Article = newValue.Article;
-            // this.data.Comodity= newValue.Comodity;
-            // this.data.UnitFrom=newValue.Unit;
-            // this.data.SewingOutId=newValue.Id;
-            // this.data.SewingOutNo=newValue.SewingOutNo;
-            // var items=[];
-
-            // Promise.resolve(this.service.searchSewingOut({ filter: JSON.stringify({ RONo: this.data.RONo, UnitToId: this.data.Unit.Id, SewingTo: "FINISHING" }) }))
-            //         .then(result => {
-            //             for(var sewingOut of result.data){
-            //                 for(var sewingOutItem of sewingOut.Items){
-            //                     var item={};
-            //                     if(sewingOutItem.RemainingQuantity>0){
-            //                         if(sewingOut.IsDifferentSize){
-            //                             for(var sewingOutDetail of sewingOutItem.Details){
-            //                                 item={};
-            //                                 item.SewingOutItemId=sewingOutItem.Id;
-            //                                 item.SewingOutDetailId=sewingOutDetail.Id;
-            //                                 item.Quantity=sewingOutDetail.Quantity;
-            //                                 item.Product=sewingOutItem.Product;
-            //                                 item.Uom=sewingOutItem.Uom;
-            //                                 item.Size=sewingOutDetail.Size;
-            //                                 item.Color=sewingOutItem.Color;
-            //                                 item.DesignColor=sewingOutItem.DesignColor;
-            //                                 item.RemainingQuantity=sewingOutDetail.Quantity;
-            //                                 item.BasicPrice=sewingOutItem.BasicPrice;
-            //                                 item.ComodityPrice=this.data.Price;
-            //                                 item.Price=(sewingOutItem.BasicPrice + (this.data.Price * 75/100)) * sewingOutDetail.Quantity;
-            //                                 this.data.Items.push(item);
-            //                             }
-            //                         }
-            //                         else{
-            //                             item.SewingOutItemId=sewingOutItem.Id;
-            //                             item.Quantity=sewingOutItem.Quantity;
-            //                             item.Product=sewingOutItem.Product;
-            //                             item.Uom=sewingOutItem.Uom;
-            //                             item.Size=sewingOutItem.Size;
-            //                             item.Color=sewingOutItem.Color;
-            //                             item.DesignColor=sewingOutItem.DesignColor;
-            //                             item.RemainingQuantity=sewingOutItem.Quantity;
-            //                             item.BasicPrice=sewingOutItem.BasicPrice;
-            //                             item.ComodityPrice=this.data.Price;
-            //                             item.Price=(sewingOutItem.BasicPrice + (this.data.Price * 75/100)) * sewingOutItem.Quantity;
-            //                             this.data.Items.push(item);
-            //                         }
-                                
-            //                     }
-            //                 }
-            //         }
-            //     });
-            }
-        
-        
+                var items=[];
+                for(var item of result.items){
+                    for(var detail of item.fulfillments){
+                        var doItem={};
+                        doItem.Product=detail.product;
+                        items.push(doItem);
+                    }
+                }
+                this.data.Items=items;
+                console.log(this.data.Items)
+            });
+            
+        }
         else {
             this.context.selectedURNViewModel.editorValue = "";
             this.data.RONo = null;
