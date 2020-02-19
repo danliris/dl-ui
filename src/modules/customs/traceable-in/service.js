de56d75df72ab7bbec226a3bc009597ebc262c68
putrapradesa <49633721+putrapradesa@users.noreply.github.com>
2020-02-19 11:15:31 +0700

    Add Traceablein module, route & loader

---
+++ src/modules/customs/traceable-in/service.js
@@ -0,0 +1,26 @@
+
+import {inject, Lazy} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import {RestService} from '../../../utils/rest-service';
+
+
+const serviceUri = 'customs-reports';
+
+export class Service extends RestService {
+
+    constructor(http, aggregator, config, endpoint) {
+        super(http, aggregator, config, "customs-report");
+    }
+
+    search(info) { 
+        var endpoint = `${serviceUri}/traceable/in`;
+        return super.list(endpoint, info);
+
+        //return super.get(endpoint);
+    }
+    
+    generateExcel(info) {
+        var endpoint = `${serviceUri}/traceable/in/download?bcno=${info.bcno}&type=${info.type}`;
+        return super.getXls(endpoint);
+    }
+}
