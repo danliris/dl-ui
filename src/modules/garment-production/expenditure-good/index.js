1603f14a5e840ade086051b8de488667b767c8a6
achensetyawati <achenzetya@gmail.com>
2020-02-24 10:42:06 +0700

    Update Expenditure Good

--- src/modules/garment-production/expenditure-good/index.js
+++ src/modules/garment-production/expenditure-good/index.js
@@ -1,10 +1,10 @@
 export class Index {
     configureRouter(config, router) {
         config.map([
-            { route: ['', 'list'], moduleId: './list', name: 'list', nav: true, title: 'List: Barang Jadi' },
-            { route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create: Barang Jadi' },
-            { route: 'view/:id', moduleId: './view', name: 'view', nav: false, title: 'View: Barang Jadi' },
-            { route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit: Barang Jadi' },
+            { route: ['', 'list'], moduleId: './list', name: 'list', nav: true, title: 'List: Pengeluaran Barang Jadi' },
+            { route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create: Pengeluaran Barang Jadi' },
+            { route: 'view/:id', moduleId: './view', name: 'view', nav: false, title: 'View: Pengeluaran Barang Jadi' },
+            { route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit: Pengeluaran Barang Jadi' },
         ]);
         this.router = router;
     }
