e28c36c1f4a977075d0ffe387ccd0a12eee215b8
achensetyawati <achenzetya@gmail.com>
2020-03-20 10:07:33 +0700

    Update Production flow feature

--- src/modules/garment-production/production-flow-feature/list.js
+++ src/modules/garment-production/production-flow-feature/list.js
@@ -423,7 +423,7 @@ export class List {
         cutColumns.push({ field: 'CuttingNo', title: 'No Bon' });
         cutColumns.push({ field: 'date', title: 'Tanggal', formatter: value => moment(value).format("DD MMM YYYY") });
         cutColumns.push({ field: 'color', title: 'Warna' });
-
+        this.cuttingSizes.sort();
         for(let size of this.cuttingSizes) {
             cutColumns.push({ field: `${size}`, title: size,
                 formatter: (cell) => {return cell } });
@@ -443,8 +443,8 @@ export class List {
         loadingColumns.push({ field: 'loadingNo', title: 'No Bon' });
         loadingColumns.push({ field: 'date', title: 'Tanggal', formatter: value => moment(value).format("DD MMM YYYY") });
         loadingColumns.push({ field: 'color', title: 'Warna' });
-
-        for(let size of this.cuttingSizes) {
+        this.loadingSizes.sort();
+        for(let size of this.loadingSizes) {
             loadingColumns.push({ field: `${size}`, title: size,
                 formatter: (cell) => {return cell } });
         }
@@ -463,7 +463,7 @@ export class List {
         sewingColumns.push({ field: 'sewingNo', title: 'No Bon' });
         sewingColumns.push({ field: 'date', title: 'Tanggal', formatter: value => moment(value).format("DD MMM YYYY") });
         sewingColumns.push({ field: 'color', title: 'Warna' });
-
+        this.sewingSizes.sort();
         for(let size of this.sewingSizes) {
             sewingColumns.push({ field: `${size}`, title: size,
                 formatter: (cell) => {return cell } });
@@ -483,7 +483,7 @@ export class List {
         finishingColumns.push({ field: 'finishingNo', title: 'No Bon' });
         finishingColumns.push({ field: 'date', title: 'Tanggal', formatter: value => moment(value).format("DD MMM YYYY") });
         finishingColumns.push({ field: 'color', title: 'Warna' });
-
+        this.finishingSizes.sort();
         for(let size of this.finishingSizes) {
             finishingColumns.push({ field: `${size}`, title: size,
                 formatter: (cell) => {return cell } });
@@ -503,7 +503,7 @@ export class List {
         expenditureColumns.push({ field: 'expenditureNo', title: 'No Bon' });
         expenditureColumns.push({ field: 'date', title: 'Tanggal', formatter: value => moment(value).format("DD MMM YYYY") });
         expenditureColumns.push({ field: 'color', title: 'Warna' });
-
+        this.expenditureSizes.sort();
         for(let size of this.expenditureSizes) {
             expenditureColumns.push({ field: `${size}`, title: size,
                 formatter: (cell) => {return cell } });
