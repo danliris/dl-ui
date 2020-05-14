8fcbcccea685745ef5747a0f6147d79af8af48db
skydivine <17161989+skydivine@users.noreply.github.com>
2020-05-14 20:38:54 +0700

    Enhance Area Shipping Out

--- src/modules/packing-sku-inventory/shipping-area-output/template/item.js
+++ src/modules/packing-sku-inventory/shipping-area-output/template/item.js
@@ -13,19 +13,22 @@ export class CartItem {
         this.options = context.options;
         this.contextOptions = context.context.options;
         this.destinationArea = this.contextOptions.destinationArea;
-
-        if(this.data.deliveryOrderSales){
-            this.selectedDOSales = {};
-            this.selectedDOSales.Id = this.data.deliveryOrderSales.id;
-            this.selectedDOSales.DoNO = this.data.deliveryOrderSales.no;
+        this.isSales = this.contextOptions.isSales;
+        if (this.data.qty) {
+            this.data.previousBalance = this.data.qty;
         }
+        // if(this.data.deliveryOrderSales){
+        //     this.selectedDOSales = {};
+        //     this.selectedDOSales.Id = this.data.deliveryOrderSales.id;
+        //     this.selectedDOSales.DoNO = this.data.deliveryOrderSales.no;
+        // }
         if (this.data.productionOrder && this.data.productionOrder.id) {
             this.selectedProductionOrder = {};
             this.selectedProductionOrder.Id = this.data.productionOrder.id;
             this.selectedProductionOrder.OrderNo = this.data.productionOrder.no;
             this.selectedProductionOrder.OrderType = {};
             this.selectedProductionOrder.OrderType.Name = this.data.productionOrder.type;
-            this.selectedProductionOrder.OrderQuantity = this.data.balance;
+            this.selectedProductionOrder.OrderQuantity = this.data.productionOrder.orderQuantity;
             this.selectedProductionOrder.Construction = this.data.construction;
             this.selectedProductionOrder.Buyer = {};
             this.selectedProductionOrder.Buyer.Name = this.data.buyer;
@@ -35,8 +38,7 @@ export class CartItem {
             this.selectedProductionOrder.Details[0].ColorRequest = this.data.color;
             this.selectedProductionOrder.DesignCode = this.data.motif;
             this.selectedProductionOrder.Uom = {};
-            this.selectedProductionOrder.Uom.Unit = this.data.unit;
-            this.selectedProductionOrder.OrderQuantity = this.data.balance;
+            this.selectedProductionOrder.Uom.Unit = this.data.uomUnit;
             if (this.selectedProductionOrder.OrderNo.charAt(0) === 'P') {
                 this.data.unit = "PRINTING"
             } else {
@@ -45,6 +47,9 @@ export class CartItem {
         }
     }
 
+    changeCheckBox() {
+        this.context.context.options.checkedAll = this.context.context.items.reduce((acc, curr) => acc && curr.data.IsSave, true);
+    }
 
     controlOptions = {
         control: {
@@ -67,7 +72,7 @@ export class CartItem {
             this.data.productionOrder.id = this.selectedProductionOrder.Id;
             this.data.productionOrder.no = this.selectedProductionOrder.OrderNo;
             this.data.productionOrder.type = this.selectedProductionOrder.OrderType.Name;
-            this.data.balance = this.selectedProductionOrder.OrderQuantity;
+            this.data.productionOrder.orderQuantity = this.selectedProductionOrder.OrderQuantity;
             if (this.selectedProductionOrder.Construction) {
                 this.data.construction = this.selectedProductionOrder.Construction;
             } else {
@@ -78,7 +83,6 @@ export class CartItem {
             this.data.color = this.selectedProductionOrder.Details[0].ColorRequest;
             this.data.motif = this.selectedProductionOrder.DesignCode;
             this.data.uomUnit = this.selectedProductionOrder.Uom.Unit;
-            this.data.balance = this.selectedProductionOrder.OrderQuantity;
             if (this.selectedProductionOrder.OrderNo.charAt(0) === 'P') {
                 this.data.unit = "PRINTING"
             } else {
@@ -90,12 +94,12 @@ export class CartItem {
         }
     }
 
-    @bindable selectedDOSales;
-    selectedDOSalesChanged(n, o) {
-        if (this.selectedDOSales) {
-            this.data.deliveryOrderSales = {};
-            this.data.deliveryOrderSales.id = this.selectedDOSales.Id;
-            this.data.deliveryOrderSales.no = this.selectedDOSales.DoNO;
-        }
-    }
+    // @bindable selectedDOSales;
+    // selectedDOSalesChanged(n, o) {
+    //     if (this.selectedDOSales) {
+    //         this.data.deliveryOrderSales = {};
+    //         this.data.deliveryOrderSales.id = this.selectedDOSales.Id;
+    //         this.data.deliveryOrderSales.no = this.selectedDOSales.DoNO;
+    //     }
+    // }
 }
