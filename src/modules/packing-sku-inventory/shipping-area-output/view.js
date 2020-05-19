888ad1f532a53472f4265930e81e438e392a0d9b
skydivine <17161989+skydivine@users.noreply.github.com>
2020-05-19 16:37:19 +0700

    Enhance When Viewing Detail Movement Area

--- src/modules/packing-sku-inventory/shipping-area-output/view.js
+++ src/modules/packing-sku-inventory/shipping-area-output/view.js
@@ -13,6 +13,7 @@ export class View {
     async activate(params) {
         var id = params.id;
         this.data = await this.service.getById(id);
+        this.data.shippingProductionOrders = this.data.shippingProductionOrders.filter(s => s.hasNextAreaDocument === false);
         //this.spp = await this.service.getSPPbySC(this.data.salesContractNo);
         this.canEdit=true;
         
