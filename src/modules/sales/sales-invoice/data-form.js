da39a328f1eb685749f302319e858aec726a02bc
Franciskus Napitupulu <8franciskusna70@gmail.com>
2020-05-27 20:55:35 +0700

    EnhanceSalesInvoiceChangeGetDataFromShipmentDocumentToShippingOutput

--- src/modules/sales/sales-invoice/data-form.js
+++ src/modules/sales/sales-invoice/data-form.js
@@ -24,6 +24,7 @@ export class DataForm {
   @bindable VatType;
   @bindable Tempo;
   @bindable Sales;
+  @bindable selectedPaymentType;
 
   constructor(service, serviceCore, bindingSignaler, bindingEngine) {
     this.service = service;
@@ -55,9 +56,10 @@ export class DataForm {
     }
 
     if (this.data.Buyer && this.data.Buyer.Id) {
-      this.selectedBuyer = await this.serviceCore.getBuyerById(
-        this.data.Buyer.Id
-      );
+      // this.selectedBuyer = await this.serviceCore.getBuyerById(
+      //   this.data.Buyer.Id
+      // );
+      this.selectedBuyer = this.data.Buyer;
     }
 
     if (this.data.Unit && this.data.Unit.Id) {
@@ -89,8 +91,13 @@ export class DataForm {
     if (salesInvoiceTime && dueTime) {
       this.Tempo = (dueTime - salesInvoiceTime) / (1000 * 60 * 60 * 24);
     }
+
     if (this.data.Sales) {
       this.Sales = this.data.Sales;
+    }
+
+    if (this.data.PaymentType) {
+      this.selectedPaymentType = this.data.PaymentType;
     }
   }
 
@@ -111,6 +118,13 @@ export class DataForm {
     }
     this.data.TotalPayment = totalPayment;
     return totalPayment;
+  }
+
+  selectedPaymentTypeChanged(newValue, oldValue) {
+    if (this.selectedPaymentType) {
+      this.data.PaymentType = this.selectedPaymentType;
+      this.itemOptions.PaymentType = this.data.PaymentType;
+    }
   }
 
   SalesInvoiceDateChanged(newValue, oldValue) {
@@ -202,7 +216,7 @@ export class DataForm {
     "PPN BUMN",
     "PPN Retail",
   ];
-  paymentTypeOptions = ["", "Meter", "Yard"];
+  paymentTypeOptions = ["", "MTR", "YARD"];
 
   enterDelegate(event) {
     if (event.charCode === 13) {
@@ -238,6 +252,7 @@ export class DataForm {
       this.data.Buyer.NPWP = this.selectedBuyer.NPWP;
       this.data.Buyer.NIK = this.selectedBuyer.NIK;
       this.itemOptions.BuyerId = this.data.Buyer.Id;
+      this.itemOptions.HasSalesInvoice = false;
     } else {
       this.data.Buyer.Id = null;
       this.data.Buyer.Name = null;
@@ -246,6 +261,7 @@ export class DataForm {
       this.data.Buyer.NPWP = null;
       this.data.Buyer.NIK = null;
       this.itemOptions.BuyerId = null;
+      this.itemOptions.HasSalesInvoice = false;
       this.data.SalesInvoiceDetails = [];
     }
   }
--- src/modules/sales/sales-invoice/template/sales-invoice-item.js
+++ src/modules/sales/sales-invoice/template/sales-invoice-item.js
@@ -2,14 +2,10 @@ import { inject, bindable, BindingEngine } from "aurelia-framework";
 import { SalesInvoiceDetail } from "./sales-invoice-detail";
 import { DataForm } from "./../data-form";
 
-var UomLoader = require("../../../../loader/uom-loader");
-var UomDyeingLoader = require("../../../../loader/uom-dyeing-loader");
-
 @inject(SalesInvoiceDetail, DataForm, BindingEngine)
 export class SalesInvoiceItem {
-  @bindable Total;
+  @bindable QuantityItem;
   @bindable Price;
-  @bindable isDyeing;
 
   constructor(salesInvoiceDetail, dataForm, bindingEngine) {
     this.salesInvoiceDetail = salesInvoiceDetail;
@@ -22,93 +18,45 @@ export class SalesInvoiceItem {
     this.error = context.error;
     this.options = context.options;
     this.readOnly = context.options.readOnly;
-    this.Total = this.data.Total;
+    this.PaymentType = context.context.options.PaymentType;
+
+    this.QuantityItem = this.data.QuantityItem;
+    this.ConvertValue = this.data.ConvertValue;
     this.Price = this.data.Price;
-    this.getAmount = this.Total * this.Price;
-    this.data.Amount = this.getAmount;
 
-    if (this.data.Uom) {
-      this.selectedUom = {
-        Id: this.data.Uom.Id,
-        Unit: this.data.Uom.Unit,
-      };
+    if (this.PaymentType == "MTR") {
+      this.getAmount = this.QuantityItem * this.Price;
+      this.data.Amount = this.getAmount;
+      this.data.Price = this.Price;
+    } else if (this.PaymentType == "YARD") {
+      this.getAmount = this.ConvertValue * this.Price;
+      this.data.Amount = this.getAmount;
+      this.data.Price = this.Price;
+    } else {
+      this.getAmount = 0;
+      this.data.Amount = this.getAmount;
+      this.data.Price = this.Price;
     }
-  }
-
-  TotalChanged(newValue, oldValue) {
-    this.getAmount = this.Total * this.Price;
     this.data.Amount = this.getAmount;
-    this.data.Total = this.Total;
   }
 
   PriceChanged(newValue, oldValue) {
-    this.getAmount = this.Total * this.Price;
-    this.data.Amount = this.getAmount;
-    this.data.Price = this.Price;
+    if (this.PaymentType == "MTR") {
+      this.getAmount = this.QuantityItem * this.Price;
+      this.data.Amount = this.getAmount;
+      this.data.Price = this.Price;
+    } else if (this.PaymentType == "YARD") {
+      this.getAmount = this.ConvertValue * this.Price;
+      this.data.Amount = this.getAmount;
+      this.data.Price = this.Price;
+    } else {
+      this.getAmount = 0;
+      this.data.Amount = this.getAmount;
+      this.data.Price = this.Price;
+    }
   }
 
   AmountChanged(newValue, oldValue) {
     this.data.Amount = this.getAmount;
-  }
-
-  @bindable selectedUom;
-  selectedUomChanged(newValue, oldValue) {
-    if (this.selectedUom && this.selectedUom.Id) {
-      this.data.Uom = {};
-      this.data.Uom.Id = this.selectedUom.Id;
-      this.data.Uom.Unit = this.selectedUom.Unit;
-      if (newValue.Unit == "MTR") {
-        this.data.ConvertValue = parseInt(this.data.Total * (10936 / 10000));
-        this.data.ConvertUnit = "YARD";
-      } else if (newValue.Unit == "YARD") {
-        this.data.ConvertValue = parseInt(this.data.Total / (10936 / 10000));
-        this.data.ConvertUnit = "MTR";
-      } else {
-        this.data.ConvertValue = 0;
-        this.data.ConvertUnit = null;
-      }
-    } else {
-      this.data.Uom.Id = null;
-      this.data.Uom.Unit = null;
-    }
-  }
-
-  get uomLoader() {
-    var isdyeing = 0;
-    var refType = [
-      { code: "BPF", entity: "dyeing" },
-      { code: "BSF", entity: "dyeing" },
-      { code: "RPF", entity: "dyeing" },
-      { code: "BPR", entity: "printing" },
-      { code: "BSR", entity: "printing" },
-      { code: "RPR", entity: "printing" },
-      { code: "BON", entity: "dyeing" },
-      { code: "GPF", entity: "dyeing" },
-      { code: "RGF", entity: "dyeing" },
-      { code: "GPR", entity: "printing" },
-      { code: "RGR", entity: "printing" },
-      { code: "RON", entity: "dyeing" },
-      { code: "BRG", entity: "weaving" },
-      { code: "RRG", entity: "weaving" },
-    ];
-    var findType = refType.filter((data) => {
-      return data.code == this.dataForm.data.SalesInvoiceType;
-    });
-
-    if (findType.length > 0) {
-      if (findType[0].entity == "dyeing") {
-        isdyeing = 1;
-      } else {
-        isdyeing = 0;
-      }
-    } else {
-      isdyeing = 0;
-    }
-    this.isDyeing = isdyeing;
-    if (isdyeing) {
-      return UomDyeingLoader;
-    } else {
-      return UomLoader;
-    }
   }
 }
