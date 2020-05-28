2a32db24a03bf8be14fe4ff1c8e94901a1e5fb9c
putrapradesa <49633721+putrapradesa@users.noreply.github.com>
2020-05-28 15:45:04 +0700

    Update finishing-printing-sales-contract & production-order-report

--- src/modules/sales/finishing-printing-sales-contract/templates/detail-item.js
+++ src/modules/sales/finishing-printing-sales-contract/templates/detail-item.js
@@ -1,17 +1,35 @@
 import { inject, bindable, computedFrom } from 'aurelia-framework';
 import { Container } from 'aurelia-dependency-injection';
 import { Config } from "aurelia-api"
+var colorLoader = require('../../../../loader/color-type-loader');
 
 export class DetailItem {
+  @bindable ColorType;
   activate(item) {
     this.data = item.data;
     this.error = item.error;
     this.options = item.options;
+    this.ColorType = this.data.ColorType
+    console.log(this.data);
   }
 
   controlOption = {
     control: {
       length: 12
     }
+  }
+  ColorTypeChanged() {
+    if (this.ColorType) {
+      console.log(this.ColorType);
+      this.data.Color = this.ColorType.Name;
+      this.data.ColorType = this.ColorType;
+    } else {
+      this.data.ColorType = {};
+      // this.data.colorTypeId = {};
+      this.data.Color = '';
+    }
+  }
+  get loader() {
+    return colorLoader;
   }
 }
