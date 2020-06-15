18384dd6fc31a13e6a6aa140a962209016cb71a3
kuswandanu <kuswandanu@gmail.com>
2020-06-15 14:06:45 +0700

    Garment Shipping Cover Letter - Shipping Staff

--- src/modules/garment-shipping/cover-letter/data-form.js
+++ src/modules/garment-shipping/cover-letter/data-form.js
@@ -55,13 +55,26 @@ export class DataForm {
 
     selectedPackingListChanged(newValue, oldValue) {
         if (newValue) {
-            this.data.packingListId = newValue.id;
+            this.data.packingListId = newValue.packingListId;
+            this.data.invoiceId = newValue.id;
             this.data.invoiceNo = newValue.invoiceNo;
-            this.data.order = newValue.buyerAgent;
-            this.data.exportEstimationDate = newValue.exportEstimationDate;
+            this.data.shippingStaff = {
+                id: newValue.shippingStaffId,
+                name: newValue.shippingStaff
+            };
+
+            if (this.data.packingListId) {
+                this.service.getPackingListById(this.data.packingListId)
+                    .then(packingList => {
+                        this.data.order = packingList.buyerAgent;
+                        this.data.exportEstimationDate = packingList.exportEstimationDate;
+                    });
+            }
         } else {
             this.data.packingListId = 0;
+            this.data.invoiceId = 0;
             this.data.invoiceNo = null;
+            this.data.shippingStaff = null;
             this.data.order = null;
             this.data.exportEstimationDate = null;
         }
