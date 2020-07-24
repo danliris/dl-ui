8a407985c57fda027454f5fb6cc0898c33a270f8
Ahmad Mukhtar <amx23baradja@gmail.com>
2020-07-24 11:23:25 +0700

    Update Verifikasi VB

--- src/modules/purchasing/verification-vb/edit.js
+++ src/modules/purchasing/verification-vb/edit.js
@@ -1,8 +1,8 @@
 import { inject, Lazy } from 'aurelia-framework';
 import { Router } from 'aurelia-router';
 import { Service } from './service';
-import { Dialog } from '../../../au-components/dialog/dialog';
-
+import { Dialog } from '../../../components/dialog/dialog';
+import { AlertView } from './custom-dialog-view/alert-view';
 
 @inject(Router, Service, Dialog)
 export class Edit {
@@ -10,6 +10,10 @@ export class Edit {
         this.router = router;
         this.service = service;
         this.dialog = dialog;
+        this.submitContext = {
+            verifiedAlert: false,
+            position: 0,
+        };
     }
 
     isEdit = true;
@@ -19,23 +23,37 @@ export class Edit {
         this.data = await this.service.getById(id);
     }
 
-    cancelCallback(event) {
-        this.router.navigateToRoute('view', { id: this.data.Id });
+    cancel(event) {
+        this.router.navigateToRoute('view', { id: this.data.numberVB.Id });
     }
 
-    saveCallback(event) {
-        this.dialog.prompt('Apakah anda yakin akan menyimpan perubahan data ini?', 'Ubah Verifikasi VB')
-            .then((response) => {
-                if (response.ok) {
-                    this.service.update(this.data)
-                        .then(result => {
-                            this.router.navigateToRoute('view', { id: this.data.Id });
-                        })
-                        .catch(e => {
-                            this.error = e;
-                        })
-                }
-            })
+    Submit(context) {
 
+        var Data = this.data;
+        this.submitContext.verifiedAlert = context == "VerifiedAlert" ? true : false;
+        this.dialog.show(AlertView, this.submitContext)
+            .then(response => {
+
+                if (!response.wasCancelled) {
+                    if (response.output.context == "Clearance") {
+                        // Data.SubmitPosition = 5;
+                    } else if (response.output.context == "Cashier") {
+                        Data.isVerified = true;
+                        Data.isNotVeridied = true;
+                        // Data.SubmitPosition = 4;
+                    } else {
+                        // Data.SubmitPosition = 6;
+                        Data.isVerified = false;
+                        Data.isNotVeridied = false;
+                        Data.Remark = response.output.Remark;
+                        Data.Reason = response.output.Remark;
+                    }
+                    console.log(Data)
+                    this.service.create(Data).then(result => {
+                        alert("Data berhasil dibuat");
+                        this.router.navigateToRoute('create', {}, { replace: true, trigger: true });
+                    });
+                }
+            });
     }
 }
