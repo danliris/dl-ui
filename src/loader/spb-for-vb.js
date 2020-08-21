f4fe699d5e7f6509e4e077379ff532cfd4679c32
leslieaula <cristiano.ulo@gmail.com>
2020-08-22 00:50:26 +0700

    Fix SPB Loader

--- src/loader/spb-for-vb.js
+++ src/loader/spb-for-vb.js
@@ -1,14 +1,14 @@
 import { Container } from 'aurelia-dependency-injection';
 import { Config } from "aurelia-api";
 
-const resource = 'unit-payment-orders/by-user';
+const resource = 'unit-payment-orders/by-po-ext-ids';
 
 module.exports = function (keyword, filter) {
 
     var config = Container.instance.get(Config);
-    var endpoint = config.getEndpoint("purchasing");
+    var endpoint = config.getEndpoint("purchasing-azure");
 
-    return endpoint.find(resource, { keyword: keyword, filter: JSON.stringify(filter), size: 10 })
+    return endpoint.find(resource, { keyword: keyword, size: 10, poExtIds: filter.poExtIds })
         .then(results => {
             return results.data.map(unitPaymentOrder => {
                 unitPaymentOrder.toString = function () {
