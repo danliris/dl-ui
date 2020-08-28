ce1c6177eabc4ca956ccf8a96f4010966f227a98
Leslie Aula <cristiano.ulo@gmail.com>
2020-08-28 10:36:19 +0700

    Fix Realization Expedition

--- src/modules/expedition/vb-realization-expedition-report/index.js
+++ src/modules/expedition/vb-realization-expedition-report/index.js
@@ -1,7 +1,7 @@
 export class Index {
     configureRouter(config, router) {
         config.map([
-            { route: ['', 'list'], moduleId: './list', name: 'list', nav: true, title: 'Laporan Kwitansi' },
+            { route: ['', 'list'], moduleId: './list', name: 'list', nav: true, title: 'Laporan Ekspedisi' },
         ]);
 
         this.router = router;
