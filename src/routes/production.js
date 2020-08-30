da95d6f94c92ac6ecc67496afeb07b7c25e9a504
skydivine <17161989+skydivine@users.noreply.github.com>
2020-08-31 00:07:30 +0700

    Add Loss Event Category

--- src/routes/production.js
+++ src/routes/production.js
@@ -158,6 +158,19 @@ module.exports = [
         }
     },
     {
+        route: 'production/finishing-printing/loss-event-category',
+        name: 'losses-event-category',
+        moduleId: './modules/production/finishing-printing/loss-event-category/index',
+        nav: true,
+        title: 'Master Kategori Losses Event',
+        auth: true,
+        settings: {
+            group: "production",
+            permission: { "C9": 1, "F1": 1, "F2": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
+    },
+    {
         route: 'production/finishing-printing/kanban',
         name: 'kanban',
         moduleId: './modules/production/finishing-printing/kanban/index',
