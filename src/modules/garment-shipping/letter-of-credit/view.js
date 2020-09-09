ff6dd52655eb98878908811c70f11f1a5fd14019
kuswandanu <kuswandanu@gmail.com>
2020-09-09 16:15:05 +0700

    [letter-of-credit] add amend and invoice

--- src/modules/garment-shipping/letter-of-credit/view.js
+++ src/modules/garment-shipping/letter-of-credit/view.js
@@ -1,6 +1,8 @@
 import { inject, Lazy } from 'aurelia-framework';
 import { Router } from 'aurelia-router';
 import { Service } from './service';
+import moment from 'moment';
+import numeral from 'numeral';
 
 @inject(Router, Service)
 export class View {
@@ -14,6 +16,31 @@ export class View {
         var id = params.id;
         this.data = await this.service.getById(id);
         this.selectedPackingList = this.data.invoiceNo;
+
+        const amendLC = await this.service.searchAmendLC({ filter: JSON.stringify({ documentCreditNo: this.data.documentCreditNo }) });
+        this.amendLCData = [];
+        for (const amend of amendLC.data) {
+            this.amendLCData.push({
+                amendNumber: amend.amendNumber,
+                amount: numeral(amend.amount).format("0,000.00"),
+            });
+        }
+        this.finalAmount = this.data.totalAmount + amendLC.data.reduce((acc, cur) => acc += cur.amount, 0);
+
+        const packingList = await this.service.searchPackingList({ filter: JSON.stringify({ lcNo: this.data.documentCreditNo, isUsed: true }) });
+        if (packingList.data.length > 0) {
+            const invoice = await this.service.searchInvoice({ filter: JSON.stringify({ lcNo: this.data.documentCreditNo }) });
+            this.invoiceData = [];
+            for (const pl of packingList.data) {
+                const inv = invoice.data.find(i => i.packingListId == pl.id);
+                this.invoiceData.push({
+                    invoiceNo: pl.invoiceNo,
+                    amountToBePaid: numeral(inv.amountToBePaid).format("0,000.00"),
+                    truckingDate: moment(pl.truckingDate).format("DD MMM YYYY")
+                });
+            }
+            this.totalInvoice = invoice.data.reduce((acc, cur) => acc += cur.amountToBePaid, 0);
+        }
     }
 
     cancelCallback(event) {
