73fc8da20a5a0afb149fab79ef786e2d2666786f
achensetyawati <setyawati@moonlay.com>
2020-09-15 16:22:48 +0700

    update shipping instruction and cover letter

--- src/modules/garment-shipping/shipping-instruction/data-form.js
+++ src/modules/garment-shipping/shipping-instruction/data-form.js
@@ -53,6 +53,9 @@ export class DataForm {
         this.error = context.error;
 
         if(this.data.id){
+            if(this.data.forwarder){
+                this.data.forwarder.attn=this.data.attn;
+            }
             this.selectedForwarder=this.data.forwarder;
             this.selectedInvoice={
                 invoiceNo: this.data.invoiceNo
@@ -70,12 +73,13 @@ export class DataForm {
                 id: newValue.Id || newValue.id,
                 code:newValue.Code || newValue.code,
                 name:newValue.Name || newValue.name,
-                attn: newValue.Attention || newValue.attn,
+                //attn: newValue.Attention || newValue.attn,
                 address: newValue.Address || newValue.address,
                 phone: newValue.PhoneNumber ||newValue.phone,
                 fax: newValue.FaxNumber || newValue.fax,   
             };                           
             this.data.fax=newValue.FaxNumber;
+            this.data.attn=newValue.Attention || newValue.attn;
         }  
         // _attnName = newValue.attn;   
         // this.data.attn = _attnName;
@@ -92,6 +96,7 @@ export class DataForm {
             this.data.shippingStaffName=newValue.shippingStaff;
             this.data.shippingStaffId=newValue.shippingStaffId;
             this.data.buyerAgentAddress=newValue.consigneeAddress;
+            
             this.service.searchPackingList({filter : JSON.stringify({ InvoiceNo: this.data.invoiceNo })})
             .then(result=>{
                 var pl= result.data[0];
