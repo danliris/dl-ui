67ca1666120591f9d5e22ec316d2e709bca00b07
Muhamad Fajri <hurifajri@gmail.com>
2020-09-24 13:22:43 +0700

    Enhancement  Realisasi VB Non PO Inklaring (UI)

--- src/modules/purchasing/realization-vb-non-po/list.js
+++ src/modules/purchasing/realization-vb-non-po/list.js
@@ -1,45 +1,51 @@
-import { inject } from 'aurelia-framework';
+import { inject } from "aurelia-framework";
 import { Service } from "./service";
-import { Router } from 'aurelia-router';
-import moment from 'moment';
+import { Router } from "aurelia-router";
+import moment from "moment";
 
 @inject(Router, Service)
 export class List {
   dataToBePosted = [];
 
   rowFormatter(data, index) {
-    if (data.isPosted)
-      return { classes: "success" }
-    else
-      return {}
+    if (data.isPosted) return { classes: "success" };
+    else return {};
   }
 
-  context = ["Detail", "Cetak Bukti Realisasi"]
+  context = ["Detail", "Cetak Bukti Realisasi"];
 
   columns = [
     { field: "DocumentNo", title: "No. Realisasi VB" },
     { field: "VBRequestDocumentNo", title: "No. Permohonan VB" },
     {
-      field: "Date", title: "Tanggal Realisasi", formatter: function (value, data, index) {
+      field: "Date",
+      title: "Tanggal Realisasi",
+      formatter: function (value, data, index) {
         return moment(value).format("DD MMM YYYY");
-      }
+      },
     },
     {
-      field: "VBRequestDocumentRealizationEstimationDate", title: "Tanggal Estimasi", formatter: function (value, data, index) {
+      field: "VBRequestDocumentRealizationEstimationDate",
+      title: "Tanggal Estimasi",
+      formatter: function (value, data, index) {
         if (data.VBRequestDocumentNo)
           return moment(value).format("DD MMM YYYY");
-        else
-          return "-";
-      }
+        else return "-";
+      },
     },
     // { field: "UnitLoad", title: "Beban Unit" },
     { field: "CreatedBy", title: "Dibuat oleh" },
     {
-      field: "Position", title: "Status Verifikasi",
+      field: "Position",
+      title: "Status Verifikasi",
       formatter: function (value, row, index) {
-        return value > 3 && value != 6 ? "Sudah" : value == 6 ? "Ditolak" : "Belum";
-      }
-    }
+        return value > 3 && value != 6
+          ? "Sudah"
+          : value == 6
+          ? "Ditolak"
+          : "Belum";
+      },
+    },
   ];
 
   async activate(params) {
@@ -49,31 +55,51 @@ export class List {
   loader = (info) => {
     let order = {};
 
-    if (info.sort)
-      order[info.sort] = info.order;
-    else
-      order["LastModifiedUtc"] = "desc";
+    if (info.sort) order[info.sort] = info.order;
+    else order["LastModifiedUtc"] = "desc";
 
     let arg = {
       page: parseInt(info.offset / info.limit, 10) + 1,
       size: info.limit,
       keyword: info.search,
-      order: order
+      order: order,
       // filter: JSON.stringify({ VBRealizeCategory: "NONPO" }),
     };
 
-    return this.service.search(arg)
-      .then(result => {
-        return {
-          total: result.info.total,
-          data: result.data
-        }
-      });
-  }
+    return this.service.search(arg).then((result) => {
+      return {
+        total: result.info.total,
+        data: result.data,
+      };
+    });
+  };
 
   constructor(router, service) {
     this.service = service;
     this.router = router;
+
+    this.initTab();
+  }
+
+  initTab() {
+    this.tabs = ["Realisasi VB Non PO", "Realisasi VB Inklaring Non PO"];
+
+    this.activeTab = this.tabs[0];
+  }
+
+  changeTab(tab) {
+    if (tab !== this.activeTab) {
+      this.activeTab = tab;
+      // this.tableList.refresh();
+    }
+  }
+
+  create(activeTab) {
+    if (activeTab === "Realisasi VB Non PO") {
+      this.router.navigateToRoute("create");
+    } else {
+      this.router.navigateToRoute("create-inklaring");
+    }
   }
 
   contextClickCallback(event) {
@@ -81,7 +107,10 @@ export class List {
     var data = arg.data;
     switch (arg.name) {
       case "Detail":
-        this.router.navigateToRoute('view', { id: data.Id, search: this.ressearch });
+        this.router.navigateToRoute("view", {
+          id: data.Id,
+          search: this.ressearch,
+        });
         break;
       case "Cetak Bukti Realisasi":
         this.service.getSalesReceiptPdfById(data.Id);
@@ -100,15 +129,14 @@ export class List {
 
   posting() {
     if (this.dataToBePosted.length > 0) {
-      this.service.post(this.dataToBePosted).then(result => {
-        this.table.refresh();
-      }).catch(e => {
-        this.error = e;
-      })
+      this.service
+        .post(this.dataToBePosted)
+        .then((result) => {
+          this.table.refresh();
+        })
+        .catch((e) => {
+          this.error = e;
+        });
     }
-  }
-
-  create() {
-    this.router.navigateToRoute('create');
   }
 }
