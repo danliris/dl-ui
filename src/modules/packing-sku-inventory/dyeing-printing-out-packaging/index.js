52b11429838fca36269b29ccb15f2d88418c857b
skydivine <17161989+skydivine@users.noreply.github.com>
2020-09-27 22:31:01 +0700

    Enhance Form

--- src/modules/packing-sku-inventory/dyeing-printing-out-packaging/index.js
+++ src/modules/packing-sku-inventory/dyeing-printing-out-packaging/index.js
@@ -4,7 +4,7 @@ export class Index {
             { route: ['', 'list'], moduleId: './list', name: 'list', nav: false, title: 'List: Packing - Dyeing & Printing' },
             { route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create: Packing - Dyeing & Printing' },
             { route: 'view/:id', moduleId: './view', name: 'view', nav: false, title: 'View:  Packing - Dyeing & Printing' },
-            { route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit: Packing - Dyeing & Printing' },
+            // { route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit: Packing - Dyeing & Printing' },
             { route: 'excel', moduleId: './excel', name: 'excel', nav: false, title: 'Excel: Download Pencatatan Keluar Packing - Dyeing/Printing' }
         ]);
 
