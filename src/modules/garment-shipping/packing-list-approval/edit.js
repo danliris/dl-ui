954e4165c425313306426689a7569f85cb68598a
kuswandanu <kuswandanu@gmail.com>
2020-10-14 16:50:16 +0700

    [fix] Packing List - Price FOB existing Value

--- src/modules/garment-shipping/packing-list-approval/edit.js
+++ src/modules/garment-shipping/packing-list-approval/edit.js
@@ -33,7 +33,6 @@ export class Edit {
             for (const item of this.data.items) {
                 item.buyerAgent = this.data.buyerAgent;
                 item.section = this.data.section;
-                item.priceFOB = item.priceRO;
             }
         }
 
