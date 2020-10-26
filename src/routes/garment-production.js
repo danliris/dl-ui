e55ea293984bf18738b985b95fbe49f9eb322870
kuswandanu <kuswandanu@gmail.com>
2020-10-26 08:21:34 +0700

    Draft Packing List

--- src/routes/garment-production.js
+++ src/routes/garment-production.js
@@ -900,4 +900,17 @@ module.exports = [
             iconClass: "fa fa-dashboard"
         }
     },
+    {
+        route: "garment-production/packing-list-draft",
+        name: "garment-production/packing-list-draft",
+        moduleId: "modules/garment-production/packing-list-draft/index",
+        nav: true,
+        title: "Draft Packing List",
+        auth: true,
+        settings: {
+            group: "g-production",
+            permission: { "C9": 1 },
+            iconClass: "fa fa-dashboard"
+        }
+    },
 ];
