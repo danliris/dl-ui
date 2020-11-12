4405e9790de3a11feb074c388c2ad00667c7f849
Muhamad Fajri <hurifajri@gmail.com>
2020-11-12 21:09:51 +0700

    Integrating accounting category in category

--- src/modules/master/category/list.js
+++ src/modules/master/category/list.js
@@ -10,9 +10,10 @@ export class List {
   columns = [
     { field: "code", title: "Kode" },
     { field: "name", title: "Nama" },
-    { field: "Category.Pembukuan", title: "Kategori Pembukuan" },
+    { field: "AccountingCategory.Name", title: "Kategori Pembukuan" },
   ];
 
+  /*
   loader = (info) => {
     var order = {};
     if (info.sort) order[info.sort] = info.order;
@@ -30,6 +31,44 @@ export class List {
         total: result.info.total,
         data: result.data,
       };
+    });
+  };
+  */
+
+  loader = (info) => {
+    var order = {};
+    if (info.sort) order[info.sort] = info.order;
+
+    var arg = {
+      page: parseInt(info.offset / info.limit, 10) + 1,
+      size: info.limit,
+      keyword: info.search,
+      select: ["code", "name"],
+      order: order,
+    };
+
+    return this.service.search(arg).then((result) => {
+      var resultPromise = [];
+      if (result && result.data && result.data.length > 0) {
+        resultPromise = result.data.map((datum) => {
+          if (datum.AccountingCategoryId !== 0) {
+            return this.service
+              .getAccountingCategory(datum.AccountingCategoryId)
+              .then((ac) => {
+                datum.AccountingCategory = ac;
+                return Promise.resolve(datum);
+              });
+          } else {
+            return Promise.resolve(datum);
+          }
+        });
+      }
+      return Promise.all(resultPromise).then((newResult) => {
+        return {
+          total: result.info.total,
+          data: newResult,
+        };
+      });
     });
   };
 
