6365f6e298c41d59feed8e209e5be269330002ea
Muhamad Fajri <hurifajri@gmail.com>
2020-11-20 09:57:31 +0700

    Format date

--- src/modules/expedition/reports/detail-credit-balance-report/list.js
+++ src/modules/expedition/reports/detail-credit-balance-report/list.js
@@ -36,14 +36,26 @@ export class List {
   };
 
   columns = [
-    { field: "ReceiptDate", title: "Tanggal SPB" },
+    {
+      field: "ReceiptDate",
+      title: "Tanggal SPB",
+      formatter: function (value, data, index) {
+        return moment.utc(value).local().format("DD MMM YYYY");
+      },
+    },
     { field: "UPONo", title: "No SPB" },
     { field: "URNNo", title: "No BP" },
     { field: "InvoiceNo", title: "No Invoice" },
     { field: "SupplierName", title: "Supplier" },
     { field: "CategoryName", title: "Kategori" },
     { field: "AccountingUnitName", title: "Unit" },
-    { field: "DueDate", title: "Jatuh Tempo" },
+    {
+      field: "DueDate",
+      title: "Jatuh Tempo",
+      formatter: function (value, data, index) {
+        return moment.utc(value).local().format("DD MMM YYYY");
+      },
+    },
     { field: "CurrencyCode", title: "Currency" },
     { field: "TotalSaldo", title: "Saldo" },
   ];
