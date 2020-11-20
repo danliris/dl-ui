3acf846b63dd1d4ef2291277eebd39e908fb1ca2
Arie Nandjar <52964987+arijulianton20@users.noreply.github.com>
2020-11-21 01:08:14 +0700

    [UPDATE] Fix UI List Table UnpaidDispositionReportDetail

--- src/modules/purchasing/unpaid-disposition-report-detail/list.js
+++ src/modules/purchasing/unpaid-disposition-report-detail/list.js
@@ -3,13 +3,14 @@ import { Service } from "./service";
 import { Router } from 'aurelia-router';
 import moment from 'moment';
 import numeral from 'numeral';
+import { data } from 'jquery';
 
 var CategoryLoader = require('../../../loader/category-loader');
 var DivisionLoader = require('../../../loader/division-loader');
-var UnitLoader = require('../../../loader/unit-loader');
-var AccountingCategoryLoader = require('../../../loader/accounting-category-loader');
+// var UnitLoader = require('../../../loader/unit-loader');
+// var AccountingCategoryLoader = require('../../../loader/accounting-category-loader');
 var AccountingUnitLoader = require('../../../loader/accounting-unit-loader');
-var UnitReceiptNoteLoader = require('../../../loader/unit-receipt-note-basic-loader');
+// var UnitReceiptNoteLoader = require('../../../loader/unit-receipt-note-basic-loader');
 
 @inject(Router, Service)
 export class List {
@@ -29,37 +30,37 @@ export class List {
         showToggle: false,
     }
 
-    columns = [{
-            field: 'DispositionDate',
-            title: 'Tgl Disposisi',
-            formatter: function(value, data, index) {
-                return moment(value).format("DD MMM YYYY");
-            }
-        },
-        { field: 'DispositionNo', title: 'No Disposisi' },
-        { field: 'URNNo', title: 'No SPB' },
-        { field: 'UPONo', title: 'No BP' },
-        { field: 'InvoiceNo', title: 'No Invoice' },
-        { field: 'SupplierName', title: 'Supplier' },
-        { field: 'CategoryName', title: 'Kategori' },
-        { field: 'AccountingUnitName', title: 'Unit' },
-        {
-            field: 'PaymentDueDate',
-            title: 'Jatuh Tempo',
-            formatter: function(value, data, index) {
-                return moment(value).format("DD MMM YYYY");
-            }
-        },
-        { field: 'CurrencyCode', title: 'Currency' },
-        {
-            field: 'Total',
-            title: 'Saldo',
-            formatter: function(value, data, index) {
-                return numeral(value).format('0,000.00');
-            },
-            align: "right"
-        }
-    ];
+    // columns = [{
+    //         field: 'DispositionDate',
+    //         title: 'Tgl Disposisi',
+    //         formatter: function(value, data, index) {
+    //             return moment(value).format("DD MMM YYYY");
+    //         }
+    //     },
+    //     { field: 'DispositionNo', title: 'No Disposisi' },
+    //     { field: 'URNNo', title: 'No SPB' },
+    //     { field: 'UPONo', title: 'No BP' },
+    //     { field: 'InvoiceNo', title: 'No Invoice' },
+    //     { field: 'SupplierName', title: 'Supplier' },
+    //     { field: 'CategoryName', title: 'Kategori' },
+    //     { field: 'AccountingUnitName', title: 'Unit' },
+    //     {
+    //         field: 'PaymentDueDate',
+    //         title: 'Jatuh Tempo',
+    //         formatter: function(value, data, index) {
+    //             return moment(value).format("DD MMM YYYY");
+    //         }
+    //     },
+    //     { field: 'CurrencyCode', title: 'Currency' },
+    //     {
+    //         field: 'Total',
+    //         title: 'Saldo',
+    //         formatter: function(value, data, index) {
+    //             return numeral(value).format('0,000.00');
+    //         },
+    //         align: "right"
+    //     }
+    // ];
 
     columnsUnitValas = [
         { header: 'Unit', value: 'Unit' },
@@ -75,6 +76,9 @@ export class List {
     constructor(router, service) {
         this.service = service;
         this.router = router;
+
+        this.data = [];
+        this.isEmpty = true;
     }
 
     titles = [
@@ -96,6 +100,8 @@ export class List {
         if (title !== this.activeTitle) {
             this.activeTitle = title;
             this.unitSummary = [];
+            this.data = [];
+            this.isEmpty = true;
 
             switch (title) {
                 case "Lokal":
@@ -116,24 +122,104 @@ export class List {
                     break;
             }
 
-            this.tableList.refresh();
+            // this.tableList.refresh();
         }
     }
 
-    search() {
-        this.isSearch = true;
-        this.tableList.refresh();
+    async search() {
+        // this.isSearch = true;
+        // this.tableList.refresh();
+
+        let arg = {
+            categoryId: this.category ? this.category.Id : 0,
+            accountingUnitId: this.accountingUnit ? this.accountingUnit.Id : 0,
+            divisionId: this.division ? this.division.Id : 0,
+            dateTo: this.dateTo ? moment(this.dateTo).format("YYYY-MM-DD") : "",
+            isValas: this.isValas,
+            isImport: this.isImport
+        };
+
+        this.unitSummary = this.unitSummary.splice(0, this.unitSummary.length);
+
+        this.data = await this.service.search(arg)
+            .then(result => {
+                if (result && result.UnitSummaries.length > 0)
+                    this.unitSummary = result.UnitSummaries.map(
+                        (item) => ({
+                            Unit: item.Unit,
+                            CurrencyCode: item.CurrencyCode,
+                            Total: numeral(item.SubTotal).format("0,000.00"),
+                        })
+                    );
+
+                let viewDataSet = [];
+                let categoryDataSet = [];
+                let key = 0;
+                let dataCount = result.Reports.length;
+                for (let data of result.Reports) {
+                    viewDataSet.push({
+                        DispositionDate: data.DispositionDate ? moment(this.dateTo).format("YYYY-MM-DD") : "",
+                        DispositionNo: data.DispositionNo,
+                        URNNo: data.URNNo,
+                        UPONo: data.UPONo,
+                        InvoiceNo: data.InvoiceNo,
+                        SupplierName: data.SupplierName,
+                        CategoryName: data.CategoryName,
+                        AccountingUnitName: data.AccountingUnitName,
+                        PaymentDueDate: data.PaymentDueDate ? moment(this.dateTo).format("YYYY-MM-DD") : "",
+                        CurrencyCode: data.CurrencyCode,
+                        Total: numeral(data.Total).format('0,000.00')
+                    });
+
+                    let indexCategoryDataSet = categoryDataSet.findIndex(x => x.CurrencyCode === data.CurrencyCode);
+                    let nextData = result.Reports.slice(key + 1, key + 2)[0];
+
+                    if ((nextData && nextData.CategoryId != data.CategoryId) || key == dataCount - 1) {
+                        if (indexCategoryDataSet >= 0)
+                            categoryDataSet[indexCategoryDataSet].Total += data.Total;
+                        else
+                            categoryDataSet.push({
+                                TotalCategoryByCurrency: "JUMLAH",
+                                CurrencyCode: data.CurrencyCode,
+                                Total: data.Total
+                            });
+
+                        for (let dataTobePush of categoryDataSet) {
+                            dataTobePush.Total = numeral(dataTobePush.Total).format('0,000.00');
+
+                            viewDataSet.push(dataTobePush);
+                        }
+
+                        categoryDataSet = [];
+                    } else {
+                        if (indexCategoryDataSet >= 0)
+                            categoryDataSet[indexCategoryDataSet].Total += data.Total;
+                        else
+                            categoryDataSet.push({
+                                TotalCategoryByCurrency: "JUMLAH",
+                                CurrencyCode: data.CurrencyCode,
+                                Total: data.Total
+                            });
+                    }
+                    key++;
+                }
+
+                this.isEmpty = dataCount - 1 > 0 ? false : true;
+                return viewDataSet;
+            });
+        console.log(this.data);
     }
 
     reset() {
-        console.log("reset");
+        this.isEmpty = true;
         this.division = null;
         this.accountingCategory = null;
         this.accountingUnit = null;
         this.dateTo = null;
         this.isSearch = false;
-        this.unitSummary = {};
-        this.tableList.refresh();
+        this.unitSummary = [];
+        this.data = [];
+        // this.tableList.refresh();
     }
 
     getExcel() {
@@ -187,6 +273,7 @@ export class List {
     // get accountingCategoryLoader() {
     //     return AccountingCategoryLoader;
     // }
+
     // accountingCategoryView = (AccountingCategoryLoader) => {
     //     return `${AccountingCategoryLoader.Code} - ${AccountingCategoryLoader.Name}`
     // }
@@ -198,40 +285,48 @@ export class List {
         return `${accountingUnit.Code} - ${accountingUnit.Name}`
     }
 
-    loader = (info) => {
-        let order = {};
+    // loader = (info) => {
+    //     let order = {};
 
-        let arg = {
-            categoryId: this.category ? this.category.Id : 0,
-            accountingUnitId: this.accountingUnit ? this.accountingUnit.Id : 0,
-            divisionId: this.division ? this.division.Id : 0,
-            dateTo: this.dateTo ? moment(this.dateTo).format("YYYY-MM-DD") : "",
-            isValas: this.isValas,
-            isImport: this.isImport
-        };
+    //     let arg = {
+    //         categoryId: this.category ? this.category.Id : 0,
+    //         accountingUnitId: this.accountingUnit ? this.accountingUnit.Id : 0,
+    //         divisionId: this.division ? this.division.Id : 0,
+    //         dateTo: this.dateTo ? moment(this.dateTo).format("YYYY-MM-DD") : "",
+    //         isValas: this.isValas,
+    //         isImport: this.isImport
+    //     };
 
-        this.unitSummary = this.unitSummary.splice(0, this.unitSummary.length);
+    //     this.unitSummary = this.unitSummary.splice(0, this.unitSummary.length);
 
-        if (this.isSearch)
-            return this.service.search(arg)
-                .then(result => {
-                    if (result && result.UnitSummaries.length > 0)
-                        this.unitSummary = result.UnitSummaries.map(
-                            (item) => ({
-                                Unit: item.Unit,
-                                CurrencyCode: item.CurrencyCode,
-                                Total: numeral(item.SubTotal).format("0,000.00"),
-                            })
-                        );
-                    return {
-                        total: result.Reports.length,
-                        data: result.Reports
-                    }
-                });
-        else
-            return {
-                total: 0,
-                data: []
-            }
+    //     if (this.isSearch)
+    //         return this.service.search(arg)
+    //             .then(result => {
+    //                     if (result && result.UnitSummaries.length > 0) {
+    //                         this.unitSummary = result.UnitSummaries.map(
+    //                             (item) => ({
+    //                                 Unit: item.Unit,
+    //                                 CurrencyCode: item.CurrencyCode,
+    //                                 Total: numeral(item.SubTotal).format("0,000.00"),
+    //                             })
+    //                         );
+
+    //                         return {
+    //                             total: result.Reports.length,
+    //                             data: result.Reports
+    //                         }
+    //                     });
+    //                 else
+    //                     return {
+    //                         total: 0,
+    //                         data: []
+    //                     }
+    //             }
+    // }
+}
+
+export class KeysValueConverter {
+    toView(obj) {
+        return Reflect.ownKeys(obj);
     }
 }
