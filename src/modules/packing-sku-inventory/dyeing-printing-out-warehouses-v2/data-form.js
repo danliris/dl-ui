02c8a5971214b9180be1c0f312a7b6f0b4cbd10a
putrapradesa <49633721+putrapradesa@users.noreply.github.com>
2020-12-07 15:29:02 +0700

    Update dyeing-printing-out-packaging

--- src/modules/packing-sku-inventory/dyeing-printing-out-warehouses-v2/data-form.js
+++ src/modules/packing-sku-inventory/dyeing-printing-out-warehouses-v2/data-form.js
@@ -78,11 +78,9 @@ export class DataForm {
   async bind(context) {
     this.context = context;
     this.data = this.context.data;
-
     this.data.area = "GUDANG JADI";
-
+    //console.log(this.error);
     this.error = this.context.error;
-
     this.cancelCallback = this.context.cancelCallback;
     this.deleteCallback = this.context.deleteCallback;
     this.editCallback = this.context.editCallback;
@@ -157,7 +155,10 @@ export class DataForm {
 
     }
   }
-
+  removeItems() {
+    // this.itemOptions.PackagingList = this.data.PackagingList;
+    this.bind();
+  }
   @bindable ItemsCollection;
   @bindable destinationArea;
   destinationAreaChanged(n, o) {
