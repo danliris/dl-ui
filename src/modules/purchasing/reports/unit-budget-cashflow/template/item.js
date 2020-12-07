3f9f7044ad6d186907adbcf57b89f4f078af7398
Muhamad Fajri <hurifajri@gmail.com>
2020-12-07 17:25:10 +0700

    Update unit budget cashflow

--- src/modules/purchasing/reports/unit-budget-cashflow/template/item.js
+++ src/modules/purchasing/reports/unit-budget-cashflow/template/item.js
@@ -1,15 +1,12 @@
 export class Item {
+  constructor() {}
 
-    constructor() {
-
+  activate(context) {
+    this.data = context.data;
+    if (context.context.options) {
+      this.readOnly = context.context.options.readOnly;
     }
 
-    activate(context) {
-        this.data = context.data
-        if (context.context.options) {
-            this.readOnly = context.context.options.readOnly;
-        }
-
-        console.log(context);
-    }
+    // console.log(context);
+  }
 }
