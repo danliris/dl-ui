20da80bd51c5b501858783ad38e0058c4cf07902
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2020-12-10 11:37:06 +0700

    Add Case if InvoiceNo is exist in GarmentExpenditureGoods

--- src/modules/garment-shipping/cover-letter/view.js
+++ src/modules/garment-shipping/cover-letter/view.js
@@ -1,19 +1,25 @@
 import { inject, Lazy } from 'aurelia-framework';
 import { Router } from 'aurelia-router';
-import { Service } from './service';
+import { Service, ProductionService } from './service';
 
-@inject(Router, Service)
+@inject(Router, Service, ProductionService)
 export class View {
 
-    constructor(router, service) {
+    constructor(router, service, productionService) {
         this.router = router;
         this.service = service;
+        this.productionService = productionService;
     }
 
     async activate(params) {
         var id = params.id;
         this.data = await this.service.getById(id);
         this.selectedPackingList = this.data.invoiceNo;
+        
+        let exist = await this.productionService.getExpenditureGoodByInvoiceNo({ size: 1, filter: JSON.stringify({ Invoice: this.data.invoiceNo }) });
+        if (exist.data.length > 0) {
+          this.deleteCallback = undefined;
+        }
     }
 
     cancelCallback(event) {
