fbc475bf8b7b37d6bc6486753aa4a80f1ee7eba5
Muhamad Fajri <hurifajri@gmail.com>
2020-12-11 21:23:04 +0700

    Update unit budget cashflow

--- src/modules/purchasing/reports/unit-budget-cashflow/service.js
+++ src/modules/purchasing/reports/unit-budget-cashflow/service.js
@@ -1,27 +1,26 @@
-import { inject, Lazy } from 'aurelia-framework';
-import { HttpClient } from 'aurelia-fetch-client';
-import { RestService } from '../../../../utils/rest-service';
+import { inject, Lazy } from "aurelia-framework";
+import { HttpClient } from "aurelia-fetch-client";
+import { RestService } from "../../../../utils/rest-service";
 
-const serviceUri = 'budget-cashflows';
+const serviceUri = "budget-cashflows";
 
 export class Service extends RestService {
+  constructor(http, aggregator, config, endpoint) {
+    super(http, aggregator, config, "purchasing-azure");
+  }
 
-    constructor(http, aggregator, config, endpoint) {
-        super(http, aggregator, config, "purchasing-azure");
-    }
+  getBestCase(query) {
+    let endpoint = `${serviceUri}/best-case`;
+    return super.list(endpoint, query);
+  }
 
-    getBestCase(query) {
-        let endpoint = `${serviceUri}/best-case`;
-        return super.list(endpoint, query);
-    }
+  getWorstCase(query) {
+    let endpoint = `${serviceUri}/worst-case`;
+    return super.list(endpoint, query);
+  }
 
-    getWorstCase(query) {
-        let endpoint = `${serviceUri}/worst-case`;
-        return super.list(endpoint, query);
-    }
-
-    upsertWorstCase(form) {
-        let endpoint = `${serviceUri}/worst-case`;
-        return super.put(endpoint, form);
-    }
+  upsertWorstCase(form) {
+    let endpoint = `${serviceUri}/worst-case`;
+    return super.put(endpoint, form);
+  }
 }
