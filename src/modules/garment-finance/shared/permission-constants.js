ec39b2fe5074a5dc6ea5f41abb4995b7921084f0
Muhamad Fajri <hurifajri@gmail.com>
2020-12-30 10:30:39 +0700

    Task #3446 - Penerimaan Dokumen Pembelian Garment Retur UI

--- src/modules/garment-finance/shared/permission-constants.js
+++ src/modules/garment-finance/shared/permission-constants.js
@@ -1,23 +1,31 @@
 export const VERIFICATION = {
-    key: 'VERIFICATION',
-    label: 'Verifikasi',
-    position: 3, // VERIFICATION ACCEPTED
-    positionAutocomplete: 2, // SEND TO VERIFICATION
-    code: 'B13',
+  key: "VERIFICATION",
+  label: "Verifikasi",
+  position: 3, // VERIFICATION ACCEPTED
+  positionAutocomplete: 2, // SEND TO VERIFICATION
+  code: "B13",
 };
 
 export const CASHIER = {
-    key: 'CASHIER',
-    label: 'Kasir',
-    position: 5, // CASHIER ACCEPTED
-    positionAutocomplete: 4, // SEND TO CASHIER
-    code: 'B11',
+  key: "CASHIER",
+  label: "Kasir",
+  position: 5, // CASHIER ACCEPTED
+  positionAutocomplete: 4, // SEND TO CASHIER
+  code: "B11",
 };
 
 export const ACCOUNTING = {
-    key: 'ACCOUNTING',
-    label: 'Akuntansi',
-    position: 8, // ACCOUNTING ACCEPTED
-    positionAutocomplete: 7, // SEND TO ACCOUNTING
-    code: 'B12',
+  key: "ACCOUNTING",
+  label: "Akuntansi",
+  position: 8, // ACCOUNTING ACCEPTED
+  positionAutocomplete: 7, // SEND TO ACCOUNTING
+  code: "B12",
+};
+
+export const RETUR = {
+  key: "RETUR",
+  label: "Retur",
+  position: 1,
+  positionAutocomplete: 6,
+  code: "PG",
 };
