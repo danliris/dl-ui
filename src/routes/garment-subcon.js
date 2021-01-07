6dd07b618684b047c28500f35bd944afe016cc35
achensetyawati <setyawati@moonlay.com>
2021-01-07 09:49:52 +0700

    Update garment-subcon.js

--- src/routes/garment-subcon.js
+++ src/routes/garment-subcon.js
@@ -11,5 +11,18 @@ module.exports = [
             permission: { "E": 1, "K": 1, "C9": 1,  "B9": 1, "C5": 1, "P1A": 1, "C2A": 1, "C2B": 1, "FP": 1, "P": 1, "FC": 1,  "PG": 1, "C1A": 1, "C1B": 1, "KK": 1, "B1": 1 },
             iconClass: 'fa fa-dashboard'
         }
+    },
+    {
+        route: '/garment-subcon/service-subcon-cutting',
+        name: 'service-subcon-cutting',
+        moduleId: './modules/garment-subcon/garment-service-subcon-cutting/index',
+        nav: true,
+        title: 'Subcon Jasa - Cutting',
+        auth: true,
+        settings: {
+            group: "g-subcon",
+            permission: { "E": 1, "K": 1, "C9": 1,  "B9": 1, "C5": 1, "P1A": 1, "C2A": 1, "C2B": 1, "FP": 1, "P": 1, "FC": 1,  "PG": 1, "C1A": 1, "C1B": 1, "KK": 1, "B1": 1 },
+            iconClass: 'fa fa-dashboard'
+        }
     }
 ]
