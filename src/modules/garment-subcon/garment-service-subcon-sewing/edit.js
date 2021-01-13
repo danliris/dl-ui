a4988feeaf42652d7ba9c0e719fcd1f54134b60f
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-01-13 14:33:54 +0700

    Update Rule GarmentServiceSubconSewing
    If Qty 0 then isSave = false

--- src/modules/garment-subcon/garment-service-subcon-sewing/edit.js
+++ src/modules/garment-subcon/garment-service-subcon-sewing/edit.js
@@ -21,31 +21,30 @@ export class View {
       this.data.BuyerView = this.data.Buyer.Code + ' - ' + this.data.Buyer.Name;
 
       let ssSewingItems = [];
-        let ssSewing = await this.service.searchComplete({ size: 100, filter: JSON.stringify({ RONo: this.data.RONo }) });
-        console.log(ssSewing)
-        if (ssSewing.data.length > 0) {
-          for (var ssS of ssSewing.data) {
-            for (var ssSItem of ssS.Items) {
-              var item = {};
-              item.sewingInItemId = ssSItem.SewingInItemId;
-              item.qty = ssSItem.Quantity;
-              if (ssSewingItems[ssSItem.SewingInItemId]) {
-                ssSewingItems[ssSItem.SewingInItemId].qty += ssSItem.Quantity;
-              }
-              else {
-                ssSewingItems[ssSItem.SewingInItemId] = item;
-              }
+      let ssSewing = await this.service.searchComplete({ size: 100, filter: JSON.stringify({ RONo: this.data.RONo }) });
+      if (ssSewing.data.length > 0) {
+        for (var ssS of ssSewing.data) {
+          for (var ssSItem of ssS.Items) {
+            var item = {};
+            item.sewingInItemId = ssSItem.SewingInItemId;
+            item.qty = ssSItem.Quantity;
+            if (ssSewingItems[ssSItem.SewingInItemId]) {
+              ssSewingItems[ssSItem.SewingInItemId].qty += ssSItem.Quantity;
+            }
+            else {
+              ssSewingItems[ssSItem.SewingInItemId] = item;
             }
           }
         }
+      }
 
       for (var a of this.data.Items) {
         var SewingIn = await this.service.GetSewingInById(a.SewingInId);
         var sewIn = SewingIn.Items.find(x => x.Id == a.SewingInItemId);
         if (sewIn) {
           var qtyOut = 0;
-          if(ssSewingItems[sewIn.Id]){
-            qtyOut+=ssSewingItems[sewIn.Id].qty;
+          if (ssSewingItems[sewIn.Id].sewingInItemId != a.SewingInItemId) {
+            qtyOut += ssSewingItems[sewIn.Id].qty;
           }
           a.SewingInDate = SewingIn.SewingInDate;
           a.SewingInQuantity = sewIn.RemainingQuantity - qtyOut;
@@ -71,6 +70,12 @@ export class View {
     if (this.data && this.data.IsDifferentSize) {
       if (this.data.Items) {
         for (var item of this.data.Items) {
+          if (item.Quantity > 0) {
+            item.IsSave = true;
+          }
+          else {
+            item.IsSave = false;
+          }
           if (item.IsSave) {
             item.TotalQuantity = 0;
             if (this.data.SewingInDate == null || this.data.SewingInDate < item.SewingInDate)
@@ -83,6 +88,12 @@ export class View {
     else if (this.data && !this.data.IsDifferentSize) {
       if (this.data.Items) {
         for (var item of this.data.Items) {
+          if (item.Quantity > 0) {
+            item.IsSave = true;
+          }
+          else {
+            item.IsSave = false;
+          }
           if (item.IsSave) {
             if (this.data.SewingInDate == null || this.data.SewingInDate < item.SewingInDate)
               this.data.SewingInDate = item.SewingInDate;
