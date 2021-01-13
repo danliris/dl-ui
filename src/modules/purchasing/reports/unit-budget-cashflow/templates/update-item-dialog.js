bb6dfc2e795ddf10962c5ee8a79e1f1490af9d64
Muhamad Fajri <hurifajri@gmail.com>
2021-01-13 17:24:52 +0700

    Update UI & functionalities budget cashflow unit

--- src/modules/purchasing/reports/unit-budget-cashflow/templates/update-item-dialog.js
+++ src/modules/purchasing/reports/unit-budget-cashflow/templates/update-item-dialog.js
@@ -1,10 +1,11 @@
 import { inject, useView } from "aurelia-framework";
 import { DialogController } from "aurelia-dialog";
 import { Service } from "../service";
+import moment from "moment";
 
 @inject(DialogController, Service)
-@useView("./add-item-dialog.html")
-export class AddItemDialog {
+@useView("./update-item-dialog.html")
+export class UpdateItemDialog {
   constructor(controller, service) {
     this.controller = controller;
     this.service = service;
@@ -18,20 +19,38 @@ export class AddItemDialog {
     };
   }
 
-  activate(data) {
-    console.log("add-item-dialog, activate(data)", data);
+  async activate(data) {
+    console.log("update-item-dialog, activate(data)", data);
     this.data = data;
     this.data.UnitId = data.Unit.Id;
     this.data.DivisionId = data.Unit.Division.Id;
-    this.data.Date = data.Date;
     this.data.CashflowSubCategoryId = data.Info.CashflowSubCategoryId;
-    this.data.Items = [];
     this.error = {};
+
+    let date = data.Date
+      ? moment(data.Date).format("YYYY-MM-DD")
+      : moment(new Date()).format("YYYY-MM-DD");
+    this.data.Date = date;
+    // this.data.Date = data.Date;
+
+    let args = {
+      unitId: this.data.UnitId,
+      subCategoryId: this.data.CashflowSubCategoryId,
+      date,
+    };
+
+    this.data.Items = await this.service.getItems(args).then((result) => {
+      result.data = result.data.map((item) => {
+        item.CurrencyId = item.Currency.Id;
+        return item;
+      });
+      return result.data;
+    });
   }
 
   save() {
     this.service
-      .create(this.data)
+      .update(this.data)
       .then(() => {
         // alert("Data berhasil dibuat");
         this.controller.ok(this.data);
