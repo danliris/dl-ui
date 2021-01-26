82066dbef82c41a3f01501691b99aebec00a9558
Muhamad Fajri <hurifajri@gmail.com>
2021-01-26 07:34:02 +0700

    Update division and all division

--- src/modules/purchasing/reports/division-all-budget-cashflow/service.js
+++ src/modules/purchasing/reports/division-all-budget-cashflow/service.js
@@ -2,31 +2,31 @@ import { inject, Lazy } from "aurelia-framework";
 import { HttpClient } from "aurelia-fetch-client";
 import { RestService } from "../../../../utils/rest-service";
 
-const serviceUri = "budget-cashflows";
+const serviceUri = "budget-cashflows/divisions";
 
 export class Service extends RestService {
   constructor(http, aggregator, config, endpoint) {
-    super(http, aggregator, config, "purchasing-azure");
+    super(http, aggregator, config, "finance");
   }
 
-  search() {
-    return fetch(
-      "http://localhost:9000/src/modules/purchasing/reports/division-all-budget-cashflow/dummy.json"
-    ).then((response) => response.json());
+  search(arg) {
+    let endpoint = `${serviceUri}`;
+    return super.list(endpoint, arg);
   }
 
-  // getDivision(query) {
-  //   let endpoint = `${serviceUri}/division`;
-  //   return super.list(endpoint, query);
-  // }
+  getXls(query) {
+    let endpoint = `${serviceUri}/download/xls?date=${query.date}`;
+    return super.getXls(endpoint);
+  }
 
-  // getXls(query) {
-  //   let endpoint = `${serviceUri}/division/xls?dueDate=${query.dueDate}`;
-  //   return super.getXls(endpoint);
-  // }
+  getPdf(query) {
+    let endpoint = `${serviceUri}/download/pdf?date=${query.date}`;
+    return super.getPdf(endpoint);
+  }
 
-  // getPdf(query) {
-  //   let endpoint = `${serviceUri}/division/pdf?dueDate=${query.dueDate}`;
-  //   return super.getPdf(endpoint);
+  // search() {
+  //   return fetch(
+  //     "http://localhost:9000/src/modules/purchasing/reports/division-all-budget-cashflow/dummy.json"
+  //   ).then((response) => response.json());
   // }
 }
