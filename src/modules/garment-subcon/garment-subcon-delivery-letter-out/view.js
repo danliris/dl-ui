58cf46af2d29c3196278d4854ce66ef0621443a7
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-02-08 16:52:03 +0700

    Add Feature Edit + Delete GarmentSubconDeliveryLetterOut

--- src/modules/garment-subcon/garment-subcon-delivery-letter-out/view.js
+++ src/modules/garment-subcon/garment-subcon-delivery-letter-out/view.js
@@ -30,23 +30,24 @@ export class View {
         this.router.navigateToRoute('list');
     }
 
-    // deleteCallback(event) {
-    //     if (confirm(`Hapus ${this.data.DLNo}?`))
-    //         this.service.delete(this.data)
-    //             .then(result => {
-    //                 this.cancelCallback();
-    //             })
-    //             .catch(e => {
-    //                 this.error = e;
-    //                 if (typeof (this.error) == "string") {
-    //                     alert(this.error);
-    //                 } else {
-    //                     alert("Missing Some Data");
-    //                 }
-    //             })
-    // }
-    // editCallback(event) {
-    //     this.router.navigateToRoute('edit', { id: this.data.Id });
-    // }
+    deleteCallback(event) {
+        if (confirm(`Hapus ${this.data.DLNo}?`))
+            this.service.delete(this.data)
+                .then(result => {
+                    this.cancelCallback();
+                })
+                .catch(e => {
+                    this.error = e;
+                    if (typeof (this.error) == "string") {
+                        alert(this.error);
+                    } else {
+                        alert("Missing Some Data");
+                    }
+                })
+    }
+    
+    editCallback(event) {
+        this.router.navigateToRoute('edit', { id: this.data.Id });
+    }
 
 }
