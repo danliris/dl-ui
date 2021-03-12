ccf78d3a14dbf1dd0e8b0ad861a241213c49aa17
ryan <ryan.santosa@moonlay.com>
2021-03-12 08:10:40 +0700

    add menu garment diposition pembayaran

---
+++ src/modules/garment-purchasing/purchasing-disposition/view.js
@@ -0,0 +1,88 @@
+import { inject, Lazy } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+import { Service } from './service';
+
+
+@inject(Router, Service)
+export class View {
+  constructor(router, service) {
+    this.router = router;
+    this.service = service;
+  }
+    hasCancel = true;
+    hasEdit = true;
+    hasDelete = true;
+  async activate(params) {
+    var id = params.id;
+    this.data = await this.service.getById(id);
+    
+    if(this.data.Currency){
+        this.selectedCurrency=this.data.Currency;
+    }
+
+    if(this.data.Supplier){
+        this.selectedSupplier=this.data.Supplier;
+    }
+
+    if(this.data.Division){
+        this.selectedDivision=this.data.Division;
+    }
+
+    if(this.data.Category){
+        this.selectedCategory=this.data.Category;
+    }
+    if(this.data.Position !=1){
+      this.hasDelete=false;
+    }
+    if(this.data.Position !=1 && this.data.Position !=6){
+      this.hasEdit=false;
+    }
+    if(this.data.SupplierId){
+        this.selectedSupplier = {
+            _id : this.data.SupplierId,
+            name : this.data.SupplierName,
+            code : this.data.SupplierCode,
+        }
+    }
+
+    // if(this.data.CurrencyId){
+    //     this.selectedCurrency = {
+    //         Id : this.data.CurrencyId,
+    //         Code :this.data.CurrencyCode,
+    //         Rate : this.data.CurrencyRate,
+    //         Name : this.data.CurrencyName
+    //     }
+    // }
+    // this.data.Items
+    console.log("view",this.data);
+  }
+
+  // cancelCallback(event) {
+  //   this.router.navigateToRoute('list');
+  // }
+
+  // editCallback(event) {
+  //   this.router.navigateToRoute('edit', { id: this.data.Id });
+  // }
+
+  // deleteCallback(event) {
+  //   this.service.delete(this.data)
+  //     .then(result => {
+  //       this.cancelCallback();
+  //     });
+  // }
+
+  cancel(event) {
+        this.router.navigateToRoute('list');
+    }
+
+    edit(event) {
+        this.router.navigateToRoute('edit', { id: this.data.Id });
+    }
+
+    delete(event) {
+        this.service.delete(this.data).then(result => {
+            this.cancel();
+        });
+    }
+}
