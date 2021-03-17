9154aa2adf981c1d4b12d92f89c6a5f457546e54
Leslie Aula <leslie.aula@moonlay.com>
2021-03-17 11:58:13 +0700

    Fix Verifikasi

--- src/modules/garment-finance/garment-disposition-verification/index.js
+++ src/modules/garment-finance/garment-disposition-verification/index.js
@@ -1,10 +1,10 @@
 export class Index {
     configureRouter(config, router) {
         config.map([
-            { route: ['', 'list'], moduleId: './list', name: 'list', nav: true, title: 'List: Verifikasi Nota Intern' },
-            { route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create: Verifikasi Nota Intern' },
-            { route: 'view/:id', moduleId: './view', name: 'view', nav: false, title: 'View: Verifikasi Nota Intern' },
-            { route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit: Verifikasi Nota Intern' }
+            { route: ['', 'list'], moduleId: './list', name: 'list', nav: true, title: 'List: Verifikasi Disposisi' },
+            { route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create: Verifikasi Disposisi' },
+            { route: 'view/:id', moduleId: './view', name: 'view', nav: false, title: 'View: Verifikasi Disposisi' },
+            { route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit: Verifikasi Disposisi' }
         ]);
 
         this.router = router;
--- src/modules/garment-finance/garment-disposition-verification/service.js
+++ src/modules/garment-finance/garment-disposition-verification/service.js
@@ -2,7 +2,7 @@ import { inject, Lazy } from 'aurelia-framework';
 import { HttpClient } from 'aurelia-fetch-client';
 import { RestService } from '../../../utils/rest-service';
 
-const uri = 'garment-purchasing-expeditions';
+const uri = 'garment-disposition-expeditions';
 
 export class Service extends RestService {
     constructor(http, aggregator, config, endpoint) {
