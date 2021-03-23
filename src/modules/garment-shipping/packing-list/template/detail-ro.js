3b102650fb6b1d9e48a6af95058b4611a7ddccbd
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-03-23 16:05:53 +0700

    Add Total Quantity In Detail Measurement

--- src/modules/garment-shipping/packing-list/template/detail-ro.js
+++ src/modules/garment-shipping/packing-list/template/detail-ro.js
@@ -163,25 +163,83 @@ export class Item {
     get totalQty() {
         let qty = 0;
         if (this.data.details) {
-            for (var detail of this.data.details) {
-                if (detail.cartonQuantity && detail.quantityPCS) {
+            const newDetails = this.data.details.map(d => {
+                return {
+                    carton1: d.carton1,
+                    carton2: d.carton2,
+                    cartonQuantity: d.cartonQuantity,
+                    quantityPCS: d.quantityPCS,
+                    index: d.index
+                };
+            }).filter((value, index, self) => self.findIndex(f => value.carton1 == f.carton1 && value.carton2 == f.carton2 && value.index == f.index) === index);
+            for (const detail of newDetails) {
+                const cartonExist = false;
+                const indexItem = this.context.context.options.header.items.indexOf(this.data);
+                if (indexItem > 0) {
+                    for (let i = 0; i < indexItem; i++) {
+                        const item = this.context.context.options.header.items[i];
+                        for (const prevDetail of item.details) {
+                            if (detail.carton1 == prevDetail.carton1 && detail.carton2 == prevDetail.carton2 && detail.index == prevDetail.index) {
+                                cartonExist = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (!cartonExist) {
                     qty += detail.cartonQuantity * detail.quantityPCS;
                 }
             }
+            return qty;
         }
-        return qty;
+        // for (var detail of this.data.details) {
+        //     if (detail.cartonQuantity && detail.quantityPCS) {
+        //         qty += detail.cartonQuantity * detail.quantityPCS;
+        //     }
+        // }
+        // return qty;
     }
 
     get totalCtn() {
         let qty = 0;
         if (this.data.details) {
-            for (var detail of this.data.details) {
-                if (detail.cartonQuantity) {
+            const newDetails = this.data.details.map(d => {
+                return {
+                    carton1: d.carton1,
+                    carton2: d.carton2,
+                    cartonQuantity: d.cartonQuantity,
+                    index: d.index
+                };
+            }).filter((value, index, self) => self.findIndex(f => value.carton1 == f.carton1 && value.carton2 == f.carton2 && value.index == f.index) === index);
+            for (const detail of newDetails) {
+                const cartonExist = false;
+                const indexItem = this.context.context.options.header.items.indexOf(this.data);
+                if (indexItem > 0) {
+                    for (let i = 0; i < indexItem; i++) {
+                        const item = this.context.context.options.header.items[i];
+                        for (const prevDetail of item.details) {
+                            if (detail.carton1 == prevDetail.carton1 && detail.carton2 == prevDetail.carton2 && detail.index == prevDetail.index) {
+                                cartonExist = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (!cartonExist) {
                     qty += detail.cartonQuantity;
                 }
             }
+            return qty;
         }
-        return qty;
+        
+        // if (this.data.details) {
+        //     for (var detail of this.data.details) {
+        //         if (detail.cartonQuantity) {
+        //             qty += detail.cartonQuantity;
+        //         }
+        //     }
+        // }
+        // return qty;
     }
 
     get amount() {
