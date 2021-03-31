13d1ce40abbb3f465cf98e20cc5cc801315dd0dc
Arie Nandjar <arijulianton20@gmail.com>
2021-03-31 17:58:28 +0700

    [UPDATE] Integrate List & Detail, Fix Create MemoGarmentPurchasing

--- src/modules/garment-finance/memo-garment-purchasing/view.js
+++ src/modules/garment-finance/memo-garment-purchasing/view.js
@@ -1,103 +1,64 @@
 import { inject, Lazy } from 'aurelia-framework';
 import { Router } from 'aurelia-router';
-import Service from './service';
-import { Dialog } from '../../../au-components/dialog/dialog'
-
+import { Service } from './service';
+import { Dialog } from '../../../au-components/dialog/dialog';
 
 @inject(Router, Service, Dialog)
 export class View {
-    controlOptions = {
-        label: {
-            length: 4,
-        },
-        control: {
-            length: 4,
-        },
-    };
-
-    formOptions = {
-        cancelText: 'Kembali',
-        editText: 'Ubah',
-        deleteText: 'Hapus'
-    };
-
     constructor(router, service, dialog) {
         this.router = router;
         this.service = service;
         this.dialog = dialog;
-
-        // this.collection = {
-        //     columns: ['No. Disposisi', 'Tanggal Disposisi', 'Tanggal Jatuh Tempo', 'Nomor Proforma/Invoice', 'Supplier', 'Kategori', 'Divisi', 'PPN', 'Jumlah dibayar ke Supplier', 'Mata Uang', ''],
-        // };
-        this.collection = {
-            columns: ['No. Disposisi', 'Tanggal Disposisi', 'Tanggal Jatuh Tempo', 'Nomor Proforma/Invoice', 'Supplier', 'Kategori', 'PPN', 'Total Pembayaran','Total Yang dibayar','Total Yang sudah dibayar', 'Mata Uang', ''],
-        };
     }
 
     async activate(params) {
-        var id = params.id;
+        let id = params.id;
         this.data = await this.service.getById(id);
-        let totalAmount =0;
-        for (var a of this.data.Items) {
-            a.SupplierName = this.data.Supplier.Name;
-            a.Currency = this.data.AccountBank.Currency.Code;
-            totalAmount += a.TotalPaidPayment;
-        }
-        this.IDR = false;
-        this.sameCurrency = false;
-        this.Amount = totalAmount;
 
-        // if (this.data.AccountBank.Currency.Code == "IDR") {
-        //     this.IDR = true;
-        //     if (this.data.CurrencyCode == "IDR") {
-        //         this.sameCurrency = true;
-        //     }
-        //     this.Amount = this.data.Amount * this.data.CurrencyRate;
+        console.log(this);
 
-        // }
-
-        // this.collection = {
-        //     columns: ['No. Disposisi', 'Tanggal Disposisi', 'Tanggal Jatuh Tempo', 'Nomor Proforma/Invoice', 'Supplier', 'Kategori', 'PPN', 'Total Pembayaran','Total Yang dibayar','Total Yang sudah dibayar', 'Mata Uang', ''],
-        // };
-
-        // if (!this.IDR || this.sameCurrency) {
-        //     this.collection = {
-        //         columns: ['No. Disposisi', 'Tanggal Disposisi', 'Tanggal Jatuh Tempo', 'Nomor Proforma/Invoice', 'Supplier', 'Kategori', 'Divisi', 'PPN', 'Jumlah dibayar ke Supplier', 'Mata Uang', ''],
-        //     };
-        // } else {
-        //     this.collection = {
-        //         columns: ['No. Disposisi', 'Tanggal Disposisi', 'Tanggal Jatuh Tempo', 'Nomor Proforma/Invoice', 'Supplier', 'Kategori', 'Divisi', 'PPN', 'Jumlah dibayar ke Supplier', 'Mata Uang', 'Jumlah dibayar ke Supplier(IDR)', 'Mata Uang', ''],
-        //     };
-        // }
-
-        if (this.data.IsPosted) {
-            this.editCallback = undefined;
-            this.deleteCallback = undefined;
+        if (this.data.Status == "POSTED") {
+            this.hasPosting = false;
+            this.editCallback = false;
+            this.deleteCallback = false;
+        } else {
+            this.hasPosting = true;
         }
 
-        this.collectionOptions = {
-            IDR: this.IDR,
-            rate: this.data.CurrencyRate,
-            SameCurrency: this.sameCurrency,
-            payToSupplierCurrency: this.data.CurrencyCode
-        };
+    }
+
+    list() {
+        this.router.navigateToRoute('list');
     }
 
     cancelCallback(event) {
-        this.router.navigateToRoute('list');
+        this.list();
     }
 
     editCallback(event) {
         this.router.navigateToRoute('edit', { id: this.data.Id });
     }
 
-    deleteCallback(event) {
-        this.dialog.prompt("Apakah anda yakin akan menghapus data?", "Hapus Data")
+    postingCallback(event) {
+        this.dialog.prompt('Transaksi yang sudah di POSTING tidak dapat diubah dan dihapus. Apakah anda yakin?', 'Posting Jurnal Transaksi')
             .then(response => {
                 if (response.ok) {
-                    this.service.delete(this.data).then(result => {
-                        this.cancelCallback();
-                    });
+                    this.service.posting(this.data)
+                        .then(result => {
+                            this.list();
+                        });
+                }
+            });
+    }
+
+    deleteCallback(event) {
+        this.dialog.prompt('Apakah anda yakin akan menghapus data ini?', 'Hapus Data Jurnal Transaksi')
+            .then(response => {
+                if (response.ok) {
+                    this.service.delete(this.data)
+                        .then(result => {
+                            this.list();
+                        });
                 }
             });
     }
