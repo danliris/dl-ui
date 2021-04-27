2b9cbc4ce62864c3af90a120006243ff5d82d63c
Leslie Aula <leslie.aula@moonlay.com>
2021-04-27 08:37:49 +0800

    Add Disposition View

--- src/modules/accounting/garment-purchasing-job-memo-details/template/create-view.js
+++ src/modules/accounting/garment-purchasing-job-memo-details/template/create-view.js
@@ -3,7 +3,7 @@ import { Service } from '../service';
 import moment from 'moment';
 var MemoGarmentPurchasingLoader = require('../../../../loader/memo-garment-purchasing-loader');
 
-@inject(Service,  BindingEngine)
+@inject(Service, BindingEngine)
 export class DataForm {
     @bindable readOnly;
     @bindable packingReadOnly = false;
@@ -34,20 +34,7 @@ export class DataForm {
         this.saveCallback = this.context.saveCallback;
     }
 
-    itemsColumns = [
-        { header: "No. Surat Jalan", value: "Product" },
-        { header: "No. Nota Intern", value: "Quantity" },
-        { header: "No. BP Besar", value: "AvailableQuantity" },
-        { header: "No. BP Kecil", value: "Weight" },
-        { header: "Kode Supplier", value: "WeightTotal" },
-        { header: "Keterangan", value: "Length" },
-        { header: "Mata Uang", value: "LengthTotal" },
-        { header: "Rate Bayar", value: "Remark" },
-        { header: "Rate Beli", value: "Notes" },
-        { header: "Saldo Akhir", value: "Notes" },
-        { header: "Jumlah", value: "Notes" },
-        { header: "Jumlah dalam Rupiah", value: "Notes" }
-    ]
+    itemsColumns = ["No. Disposisi"]
 
     get memoGarmentPurchasingLoader() {
         return MemoGarmentPurchasingLoader;
@@ -65,4 +52,4 @@ export class DataForm {
         }
         return '';
     }
-} 
+}
