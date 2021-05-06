6755e5579d7a0ad82b27ad724dc5d880d717078b
ANDRI PURNOMO <andrea.kinan@gmail.com>
2021-05-06 14:32:32 +0700

    Enhancement - G Shipping Module
    - Add Column QTY In PCS - Detail Omzet Garment / Unit
    - Add MultipleLine Text Description | Debit Note & Credit Note

--- src/modules/garment-shipping/monitoring/garment-detail-omzet-by-unit/monitoring.js
+++ src/modules/garment-shipping/monitoring/garment-detail-omzet-by-unit/monitoring.js
@@ -59,9 +59,10 @@ export class List {
                     var subTotalInv1 = {};
                     var subTotalInv2 = {};
                     var subTotalInv3 = {};
-
+                    var subTotalInv4 = {};
+                    
                     for (var data of result) {
-                        var Invoice = data.invoiceNo;
+                        var Invoice = data.urutan;
                          if (!dataByInvoice[Invoice]) dataByInvoice[Invoice] = [];                 
                              dataByInvoice[Invoice].push({   
 
@@ -73,6 +74,7 @@ export class List {
                              roNumber : data.roNumber,
                              quantity : data.quantity.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }),
                              uomUnit : data.uomUnit,
+                             quantityInPCS : data.quantityInPCS.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }),
                              amount : data.amount.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }),
                              amountIDR : data.amountIDR.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }),
                              truckingDate : moment(data.truckingDate).format("DD MMM YYYY")=="01 Jan 1970" ? "-" : moment(data.truckingDate).format("DD MMM YYYY"),                  
@@ -86,6 +88,11 @@ export class List {
                                subTotalInv1[Invoice] = 0;
                             } 
                                subTotalInv1[Invoice] += data.quantity;
+
+                            if (!subTotalInv4[Invoice]) {
+                                subTotalInv4[Invoice] = 0;
+                             } 
+                                subTotalInv4[Invoice] += data.quantityInPCS;
                              
                             if (!subTotalInv2[Invoice]) {
                                subTotalInv2[Invoice] = 0;
@@ -100,6 +107,7 @@ export class List {
       
                 var invoices = [];
                 this.QtyTotal = 0;               
+                this.QtyTotal1 = 0;               
                 this.AmountTtlUSD = 0;
                 this.AmountTtlIDR = 0;
                 
@@ -107,16 +115,19 @@ export class List {
                 for (var data in dataByInvoice) {
                     invoices.push({
                     data: dataByInvoice[data],
-                    invoice: dataByInvoice[data][0].invoiceNo,
+                    invoice: dataByInvoice[data][0].urutan,
                     subTotal: (subTotalInv1[data]).toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }),
                     subTotal1: (subTotalInv2[data]).toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }),                   
                     subTotal2: (subTotalInv3[data]).toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }),                                   
+                    subTotal3: (subTotalInv4[data]).toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }),                                                       
                 });
                     this.QtyTotal += subTotalInv1[data];   
+                    this.QtyTotal1 += subTotalInv4[data];   
                     this.AmountTtlUSD += subTotalInv2[data];   
                     this.AmountTtlIDR += subTotalInv3[data];                                       
                 }
                 this.QtyTotal = this.QtyTotal.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+                this.QtyTotal1 = this.QtyTotal1.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
                 this.AmountTtlUSD = this.AmountTtlUSD.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
                 this.AmountTtlIDR = this.AmountTtlIDR.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
                 this.invoices = invoices;
@@ -146,7 +157,8 @@ export class List {
         this.dateFrom = null;
         this.dateTo = null;
         this.unit = null;
-        this.QtyTotal = null;               
+        this.QtyTotal = null;     
+        this.QtyTotal1 = null;     
         this.AmountTtlUSD = null;
         this.AmountTtlIDR = null;
         this.invoices = [];
@@ -158,6 +170,5 @@ export class List {
 
         if (_startDate > _endDate)
             this.dateTo = e.srcElement.value;
-
     } 
 }
