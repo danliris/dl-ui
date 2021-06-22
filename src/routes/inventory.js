ae09ff8a995bced1088ce25c0b1217212a15f142
achensetyawati <setyawati@moonlay.com>
2021-06-22 17:42:32 +0700

    report recap

--- src/routes/inventory.js
+++ src/routes/inventory.js
@@ -668,4 +668,18 @@ module.exports = [
           iconClass: "fa fa-dashboard",
         },
     },
+    {
+        route: "inventory/garment/leftover-warehouse/balance-stock/reports/bookkeeping/recap-stock",
+        name: "inventory/garment/leftover-warehouse/balance-stock/reports/bookkeeping/recap-stock",
+        moduleId: "modules/inventory/garment-leftover-warehouse/reports/bookkeeping/recap-stock/index",
+        nav: true,
+        title: "Report Rekap Persediaan Gudang Sisa",
+        auth: true,
+        settings: {
+          group: "Inventory",
+          permission: { C9: 1, "PG":1 },
+          subGroup: "gudang sisa garment",
+          iconClass: "fa fa-dashboard",
+        },
+    },
 ]
