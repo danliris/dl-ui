5a6706c7dce651b18423e13f469f0b94bfba2afe
Leslie Aula <leslie.aula@moonlay.com>
2021-07-07 13:43:47 +0800

    fix conflict

--- src/modules/expedition/reports/daily-bank-transaction-in/list.js
+++ src/modules/expedition/reports/daily-bank-transaction-in/list.js
@@ -11,7 +11,7 @@ let ReferenceNoLoader = require('../../../../loader/daily-bank-transaction-docum
 @inject(Service)
 export class List {
     @bindable data = [];
-    @bindable isEmpty=true;
+    @bindable isEmpty = true;
     controlOptions = {
         label: {
             length: 4,
@@ -30,50 +30,48 @@ export class List {
         this.info = {};
         this.error = {};
         this.data = [];
-        this.isEmpty =false;
+        this.isEmpty = false;
 
     }
 
     async search() {
-        let filter={}
-        if(this.info.code)
+        let filter = {}
+        if (this.info.code)
             filter.referenceNo = this.info.code.Code;
-        if(this.info.bank){
+        if (this.info.bank) {
             filter.accountBankId = this.info.bank.Id;
-            filter.accountBankName =this.info.bank.AccountName+' - '+ this.info.bank.BankName+' - '+ this.info.bank.AccountName+' - '+this.info.bank.Currency.Code;            
+            filter.accountBankName = this.info.bank.toString();
         }
-        if(this.info.division){
+        if (this.info.division)
             filter.division = this.info.division.Name;
-        }
-        if(this.info.startDate != undefined)
-            filter.startDate = moment(this.info.startDate).format("YYYY-MM-DD");
-        if(this.info.endDate != undefined)
-            filter.endDate = moment(this.info.endDate).format("YYYY-MM-DD");
-        
-        
+        if (this.info.startDate)
+            filter.startDate = this.info.startDate;
+        if (this.info.endDate)
+            filter.endDate = this.info.endDate;
+
         filter.filter = JSON.stringify(this.type);
 
         this.data = await this.service.search(filter)
             .then((result) => {
-                
+
                 if (result.data.length == 0)
                     this.isEmpty = true;
                 else
                     this.isEmpty = false;
 
-                result.data.map((item,index) => {
+                result.data.map((item, index) => {
                     let newItem = item;
-                    item.Index = index+1;
+                    item.Index = index + 1;
                     item.DateFormatted = moment(item.Date).format('DD MMM YYYY');
                     return item;
                 })
 
                 return result.data;
             });
-        if(this.data.length > 0)
+        if (this.data.length > 0)
             this.isEmpty = false;
         else
-            this.isEmpty=true;
+            this.isEmpty = true;
     }
 
     // get otherExpenditureDocumentLoader() {
@@ -83,43 +81,42 @@ export class List {
         return DivisionLoader;
     }
 
-    get accountBankLoader(){
+    get accountBankLoader() {
         return AccountBanksLoader;
     }
-    get referenceNoLoader(){
+    get referenceNoLoader() {
         return ReferenceNoLoader;
     }
     // otherExpenditureDocumentView = (otherExpenditure) => {
     //     return `${otherExpenditure.DocumentNo}`
     // }
-    divisionView=(division)=> {
+    divisionView = (division) => {
         return division.toString();
     }
 
-    accountBankView=(account)=>{
-        return account.AccountName+' - '+ account.BankName+' - '+ account.AccountName+' - '+account.Currency.Code;
+    accountBankView = (account) => {
+        return account.toString();
     }
 
-    referenceNoView =(reference)=>{
+    referenceNoView = (reference) => {
         return reference.Code;
     }
 
     excel() {
-        let filter={}
-        if(this.info.code)
+        let filter = {}
+        if (this.info.code)
             filter.referenceNo = this.info.code.Code;
-        if(this.info.bank){
+        if (this.info.bank) {
             filter.accountBankId = this.info.bank.Id;
-            filter.accountBankName =  this.info.bank.AccountName+' - '+ this.info.bank.BankName+' - '+ this.info.bank.AccountName+' - '+this.info.bank.Currency.Code;            
+            filter.accountBankName = this.info.bank.toString();
         }
-        if(this.info.division){
+        if (this.info.division)
             filter.division = this.info.division.Name;
-        }
-        if(this.info.startDate != undefined)
-            filter.startDate = moment(this.info.startDate).format("YYYY-MM-DD");
-        if(this.info.endDate != undefined)
-            filter.endDate = moment(this.info.endDate).format("YYYY-MM-DD");
-        
+        if (this.info.startDate)
+            filter.startDate = this.info.startDate;
+        if (this.info.endDate)
+            filter.endDate = this.info.endDate;
+
         filter.filter = JSON.stringify(this.type);
         this.service.getXls(filter);
     }
@@ -129,8 +126,8 @@ export class List {
         this.info.code = undefined;
         this.info.bank = undefined;
         this.info.division = undefined;
-        this.info.startDate =undefined;
-        this.info.endDate = undefined;
+        this.info.startDate = null;
+        this.info.endDate = null;
     }
 }
 export class KeysValueConverter {
