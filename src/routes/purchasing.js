f39f4afb2d136303d4d60dd20eac7ee344003416
Leslie Aula <leslie.aula@moonlay.com>
2021-07-13 18:26:54 +0800

    move menu

--- src/routes/purchasing.js
+++ src/routes/purchasing.js
@@ -592,8 +592,8 @@ module.exports = [
         title: 'Laporan Bukti Pembayaran Disposisi',
         auth: true,
         settings: {
-            group: "purchasing",
-            subGroup: "laporan",
+            group: "finance",
+            subGroup: "ekspedisi disposisi",
             permission: { "P1": 1, "P3": 1, "P4": 1, "P6": 1, "P7": 1, "PI": 1, "PG": 1, "PK": 1, "C9": 1 },
             iconClass: 'fa fa-dashboard'
         }
