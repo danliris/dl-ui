dd78f6cd6df3e555c9c1e45c598ce38318509825
achensetyawati <setyawati@moonlay.com>
2021-08-02 15:24:40 +0700

    Update garment-finance-memorial-loader.js

--- src/loader/garment-finance-memorial-loader.js
+++ src/loader/garment-finance-memorial-loader.js
@@ -1,7 +1,7 @@
 import { Container } from 'aurelia-dependency-injection';
 import { Config } from "aurelia-api";
 
-const resource = '/garment-memorials';
+const resource = 'garment-memorials';
 
 module.exports = function (keyword, filter) {
 
