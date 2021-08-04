0094ce667de04d90a1a5ae96b1dbf690525cff16
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-08-04 15:49:35 +0700

    Update item.js
    Change Sum Rule in Item BankCashReceiptDetails

--- src/modules/garment-finance/bank-cash/bank-cash-receipt-details/templates/item.js
+++ src/modules/garment-finance/bank-cash/bank-cash-receipt-details/templates/item.js
@@ -87,7 +87,6 @@ export class Item {
 			this.coreService.getGarmentCurrencies(args)
 				.then(result => {
 					let currency = result.data.slice().pop();
-					console.log(currency);
 					this.data.Currency = {
 						Id: currency.Id,
 						Code: currency.Code,
@@ -106,7 +105,7 @@ export class Item {
 					if (invoice.cmtPrice > 0) {
 						isCmt = true;
 					}
-					totalAmount += invoice.cmtPrice;
+					totalAmount += invoice.cmtPrice * invoice.quantity;
 				}
 				if (isCmt == false) {
 					totalAmount = 0;
