aba5cb0d3b89ee49a8453c6c14738cb89648426d
Leslie Aula <leslie.aula@moonlay.com>
2021-08-18 16:05:20 +0800

    Fix Saldo Hutang dan Jurnal VB

--- src/modules/expedition/reports/creditor-balance-detail-report/index.js
+++ src/modules/expedition/reports/creditor-balance-detail-report/index.js
@@ -2,7 +2,6 @@ export class Index {
     configureRouter(config, router) {
         config.map([
             { route: ['', 'list'], moduleId: './list', name: 'list', nav: true, title: 'Saldo Hutang Lokal Valas' },
-            { route: ['detail'], moduleId: './detai;', name: 'detail', nav: true, title: 'Rincian Saldo Hutang' },
         ]);
 
         this.router = router;
--- src/modules/expedition/reports/local-credit-balance-report/index.js
+++ src/modules/expedition/reports/local-credit-balance-report/index.js
@@ -2,6 +2,7 @@ export class Index {
     configureRouter(config, router) {
         config.map([
             { route: ['', 'list'], moduleId: './list', name: 'list', nav: true, title: 'Saldo Hutang' },
+            { route: ['detail'], moduleId: './detail', name: 'detail', nav: true, title: 'Rincian Saldo Hutang' },
         ]);
 
         this.router = router;
