9000fcee0299c388786c4a92cc9a2ed5855c5dfc
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-09-13 08:44:47 +0700

    Update garment-finance.js

--- src/routes/garment-finance.js
+++ src/routes/garment-finance.js
@@ -389,8 +389,8 @@ module.exports = [
       permission: { "B9": 1, "B13": 1, "B4": 1, "B11": 1, "B1": 1, "B12": 1, "PG": 1, "APG": 1, "C9": 1 },
       iconClass: "fa fa-dashboard",
     }
-    },
-    {
+  },
+  {
     route: "garment-finance/bank-cash/bank-cash-receipt-details",
     name: "bank-cash-receipt-details",
     moduleId: "./modules/garment-finance/bank-cash/bank-cash-receipt-details/index",
@@ -404,8 +404,8 @@ module.exports = [
       iconClass: "fa fa-dashboard",
     }
   },
-  
-{
+
+  {
     route: "garment-finance/memorials",
     name: "memorials",
     moduleId: "./modules/garment-finance/bank-cash/memorial/index",
@@ -424,7 +424,7 @@ module.exports = [
     name: "memorial-details",
     moduleId: "./modules/garment-finance/bank-cash/memorial-detail/index",
     nav: true,
-    title: "Rincian Memorial",
+    title: "Rincian Memorial - Export",
     auth: true,
     settings: {
       group: "g-finance",
