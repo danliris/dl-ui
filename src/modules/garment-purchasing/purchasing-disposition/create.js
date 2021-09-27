ba02fbe1d5a47d7070baf59bd2c02a0491c8db07
ichwankm <ichwankm@gmail.com>
2021-09-27 11:57:57 +0700

    delete unused space

--- src/modules/garment-purchasing/purchasing-disposition/create.js
+++ src/modules/garment-purchasing/purchasing-disposition/create.js
@@ -62,7 +62,7 @@ export class Create {
                             // pph=parseFloat(detail.PriceTotal)*parseFloat(rate)*0.01;
                             pph = item.IncomeTaxValue;
                         }
-                        if (item.IsPayVAT) {
+                        if(item.IsPayVAT) {
                             // ppn=detail.PriceTotal*0.1;
                             ppn = item.VatValue;
                         }
@@ -72,7 +72,7 @@ export class Create {
                         }
                         this.data.IncomeTaxValue+=pph;
                         this.data.IncomeTaxValueView +=pphView;                        
-                        this.data.VatValue += ppn;
+                        this.data.VatValue+=ppn;
                         this.data.VatValueView+=ppnView;                        
                         this.data.DPP+=item.DPPValue;
                         incomeTaxCalculate +=pph;
