ed856e793058b87fe3e23e1d0f481a38fc68848f
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-10-28 15:14:32 +0700

    Add Feature Garment Subcon Shrinkage Panel

---
+++ src/modules/garment-subcon/garment-service-subcon-shrinkage-panel/index.js
@@ -0,0 +1,11 @@
+export class Index {
+	configureRouter(config, router) {
+		config.map([
+			{ route: ['', 'list'], moduleId: './list', name: 'list', nav: true, title: 'List: Subcon Jasa - Sewing' },
+			{ route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create: Subcon Jasa - Sewing' },
+			{ route: 'view/:id', moduleId: './view', name: 'view', nav: false, title: 'View: Subcon Jasa - Sewing' },
+			{ route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit: Subcon Jasa - Sewing' },
+		]);
+		this.router = router;
+	}
+}
