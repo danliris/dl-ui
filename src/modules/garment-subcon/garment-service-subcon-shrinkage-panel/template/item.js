c3aedfe38b4f49696a6efacdc951583a33841f1e
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-10-29 14:54:46 +0700

    BugFix When Update State

--- src/modules/garment-subcon/garment-service-subcon-shrinkage-panel/template/item.js
+++ src/modules/garment-subcon/garment-service-subcon-shrinkage-panel/template/item.js
@@ -73,66 +73,65 @@ export class Item {
 	}
 
 	async selectedUENChanged(newValue, oldValue) {
-		if (this.isCreate) {
-			if (newValue) {
-				this.data.Details.splice(0);
-				this.data.UnitExpenditureNo = newValue.UENNo;
-				this.data.ExpenditureDate = newValue.ExpenditureDate;
-				this.data.UnitSender = {
-					Id: newValue.UnitSenderId,
-					Code: newValue.UnitSenderCode,
-					Name: newValue.UnitSenderName
-				};
-
-				this.data.UnitRequest = {
-					Id: newValue.UnitRequestId,
-					Code: newValue.UnitRequestCode,
-					Name: newValue.UnitRequestName
-				};
-				newValue.Items.map(async i => {
-					const dataUnitDOItem = await this.purchasingService.getUnitDeliveryOrderItems(i.UnitDOItemId);
-					const detail = {};
-					detail.Product = {
-						Id: i.ProductId,
-						Code: i.ProductCode,
-						Name: i.ProductName,
-						Remark: i.ProductRemark,
-					};
-					detail.Quantity = i.Quantity;
-					detail.DesignColor = dataUnitDOItem.DesignColor;
-					detail.Uom = {
-						Id: i.UomId,
-						Unit: i.UomUnit
-					}
-					this.data.Details.push(detail);
-				})
-
-			} else {
-				this.data.UENNo = null;
-			}
-		}
-	}
-
-	get garmentUENLoader() {
-		return async (keyword) => {
-			var info = {
-				keyword: keyword,
-				filter: JSON.stringify({ ExpenditureType: "SUBCON" })
+		if (newValue) {
+			this.data.Details.splice(0);
+			this.data.UnitExpenditureNo = newValue.UENNo;
+			this.data.ExpenditureDate = newValue.ExpenditureDate;
+			this.data.UnitSender = {
+				Id: newValue.UnitSenderId,
+				Code: newValue.UnitSenderCode,
+				Name: newValue.UnitSenderName
 			};
 
-			let dataSubconShrinkage = await this.service.searchComplete({});
-			let uenNo = [];
-			dataSubconShrinkage.data.map(x => {
-				x.Items.map(item => {
-					uenNo.push(item.UnitExpenditureNo);
-				})
-			});
+			this.data.UnitRequest = {
+				Id: newValue.UnitRequestId,
+				Code: newValue.UnitRequestCode,
+				Name: newValue.UnitRequestName
+			};
+			newValue.Items.map(async i => {
+				const dataUnitDOItem = await this.purchasingService.getUnitDeliveryOrderItems(i.UnitDOItemId);
+				const detail = {};
+				detail.Product = {
+					Id: i.ProductId,
+					Code: i.ProductCode,
+					Name: i.ProductName,
+					Remark: i.ProductRemark,
+				};
+				detail.Quantity = i.Quantity;
+				detail.DesignColor = dataUnitDOItem.DesignColor;
+				detail.Uom = {
+					Id: i.UomId,
+					Unit: i.UomUnit
+				}
+				this.data.Details.push(detail);
+			})
 
-			return this.purchasingService.getUnitExpenditureNotes(info)
-				.then((result) => {
-					let data = result.data.filter(x => !uenNo.includes(x.UENNo));
-					return data;
-				});
+		} else {
+			this.data.UENNo = null;
 		}
 	}
+}
+
+	get garmentUENLoader() {
+	return async (keyword) => {
+		var info = {
+			keyword: keyword,
+			filter: JSON.stringify({ ExpenditureType: "SUBCON" })
+		};
+
+		let dataSubconShrinkage = await this.service.searchComplete({});
+		let uenNo = [];
+		dataSubconShrinkage.data.map(x => {
+			x.Items.map(item => {
+				uenNo.push(item.UnitExpenditureNo);
+			})
+		});
+
+		return this.purchasingService.getUnitExpenditureNotes(info)
+			.then((result) => {
+				let data = result.data.filter(x => !uenNo.includes(x.UENNo));
+				return data;
+			});
+	}
+}
 }
