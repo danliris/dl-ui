9f7989715b731c72e32da22cd1ad8a943cfe6fc7
ardana96 <ardanagama@gmail.com>
2021-11-22 15:00:34 +0700

    update Bon WareHouse

--- src/modules/packing-sku-inventory/dyeing-printing-in-warehouses/template/production-order-item.js
+++ src/modules/packing-sku-inventory/dyeing-printing-in-warehouses/template/production-order-item.js
@@ -12,6 +12,7 @@ export class ProductionOrderItem {
         this.error = context.error;
         this.options = context.options;
         this.contextOptions = context.context.options;
+        this.type = this.contextOptions.type;
     }
 
     controlOptions = {
