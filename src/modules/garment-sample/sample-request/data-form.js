edef0d0596bf98f6606312351c56b42221d71221
achensetyawati <setyawati@moonlay.com>
2021-12-14 17:00:53 +0700

    update Sample Request

--- src/modules/garment-sample/sample-request/data-form.js
+++ src/modules/garment-sample/sample-request/data-form.js
@@ -78,6 +78,13 @@ export class DataForm {
             };
 
         }
+        
+        this.data.ImagesFile = this.data.ImagesFile ? this.data.ImagesFile : [];
+        this.data.ImagesName = this.data.ImagesName ? this.data.ImagesName : [];
+        this.imagesSrc = this.data.ImagesFile.slice();
+        this.data.DocumentsFile = this.data.DocumentsFile || [];
+        this.data.DocumentsFileName = this.data.DocumentsFileName || [];
+        this.documentsPathTemp = [].concat(this.data.DocumentsPath);
     }
 
     roView = (costCal) => {
@@ -117,6 +124,7 @@ export class DataForm {
     get addItems() {
         return (event) => {
           this.data.SampleProducts.push({});
+          this.data.SampleProducts.forEach((m, i) => m.index = i);
         };
       }
     
@@ -129,6 +137,7 @@ export class DataForm {
     get addSpecs() {
         return (event) => {
           this.data.SampleSpecifications.push({});
+          this.data.SampleSpecifications.forEach((m, i) => m.index = i);
         };
     }
     
@@ -136,5 +145,81 @@ export class DataForm {
         return (event) => {
           this.error = null;
         };
+    }
+
+    
+    @bindable imageUpload;
+    imageUploadChanged(newValue) {
+        if (newValue) {
+        let imageInput = document.getElementById('imageInput');
+        let reader = new FileReader();
+        reader.onload = event => {
+            let base64Image = event.target.result;
+            this.imagesSrc.push(base64Image);
+            this.imagesSrcChanged(this.imagesSrc);
+        }
+        reader.readAsDataURL(imageInput.files[0]);
+        this.imageUpload = null;
+        }
+    }
+
+    @bindable imagesSrc = [];
+    imagesSrcChanged(newValue) {
+        this.data.ImagesFile = [];
+        newValue.forEach(imageSrc => {
+        this.data.ImagesFile.push(imageSrc);
+        })
+    }
+
+    removeImage(index) {
+        this.imagesSrc.splice(index, 1);
+        this.data.ImagesName.splice(index, 1);
+        this.imagesSrcChanged(this.imagesSrc);
+    }
+
+    onAddDocument() {
+        this.data.DocumentsFile.push("");
+        this.data.DocumentsFileName.push("");
+        this.documentsPathTemp.push("");
+    }
+
+    onRemoveDocument(index) {
+        this.data.DocumentsFile.splice(index, 1);
+        this.data.DocumentsFileName.splice(index, 1);
+        this.documentsPathTemp.splice(index, 1);
+    }
+
+    downloadDocument(index) {
+        // this.service.getFile((this.documentsPathTemp[index] || '').replace('/sales/', ''), this.data.DocumentsFileName[index]);
+
+        const linkSource = this.data.DocumentsFile[index];
+        const downloadLink = document.createElement("a");
+        const fileName = this.data.DocumentsFileName[index];
+
+        downloadLink.href = linkSource;
+        downloadLink.download = fileName;
+        downloadLink.click();
+    }
+
+  documentInputChanged(index) {
+    let documentInput = document.getElementById('documentInput' + index);
+
+    if (documentInput.files[0]) {
+        let reader = new FileReader();
+        reader.onload = event => {
+            let base64Document = event.target.result;
+            const base64Content = base64Document.substring(base64Document.indexOf(',') + 1);
+            if (base64Content.length * 6 / 8 > 52428800) {
+            documentInput.value = "";
+            this.data.DocumentsFile[index] = "";
+            this.data.DocumentsFileName[index] = "";
+            alert("Maximum Document Size is 50 MB")
+            } else {
+            this.data.DocumentsFile[index] = base64Document;
+            this.data.DocumentsFileName[index] = documentInput.value.replace(/^.*[\\\/]/, '');
+            }
+        }
+        reader.readAsDataURL(documentInput.files[0]);
+        }
     }
 }
