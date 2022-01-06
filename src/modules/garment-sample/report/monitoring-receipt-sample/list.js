99e806b769d1a37db94e320243d33a2621bf1e87
mocia <mo_cia@yahoo.com>
2022-01-06 14:06:37 +0700

    merge kolom - monitoring penerimaan sample

--- src/modules/garment-sample/report/monitoring-receipt-sample/list.js
+++ src/modules/garment-sample/report/monitoring-receipt-sample/list.js
@@ -24,155 +24,86 @@ export class List {
         length: 5
         }
     }
-    search() {
-        this.args.page = 1;
-        this.args.total = 0;
-        this.searching();
+    rowFormatter(data, index) {
+        if (data.status === "Belum")
+            return { classes: "danger" };
+        else if(data.status === "Sudah")
+            return { classes: "success" };
+        else
+            return {};
     }
-   async bind(context) {
-        this.context = context;
-      
-    }
+    
     searching() {
+        var locale = 'id-ID';
+        
         var info = {
            
             receivedDateFrom : this.receivedDateFrom ? moment(this.receivedDateFrom).format("YYYY-MM-DD") : "",
             receivedDateTo : this.receivedDateTo ? moment(this.receivedDateTo).format("YYYY-MM-DD") : ""
         }
+        
         this.service.search(info)
             .then(result => {
-                this.data = result.data;
-                console.log(result)
+                this.data = result;
+                console.log(result);
                 var temp = [];
-                this.args.total = result.info.total;
+                //this.args.total = result.length;
                 var count = 0;
                 for (var item of this.data) {
-                    if (!temp[item.bookingOrderNo]) {
+                    if (!temp[item.sampleRequestNo] ) {
                         count = 1;
-                        temp[item.bookingOrderNo] = count;
+                        temp[item.sampleRequestNo] = count;
                     }
                     else {
                         count++;
-                        temp[item.bookingOrderNo] = count;
-                        item.bookingOrderNo = null;
+                        temp[item.sampleRequestNo] = count;
+                        item.sampleRequestNo = null;
                     }
                 }
 
                 for (var item of this.data) {
-                    if (item.bookingOrderNo != null) {
-                        item.row_count = temp[item.bookingOrderNo];
+                    if (item.sampleRequestNo != null) {
+                        item.row_count = temp[item.sampleRequestNo];
                     }
-                   // item.confirmQuantity=item.confirmQuantity==0? "" : item.confirmQuantity;
-                    //item.confirmDeliveryDate =  moment(item.confirmDeliveryDate).locale(locale).format("DD MMMM YYYY")=="01 Januari 0001" ? "": moment(item.confirmDeliveryDate).locale(locale).format("DD MMMM YYYY");
-                    item.bookingOrderDate = moment(item.bookingOrderDate).locale(locale).format("DD MMMM YYYY");
-                    item.deliveryDate =moment(item.deliveryDate).locale(locale).format("DD MMMM YYYY");
-                    item.workingDeliveryDate = moment(item.workingDeliveryDate).locale(locale).format("DD MMMM YYYY");
+                     item.sentDate = moment(item.sentDate).locale(locale).format("DD MMMM YYYY");
+                    item.receivedDate =moment(item.receivedDate).locale(locale).format("DD MMMM YYYY");
+                    item.sampleRequestDate = moment(item.sampleRequestDate).locale(locale).format("DD MMMM YYYY");
                 }
                 this.fillTable();
             });
         }
-        fillTable() {
-            const columns =[
-            [
-                {
-                    field: "sampleRequestNo", rowspan: "2", title: "No.<br/>No Surat Sample<br/>Order",
-                    cellStyle: (value, row, index, field) => {
-                        return { css: { "background": "transparent" } };
-                    },
-                },
-                {
-                    field: "roNoSample", rowspan: "2", title: "RO Sample<br/>",
-                    cellStyle: (value, row, index, field) => {
-                        return { css: { "background": "transparent" } };
-                    },
-                },
-                {
-                    field: "sampleCategory", rowspan: "2", title: "Kategori<br/>Sample",
-                    cellStyle: (value, row, index, field) => {
-                        return { css: { "background": "transparent" } };
-                    },
-                },
-                {
-                    field: "sampleType", rowspan: "2", title: "Jenis<br/>Sample",
-                    cellStyle: (value, row, index, field) => {
-                        return { css: { "background": "transparent" } };
-                    },
-                },
-                {
-                    field: "buyer", rowspan: "2", title: "Buyer<br/>",
-                    cellStyle: (value, row, index, field) => {
-                        return { css: { "background": "transparent" } };
-                    },
-                },
-                {
-                    field: "style", rowspan: "2", title: "Article<br/>",
-                    cellStyle: (value, row, index, field) => {
-                        return { css: { "background": "transparent" } };
-                    },
-                },
-                {
-                    field: "color", rowspan: "2", title: "Color<br/>",
-                    cellStyle: (value, row, index, field) => {
-                        return { css: { "background": "transparent" } };
-                    },
-                },
-                {
-                    field: "sizeName", rowspan: "2", title: "Size<br/>",
-                    cellStyle: (value, row, index, field) => {
-                        return { css: { "background": "transparent" } };
-                    },
-                },
-                {
-                    field: "sizeDescription", rowspan: "2", title: "Keterangan<br/>Size",
-                    cellStyle: (value, row, index, field) => {
-                        return { css: { "background": "transparent" } };
-                    },
-                },
-                {
-                    field: "quantity", rowspan: "2", title: "Quantity<br/>",
-                    cellStyle: (value, row, index, field) => {
-                        return { css: { "background": "transparent" } };
-                    },
-                },
-                {
-                    field: "sentDate", rowspan: "2", title: "Tgl<br/>Shipment",
-                    cellStyle: (value, row, index, field) => {
-                        return { css: { "background": "transparent" } };
-                    },
-                },
-                {
-                    field: "receivedDate", rowspan: "2", title: "Tgl Terima<br/>Surat Sample",
-                    cellStyle: (value, row, index, field) => {
-                        return { css: { "background": "transparent" } };
-                    },
-                },
-                {
-                    field: "garmentSectionName", rowspan: "2", title: "MD<br/>",
-                    cellStyle: (value, row, index, field) => {
-                        return { css: { "background": "transparent" } };
-                    },
-                },
-                {
-                    field: "sampleRequestDate", rowspan: "2", title: "Tgl Pembuatan<br/>Surat Sample",
-                    cellStyle: (value, row, index, field) => {
-                        return { css: { "background": "transparent" } };
-                    },
-                },
-                
-            ]];
+    fillTable() {
+        let columns = [
+            { field: 'sampleRequestNo', title: 'No Surat Sample', footerFormatter: "" },
+            { field: 'roNoSample', title: 'RO Sample', footerFormatter: "" },
+            { field: 'sampleCategory', title: 'Kategori', footerFormatter: "" },
+            { field: 'sampleType', title: 'Jenis Sample', footerFormatter: "" },
+            { field: 'buyer', title: 'Buyer', footerFormatter: "" },
+            { field: 'style', title: 'Article', footerFormatter: "" },
+            { field: 'color', title: 'Color', footerFormatter: "" },
+            { field: 'sizeName', title: 'Size', footerFormatter: "" },
+            { field: 'sizeDescription', title: 'Keterangan', footerFormatter: "" },
+            { field: 'quantity', title: 'Quantity', footerFormatter: "" },
+            { field: 'sentDate', title: 'Tgl Shipment', footerFormatter: "" },
+            { field: 'receivedDate', title: 'Tgl Terima Surat Sample', footerFormatter: "" },
+            { field: 'garmentSectionName', title: 'MD', footerFormatter: "" },
+        { field: 'sampleRequestDate', title: 'Tgl Pembuatan Surat Sample', footerFormatter: "" }
+          ]
+     
+             
     
             var bootstrapTableOptions = {
                 undefinedText: '',
                 columns: columns,
-                data: this.data,
-                rowStyle:this.rowFormatter
+                data: this.data
+                
             };
     
             bootstrapTableOptions.height = $(window).height() - $('.navbar').height() - $('.navbar').height() - 25;
             $(this.table).bootstrapTable('destroy').bootstrapTable(bootstrapTableOptions);
     
             for (const rowIndex in this.data) {
-                if(this.data[rowIndex].bookingOrderNo) {
+                if(this.data[rowIndex].sampleRequestNo) {
                     var rowSpan=this.data[rowIndex].row_count;
                     $(this.table).bootstrapTable('mergeCells', { index : rowIndex, field: "sampleRequestNo", rowspan: rowSpan, colspan: 1, rowFormatter:"red" });
                     $(this.table).bootstrapTable('mergeCells', { index : rowIndex, field: "roNoSample", rowspan: rowSpan, colspan: 1 });
