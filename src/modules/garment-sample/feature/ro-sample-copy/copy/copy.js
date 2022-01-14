608a0dbccad4bb4d1ad0df4912332fb2b8301da0
mocia <mo_cia@yahoo.com>
2022-01-14 15:33:32 +0700

    initial copy ro sample

---
+++ src/modules/garment-sample/feature/ro-sample-copy/copy/copy.js
@@ -0,0 +1,67 @@
+import { inject, Lazy } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+import { Service } from '../service';
+
+@inject(Router, Service)
+export class Edit {
+    
+    constructor(router, service) {
+        this.router = router;
+        this.service = service;
+        this.data = {};
+        this.error = {};
+    }
+
+    identityProperties = [
+        "Id",
+        "Active",
+        "CreatedUtc",
+        "CreatedBy",
+        "CreatedAgent",
+        "LastModifiedUtc",
+        "LastModifiedBy",
+        "LastModifiedAgent",
+        "IsDeleted",
+    ];
+
+    async activate(params) {
+        const id = params.id;
+        this.data = await this.service.getById(id);
+        console.log(this.data);
+        //this.clearDataProperties();
+        
+    }
+
+    clearDataProperties() {
+        this.identityProperties.concat([
+            
+            "DocumentsPath"
+        ]).forEach(prop => delete this.data[prop]);
+        (this.data.SampleProducts || {}).RONoSample = null;
+
+        this.data.SampleProducts.forEach(item => {
+            this.identityProperties.concat([
+                "Id",
+            ]).forEach(prop => delete item[prop]);
+            item.SampleSpecifications.forEach(detail => {
+                this.identityProperties.concat([
+                    "Id",
+                ]).forEach(prop => delete detail[prop]);
+            });
+        });
+    }
+
+    cancelCallback() {
+        this.router.navigateToRoute("list");
+    }
+
+    saveCallback() {
+        this.service.create(this.data)
+            .then(result => {
+                this.cancelCallback();
+            })
+            .catch(e => {
+                this.error = e;
+            })
+    }
+}
