6c142a0a32d0f15d52b91f6a78c735f035638ca9
Lucia Widyana <mo_cia@yahoo.com>
2022-03-28 10:33:43 +0700

    monitoring flow [er size, flow persediaan pembukuan

--- src/modules/accounting/sample-reports/monitoring-sample-expenditure-good-bookkeeping/list.js
+++ src/modules/accounting/sample-reports/monitoring-sample-expenditure-good-bookkeeping/list.js
@@ -36,8 +36,10 @@ export class List {
                       
                     _data.expenditureDate= moment(_data.expenditureDate).format("YYYY-MM-DD");
                     _data.pebDate=  moment(_data.pebDate).format("DD MMM YYYY") == "01 Jan 1970" || moment(_data.pebDate).format("DD MMM YYYY") == "01 Jan 1900" ? "-" : moment(_data.pebDate).format("YYYY-MM-DD");
-                    _data.price=_data.price.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+                    _data.prices=_data.price.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
                     _data.qtys=_data.qty.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+                    _data.nominals=(_data.qty * _data.price).toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+                 
                     this.data.push(_data);
 
                  }
@@ -77,6 +79,20 @@ export class List {
             for(var item of this.data)
             {
                 sum += item.qty;
+            }
+        }
+        
+        return sum.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }); 
+    }
+
+    get sumNominal()
+    {
+        var sum=0;
+        if(this.data)
+        {
+            for(var item of this.data)
+            {
+                sum += item.qty * item.price;
             }
         }
         
