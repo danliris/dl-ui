6120ab5f1c4d348f0e945d8e220f5d89f71d9ea9
supri0343 <63761987+supri0343@users.noreply.github.com>
2022-06-06 08:29:57 +0700

    Add_monitoring_Aval-Besar&Aval-Kecil
    _

--- src/routes/customs.js
+++ src/routes/customs.js
@@ -125,4 +125,119 @@ module.exports = [
 
     },
 
+
+    {
+        route: '/customs/pemasukan-tc-kecil',
+        name: 'monitoring-pemasukan-tc-kecil',
+        moduleId: './modules/customs/tc-kecil-in/index',
+        nav: true,
+        title: 'Laporan Monitoring Pemasukan Aval TC Kecil',
+        auth: true,
+        settings: {
+            group: "customs",
+            permission: { "C9": 1,"PG": 1,"B1": 1 ,"C2A":1},
+            iconClass: 'fa fa-dashboard'
+        },
+
+    },
+    {
+        route: '/customs/pengeluaran-tc-kecil',
+        name: 'monitoring-pengeluaran-tc-kecil',
+        moduleId: './modules/customs/tc-kecil-out/index',
+        nav: true,
+        title: 'Laporan Monitoring Pengeluaran Aval TC Kecil',
+        auth: true,
+        settings: {
+            group: "customs",
+            permission: { "C9": 1,"PG": 1,"B1": 1 ,"C2A":1 },
+            iconClass: 'fa fa-dashboard'
+        },
+
+    },
+    
+    {
+        route: '/customs/pemasukan-sampah-sapuan',
+        name: 'monitoring-pemasukan-sampah-sapuan',
+        moduleId: './modules/customs/sapuan-in/index',
+        nav: true,
+        title: 'Laporan Monitoring Pemasukan Aval Sampah Sapuan',
+        auth: true,
+        settings: {
+            group: "customs",
+            permission: { "C9": 1,"PG": 1,"B1": 1 ,"C2A":1 },
+            iconClass: 'fa fa-dashboard'
+        },
+
+    },
+    {
+        route: '/customs/pengeluaran-sampah-sapuan',
+        name: 'monitoring-pengeluaran-sampah-sapuan',
+        moduleId: './modules/customs/sapuan-out/index',
+        nav: true,
+        title: 'Laporan Monitoring Pengeluaran Aval Sampah Sapuan',
+        auth: true,
+        settings: {
+            group: "customs",
+            permission: {  "C9": 1,"PG": 1,"B1": 1 ,"C2A":1},
+            iconClass: 'fa fa-dashboard'
+        },
+
+    },
+    {
+        route: '/customs/pemasukan-aval-besar',
+        name: 'monitoring-pemasukan-aval-besar',
+        moduleId: './modules/customs/aval-besar-in/index',
+        nav: true,
+        title: 'Laporan Monitoring Pemasukan Aval Besar',
+        auth: true,
+        settings: {
+            group: "customs",
+            permission: {  "C9": 1,"PG": 1,"B1": 1 ,"C2A":1},
+            iconClass: 'fa fa-dashboard'
+        },
+
+    },
+    {
+        route: '/customs/pengeluaran-aval-besar',
+        name: 'monitoring-pengeluaran-aval-besar',
+        moduleId: './modules/customs/aval-besar-out/index',
+        nav: true,
+        title: 'Laporan Monitoring Pengeluaran Aval Besar',
+        auth: true,
+        settings: {
+            group: "customs",
+            permission: {  "C9": 1,"PG": 1,"B1": 1 ,"C2A":1},
+            iconClass: 'fa fa-dashboard'
+        },
+
+    },
+    {
+        route: '/customs/pemasukan-aval-komponen',
+        name: 'monitoring-pemasukan-aval-komponen',
+        moduleId: './modules/customs/aval-komponen-in/index',
+        nav: true,
+        title: 'Laporan Monitoring Pemasukan Aval Komponen',
+        auth: true,
+        settings: {
+            group: "customs",
+            permission: {  "C9": 1,"PG": 1,"B1": 1 ,"C2A":1},
+            iconClass: 'fa fa-dashboard'
+        },
+
+    },
+    {
+        route: '/customs/pengeluaran-aval-komponen',
+        name: 'monitoring-pengeluaran-aval-komponen',
+        moduleId: './modules/customs/aval-komponen-out/index',
+        nav: true,
+        title: 'Laporan Monitoring Pengeluaran Aval Komponen',
+        auth: true,
+        settings: {
+            group: "customs",
+            permission: {  "C9": 1,"PG": 1,"B1": 1 ,"C2A":1},
+            iconClass: 'fa fa-dashboard'
+        },
+
+    },
+    
 ]
