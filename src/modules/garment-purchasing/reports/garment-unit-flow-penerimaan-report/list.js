260d674c5f36f7b9f358a8374891f8c38800ca6e
supri0343 <63761987+supri0343@users.noreply.github.com>
2022-10-19 09:53:48 +0700

    Update list.js

--- src/modules/garment-purchasing/reports/garment-unit-flow-penerimaan-report/list.js
+++ src/modules/garment-purchasing/reports/garment-unit-flow-penerimaan-report/list.js
@@ -225,10 +225,12 @@ export class List {
             filter.category = this.category;
             filter.categoryname = this.categoryname;
         }
-        if (this.unit){
-          filter.unit = this.unit;
-          filter.unitname = this.unitname;
-        }
+        // if (this.unit){
+        //   filter.unit = this.unit;
+        //   filter.unitname = this.unitname;
+        // }
+        filter.unit = this.unit;
+        filter.unitname = this.unitname;
 
         if (this.dateFrom && this.dateFrom != 'Invalid Date') {
             filter.dateFrom = this.dateFrom;
