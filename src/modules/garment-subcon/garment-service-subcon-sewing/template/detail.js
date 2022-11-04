552249836305300b13d79e2cbf34604de51cb936
Joifarflex <joifarjosuajosua@gmail.com>
2022-11-04 13:36:31 +0700

    enhance ui subcon sewing
    update ui table column,
    update enable edit uom detail,
    update list create uom options

--- src/modules/garment-subcon/garment-service-subcon-sewing/template/detail.js
+++ src/modules/garment-subcon/garment-service-subcon-sewing/template/detail.js
@@ -1,4 +1,5 @@
 import { computedFrom } from "aurelia-framework";
+const UomLoader = require('../../../../loader/uom-loader');
 
 export class Detail {
     activate(context) {
@@ -7,10 +8,17 @@ export class Detail {
         this.error = context.error;
         this.isCreate = context.context.options.isCreate;
         this.isEdit = context.context.options.isEdit;
-        this.readOnly = context.context.options.readOnly;
-        this.isView = context.context.options.isView;
+        
         this.itemOptions=context.context.options;
 
         console.log(this.itemOptions);
+    }
+
+    get uomLoader(){
+        return UomLoader;
+    }
+
+	uomView = (Uom) => {
+        return `${Uom.Unit || Uom.unit}`
     }
 }
