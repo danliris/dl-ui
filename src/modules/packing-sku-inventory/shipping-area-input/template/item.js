e81dde1c9042669fc6b070586d2c8f27a2bf2836
ardana96 <ardanagama@gmail.com>
2022-12-09 09:11:25 +0700

    Update Shipping in for retur from buyer

--- src/modules/packing-sku-inventory/shipping-area-input/template/item.js
+++ src/modules/packing-sku-inventory/shipping-area-input/template/item.js
@@ -171,6 +171,7 @@ export class CartItem {
             this.data.yarnMaterial.id = this.selectedProductionOrder.YarnMaterial.Id;
             this.data.yarnMaterial.name = this.selectedProductionOrder.YarnMaterial.Name;
 
+            this.data.productTextile = {};
             this.data.productTextile.name = this.selectedProductionOrder.ProductTextile.Name;
 
             if (this.selectedProductionOrder.ProcessType.Unit) {
