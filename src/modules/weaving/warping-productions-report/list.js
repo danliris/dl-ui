027cb7d0ed9d7d74527b58d9d371ff6c7d0274ec
Lucia Widyana <mo_cia@yahoo.com>
2023-02-27 09:52:27 +0700

    update weaving dailyOperation machine

--- src/modules/weaving/warping-productions-report/list.js
+++ src/modules/weaving/warping-productions-report/list.js
@@ -1,239 +1,170 @@
-import {
-  inject,
-  bindable
-} from 'aurelia-framework';
-import {
-  Service
-} from "./service";
-import {
-  Router
-} from 'aurelia-router';
+import {inject} from 'aurelia-framework';
+import {Service} from "./service";
+import {Router} from 'aurelia-router';
 import moment from 'moment';
-import {
-  index
-} from '../../../samples/index';
-import {
-  Item
-} from '../../accounting/journal-transaction/templates/item';
+const UnitLoader = require('../../../loader/garment-units-loader');
 
 @inject(Router, Service)
 export class List {
+    constructor(router, service) {
+        this.service = service;
+        this.router = router;
 
-  months = ["", "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
-
-  years = [];
-
-  constructor(router, service) {
-    this.service = service;
-    this.router = router;
-
-    this.currentYearItem = parseInt(moment().format('YYYY'));
-    this.minYearItem = this.currentYearItem - 10;
-    this.maxYearItem = this.currentYearItem + 10;
-
-    for (var i = parseInt(this.minYearItem); i <= parseInt(this.maxYearItem); i++) {
-      this.years.push(i.toString());
     }
-  }
-
-  bind() {
-    this.reset();
-  }
-
-  controlOptions = {
-    label: {
-      length: 3
-    },
-    control: {
-      length: 6
+    bind(context) {
+        this.context = context;
     }
-  }
+    shiftOptions = [
+      { text: "I", value: 1 },
+      { text: "II", value: 2 },
+      { text: "III", value: 3 } 
+  ];
+    MCNOOptions = [
+    { text: "WP 1", value: 3 },
+    { text: "WP 2", value: 3 },
+    { text: "WP 3", value: 3 } 
+  ];
+  
+    searching() {
+        var info = {
+            shift : this.info.shift ? this.info.shift.text: "",
+            mcNo : this.info.mcNo ? this.info.mcNo.text: "",
+            sp: this.info.sp ? this.info.sp: "",
+            code: this.info.code ? this.info.code: "",
+            threadNo : this.info.threadNo ? this.info.threadNo: "",
+            fromDate : this.fromDate ? moment(this.fromDate).format("YYYY-MM-DD") : "",
+            toDate : this.toDate ? moment(this.toDate).format("YYYY-MM-DD") : ""
+        }
+        console.log(info);
+        this.service.getReportData(info)
+            .then(result => {
+              this.data= result.data;
+                // this.data=[];
+                // console.log(result);
+                // for(var _data of result){
+                //   this.data.push(_data);
 
-  searchWarpingProductions() {
-    if (false) {
-      alert("");
-    } else {
-      this.error = {};
-      var errorIndex = 0;
-      if (this.Year) {
-        var YearContainer = this.Year;
-      } else {
-        this.error.Year = "Tahun Harus Diisi";
-        errorIndex++;
-      }
-      if (this.Month) {
-        var MonthContainer = this.Month;
-      } else {
-        this.error.Month = "Bulan Harus Diisi";
-        errorIndex++;
-      }
+                //  }
+            });
+    }
+    
+    ExportToExcel() {
+        var info = {
+            unit : this.unit ? this.unit.Id : "",
+            dateFrom : this.dateFrom ? moment(this.dateFrom).format("YYYY-MM-DD") : "",
+            dateTo : this.dateTo ? moment(this.dateTo).format("YYYY-MM-DD") : ""
+      
+        }
+        this.service.generateExcel(info);
+    }
 
-      var MonthInNumber = 0;
+    get unitLoader(){
+        return UnitLoader;
+    }
+    unitView = (unit) => {
+        return `${unit.Code} - ${unit.Name}`;
+    
+    }
 
-      switch (MonthContainer) {
-        case "Januari":
-          MonthInNumber = 1;
-          break;
-        case "Februari":
-          MonthInNumber = 2;
-          break;
-        case "Maret":
-          MonthInNumber = 3;
-          break;
-        case "April":
-          MonthInNumber = 4;
-          break;
-        case "Mei":
-          MonthInNumber = 5;
-          break;
-        case "Juni":
-          MonthInNumber = 6;
-          break;
-        case "Juli":
-          MonthInNumber = 7;
-          break;
-        case "Agustus":
-          MonthInNumber = 8;
-          break;
-        case "September":
-          MonthInNumber = 9;
-          break;
-        case "Oktober":
-          MonthInNumber = 10;
-          break;
-        case "November":
-          MonthInNumber = 11;
-          break;
-        case "Desember":
-          MonthInNumber = 12;
-          break;
-        default:
-          MonthInNumber = 0;
-          break;
-      }
-
-      var arg = {
-        month: MonthInNumber,
-        year: YearContainer
-      };
-      if (errorIndex == 0) {
-        this.service.getReportData(arg).then(result => {
-          this.data = result.data;
-
-          result.data._ProcessedList = [];
-          var index = 1;
-          for (var item of result.data.ProcessedList) {
-            var productionDatum = {
-              Day: index,
-              TotalValue: 0,
-              _DailyProcessed: []
-            };
-
-            var valueEach = 0;
-            var totalValue = 0;
-            for (var headerItem of result.data.Headers) {
-              if (item.DailyProcessedPerOperator.length > 0) {
-
-                var processedItemIndex = item.DailyProcessedPerOperator.findIndex(o => o.Group == headerItem.Group && o.Name == headerItem.Name);
-                if (processedItemIndex >= 0) {
-                  valueEach = item.DailyProcessedPerOperator[processedItemIndex].Total;
-                  totalValue = totalValue + valueEach;
-
-                  productionDatum.TotalValue = totalValue;
-                  productionDatum._DailyProcessed.push({
-                    Value: valueEach
-                  });
-                } else {
-                  productionDatum._DailyProcessed.push({
-                    Value: 0
-                  });
-                }
-              } else {
-                productionDatum._DailyProcessed.push({
-                  Value: 0
-                });
-              }
+    
+    reset() {
+        this.dateFrom = null;
+        this.dateTo = null;
+        this.unit = null;
+    }
+    get sumStock()
+    {
+        var sum=0;
+        if(this.data)
+        {
+            for(var item of this.data)
+            {
+                sum += item.stock;
             }
-            result.data._ProcessedList.push(productionDatum);
-            index++;
-          }
-          return result;
-        });
-      }
+        }
+        
+        return sum.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }); 
     }
-  }
-
-  reset() {
-    this.Month = null;
-    this.MonthContainer = null;
-    this.MonthInNumber = null;
-    this.Year = null;
-    this.YearContainer = null;
-    this.data = [];
-  }
-
-  exportToExcel() {
-    this.error = {};
-    var errorIndex = 0;
-    if (this.Year) {
-      var YearContainer = this.Year;
-    } else {
-      this.error.Year = "Tahun Harus Diisi";
-      errorIndex++;
+    get sumReceipt()
+    {
+        var sum=0;
+        if(this.data)
+        {
+            for(var item of this.data)
+            {
+                sum += item.receipt;
+            }
+        }
+        
+        return sum.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }); 
     }
-    if (this.Month) {
-      var MonthContainer = this.Month;
-    } else {
-      this.error.Month = "Bulan Harus Diisi";
-      errorIndex++;
+    
+    get sumMainFabricExpenditure()
+    {
+        var sum=0;
+        if(this.data)
+        {
+            for(var item of this.data)
+            {
+                sum += item.mainFabricExpenditure;
+            }
+        }
+       
+        return sum.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }); 
     }
 
-    var MonthInNumber = 0;
-
-    switch (MonthContainer) {
-      case "Januari":
-        MonthInNumber = 1;
-        break;
-      case "Februari":
-        MonthInNumber = 2;
-        break;
-      case "Maret":
-        MonthInNumber = 3;
-        break;
-      case "April":
-        MonthInNumber = 4;
-        break;
-      case "Mei":
-        MonthInNumber = 5;
-        break;
-      case "Juni":
-        MonthInNumber = 6;
-        break;
-      case "Juli":
-        MonthInNumber = 7;
-        break;
-      case "Agustus":
-        MonthInNumber = 8;
-        break;
-      case "September":
-        MonthInNumber = 9;
-        break;
-      case "Oktober":
-        MonthInNumber = 10;
-        break;
-      case "November":
-        MonthInNumber = 11;
-        break;
-      case "Desember":
-        MonthInNumber = 12;
-        break;
-      default:
-        MonthInNumber = 0;
-        break;
+    get sumNonMainFabricExpenditure()
+    {
+        var sum=0;
+        if(this.data)
+        { for(var item of this.data)
+            {
+                sum += item.nonMainFabricExpenditure;
+            }
+        }
+       
+        return sum.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }); 
     }
 
-    if (errorIndex == 0) {
-      this.service.getReportPdf(MonthInNumber, YearContainer);
+    get sumExpenditure()
+    {
+        var sum=0;
+        if(this.data)
+        {
+            for(var item of this.data)
+            {
+                sum += item.expenditure;
+            }
+        }
+        return sum.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }); 
     }
-  }
+    
+    get sumAval()
+    {
+        var sum=0;
+        if(this.data)
+        {
+            for(var item of this.data)
+            {
+                sum += item.aval;
+            } 
+        }
+        
+        return sum.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }); 
+    }
+    get sumRemainQty()
+    {
+        var sum=0;
+        if(this.data)
+        {
+            for(var item of this.data)
+            {
+                sum += item.remainQty;
+            }
+        }
+        
+        return sum.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }); 
+    }
+    
+    
 }
