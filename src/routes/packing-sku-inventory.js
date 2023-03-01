0f36f146b80158c2895b7308d8da58282ce7cf72
ardana96 <ardanagama@gmail.com>
2023-03-01 14:32:44 +0700

    Update Route PS-Inventory

--- src/routes/packing-sku-inventory.js
+++ src/routes/packing-sku-inventory.js
@@ -470,7 +470,7 @@ module.exports = [
       group: "ps-inventory",
       subGroup: "stok",
       // permission: { "C9": 1, "F1": 1, "F2": 1, "A2": 1, "W2": 1 },
-      permission :{"*":1},
+      permission :{"S37":1},
       iconClass: 'fa fa-dashboard'
     }
   },
@@ -500,8 +500,7 @@ module.exports = [
       group: "ps-inventory",
       subGroup: "stok",
       // permission: { "C9": 1, "F1": 1, "F2": 1, "A2": 1, "W2": 1 },
-      //permission :{"S28":1},
-      permission :{"*":1},
+      permission :{"S28":1},
       iconClass: 'fa fa-dashboard'
     }
   },
@@ -517,7 +516,7 @@ module.exports = [
       subGroup: "stok",
       // permission: { "C9": 1, "F1": 1, "F2": 1, "A2": 1, "W2": 1 },
       //permission :{"S28":1},
-      permission : {"*" : 1},
+      permission : {"S38" : 1},
       iconClass: 'fa fa-dashboard'
     }
   },
