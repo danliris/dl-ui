bfbf36d51011bd4081f9c53d006a0f3da40bf3c5
ardana96 <ardanagama@gmail.com>
2023-03-01 09:33:41 +0700

    Add Stock Opname Report

--- src/routes/packing-sku-inventory.js
+++ src/routes/packing-sku-inventory.js
@@ -470,7 +470,7 @@ module.exports = [
       group: "ps-inventory",
       subGroup: "stok",
       // permission: { "C9": 1, "F1": 1, "F2": 1, "A2": 1, "W2": 1 },
-      permission :{"S37":1},
+      permission :{"*":1},
       iconClass: 'fa fa-dashboard'
     }
   },
