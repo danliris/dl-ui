6d6c4bc26d0d239e849a9db1fe02ecf2ecc9e315
ardana96 <ardanagama@gmail.com>
2023-03-01 14:32:44 +0700

    Update Route PS-Inventory

--- src/routes/packing-sku-inventory.js
+++ src/routes/packing-sku-inventory.js
@@ -548,7 +548,7 @@ module.exports = [
       group: "ps-inventory",
       subGroup: "stok",
       // permission: { "C9": 1, "F1": 1, "F2": 1, "A2": 1, "W2": 1 },
-      permission :{"*":1},
+      permission :{"S37":1},
       iconClass: 'fa fa-dashboard'
     }
   },
@@ -566,7 +566,6 @@ module.exports = [
 
       subGroup: "stok",
       // permission: { "C9": 1, "F1": 1, "F2": 1, "A2": 1, "W2": 1 },
-
       iconClass: 'fa fa-dashboard'
     }
   },
@@ -758,5 +757,19 @@ module.exports = [
 
       iconClass: 'fa fa-dashboard'
     }
+  },
+  {
+    route: "track",
+    name: "track",
+    moduleId: "./modules/master/track/index",
+    nav: true,
+    title: "Jalur / Rak",
+    auth: true,
+    settings: {
+      group: "ps-inventory",
+      subGroup: "master",
+      permission: { "S39": 1 },
+      iconClass: "fa fa-dashboard",
+    },
   }
 ];
