c0228c22065c000399deed9a5f10e91d9cf975f9
Lucia Widyana <mo_cia@yahoo.com>
2023-03-03 17:09:13 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -31,11 +31,11 @@ export async function configure(aurelia) {
             }
 
 
-            var core = "https://com-danliris-service-core.azurewebsites.net/v1/";
+            var core = "https://com-dl-service-core.azurewebsites.net/v1/";
             var auth = "https://com-danliris-service-auth.azurewebsites.net/v1/";
             var production = "https://dl-production-webapi.azurewebsites.net/v1/";
-            var productionAzure = "https://com-danliris-service-finishing-printing-dev.azurewebsites.net/v1/";
-            var purchasing = "https://dl-purchasing-webapi.azurewebsites.net/v1/";
+            var productionAzure = "https://com-danliris-service-finishing-printing.azurewebsites.net/v1/";
+            var purchasing = "https://dl-purchasing-webapi.azurewebsites-dev.net/v1/";
             var purchasingAzure = "https://com-danliris-service-purchasing.azurewebsites.net/v1/";
             var garmentPurchasing = "https://dl-purchasing-garment-webapi.azurewebsites.net/v1/";
             var inventory = "https://dl-inventory-webapi.azurewebsites.net/v1/";
