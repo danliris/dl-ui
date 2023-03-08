b58b8f07def7e47f784f1ef3aca885b99daab44f
Andreasaja <107447911+Andreasaja@users.noreply.github.com>
2023-03-08 08:56:52 +0700

    change date format to d mmmm yyyy

--- src/modules/dashboard-dp/main/list.js
+++ src/modules/dashboard-dp/main/list.js
@@ -11,7 +11,9 @@ export class List {
     { field: "month", title: "Bulan" },
     { field: "year", title: "Tahun" },
     { field: "updatedAt", title: "Tanggal Update", formatter: function (value, data, index) {
-      return moment.utc(value).local().format('DD-MM-YYYY HH:mm');
+      // return moment.utc(value).local().format('DD-MM-YYYY HH:mm');
+      return moment.utc(value).local().format('D MMMM YYYY HH:mm:ss');
+      
     }},
   ];
 
