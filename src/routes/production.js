dae335ff419f59c3f4372257023aa33a271da42f
achensetyawati <achenzetya@gmail.com>
2023-03-14 08:30:32 +0700

    monitoring daily output

--- src/routes/production.js
+++ src/routes/production.js
@@ -356,7 +356,7 @@ module.exports = [
     {
         route: 'production/finishing-printing/reports/daily-operation-machine',
         name: 'daily-operation-machine',
-        moduleId: './modules/production/finishing-printing/reports/daily-operation-machine/index',
+        moduleId: './modules/production/finishing-printing/reports/daily-operation-machine-etl/index',
         nav: true,
         title: 'Laporan Output Mesin per Hari',
         auth: true,
