3db63ea5c9ffaed6810c0e8bf654dc6a6dddfd2d
achensetyawati <achenzetya@gmail.com>
2023-05-31 13:27:02 +0700

    add ProductionSubmissionReport

---
+++ src/modules/production/finishing-printing/reports/production-submission-report/service.js
@@ -0,0 +1,41 @@
+import { buildQueryString } from 'aurelia-path';
+import { RestService } from '../../../../../utils/rest-service';
+
+const uriGRC = 'GetProductionSubmissionReport';
+const uriDownload = 'GetProductionSubmissionDownloadReport';
+
+export class Service extends RestService {
+    constructor(http, aggregator, config, api) {
+        super(http, aggregator, config, "dyeing");
+    }
+    
+    search(info) {
+        var endpoint = `${uriGRC}`;
+        return super.list(endpoint, info);
+    }
+
+    generateExcel(info) {
+        var endpoint = `${uriDownload}`;
+        var query = '';
+        if (info.area) {
+            if (query === '') query = `area=${info.area}`;
+            else query = `${query}&area=${info.area}`;
+        }
+        if (info.idmesin) {
+            if (query === '') query = `idmesin=${info.idmesin}`;
+            else query = `${query}&idmesin=${info.idmesin}`;
+        }
+        if (info.startdate) {
+            if (query === '') query = `startdate=${info.startdate}`;
+            else query = `${query}&startdate=${info.startdate}`;
+        }
+        if (info.finishdate) {
+            if (query === '') query = `finishdate=${info.finishdate}`;
+            else query = `${query}&finishdate=${info.finishdate}`;
+        }
+        if (query !== '')
+            endpoint = `${uriDownload}?${query}`;
+        var filename= "Laporan Output Mesin Per Hari " + info.area;
+        return super.getXls_AF(endpoint,"", filename);
+    }
+}
