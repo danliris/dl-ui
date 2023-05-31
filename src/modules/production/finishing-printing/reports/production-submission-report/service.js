31285a591a37188d7801940600ce72bc074158f0
achensetyawati <achenzetya@gmail.com>
2023-05-31 16:13:30 +0700

    add product-submission-report

--- src/modules/production/finishing-printing/reports/production-submission-report/service.js
+++ src/modules/production/finishing-printing/reports/production-submission-report/service.js
@@ -1,41 +1,21 @@
 import { buildQueryString } from 'aurelia-path';
 import { RestService } from '../../../../../utils/rest-service';
 
-const uriGRC = 'GetProductionSubmissionReport';
-const uriDownload = 'GetProductionSubmissionDownloadReport';
+const serviceUri = "dyeing-printing/report/production-submission-reports";
 
 export class Service extends RestService {
-    constructor(http, aggregator, config, api) {
-        super(http, aggregator, config, "dyeing");
-    }
-    
+  constructor(http, aggregator, config, endpoint) {
+    super(http, aggregator, config, "packing-inventory");
+  }
+
     search(info) {
-        var endpoint = `${uriGRC}`;
+        let endpoint = `${serviceUri}`;
         return super.list(endpoint, info);
     }
 
     generateExcel(info) {
-        var endpoint = `${uriDownload}`;
-        var query = '';
-        if (info.area) {
-            if (query === '') query = `area=${info.area}`;
-            else query = `${query}&area=${info.area}`;
-        }
-        if (info.idmesin) {
-            if (query === '') query = `idmesin=${info.idmesin}`;
-            else query = `${query}&idmesin=${info.idmesin}`;
-        }
-        if (info.startdate) {
-            if (query === '') query = `startdate=${info.startdate}`;
-            else query = `${query}&startdate=${info.startdate}`;
-        }
-        if (info.finishdate) {
-            if (query === '') query = `finishdate=${info.finishdate}`;
-            else query = `${query}&finishdate=${info.finishdate}`;
-        }
-        if (query !== '')
-            endpoint = `${uriDownload}?${query}`;
-        var filename= "Laporan Output Mesin Per Hari " + info.area;
-        return super.getXls_AF(endpoint,"", filename);
+        let endpoint = `${serviceUri}/download?${buildQueryString(info)}`;
+        return super.getXls(endpoint);
     }
+
 }
