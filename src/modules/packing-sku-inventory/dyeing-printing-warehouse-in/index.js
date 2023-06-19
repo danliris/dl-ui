423a76c0b3daee310f83c502cc9a1f956033db34
ardana96 <ardanagama@gmail.com>
2023-06-19 09:34:16 +0700

    Inv Transaction - Enhancement DP warehouse IN

--- src/modules/packing-sku-inventory/dyeing-printing-warehouse-in/index.js
+++ src/modules/packing-sku-inventory/dyeing-printing-warehouse-in/index.js
@@ -5,28 +5,28 @@ export class Index {
           moduleId: "./list",
           name: "list",
           nav: false,
-          title: "List Stop Opname Gudang Barang Jadi - Dyeing & Printing"
+          title: "List Gudang Barang Jadi - Dyeing & Printing"
         },
         {
           route: "create",
           moduleId: "./create",
           name: "create",
           nav: false,
-          title: "Create: Stop Opname Gudang Barang Jadi - Dyeing & Printing"
+          title: "Create: Gudang Barang Jadi - Dyeing & Printing"
         },
         {
           route: 'view/:id',
           moduleId: './view',
           name: 'view',
           nav: false,
-          title: 'View: Stop Opname Gudang Barang Jadi - Dyeing & Printing'
+          title: 'View: Gudang Barang Jadi - Dyeing & Printing'
         },
         {
           route: 'edit/:id',
           moduleId: './edit',
           name: 'edit',
           nav: false,
-          title: 'Edit: Stop Opname Gudang Barang Jadi - Dyeing & Printing'
+          title: 'Edit: Gudang Barang Jadi - Dyeing & Printing'
         },
         { route: 'excel', 
         moduleId: './excel', 
