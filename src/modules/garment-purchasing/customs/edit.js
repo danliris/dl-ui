cea64a84caefc2a6f7dba7aad7b6e84044f1d1eb
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-06-22 13:46:44 +0700

    enhance-GarmentBeacukais-AddDONonPO

--- src/modules/garment-purchasing/customs/edit.js
+++ src/modules/garment-purchasing/customs/edit.js
@@ -25,9 +25,16 @@ export class Edit {
         this.data = await this.service.getById(id);
         var supplierId=this.data.supplier._id ? this.data.supplier._id : this.data.supplier.Id;
         var currencyCode=this.data.currency.code ? this.data.currency.code : this.data.currency.Code;
+        //Get Data DO
         var dataDelivery = await this.service.searchDeliveryOrder({ "supplier" : `${supplierId}`, "currency" : `${currencyCode}` });
+        var dataDeliveryNonPO = await this.service.searchDeliveryOrderNonPO({ "supplier" : `${supplierId}`, "currency" : `${currencyCode}` });
+
+        //Mapping items table by data Items
+        this.data.deliveryOrders = this.data.items.filter(x => x.deliveryOrderNonPO == null);
+        this.data.deliveryOrderNonPO = this.data.items.filter(x => x.deliveryOrderNonPO != null);
+
+        //Mapping data DO With PO
         var items = [];
-        this.data.deliveryOrders= this.data.items;
         for(var a of this.data.deliveryOrders){
             a["selected"] = true;
             a["isView"] = true;
@@ -37,6 +44,7 @@ export class Edit {
             a["quantity"] = a.quantity;
             a["price"] = a.deliveryOrder.totalAmount;
             a["doId"]=a.deliveryOrder.Id;
+            a["IsPO"]=a.deliveryOrder.IsPO;
            
             items.push(a);
         }
@@ -55,12 +63,49 @@ export class Edit {
              }
             a["quantity"] = quantity;
             a["price"] = totPrice;
+            a["IsPO"]=true;
             a.Id=0;
             a._id=0;
             items.push(a);
         }
         this.data.deliveryOrders = items;
+ 
+        //Mapping data DO Non PO
+        var itemsNonPO = [];
+        for(var a of this.data.deliveryOrderNonPO){
+            a["selected"] = true;
+            a["isView"] = true;
+            a["doNo"]=a.deliveryOrderNonPO.doNo;
+            a["doDate"]=a.deliveryOrderNonPO.doDate;
+            a["arrivalDate"]=a.deliveryOrderNonPO.arrivalDate;
+            a["quantity"] = a.quantity;
+            a["price"] = a.deliveryOrderNonPO.totalAmount;
+            a["doId"]=a.deliveryOrderNonPO.Id;
+            a["IsPO"]=a.deliveryOrderNonPO.IsPO;
+           
+            itemsNonPO.push(a);
+        }
 
+        for(var a of dataDeliveryNonPO.data){
+            a["doId"]=a.Id;
+            a["selected"] = false;
+            a["isView"] = true;
+            var quantity = 0;
+            var totPrice = 0;
+            for (var b of a.items) {
+                quantity += b.Quantity;
+                var priceTemp = b.Quantity * b.PricePerDealUnit;
+                totPrice += priceTemp;
+            }
+            a["quantity"] = quantity;
+            a["price"] = totPrice.toFixed(3);
+            a["IsPO"]=false;
+            a.Id=0;
+            a._id=0;
+            itemsNonPO.push(a);
+        }
+        this.data.deliveryOrderNonPO = itemsNonPO;
+      
         this.data.customsDate = moment(this.data.customsDate).format("YYYY-MM-DD");
         this.data.validateDate = moment(this.data.validateDate).format("YYYY-MM-DD");
     }
@@ -93,10 +138,11 @@ export class Edit {
         if(this.data.validateDate && this.data.validateDate !== "")
             this.data.validateDate = moment(this.data.validateDate).format("YYYY-MM-DD");
         var dataCustoms = Object.assign({}, this.data);
+
         var items = [];
         var isSelectedData = false;
+        //Mapping DO with PO
         if(dataCustoms.deliveryOrders && dataCustoms.deliveryOrders.length > 0){
-            this.item = "";
             for(var a of dataCustoms.deliveryOrders){
                 if(a){
                     var deliveryOrder={};
@@ -105,15 +151,40 @@ export class Edit {
                     deliveryOrder.doDate=a.doDate;
                     deliveryOrder.totalAmount=a.totalAmount;
                     deliveryOrder.arrivalDate=a.arrivalDate;
+                    deliveryOrder.IsPO=a.IsPO;
                     a.deliveryOrder=deliveryOrder;
-                    //items.push({deliveryOrder : deliveryOrder});
                     items.push(a);
                     isSelectedData = true;
                 }
             }
-            dataCustoms.deliveryOrders = items;
-            dataCustoms.items=items;
+            dataCustoms.deliveryOrders = items.filter(x => x.deliveryOrder.IsPO == true);
+
         }
+
+        //Mapping DO Non PO
+        if(dataCustoms.deliveryOrderNonPO && dataCustoms.deliveryOrderNonPO.length > 0){
+            for(var a of dataCustoms.deliveryOrderNonPO){
+                if(a){
+                    var deliveryOrderNonPO={};
+                    deliveryOrderNonPO.doNo=a.doNo;
+                    deliveryOrderNonPO.Id=a.doId;
+                    deliveryOrderNonPO.doDate=a.doDate;
+                    deliveryOrderNonPO.totalAmount=a.totalAmount;
+                    deliveryOrderNonPO.arrivalDate=a.arrivalDate;
+                    deliveryOrderNonPO.IsPO=a.IsPO;
+                    a.deliveryOrder=deliveryOrderNonPO;
+                    // items.push({deliveryOrder : deliveryOrderNonPO});
+                    items.push(a);
+                    isSelectedData = true;
+                }
+            }
+            dataCustoms.deliveryOrderNonPO = items.filter(x => x.deliveryOrder.IsPO == false);
+        }
+  
+        if(items.length > 0){
+            dataCustoms.items = items;
+        }
+
         if(isSelectedData){
             this.service.update(dataCustoms)
                 .then(result => {
