10c3c6e38ff4ce5b4ac6335f52fa092e9981cac4
achensetyawati <achenzetya@gmail.com>
2023-06-23 16:58:12 +0700

    update warping-daily-operation-report

--- src/modules/weaving/warping-daily-operation-report/list.js
+++ src/modules/weaving/warping-daily-operation-report/list.js
@@ -15,17 +15,17 @@ export class List {
         this.context = context;
     }
     shiftOptions = [
-      { text: "", value: 0 },
-      { text: "I", value: 1 },
-      { text: "II", value: 2 },
-      { text: "III", value: 3 } 
-  ];
+        { text: "", value: 0 },
+        { text: "I", value: 1 },
+        { text: "II", value: 2 },
+        { text: "III", value: 3 } 
+    ];
     MCNOOptions = [
-    { text: "", value: 0 },
-    { text: "WP 1", value: 1 },
-    { text: "WP 2", value: 2 },
-    { text: "WP 3", value: 3 } 
-  ];
+        { text: "", value: 0 },
+        { text: "WP 1", value: 1 },
+        { text: "WP 2", value: 2 },
+        { text: "WP 3", value: 3 } 
+    ];
   
     searching() {
         var info = {
@@ -44,43 +44,17 @@ export class List {
                 
             });
     }
-    get sumThreadCut()
-    {
-        var sum=0;
-        if(this.data)
-        {
-            for(var item of this.data)
-            {
-                sum += item.ThreadCut;
-            }
-        }
-        
-        return sum;//.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }); 
-    }
-    get sumLength()
-    {
-        var sum=0;
-        if(this.data)
-        {
-            for(var item of this.data)
-            {
-                sum += item.Length;
-            }
-        }
-        
-        return sum;//.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 }); 
-    }
+    
     ExportToExcel() {
-     
-    var info = {
-      shift : this.info.shift ? this.info.shift.text: "",
-      mcNo : this.info.mcNo ? this.info.mcNo.text: "",
-      sp: this.info.sp ? this.info.sp: "",
-      code: this.info.code ? this.info.code: "",
-      threadNo : this.info.threadNo ? this.info.threadNo: "",
-      fromDate : this.fromDate ? moment(this.fromDate).format("YYYY-MM-DD") : moment('0001-01-01').format("YYYY-MM-DD"),
-      toDate : this.toDate ? moment(this.toDate).format("YYYY-MM-DD") :  moment(Date.now()).format("YYYY-MM-DD")
-    }
+        var info = {
+        shift : this.info.shift ? this.info.shift.text: "",
+        mcNo : this.info.mcNo ? this.info.mcNo.text: "",
+        sp: this.info.sp ? this.info.sp: "",
+        code: this.info.code ? this.info.code: "",
+        name : this.info.name ? this.info.name: "",
+        fromDate : this.fromDate ? moment(this.fromDate).format("YYYY-MM-DD") : moment('0001-01-01').format("YYYY-MM-DD"),
+        toDate : this.toDate ? moment(this.toDate).format("YYYY-MM-DD") :  moment(Date.now()).format("YYYY-MM-DD")
+        }
         this.service.generateExcel(info);
     }
 
