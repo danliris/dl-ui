2df2e669133c3fbe490f04f24e4a1c04d64ad1d2
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-06-27 10:41:38 +0700

    enhance-AddModul-SubconDeliveryOrder

---
+++ src/modules/garment-purchasing/delivery-order-subcon/edit.js
@@ -0,0 +1,45 @@
+import { inject, Lazy } from "aurelia-framework";
+import { Router } from "aurelia-router";
+import { Service } from "./service";
+
+@inject(Router, Service)
+export class Edit {
+  hasCancel = true;
+  hasSave = true;
+  hasView = false;
+  hasEdit = true;
+  hasCreate = false;
+
+  constructor(router, service) {
+    this.router = router;
+    this.service = service;
+  }
+
+  bind() {
+    this.error = {};
+  }
+
+  async activate(params) {
+    var id = params.id;
+    this.data = await this.service.getById(id);
+    this.supplier = this.data.Supplier;
+    this.cc = {
+      RO_Number: this.data.RONo,
+    };
+  }
+
+  cancel(event) {
+    this.router.navigateToRoute("view", { id: this.data.Id });
+  }
+
+  save(event) {
+    this.service
+      .update(this.data)
+      .then((result) => {
+        this.cancel();
+      })
+      .catch((e) => {
+        this.error = e;
+      });
+  }
+}
---
+++ src/modules/garment-purchasing/delivery-order-subcon/view.js
@@ -0,0 +1,55 @@
+import { inject, Lazy } from "aurelia-framework";
+import { Router } from "aurelia-router";
+import { Service } from "./service";
+
+@inject(Router, Service)
+export class View {
+  hasCancel = true;
+  hasDelete = true;
+  hasView = true;
+  hasCreate = false;
+  hasEdit = false;
+
+  constructor(router, service) {
+    this.router = router;
+    this.service = service;
+  }
+  isReceived = false;
+
+  async activate(params) {
+    var id = params.id;
+    this.isCustomsDisplay = "Ya";
+    this.data = await this.service.getById(id);
+    this.supplier = this.data.Supplier;
+    this.cc = {
+      RO_Number: this.data.RONo,
+    };
+
+    if (this.data.customsId == 0) {
+      this.hasDelete = true;
+    }
+  }
+
+  cancel(event) {
+    var r = confirm("Apakah Anda yakin akan keluar?");
+    if (r == true) {
+      this.router.navigateToRoute("list");
+    }
+  }
+
+  edit(event) {
+    var r = confirm("Apakah Anda yakin akan mengubah data ini?");
+    if (r == true) {
+      this.router.navigateToRoute("edit", { id: this.data.Id });
+    }
+  }
+
+  delete(event) {
+    var r = confirm("Apakah Anda yakin akan menghapus data ini?");
+    if (r == true) {
+      this.service.delete(this.data).then((result) => {
+        this.cancel();
+      });
+    }
+  }
+}
