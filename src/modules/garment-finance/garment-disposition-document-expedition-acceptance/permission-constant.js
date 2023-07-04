004c3ca66975e8210c37c106cd9e02cfb7cd0ed5
Lucia Widyana <mo_cia@yahoo.com>
2023-07-04 16:03:04 +0700

    addPermissionPenerimaanDokumenDisposisiGarment

---
+++ src/modules/garment-finance/garment-disposition-document-expedition-acceptance/permission-constant.js
@@ -0,0 +1,23 @@
+export const VERIFICATION = {
+    key: 'VERIFICATION',
+    label: 'Verifikasi',
+    position: 3, //VERIFICATION_DIVISION
+    positionAutocomplete: 2, //SEND_TO_VERIFICATION_DIVISION
+    code: ['J47'],
+};
+
+export const CASHIER = {
+    key: 'CASHIER',
+    label: 'Kasir',
+    position: 7, //CASHIER_DIVISION
+    positionAutocomplete: 4, //SEND_TO_CASHIER_DIVISION
+    code: ['J48'],
+};
+
+export const RETUR = {
+    key: 'RETUR',
+    label: 'Retur',
+    position: 1, //ACCOUNTING_DIVISION
+    positionAutocomplete: 6, //SEND_TO_ACCOUNTING_DIVISION
+    code: ['J49'],
+};
