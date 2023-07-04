45c6c6b1d920b8bf389b28dfbed2f00f3ef9039f
achensetyawati <achenzetya@gmail.com>
2023-07-04 13:35:39 +0700

    weavingReaching

---
+++ src/modules/weaving/daily-operation-reaching-upload/list.js
@@ -0,0 +1,69 @@
+import { inject } from "aurelia-framework";
+  import { Service } from "./service";
+  import { Router } from "aurelia-router";
+  import moment from "moment";
+  
+  @inject(Router, Service)
+  export class List {
+    tableOptions = {
+      search: false,
+      showToggle: false,
+      showColumns: false,
+      pagination: true,
+      sortable: false
+    };
+    context = ["detail"];
+    constructor(router, service) {
+      this.service = service;
+      this.router = router;
+    }
+  
+    columns = [
+        { field: "Month", title: "Periode" }, 
+        { field: "Year", title: "Tahun" },
+        { field: "CreatedDate", title: "Tanggal Upload" },
+      
+    ];
+  
+    loader = (info) => {
+      var order = {};
+      if (info.sort) order[info.sort] = info.order;
+  
+      var arg = {
+        page: parseInt(info.offset / info.limit, 10) + 1,
+        size: info.limit,
+        keyword: info.search,
+        order: order
+      }
+  
+      return this.service.search(arg).then(result => {
+        if (result.data && result.data.length > 0) {
+          console.log(result);
+          return {
+            total: result.info.total,
+            data: result.data
+          };
+        } else {
+          return {
+            total: 0,
+            data: {}
+          };
+        }
+      });
+    }
+  
+    upload() {
+      this.router.navigateToRoute("upload");
+    }
+
+    contextCallback(event) {
+        var arg = event.detail;
+        var data = arg.data;
+        switch (arg.name) {
+          case "detail":
+            this.router.navigateToRoute("view", { month: data.MonthId, year : data.Year });
+            break;
+        }
+      }
+  }
+  
