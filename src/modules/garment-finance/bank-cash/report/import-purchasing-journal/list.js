8f58d0464c4b38cea3083eed64acc4d84750a02e
AndreaZain <andrea.kinan@gmail.com>
2023-07-14 08:21:35 +0700

    Create-Adjustment-And-Journal
    Create Garrment Adjustment
    Create Sales Journal * Purchasing Journal

--- src/modules/garment-finance/bank-cash/report/import-purchasing-journal/list.js
+++ src/modules/garment-finance/bank-cash/report/import-purchasing-journal/list.js
@@ -37,17 +37,16 @@ export class List {
     
     controlOptions = {
         label: {
-            length: 4
+            length: 3
         },
         control: {
-            length: 4
+            length: 3
         }
     };
 
     activate() {
        
     }
-
      
     async searching() {
         this.data=[];
@@ -57,7 +56,7 @@ export class List {
             dateFrom : this.dateFrom ? moment(this.dateFrom).format("YYYY-MM-DD") : "",
             dateTo : this.dateTo ? moment(this.dateTo).format("YYYY-MM-DD") : ""
             // month: this.month.value,
-            // year: this.year,            
+            // year: this.year,
         };
         this.service.search(args)
         .then(result => {
@@ -67,7 +66,6 @@ export class List {
         });
         
     }
-
 
     fillTable() {
         const columns = [
@@ -93,31 +91,40 @@ export class List {
     }
 
     ExportToExcel() {
-        // if (!this.year) {
-        //     alert("Tahun Harus Diisi");
-        // }
-        // else if(!this.month){
-        //     alert("Bulan Harus Diisi");
-        // }
         if (!this.dateFrom) {
             alert("Tanggal Mulai Harus Diisi");
         }
         else if(!this.dateTo){
             alert("Tanggal Akhir Harus Diisi");
         }
-
         let args = {
-            // month: this.month.value,
-            // year: this.year,
             dateFrom : this.dateFrom ? moment(this.dateFrom).format("YYYY-MM-DD") : "",
             dateTo : this.dateTo ? moment(this.dateTo).format("YYYY-MM-DD") : ""
+            // month: this.month.value,
+            // year: this.year,
         };
         this.service.generateExcel(args);
     }
 
+    ExportDetailToExcel() {
+        if (!this.dateFrom) {
+            alert("Tanggal Mulai Harus Diisi");
+        }
+        else if(!this.dateTo){
+            alert("Tanggal Akhir Harus Diisi");
+        }
+        let args = {
+            dateFrom : this.dateFrom ? moment(this.dateFrom).format("YYYY-MM-DD") : "",
+            dateTo : this.dateTo ? moment(this.dateTo).format("YYYY-MM-DD") : ""
+            // month: this.month.value,
+            // year: this.year,
+        };
+        this.service.generateDetailExcel(args);
+    }
+
     reset() {
-        // this.year = moment().format("YYYY");
-        // this.month = { text: "January", value: 1 };
+        //this.year = moment().format("YYYY");
+        //this.month = { text: "January", value: 1 };
         this.dateFrom = null;
         this.dateTo = null;
         this.error = {};
@@ -127,20 +134,12 @@ export class List {
     }
 
     search() {
-        // if (!this.year) {
-        //     alert("Tahun Harus Diisi");
-        // }
-        // else if(!this.month){
-        //     alert("Bulan Harus Diisi");
-        // }
-
         if (!this.dateFrom) {
             alert("Tanggal Mulai Harus Diisi");
         }
         else if(!this.dateTo){
             alert("Tanggal Akhir Harus Diisi");
         }
-
         this.searching();
     }
 }
