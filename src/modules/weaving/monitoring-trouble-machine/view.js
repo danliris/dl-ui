4906c094971a2b0c6d69f84708522ee36ba852ee
achensetyawati <achenzetya@gmail.com>
2023-07-31 14:58:40 +0700

    trouble machine add week

--- src/modules/weaving/monitoring-trouble-machine/view.js
+++ src/modules/weaving/monitoring-trouble-machine/view.js
@@ -4,51 +4,61 @@ import { Service } from "./service";
 
 @inject(Router, Service)
 export class View {
-  showViewEdit = true;
-  readOnlyViewEdit = true;
-  createOnly = false;
-  constructor(router, service) {
-    this.router = router;
-    this.service = service;
-  }
+    constructor(router, service) {
+        this.router = router;
+        this.service = service;
+    }
 
-  async activate(params) {
-   // var Id = params.Id;
-   var arg = {
-    month: params.month,
-    yearPeriode: params.yearPeriode
- };
-    var result= await  this.service.getFilter(arg);
-    this.data = result.data;
-    // this.data = await this.service.getById(Id);
-    // const unit = await this.service.getUnitById(this.data.Unit);
-    // this.data.Unit = unit.Name;
+    info={size:100, page:1}
 
-    // if(this.data.MachineNumber){
-    //   this.selectedMachineDocument=this.data.MachineNumber;
-    // }
+    async activate(params) {
+        this.params=params;
+        var arg = {
+            month: params.month,
+            yearPeriode: params.yearPeriode,
+            page:this.info.page,
+            size:this.info.size
+        };
+        var MR=await this.service.getFilter(arg);
+        var idx=1;
+        for(var data of MR.data){
+            data.index=idx;
+            idx++;
+        }
+        this.data = MR.data;
+        this.info.total=MR.info.total;
+    }
+
+    list() {
+        this.router.navigateToRoute("list");
+    }
+
+    cancelCallback(event) {
+        this.list();
+    }
+
+    loadPage() {
+        this.info = {
+            month: this.params.month,
+            yearPeriode: this.params.yearPeriode,
+            page:this.info.page,
+            size:this.info.size
+        };
+        this.service.getFilter(this.info)
+            .then(result => {
+                var idx=(this.info.page-1) *100;
+                for(var data of result.data){
+                    idx++;
+                    data.index=idx;
+                }
+                this.data = result.data;
+                this.info = result.info;
+            })
+    }
     
-    // if(this.data.OrderNumber){
-    //   this.selectedOrderNumber=this.data.OrderNumber;
-    // }
-
-  }
-
-  list() {
-    this.router.navigateToRoute("list");
-  }
-
-  cancelCallback(event) {
-    this.list();
-  }
-
-  // editCallback(event) {
-  //   this.router.navigateToRoute("edit", { Id: this.data.Id });
-  // }
-
-  // deleteCallback(event) {
-  //   this.service.delete(this.data).then(result => {
-  //     this.cancelCallback(event);
-  //   });
-  // }
+    changePage(e) {
+        var page = e.detail;
+        this.info.page = page;
+        this.loadPage();
+    }
 }
