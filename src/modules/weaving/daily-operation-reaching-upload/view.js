25df1fd796351207bcf67d9c837a22d3708bb1c9
achensetyawati <achenzetya@gmail.com>
2023-08-09 08:44:18 +0700

    update detail reaching DO

--- src/modules/weaving/daily-operation-reaching-upload/view.js
+++ src/modules/weaving/daily-operation-reaching-upload/view.js
@@ -25,7 +25,7 @@ export class View {
             data.ReachingEfficiency= data.ReachingEfficiency? ((parseFloat(data.ReachingEfficiency)*100).toFixed(0)).toString() + '%' : '-';
             data.CombEfficiency= data.CombEfficiency? ((parseFloat(data.CombEfficiency)*100).toFixed(0)).toString() + '%': '-';
             data.DoffingEfficiency= data.DoffingEfficiency? ((parseFloat(data.DoffingEfficiency)*100).toFixed(2)).toString() + '%': '-';
-            data.Eff2= data.Eff2? ((parseFloat(data.Eff2)*100).toFixed(2)).toString() + '%': '-';
+            data.Eff2= data.Eff2? ((parseFloat(data.Eff2)*100).toFixed(0)).toString() + '%': '-';
             idx++;
         }
         this.data = MR.data;
@@ -57,7 +57,7 @@ export class View {
                     data.ReachingEfficiency= data.ReachingEfficiency? ((parseFloat(data.ReachingEfficiency)*100).toFixed(0)).toString() + '%' : '-';
                     data.CombEfficiency= data.CombEfficiency? ((parseFloat(data.CombEfficiency)*100).toFixed(0)).toString() + '%': '-';
                     data.DoffingEfficiency= data.DoffingEfficiency? ((parseFloat(data.DoffingEfficiency)*100).toFixed(2)).toString() + '%': '-';
-                    data.Eff2= data.Eff2? ((parseFloat(data.Eff2)*100).toFixed(2)).toString() + '%': '-';
+                    data.Eff2= data.Eff2? ((parseFloat(data.Eff2)*100).toFixed(0)).toString() + '%': '-';
                 }
                 this.data = result.data;
                 this.info = result.info;
