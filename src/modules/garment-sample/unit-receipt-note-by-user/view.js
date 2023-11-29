da2a171972823834bc70903ea6a50a8291956f3e
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-11-29 14:58:58 +0700

    BugFix-ValidationURNSample-DeleteValidation

--- src/modules/garment-sample/unit-receipt-note-by-user/view.js
+++ src/modules/garment-sample/unit-receipt-note-by-user/view.js
@@ -25,7 +25,7 @@ export class View {
                 item.showDetails = false;
                 orderQty += item.OrderQuantity;
             });
-            if (orderQty == 0 && !this.data.IsInvoice) {
+            if (orderQty == 0 /* && !this.data.IsInvoice */) {
                 this.hasDelete = true;
             }
             if (this.data.URNType == "PROSES" || this.data.URNType == "PEMBELIAN") {
