79c018e48fa654fb7a069a27466db0c7581bf46a
supri0343 <63761987+supri0343@users.noreply.github.com>
2024-03-08 14:52:18 +0700

    Sprint6-Enhance-AddType-CustomMachine

--- src/modules/customs/machine/mutation/service.js
+++ src/modules/customs/machine/mutation/service.js
@@ -1,42 +1,46 @@
-import { RestService } from '../../../../utils/rest-service';
+import { RestService } from "../../../../utils/rest-service";
 
-const serviceUri = 'machine';
+const serviceUri = "machine";
 
 class Service extends RestService {
-    constructor(http, aggregator, config, endpoint) {
-        super(http, aggregator, config, "customs-report");
-    }
+  constructor(http, aggregator, config, endpoint) {
+    super(http, aggregator, config, "customs-report");
+  }
 
-    search(info) {
-        var endpoint = `${serviceUri}/mutation`;
-        return super.list(endpoint, info);
-    }
+  search(info) {
+    var endpoint = `${serviceUri}/mutation`;
+    return super.list(endpoint, info);
+  }
 
-    getById(id) {
-        var endpoint = `${serviceUri}/mutation/${id}`;
-        return super.get(endpoint);
-    }
+  getById(id) {
+    var endpoint = `${serviceUri}/mutation/${id}`;
+    return super.get(endpoint);
+  }
 
-    update(data) {
-        var endpoint = `${serviceUri}/mutation/${data.TransactionID}`;
-        return super.put(endpoint, data);
-    }
+  update(data) {
+    var endpoint = `${serviceUri}/mutation/${data.TransactionID}`;
+    return super.put(endpoint, data);
+  }
 
-    createIN(data) {
-        var endpoint = `${serviceUri}/machinesmutation`;
-        return super.post(endpoint, data);
-    }
+  createIN(data) {
+    var endpoint = `${serviceUri}/machinesmutation`;
+    return super.post(endpoint, data);
+  }
 
-    createOut(data) {
-        var endpoint = `${serviceUri}/machinesmutation/out`;
-        return super.post(endpoint, data);
-    }
+  createOut(data) {
+    var endpoint = `${serviceUri}/machinesmutation/out`;
+    return super.post(endpoint, data);
+  }
 
-    generateExcel(info) {
-        var endpoint = `${serviceUri}/mutation/download?tipe=${info.tipe}&ctg=${info.ctg}&serial=${info.serial}`;
-        return super.getXls(endpoint);
-    }
+  generateExcel(info) {
+    var endpoint = `${serviceUri}/mutation/download?tipe=${info.tipe}&ctg=${info.ctg}&serial=${info.serial}`;
+    return super.getXls(endpoint);
+  }
 
+  MutationByBCNo(info) {
+    var endpoint = `${serviceUri}/mutation-by-bcNo`;
+    return super.list(endpoint, info);
+  }
 }
 
-export { Service }
+export { Service };
