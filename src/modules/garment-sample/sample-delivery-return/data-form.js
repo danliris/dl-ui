d7f488910530d9255049756396395585074e5eba
sarwonoaji <80030902+sarwonoaji@users.noreply.github.com>
2026-09-07 19:23:57 +0700

    Enhance Inventory Racking Sample dan Subcon

--- src/modules/garment-sample/sample-delivery-return/data-form.js
+++ src/modules/garment-sample/sample-delivery-return/data-form.js
@@ -347,6 +347,7 @@ export class DataForm {
                         HandlingUnit: unitDOItem ? unitDOItem.HandlingUnit : null,
                         Batch: unitDOItem ? unitDOItem.Batch : null,
                         BatchView: unitDOItem ? moment.parseZone(unitDOItem.Batch).utcOffset(7).format("YYYY-MM-DD") : null,
+                        LocationCode: unitDOItem ? unitDOItem.LocationCode : null,
                     }
     
                     if (itemUEN.ProductName == "FABRIC") {
