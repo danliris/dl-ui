eb191f5f73e336a89b1308b49163c60b5f59fb09
Refido Bagas Prakosa <refidobagas2607@gmail.com>
2026-09-09 09:59:50 +0700

    Fix Filter Master Product CC Old

--- src/modules/merchandiser/cost-calculation/service-core.js
+++ src/modules/merchandiser/cost-calculation/service-core.js
@@ -1,6 +1,6 @@
 import { RestService } from '../../../utils/rest-service';
 
-const garmentProductSingleByNameServiceUri = 'master/garmentProducts/byName';
+const garmentProductSingleByNameServiceUri = 'master/garmentProducts/byNameOld';
 const uomServiceUri = 'master/uoms';
 const sectionServiceUri = 'master/garment-sections';
 const serviceMasterGarmentProductUri = 'master/garmentProducts';
